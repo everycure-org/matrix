0180de750feffe5475a0bf379355db62a809099e
Laurens Vijnck <laurens@everycure.org>
2024-05-13 18:33:22 +0200

    add pre-commit

--- pipelines/matrix/src/matrix/pipelines/embeddings/nodes.py
+++ pipelines/matrix/src/matrix/pipelines/embeddings/nodes.py
@@ -8,26 +8,19 @@ from refit.v1.core.inline_has_schema import has_schema
 
 from matrix.datasets.graph import KnowledgeGraph, DrugDiseasePairGenerator
 
-def create_prm_pairs(
-    raw_tp: pd.DataFrame,
-    raw_tn: pd.DataFrame
-) -> pd.DataFrame:
-    
+
+def create_prm_pairs(raw_tp: pd.DataFrame, raw_tn: pd.DataFrame) -> pd.DataFrame:
     # Add label
-    raw_tp['y'] = 1
-    raw_tn['y'] = 0
+    raw_tp["y"] = 1
+    raw_tn["y"] = 0
 
     # Return concatenated data
     return pd.concat([raw_tp, raw_tn], axis=0).reset_index(drop=True)
 
 
 @has_schema(
-    schema={
-        "is_drug": "bool",
-        "is_disease": "bool",
-        "is_fda_approved": "bool"
-    },
-    allow_subset=True
+    schema={"is_drug": "bool", "is_disease": "bool", "is_fda_approved": "bool"},
+    allow_subset=True,
 )
 def create_feat_nodes(
     raw_nodes: pd.DataFrame,
@@ -42,20 +35,21 @@ def create_feat_nodes(
         raw_nodes: Raw nodes data.
         drug_types: List of drug types.
         disease_types: List of disease types.
-        fda_list: List of FDA approved drugs. 
+        fda_list: List of FDA approved drugs.
     """
-    
+
     raw_nodes["is_drug"] = raw_nodes["category"].apply(lambda x: x in drug_types)
     raw_nodes["is_disease"] = raw_nodes["category"].apply(lambda x: x in disease_types)
     raw_nodes["is_fda_approved"] = raw_nodes["id"].apply(lambda x: x in fda_list)
 
     return raw_nodes
 
+
 @inject_object()
 def create_model_input_nodes(
     graph: KnowledgeGraph,
     known_pairs: pd.DataFrame,
-    generator: DrugDiseasePairGenerator
+    generator: DrugDiseasePairGenerator,
 ) -> pd.DataFrame:
     return generator.generate(graph, known_pairs, n_unknown=50)
 
@@ -65,9 +59,10 @@ def make_splits(
     data: pd.DataFrame,
     splitter: _BaseKFold,
 ) -> pd.DataFrame:
-    
     all_data_frames = []
-    for iteration, (train_index, test_index) in enumerate(splitter.split(data, data["y"])):
+    for iteration, (train_index, test_index) in enumerate(
+        splitter.split(data, data["y"])
+    ):
         all_indices_in_this_fold = list(set(train_index).union(test_index))
         fold_data = data.loc[all_indices_in_this_fold, :].copy()
         fold_data.loc[:, "iteration"] = iteration
@@ -76,5 +71,3 @@ def make_splits(
         all_data_frames.append(fold_data)
 
     return pd.concat(all_data_frames, axis=0, ignore_index=True)
-
-
--- pipelines/matrix/src/matrix/pipelines/embeddings/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/embeddings/pipeline.py
@@ -14,7 +14,7 @@ def create_pipeline(**kwargs) -> Pipeline:
                     "raw.experiments.tn",
                 ],
                 outputs="prm.known_pairs",
-                name="create_int_known_pairs"
+                name="create_int_known_pairs",
             ),
             node(
                 func=nodes.create_feat_nodes,
@@ -22,30 +22,23 @@ def create_pipeline(**kwargs) -> Pipeline:
                     "raw.rtx_kg2.nodes",
                     "params:drug_types",
                     "params:disease_types",
-                    "raw.fda_drugs"
+                    "raw.fda_drugs",
                 ],
                 outputs="feat.rtx_kg2",
-                name="create_feat_nodes"
+                name="create_feat_nodes",
             ),
             node(
                 func=nodes.make_splits,
-                inputs=[
-                    "prm.known_pairs", 
-                    "params:splitter"
-                ],
+                inputs=["prm.known_pairs", "params:splitter"],
                 outputs="model_input.splits",
-                name="create_splits"
+                name="create_splits",
             ),
             node(
                 func=nodes.create_model_input_nodes,
-                inputs=[
-                    "feat.rtx_kg2", 
-                    "prm.known_pairs",
-                    "params:generator"
-                ],
+                inputs=["feat.rtx_kg2", "prm.known_pairs", "params:generator"],
                 outputs="model_input.unkown_pairs",
                 name="create_model_input_nodes",
-                tags=["create-pairs"]
+                tags=["create-pairs"],
             ),
         ]
     )
