7183917e7e4f032b205250569eecfb9619af9883
Laurens <laurens@Laurenss-MacBook-Pro.local>
2024-05-14 14:37:30 +0200

    add all

--- pipelines/matrix/src/matrix/pipelines/fabricator/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/fabricator/pipeline.py
@@ -6,10 +6,9 @@ from data_fabricator.v0.nodes.fabrication import fabricate_datasets
 
 
 def create_pairs(nodes: pd.DataFrame, num: int = 50) -> pd.DataFrame:
-    
     # Sample random pairs
-    random_drugs = nodes['id'].sample(num, replace=True, ignore_index = True)
-    random_diseases = nodes['id'].sample(num, replace=True, ignore_index = True)
+    random_drugs = nodes["id"].sample(num, replace=True, ignore_index=True)
+    random_diseases = nodes["id"].sample(num, replace=True, ignore_index=True)
 
     return pd.DataFrame(
         data=[[drug, disease] for drug, disease in zip(random_drugs, random_diseases)],
@@ -18,7 +17,7 @@ def create_pairs(nodes: pd.DataFrame, num: int = 50) -> pd.DataFrame:
 
 
 def create_fda_drugs(nodes: pd.DataFrame, num: int = 10) -> pd.DataFrame:
-    return nodes['id'].sample(num, replace=True, ignore_index = True)
+    return nodes["id"].sample(num, replace=True, ignore_index=True)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -26,32 +25,30 @@ def create_pipeline(**kwargs) -> Pipeline:
         [
             node(
                 func=fabricate_datasets,
-                inputs={
-                    "fabrication_params": "params:fabricator.rtx_kg2"
-                },
+                inputs={"fabrication_params": "params:fabricator.rtx_kg2"},
                 outputs={
                     "nodes": "raw.rtx_kg2.nodes",
                 },
                 name="fabricate_datasets",
             ),
             node(
-                func=create_pairs, 
-                inputs=["raw.rtx_kg2.nodes"], 
-                outputs="raw.experiments.tp", 
-                name="create_tp_pairs"
+                func=create_pairs,
+                inputs=["raw.rtx_kg2.nodes"],
+                outputs="raw.experiments.tp",
+                name="create_tp_pairs",
             ),
             node(
-                func=create_pairs, 
-                inputs=["raw.rtx_kg2.nodes"], 
-                outputs="raw.experiments.tn", 
-                name="create_tn_pairs"
+                func=create_pairs,
+                inputs=["raw.rtx_kg2.nodes"],
+                outputs="raw.experiments.tn",
+                name="create_tn_pairs",
             ),
             # FUTURE: Move this transformation to pipeline
             node(
-                func=create_fda_drugs, 
-                inputs=["raw.rtx_kg2.nodes"], 
-                outputs="raw.fda_drugs", 
-                name="create_fda_drugs"
+                func=create_fda_drugs,
+                inputs=["raw.rtx_kg2.nodes"],
+                outputs="raw.fda_drugs",
+                name="create_fda_drugs",
             ),
         ]
     )
