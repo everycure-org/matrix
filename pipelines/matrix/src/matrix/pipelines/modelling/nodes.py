897fa604ba9592b21e4b27b700393d65c73878d0
Laurens <laurens@Laurenss-MacBook-Pro.local>
2024-05-22 13:46:58 +0200

    move to tuning file

--- pipelines/matrix/src/matrix/pipelines/modelling/nodes.py
+++ pipelines/matrix/src/matrix/pipelines/modelling/nodes.py
@@ -1,21 +1,14 @@
 from typing import Any, Dict, List, Union
 import pandas as pd
-import numpy as np
 
 from sklearn.model_selection._split import _BaseKFold
 from sklearn.impute._base import _BaseImputer
 from sklearn.ensemble._gb import BaseGradientBoosting
-from sklearn.base import BaseEstimator, MetaEstimatorMixin
-from sklearn.metrics import f1_score
 
 from refit.v1.core.inject import inject_object
 from refit.v1.core.inline_has_schema import has_schema
 from refit.v1.core.unpack import unpack_params
 from refit.v1.core.make_list_regexable import make_list_regexable
-
-from skopt import gp_minimize
-from skopt.utils import use_named_args
-from skopt.space.space import Dimension
 
 from matrix.datasets.graph import KnowledgeGraph, DrugDiseasePairGenerator
 
@@ -212,74 +205,6 @@ def apply_transformers(
         )
 
     return data
-
-
-def f1_score_df(model: BaseEstimator, X, y):
-    y_pred = model.predict(X)
-    return f1_score(y_pred, y, average="macro")
-
-
-class GaussianSearch(BaseEstimator, MetaEstimatorMixin):
-    """
-    Adaptor class to wrap skopt's gp_minimize into sklearn's BaseEstimator compatible type.
-    """
-
-    def __init__(
-        self,
-        estimator: BaseEstimator,
-        dimensions: List[Dimension],
-        scoring: callable,
-        *,
-        splitter: _BaseKFold = None,
-        n_calls: int = 100,
-    ) -> None:
-        self._estimator = estimator
-        self._dimensions = dimensions
-        self._scoring = scoring
-        self._splitter = splitter
-        self._n_calls = n_calls
-        super().__init__()
-
-    def fit(self, X, y=None, **params):
-        """
-        Function to tune the hyperparameters of the estimator.
-
-        Args:
-            X: Feature values
-            y: Target values
-        Returns:
-            Fitted estimator.
-        """
-
-        @use_named_args(self._dimensions)
-        def evaluate_model(**params):
-            """
-            Function to evaluate model using the given splitter
-            and scoring functions. When the splitter applies kfold splitting,
-            the scores are averaged over the folds.
-            """
-            scores = []
-            for _, (train, test) in enumerate(self._splitter.split(X, y)):
-                self._estimator.fit(X[train], y[train])
-                scores.append(self._scoring(self._estimator, X[test], y[test]))
-
-            return 1.0 - np.average(scores)
-
-        result = gp_minimize(evaluate_model, self._dimensions, n_calls=self._n_calls)
-        self.best_params_ = {
-            param.name: self._extract(val)
-            for param, val in zip(self._dimensions, result.x)
-        }
-
-        return self._estimator
-
-    @staticmethod
-    def _extract(val: Any):
-        """Helper function to extract items from numpy objects"""
-        if isinstance(val, np.generic):
-            return val.item()
-
-        return val
 
 
 @unpack_params()
