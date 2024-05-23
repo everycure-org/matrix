d70e083358cf0e376ff31805ccc0d51a3c298283
Alexei Stepanenko <alexei.stepa@gmail.com>
2024-05-23 14:54:56 +0100

    added unit test for replacement sampling

--- pipelines/matrix/tests/datasets/test_graph.py
+++ pipelines/matrix/tests/datasets/test_graph.py
@@ -1,8 +1,14 @@
 import pytest
 
 import pandas as pd
+from sklearn.model_selection import ShuffleSplit
 
-from matrix.datasets.graph import KnowledgeGraph, RandomDrugDiseasePairGenerator
+from matrix.datasets.graph import (
+    KnowledgeGraph,
+    RandomDrugDiseasePairGenerator,
+    ReplacementDrugDiseasePairGenerator,
+)
+from matrix.pipelines.modelling.nodes import make_splits
 
 
 @pytest.fixture(name="graph")
@@ -26,10 +32,10 @@ def known_pairs_fixture() -> pd.DataFrame:
     """Fixture to yield a dummy known pairs dataset."""
     return pd.DataFrame(
         [
-            ["drug:drug_1", "disease:disease_1"],
-            ["drug:drug_2", "disease:disease_2"],
+            ["drug:drug_1", "disease:disease_1", 1],
+            ["drug:drug_2", "disease:disease_2", 1],
         ],
-        columns=["source", "target"],
+        columns=["source", "target", "y"],
     )
 
 
@@ -45,6 +51,31 @@ def test_random_drug_disease_pair_generator(
     # Then set of unknown pairs generated, where generated pairs
     # are distinct from known pairs and are always (drug, disease) pairs.
     assert unknown.shape[0] == 2
+    assert pd.merge(known_pairs, unknown, how="inner", on=["source", "target"]).empty
+    assert set(unknown["source"].to_list()).issubset(graph._drug_nodes)
+    assert set(unknown["target"].to_list()).issubset(graph._disease_nodes)
+
+
+def test_replacement_drug_disease_pair_generator(
+    graph: KnowledgeGraph, known_pairs: pd.DataFrame
+):
+    # Given a replacement drug disease pair generator and a test-train split for the known data
+    n_replacements = 2
+    generator = ReplacementDrugDiseasePairGenerator(
+        random_state=42, n_replacements=n_replacements
+    )
+    splitter = ShuffleSplit(n_splits=1, test_size=0.5, random_state=111)
+    known_pairs_split = make_splits(known_pairs, splitter)
+
+    # When generating unknown pairs
+    unknown = generator.generate(graph, known_pairs_split)
+
+    # Then set of unknown pairs generated, where generated pairs
+    # are distinct from known pairs and are always (drug, disease) pairs.
+    known_positives_test = known_pairs_split[
+        (known_pairs_split["y"] == 1) & (known_pairs_split["split"] == "TRAIN")
+    ]
+    assert unknown.shape[0] == 2 * n_replacements * len(known_positives_test)
     assert pd.merge(known_pairs, unknown, how="inner", on=["source", "target"]).empty
     assert set(unknown["source"].to_list()).issubset(graph._drug_nodes)
     assert set(unknown["target"].to_list()).issubset(graph._disease_nodes)
