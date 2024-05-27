292fc4442cabe2b95c391918cbb072c2b9af4337
Laurens <laurens@Laurenss-MBP.home>
2024-05-27 16:23:31 +0200

    working examples

--- pipelines/matrix/src/matrix/pipelines/modelling/injection.py
+++ pipelines/matrix/src/matrix/pipelines/modelling/injection.py
@@ -40,6 +40,7 @@ def inject_object(foo: str = "bar"):
     def decorate(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            # breakpoint()
             return func(
                 *[inject(arg) for arg in args],
                 **{k: inject(v) for k, v in kwargs.items()},
@@ -57,27 +58,33 @@ def train_model(df: pd.DataFrame, estimator, scorer):
 
 
 @inject_object(foo="baz")
-def train_decorated(estimator, scorer, test):
+def train_decorated(data, estimator, scorer, test):
+    print(data)
     print(estimator)
     print(scorer)
     print(test)
 
 
-train_model(
+# train_model(
+#     pd.DataFrame([["a", "b"]], columns=["foo", "bar"]),
+#     **inject(
+#         {
+#             "estimator": {"_object": "xgboost.XGBClassifier"},
+#             "scorer": {"_object": "sklearn.metrics.f1_score", "average": "macro"},
+#         }
+#     ),
+# )
+
+train_decorated(
     pd.DataFrame([["a", "b"]], columns=["foo", "bar"]),
-    **inject(
-        {
-            "estimator": {"_object": "xgboost.XGBClassifier"},
-            "scorer": {"_object": "sklearn.metrics.f1_score", "average": "macro"},
-        }
-    ),
+    estimator={"_object": "xgboost.XGBClassifier"},
+    scorer={"_object": "sklearn.metrics.f1_score", "average": "macro"},
+    test={"_object": "sklearn.metrics.accuracy_score"},
 )
 
 train_decorated(
     pd.DataFrame([["a", "b"]], columns=["foo", "bar"]),
-    {
-        "estimator": {"_object": "xgboost.XGBClassifier"},
-        "scorer": {"_object": "sklearn.metrics.f1_score", "average": "macro"},
-    },
-    test={"_object": "sklearn.metrics.accuracy_score"},
+    {"_object": "xgboost.XGBClassifier"},
+    {"_object": "sklearn.metrics.f1_score", "average": "macro"},
+    {"_object": "sklearn.metrics.accuracy_score"},
 )
