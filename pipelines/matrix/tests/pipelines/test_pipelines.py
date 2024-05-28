fccd9a38634954ad1a9630b29f4bfdb04daf1b77
Laurens <laurens@Laurenss-MacBook-Pro.local>
2024-05-28 16:16:39 +0200

    add memory dataset checker

--- pipelines/matrix/tests/pipelines/test_pipelines.py
+++ pipelines/matrix/tests/pipelines/test_pipelines.py
@@ -3,12 +3,15 @@ import pytest
 from kedro.framework.project import pipelines
 
 
+def _pipeline_datasets(pipeline) -> set[str]:
+    """Helper function to retrieve all datasets used by a pipeline."""
+    return set.union(*[set(node.inputs + node.outputs) for node in pipeline.nodes])
+
+
 @pytest.mark.integration
 def test_unused_catalog_entries(kedro_context, configure_matrix_project):
     """Tests whether all catalog entries are used in the pipeline."""
-    used_conf_entries = set.union(
-        *(set(n.inputs + n.outputs) for p in pipelines.values() for n in p.nodes)
-    )
+    used_conf_entries = set.union(*[_pipeline_datasets(p) for p in pipelines.values()])
     used_data_sets = {entry for entry in used_conf_entries if "params:" not in entry}
     used_params = [entry for entry in list(used_conf_entries) if "params:" in entry]
 
@@ -38,3 +41,19 @@ def test_unused_catalog_entries(kedro_context, configure_matrix_project):
     assert (
         unused_params == []
     ), f"The following parameters are not used: {unused_params}"
+
+
+@pytest.mark.integration
+def test_memory_data_sets_absent(kedro_context, configure_matrix_project):
+    """Tests no MemoryDataSets are created."""
+    used_data_sets = set.union(*[_pipeline_datasets(p) for p in pipelines.values()])
+
+    used_data_sets_wout_double_params = {
+        x.replace("params:params:", "params:") for x in used_data_sets
+    }
+
+    memory_data_sets = used_data_sets_wout_double_params - set(
+        kedro_context.catalog.list()
+    )
+
+    assert len(memory_data_sets) == 0, f"{memory_data_sets}"
