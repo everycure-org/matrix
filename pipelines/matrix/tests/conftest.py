7bb33bb2979b0fc706efe7cae40ebcbb816093b5
Laurens <laurens@Laurenss-MacBook-Pro.local>
2024-05-28 15:51:21 +0200

    add check for datasets

--- pipelines/matrix/tests/conftest.py
+++ pipelines/matrix/tests/conftest.py
@@ -1,1 +1,36 @@
 """Module with pytest fixtures."""
+import pytest
+
+from pathlib import Path
+
+from kedro.framework.context import KedroContext
+from kedro.framework.project import configure_project, settings
+from kedro.framework.hooks import _create_hook_manager
+
+from kedro.config import OmegaConfigLoader
+
+
+@pytest.fixture(name="config_loader", scope="session")
+def config_loader_fixture() -> OmegaConfigLoader:
+    """Instantiate a config loader."""
+    return OmegaConfigLoader(
+        env="base", base_env="base", conf_source=str(Path.cwd() / settings.CONF_SOURCE)
+    )
+
+
+@pytest.fixture(name="kedro_context", scope="session")
+def kedro_context_fixture(config_loader) -> KedroContext:
+    """Instantiate a KedroContext."""
+    return KedroContext(
+        env="base",
+        package_name="matrix",
+        project_path=Path.cwd(),
+        config_loader=config_loader,
+        hook_manager=_create_hook_manager(),
+    )
+
+
+@pytest.fixture(name="configure_matrix_project", scope="session")
+def configure_matrix_project_fixture():
+    """Configure the project for testing."""
+    configure_project("matrix")
