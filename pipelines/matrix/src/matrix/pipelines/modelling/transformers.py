9016831075b4d65482e35972b6dff7825aeec0d8
Laurens <laurens@Laurenss-MacBook-Pro.local>
2024-05-28 14:21:17 +0200

    add fix docstrings

--- pipelines/matrix/src/matrix/pipelines/modelling/transformers.py
+++ pipelines/matrix/src/matrix/pipelines/modelling/transformers.py
@@ -1,23 +1,38 @@
+"""Module with sklearn compatible transformers."""
 from sklearn.preprocessing import FunctionTransformer
 
 import pandas as pd
 
 
 class FlatArrayTransformer(FunctionTransformer):
-    """sklearn compatible transformer to flatten arrays in a dataframe column.
+    """sklearn compatible transformer to flatten dataframe with array column in individual columns.
 
     WARNING: Currently only supports a single input column.
     """
 
-    def __init__(self, prefix: str):
+    def __init__(self, prefix: str) -> None:
+        """Instantiate the FlatArrayTransformer.
+
+        Args:
+            prefix: Prefix to add to the column names.
+        """
         self.prefix = prefix
-        super().__init__(self.flatten_df_rows)
+        super().__init__(self._flatten_df_rows)
 
     @staticmethod
-    def flatten_df_rows(df):
+    def _flatten_df_rows(df: pd.DataFrame):
+        """Helper function to flat array column into individual columns."""
         return pd.DataFrame(df[df.columns[0]].tolist()).to_numpy()
 
     def get_feature_names_out(self, input_features=None):
+        """Get the feature names of the transformed data.
+
+        Args:
+            input_features: Input features to transform.
+
+        Returns:
+            List of feature names.
+        """
         if input_features.shape[1] > 1:
             raise ValueError("Only one input column is supported.")
 
