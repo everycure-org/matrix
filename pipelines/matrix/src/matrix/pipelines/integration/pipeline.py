dabbd88a4bbc2d00fb52b2ff7d2a2cd356ac11d3
Laurens <laurens@Laurenss-MacBook-Pro.local>
2024-05-30 15:25:45 +0200

    add edge creation

--- pipelines/matrix/src/matrix/pipelines/integration/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/integration/pipeline.py
@@ -1,14 +1,47 @@
 """Integration pipeline."""
+from typing import List
 from datetime import datetime
 
 from kedro.pipeline import Pipeline, node, pipeline
 from pyspark.sql import DataFrame
+
+from refit.v1.core.inline_has_schema import has_schema
+
 import pyspark.sql.functions as F
 
 
-def to_neo4j(raw_nodes: DataFrame) -> DataFrame:
-    """Function to create int embeddings."""
-    return raw_nodes
+@has_schema(
+    schema={
+        "id": "string",
+    },
+    allow_subset=True,
+    relax=True,
+)
+def extract_nodes(raw_nodes: DataFrame, types: List[str]) -> DataFrame:
+    """Function to extract nodes with given type.
+
+    Args:
+        raw_nodes: Raw nodes DataFrame.
+        types: List of types to filter.
+    """
+    return raw_nodes.filter(F.col("category").isin(types))
+
+
+@has_schema(
+    schema={
+        "source_id": "string",
+        "target_id": "string",
+    },
+    allow_subset=True,
+    relax=True,
+)
+def extract_edges(raw_edges: DataFrame) -> DataFrame:
+    """Function to extract edges.
+
+    Args:
+        raw_edges: Raw edges DataFrame.
+    """
+    return raw_edges
 
 
 def apply_date_filter(drugs: DataFrame, cutoff_date: datetime) -> None:
@@ -21,10 +54,25 @@ def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=to_neo4j,
-                inputs=["integration.raw.rtx_kg2.nodes"],
+                func=extract_nodes,
+                inputs=["integration.raw.rtx_kg2.nodes", "params:modelling.drug_types"],
                 outputs="integration.prm.drugs",
                 name="create_neo4j_drug_nodes",
+            ),
+            node(
+                func=extract_nodes,
+                inputs=[
+                    "integration.raw.rtx_kg2.nodes",
+                    "params:modelling.disease_types",
+                ],
+                outputs="integration.prm.diseases",
+                name="create_neo4j_disease_nodes",
+            ),
+            node(
+                func=extract_edges,
+                inputs=["integration.raw.rtx_kg2.edges"],
+                outputs="integration.prm.treats",
+                name="create_neo4j_edges",
             ),
             node(
                 func=apply_date_filter,
