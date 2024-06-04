f8fd15c82046207b3df5b432a63795cd45ea59d2
Laurens <laurens@Laurenss-MacBook-Pro.local>
2024-06-04 13:55:45 +0200

    Add

--- pipelines/matrix/src/matrix/datasets/neo4j.py
+++ pipelines/matrix/src/matrix/datasets/neo4j.py
@@ -1,16 +1,14 @@
 """Module containing Neo4JDataset."""
-from typing import Any, Dict, Callable
+from typing import Any, Callable, Union, Optional
 from copy import deepcopy
+from functools import wraps
 
 from pyspark.sql import DataFrame
+from pyspark.sql.types import StructType
 
-from kedro_datasets.spark import SparkDataset
 from kedro.io.core import Version
+from kedro_datasets.spark import SparkDataset
 from kedro_datasets.spark.spark_dataset import _get_spark
-
-from pypher import Pypher
-
-from refit.v1.core.inject import inject_object
 
 
 class Neo4JSparkDataset(SparkDataset):
@@ -18,10 +16,6 @@ class Neo4JSparkDataset(SparkDataset):
 
     Kedro dataset to load and write data from Neo4J. This is essentially a wrapper
     for the [Neo4J spark connector](https://neo4j.com/docs/spark/current/).
-
-    FUTURE:
-        - Allow defining a [schema](https://neo4j.com/docs/spark/current/read/define-schema/)
-        - Test [predicate pushdown](https://neo4j.com/docs/spark/current/performance/spark/)
     """
 
     DEFAULT_LOAD_ARGS: dict[str, Any] = {}
@@ -32,7 +26,6 @@ class Neo4JSparkDataset(SparkDataset):
         *,
         url: str,
         database: str,
-        query: Dict[str, str] = None,
         load_args: dict[str, Any] = None,
         save_args: dict[str, Any] = None,
         version: Version = None,
@@ -40,12 +33,6 @@ class Neo4JSparkDataset(SparkDataset):
         metadata: dict[str, Any] = None,
     ) -> None:
         """Creates a new instance of ``Neo4JDataset``.
-
-        Example reading from a query:
-        ::
-            query:
-                object: path.to.pypher.function
-        ::
 
         Example writing nodes:
         ::
@@ -71,7 +58,6 @@ class Neo4JSparkDataset(SparkDataset):
         Args:
             url: URL of the Neo4J instance.
             database: Name of the Neo4J database.
-            query: path to function that yields a Pypher query for execution
             labels: Labels to filter the nodes.
             load_args: Arguments to pass to the load method.
             save_args: Arguments to pass to the save
@@ -81,7 +67,6 @@ class Neo4JSparkDataset(SparkDataset):
         """
         self._database = database
         self._url = url
-        self._query = query
         self._credentials = deepcopy(credentials) or {}
 
         super().__init__(
@@ -96,19 +81,12 @@ class Neo4JSparkDataset(SparkDataset):
     def _load(self) -> Any:
         spark_session = _get_spark()
 
-        read_object = (
+        return (
             spark_session.read.format("org.neo4j.spark.DataSource")
             .option("database", self._database)
             .option("url", self._url)
             .options(**self._credentials)
         )
-
-        if self._query:
-            read_object.option("query", str(self._load_query(self._query)))
-        else:
-            read_object.option(**self.metadata)
-
-        return read_object.load(**self._load_args)
 
     def _save(self, data: DataFrame) -> None:
         (
@@ -120,7 +98,32 @@ class Neo4JSparkDataset(SparkDataset):
             .save(**self._save_args)
         )
 
-    @staticmethod
-    @inject_object()
-    def _load_query(func: Callable) -> Pypher:
-        return func(Pypher())
+
+def cypher_query(query: Union[str, Callable], schema: Optional[StructType] = None):
+    """Decorator to specify Cypher query for Neo4J dataset.
+
+    The Cypher query annotator is required to use for nodes that read datasets of
+    the Neo4JSparkDataset type. The `query` argument can either be a string representing
+    a Cypher query, or a callable that yields the Cypher query.
+
+    Args:
+        query: Cypher query to use.
+        schema: Optional PySpark schema to convert to.
+    """
+
+    def decorate(func):
+        @wraps(func)
+        def wrapper(read_obj, *args, **kwargs):
+            if callable(query):
+                read_obj.option("query", query(*args, **kwargs))
+            else:
+                read_obj.option("query", query)
+
+            if schema:
+                read_obj.schema(schema)
+
+            return func(read_obj.load(), *args, **kwargs)
+
+        return wrapper
+
+    return decorate
