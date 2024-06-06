345daeae5ce55ce262e99d752165ffd73b5515c6
Laurens <laurens@Laurenss-MacBook-Pro.local>
2024-06-06 18:47:27 +0200

    modular pipeline

--- pipelines/matrix/src/matrix/pipelines/modelling/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/modelling/pipeline.py
@@ -1,13 +1,92 @@
 """Modelling pipeline."""
-from kedro.pipeline import Pipeline, node, pipeline
+from kedro.pipeline import Pipeline, node
+from kedro.pipeline.modular_pipeline import pipeline
 
+from matrix import settings
 
 from . import nodes
 
 
+def create_modeling_pipeline(**kwargs) -> Pipeline:
+    """Function creates a modeling pipeline."""
+    return pipeline(
+        [
+            node(
+                func=nodes.make_splits,
+                inputs=["prm.known_pairs", "params:model_options.splitter"],
+                outputs="model_input.splits",
+                name="create_splits",
+            ),
+            node(
+                func=nodes.create_model_input_nodes,
+                inputs=[
+                    "feat.rtx_kg2",
+                    "model_input.splits",
+                    "params:model_options.generator",
+                ],
+                outputs="model_input.enriched_splits",
+                name="enrich_splits",
+            ),
+            node(
+                func=nodes.apply_transformers,
+                inputs=[
+                    "model_input.enriched_splits",
+                    "params:model_options.transformers",
+                ],
+                outputs="model_input.transformed_splits",
+                name="transform_data",
+            ),
+            node(
+                func=nodes.tune_parameters,
+                inputs={
+                    "data": "model_input.transformed_splits",
+                    "unpack": "params:model_options.model_tuning_args",
+                },
+                outputs=[
+                    "models.model_params",
+                    "reporting.tuning_convergence_plot",
+                ],
+                name="tune_model_parameters",
+            ),
+            node(
+                func=nodes.train_model,
+                inputs={
+                    "data": "model_input.transformed_splits",
+                    "estimator": "models.model_params",
+                    "features": "params:model_options.model_tuning_args.features",
+                    "target_col_name": "params:model_options.model_tuning_args.target_col_name",
+                },
+                outputs="models.model",
+                name="train_model",
+            ),
+            node(
+                func=nodes.get_model_predictions,
+                inputs={
+                    "data": "model_input.transformed_splits",
+                    "estimator": "models.model",
+                    "features": "params:model_options.model_tuning_args.features",
+                    "target_col_name": "params:model_options.model_tuning_args.target_col_name",
+                },
+                outputs="model_output.predictions",
+                name="get_model_predictions",
+            ),
+            node(
+                func=nodes.get_model_performance,
+                inputs={
+                    "data": "model_output.predictions",
+                    "metrics": "params:model_options.metrics",
+                    "target_col_name": "params:model_options.model_tuning_args.target_col_name",
+                },
+                outputs="reporting.metrics",
+                name="get_model_performance",
+            ),
+        ]
+    )
+
+
 def create_pipeline(**kwargs) -> Pipeline:
     """Create modelling pipeline."""
-    return pipeline(
+    create_model_input = pipeline(
         [
             node(
                 func=nodes.create_feat_nodes,
@@ -31,74 +110,22 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="modelling.prm.known_pairs",
                 name="create_prm_known_pairs",
             ),
-            node(
-                func=nodes.make_splits,
-                inputs=["modelling.prm.known_pairs", "params:modelling.splitter"],
-                outputs="modelling.model_input.splits",
-                name="create_splits",
-            ),
-            node(
-                func=nodes.create_model_input_nodes,
-                inputs=[
-                    "modelling.feat.rtx_kg2",
-                    "modelling.model_input.splits",
-                    "params:modelling.generator",
-                ],
-                outputs="modelling.model_input.enriched_splits",
-                name="enrich_splits",
-            ),
-            node(
-                func=nodes.apply_transformers,
-                inputs=[
-                    "modelling.model_input.enriched_splits",
-                    "params:modelling.transformers",
-                ],
-                outputs="modelling.model_input.transformed_splits",
-                name="transform_data",
-            ),
-            node(
-                func=nodes.tune_parameters,
-                inputs={
-                    "data": "modelling.model_input.transformed_splits",
-                    "unpack": "params:modelling.model_tuning_args",
-                },
-                outputs=[
-                    "modelling.models.model_params",
-                    "modelling.reporting.tuning_convergence_plot",
-                ],
-                name="tune_model_parameters",
-            ),
-            node(
-                func=nodes.train_model,
-                inputs={
-                    "data": "modelling.model_input.transformed_splits",
-                    "estimator": "modelling.models.model_params",
-                    "features": "params:modelling.model_tuning_args.features",
-                    "target_col_name": "params:modelling.model_tuning_args.target_col_name",
-                },
-                outputs="modelling.models.model",
-                name="train_model",
-            ),
-            node(
-                func=nodes.get_model_predictions,
-                inputs={
-                    "data": "modelling.model_input.transformed_splits",
-                    "estimator": "modelling.models.model",
-                    "features": "params:modelling.model_tuning_args.features",
-                    "target_col_name": "params:modelling.model_tuning_args.target_col_name",
-                },
-                outputs="modelling.model_output.predictions",
-                name="get_model_predictions",
-            ),
-            node(
-                func=nodes.get_model_performance,
-                inputs={
-                    "data": "modelling.model_output.predictions",
-                    "metrics": "params:modelling.metrics",
-                    "target_col_name": "params:modelling.model_tuning_args.target_col_name",
-                },
-                outputs="modelling.reporting.metrics",
-                name="get_model_performance",
-            ),
         ]
     )
+
+    pipes = []
+    for pipeline_, namespaces in settings.DYNAMIC_PIPELINES_MAPPING.items():
+        for namespace in namespaces:
+            pipes.append(
+                pipeline(
+                    create_modeling_pipeline(),
+                    inputs={
+                        "prm.known_pairs": "modelling.prm.known_pairs",
+                        "feat.rtx_kg2": "modelling.feat.rtx_kg2",
+                    },
+                    namespace=f"{pipeline_}.{namespace}",
+                    tags=namespace,
+                )
+            )
+
+    return sum([create_model_input, *pipes])
