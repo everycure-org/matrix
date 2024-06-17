774482747621919830ea1afcfd9b1a13e1cd79f0
Alexei Stepanenko <alexei.stepa@gmail.com>
2024-06-17 19:53:31 +0100

    ran ruff format

--- pipelines/matrix/src/matrix/settings.py
+++ pipelines/matrix/src/matrix/settings.py
@@ -4,6 +4,7 @@ There is no need to edit this file unless you want to change values
 from the Kedro defaults. For further information, including these default values, see
 https://docs.kedro.org/en/stable/kedro_project_setup/settings.html.
 """
+
 from typing import Dict
 
 # Instantiated project hooks.
