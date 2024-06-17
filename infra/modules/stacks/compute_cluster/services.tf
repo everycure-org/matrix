f9f7d19de97a607f844f7400fd77fa3630d95723
Pascal Brokmeier <git@pascalbrokmeier.de>
2024-06-17 23:50:21 +0200

    add necessary modules

---
+++ infra/modules/stacks/compute_cluster/services.tf
@@ -0,0 +1,15 @@
+# NOTE: Don't call this ArgoCD, this will clash with the Helm naming
+# and give errors.
+module "argo" {
+  depends_on = [ module.gke ]
+  source        = "./argo"
+  repo_url      = var.gitops_repo_url
+  repo_creds    = var.gitops_repo_creds
+  repo_path     = "infra/argo/"
+  repo_revision = "HEAD"
+}
+
+module "external_secrets" {
+  depends_on = [ module.gke ]
+  source = "./external_secrets"
+}
