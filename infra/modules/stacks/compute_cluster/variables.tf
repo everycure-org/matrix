14bb977ec84002281d900b054c6c732e55408cb9
Pascal Brokmeier <git@pascalbrokmeier.de>
2024-06-17 23:50:21 +0200

    add necessary modules

---
+++ infra/modules/stacks/compute_cluster/variables.tf
@@ -0,0 +1,40 @@
+variable "name" {
+  type = string
+  default = "compute-cluster"
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "default_region" {
+  type = string
+}
+
+variable "network" {
+  type = string
+}
+
+variable "subnetwork" {
+  type = string
+}
+
+variable "zones" {
+  type = list(string)
+  default = [
+    "us-central1-a",
+    "us-central1-b",
+    "us-central1-f"
+  ]
+}
+
+variable "pod_ip_range" { type = string }
+variable "svc_ip_range" { type = string }
+variable "environment" { type = string }
+
+variable "gitops_repo_url" {
+  type = string
+}
+variable "gitops_repo_creds" {
+  type = string
+}
