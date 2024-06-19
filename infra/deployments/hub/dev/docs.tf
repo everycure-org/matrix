4bbc5d98eaeb683a2f4f61536f72604b943549ba
Pascal Brokmeier <pascal@everycure.org>
2024-06-19 16:08:45 +0200

    PR feedback complete

--- infra/deployments/hub/dev/docs.tf
+++ infra/deployments/hub/dev/docs.tf
@@ -1,13 +1,7 @@
-# documentation page. Only hosted in dev for now
+# IAM for documentation page. Only hosted in dev using AppEngine for now
 
-# IAM SA for Cloud Build
-# IAM SA for Cloud Run
-# Cloudbuild default SA config set to above SA
-# Cloud Build Trigger linked to GH
-# Cloud Run deployment
-# Artifact registry  
-
-# needed for whatever reason 
+# needed because our org policy disables the default permissions 
+# https://stackoverflow.com/questions/43905748/what-permission-is-required-for-a-service-account-to-deploy-to-google-app-engine
 locals {
   appengine_bindings = [
     "roles/appengine.deployer",
@@ -24,37 +18,7 @@ resource "google_project_iam_member" "bindings" {
   member   = "serviceAccount:mtrx-hub-dev-3of@appspot.gserviceaccount.com"
 }
 
-#resource "google_iap_tunnel_dest_group" "dest_group" {
-#  region = "us-central1"
-#  group_name = "internal-"
-#  cidrs = [
-#    "10.1.0.0/16",
-#    "192.168.10.0/24",
-#  ]
-#}
-
-import {
-  id = "mtrx-hub-dev-3of"
-  to = google_app_engine_application.appengine_app
-}
-
 resource "google_app_engine_application" "appengine_app" {
   project        = module.bootstrap_data.content.project_id
   location_id    = substr(var.default_region, 0, length(var.default_region)-1)
-#   auth_domain    = var.auth_domain
-#   database_type  = var.database_type
-#   serving_status = var.serving_status
-  # dynamic "feature_settings" {
-  #   for_each = var.feature_settings[*]
-  #   content {
-  #     split_health_checks = feature_settings.value.split_health_checks
-  #   }
-  # }
-  # dynamic "iap" {
-  #   for_each = var.iap[*]
-  #   content {
-  #     oauth2_client_id     = iap.value.oauth2_client_id
-  #     oauth2_client_secret = iap.value.oauth2_client_secret
-  #   }
-  # }
 }
