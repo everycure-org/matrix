6b8ca77434e252d2e0a8cf6f78aa4faadd8468ac
Alexei Stepanenko <alexei.stepa@gmail.com>
2024-06-20 15:00:31 +0100

    removed macro_f1 utils function

--- pipelines/matrix/src/matrix/pipelines/modelling/utils.py
+++ pipelines/matrix/src/matrix/pipelines/modelling/utils.py
@@ -16,13 +16,3 @@ def partial_(func: callable, **kwargs):
         Partially applied function.
     """
     return partial(func, **kwargs)
-
-
-def macro_f1(y_true: np.array, y_pred: np.array) -> float:
-    """Returns macro F1 score for multi-class predictions.
-
-    Args:
-        y_true: 1d array containing ground truth target values.
-        y_pred: 1d array containing estimated targets as returned by a classifier.
-    """
-    return f1_score(y_true, y_pred, average="macro")
