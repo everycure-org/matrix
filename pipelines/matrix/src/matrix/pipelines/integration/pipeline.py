d5f13eac841c86d9a4b6520042330d9534b6841b
Laurens Vijnck <laurens@everycure.org>
2024-06-20 18:53:06 +0100

    add relationships

--- pipelines/matrix/src/matrix/pipelines/integration/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/integration/pipeline.py
@@ -3,6 +3,7 @@ from kedro.pipeline import Pipeline, node, pipeline
 
 from . import nodes
 
+import pandas as pd
 
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
@@ -12,6 +13,20 @@ def _create_nodes(df: DataFrame) -> DataFrame:
     return df.withColumn(
         "label", F.split(F.col("category"), ":", limit=2).getItem(1)
     ).select("id", "label", "name")
+
+
+def _create_int_pairs(raw_tp: pd.DataFrame, raw_tn: pd.DataFrame):
+    raw_tp["y"] = 1
+    raw_tn["y"] = 0
+
+    # Concat
+    return pd.concat([raw_tp, raw_tn], axis="index").reset_index(drop=True)
+
+
+def _create_pairs_neo(df: DataFrame):
+    return df.withColumn(
+        "label", F.when(F.col("y") == 1, "THREATS").otherwise("NOT_THREATS")
+    )
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -26,14 +41,6 @@ def create_pipeline(**kwargs) -> Pipeline:
                 name="write_rtx_kg2_nodes",
                 tags=["rtx_kg2"],
             ),
-            # NOTE: Dataset is corrupt
-            # node(
-            #     func=lambda x: x,
-            #     inputs=["integration.raw.rtx_kg2.edges"],
-            #     outputs="integration.prm.rtx_kg2.edges",
-            #     name="write_rtx_kg2_edges",
-            #     tags=["rtx_kg2"],
-            # ),
             # Write Neo4J nodes
             node(
                 func=_create_nodes,
@@ -42,6 +49,32 @@ def create_pipeline(**kwargs) -> Pipeline:
                 name="create_neo4j_nodes",
                 tags=["rtx_kg2"],
             ),
+            # Construct ground_truth
+            node(
+                func=_create_int_pairs,
+                inputs=[
+                    "integration.raw.ground_truth.tp",
+                    "integration.raw.ground_truth.tn",
+                ],
+                outputs="integration.int.known_pairs@pandas",
+                name="create_int_known_pairs",
+            ),
+            node(
+                func=_create_pairs_neo,
+                inputs=[
+                    "integration.int.known_pairs@spark",
+                ],
+                outputs="integration.model_input.threats",
+                name="create_neo4j_known_pairs",
+            ),
+            # NOTE: Dataset is corrupt
+            # node(
+            #     func=lambda x: x,
+            #     inputs=["integration.raw.rtx_kg2.edges"],
+            #     outputs="integration.prm.rtx_kg2.edges",
+            #     name="write_rtx_kg2_edges",
+            #     tags=["rtx_kg2"],
+            # ),
             # node(
             #     func=nodes.extract_nodes,
             #     inputs=[
