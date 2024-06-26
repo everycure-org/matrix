464f36b29894c8f93817ffa2e289353a0f6fdcf3
Laurens Vijnck <laurens@everycure.org>
2024-06-26 11:15:44 +0200

    rm old

--- pipelines/matrix/src/matrix/pipelines/embeddings/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/embeddings/pipeline.py
@@ -8,23 +8,6 @@ from refit.v1.core.inline_has_schema import has_schema
 from kedro.pipeline import Pipeline, node, pipeline
 
 
-@has_schema(
-    schema={"id": "object", "embedding": "object"}, allow_subset=True, relax=True
-)
-def create_int_embeddings(raw_embeddings: List) -> pd.DataFrame:
-    """Function to create int embeddings."""
-    return pd.DataFrame(raw_embeddings.items(), columns=["id", "embedding"])
-
-
 def create_pipeline(**kwargs) -> Pipeline:
     """Create embeddings pipeline."""
-    return pipeline(
-        [
-            node(
-                func=create_int_embeddings,
-                inputs=["embeddings.raw.graphsage"],
-                outputs="embeddings.int.graphsage",
-                name="create_int_graphsage_embeddings",
-            ),
-        ]
-    )
+    return pipeline([])
