bcd003930131e02e39cf6fd95941e8e8d97e6ab7
Laurens Vijnck <laurens@everycure.org>
2024-06-28 17:08:15 +0200

    split pipeline runs

--- pipelines/matrix/src/matrix/pipeline_registry.py
+++ pipelines/matrix/src/matrix/pipeline_registry.py
@@ -27,5 +27,4 @@ def register_pipelines() -> Dict[str, Pipeline]:
     pipelines["embeddings"] = create_embeddings_pipeline()
     pipelines["fabricator"] = create_fabricator_pipeline()
     pipelines["integration"] = create_integration_pipeline()
-    pipelines["test"] = create_embeddings_pipeline() + create_modelling_pipeline()
     return pipelines
