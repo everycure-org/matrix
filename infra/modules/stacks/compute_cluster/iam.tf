980be6579e2931d21fd1e66d5d1ea50ad36a45d3
Pascal Brokmeier <pascal@everycure.org>
2024-06-30 18:28:44 +0200

    working secrets

--- infra/modules/stacks/compute_cluster/iam.tf
+++ infra/modules/stacks/compute_cluster/iam.tf
@@ -17,10 +17,17 @@ data "google_project" "project" {
 locals {
   es_sa = "default"
   es_ns = "external-secrets"
+  es_iam_member = "principal://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${var.project_id}.svc.id.goog/subject/ns/${local.es_ns}/sa/${local.es_sa}"
 }
 
 resource "google_project_iam_member" "external_secrets_gke_sa" {
   project = var.project_id
   role    = "roles/secretmanager.viewer"
-  member  = "principal://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${var.project_id}.svc.id.goog/subject/ns/${local.es_ns}/sa/${local.es_sa}"
+  member  = local.es_iam_member
+}
+
+resource "google_project_iam_member" "external_secrets_gke_sa_access" {
+  project = var.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = local.es_iam_member
 }
