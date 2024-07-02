dd86b0bf4fc81826defaff897502a7392f61c429
Pascal Brokmeier <pascal@everycure.org>
2024-07-02 12:28:32 +0200

    add new gcr to repo

---
+++ infra/modules/stacks/compute_cluster/gcr.tf
@@ -0,0 +1,7 @@
+resource "google_artifact_registry_repository" "image_repo" {
+  location      = var.default_region
+  project = var.project_id
+  repository_id = "matrix-images"
+  description   = "images for matrix project"
+  format        = "DOCKER"
+}
