2becafadcf96b651d959157e1ca2657419bb467c
Laurens Vijnck <laurens@everycure.org>
2024-07-03 16:36:17 +0200

    add all

--- infra/deployments/hub/dev/main.tf
+++ infra/deployments/hub/dev/main.tf
@@ -15,6 +15,7 @@ module "compute_cluster" {
   gitops_repo_url   = var.gitops_repo_url
   gitops_repo_creds = var.gitops_repo_creds
   k8s_secrets       = var.k8s_secrets
+  bucket_name       = "mtrx-us-central1-hub-dev-storage"
 }
 
 module "matrix" {
@@ -23,5 +24,4 @@ module "matrix" {
   default_region = var.default_region
   project_id     = module.bootstrap_data.content.project_id
   environment    = "dev"
-  storage_bucket = "mtrx-us-central1-hub-dev-storage"
 }
--- infra/modules/stacks/matrix/variables.tf
+++ infra/modules/stacks/matrix/variables.tf
@@ -8,10 +8,6 @@ variable "default_region" {
 
 variable "environment" { type = string }
 
-variable "bucket_name" {
-  type = string
-}
-
 variable "kg_sources" {
   type    = list(string)
   default = ["rtx-kg2", "robokop"]
