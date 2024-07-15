b07789a10103ad8068062c670a2d9815b67338c9
Pascal Brokmeier <pascal@everycure.org>
2024-07-15 13:16:35 +0200

    update cluster and config about neo4j

--- infra/modules/stacks/compute_cluster/gke.tf
+++ infra/modules/stacks/compute_cluster/gke.tf
@@ -1,6 +1,54 @@
 data "google_client_config" "default" {
 }
 
+locals {
+  base_node_pool = [
+    {
+      name            = "default-node-pool"
+      machine_type    = "e2-medium"
+      node_locations  = "us-central1-a"
+      min_count       = 1
+      max_count       = 10
+      local_ssd_count = 0
+      spot            = false
+      disk_size_gb    = 100
+      disk_type       = "pd-standard"
+      image_type      = "COS_CONTAINERD"
+      enable_gcfs     = false
+      enable_gvnic    = false
+      logging_variant = "DEFAULT"
+      auto_repair     = true
+      auto_upgrade    = true
+      # service_account    = module.k8s_sa.email
+      preemptible        = false
+      initial_node_count = 3
+      # accelerator_count           = 1
+      # accelerator_type            = "nvidia-l4"
+      # gpu_driver_version          = "LATEST"
+      # gpu_sharing_strategy        = "TIME_SHARING"
+      # max_shared_clients_per_gpu = 2
+    },
+  ]
+  cpu_node_pools = [for size in [8, 16, 32] : {
+    name = "e2-standard-${size}-nodes"
+    # machine_type       = "n4-standard-8"
+    machine_type       = "e2-standard-${size}"
+    node_locations     = "us-central1-a,us-central1-c"
+    min_count          = 0
+    max_count          = 20
+    local_ssd_count    = 0
+    disk_size_gb       = 200
+    enable_gcfs        = true
+    enable_gvnic       = true
+    initial_node_count = 0
+    }
+  ]
+  gpu_node_pools = [
+    # FUTURE add GPU pools here
+  ]
+  node_pools_combined = concat(local.base_node_pool, local.cpu_node_pools, local.gpu_node_pools)
+}
+
 # docs here https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest/submodules/private-cluster
 module "gke" {
   source              = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
@@ -31,61 +79,7 @@ module "gke" {
   node_metadata              = "UNSPECIFIED"
 
   # FUTURE: Refine mode pools
-  node_pools = [
-    {
-      name            = "default-node-pool"
-      machine_type    = "e2-medium"
-      node_locations  = "us-central1-a"
-      min_count       = 1
-      max_count       = 10
-      local_ssd_count = 0
-      spot            = false
-      disk_size_gb    = 100
-      disk_type       = "pd-standard"
-      image_type      = "COS_CONTAINERD"
-      enable_gcfs     = false
-      enable_gvnic    = false
-      logging_variant = "DEFAULT"
-      auto_repair     = true
-      auto_upgrade    = true
-      # service_account    = module.k8s_sa.email
-      preemptible        = false
-      initial_node_count = 3
-      # accelerator_count           = 1
-      # accelerator_type            = "nvidia-l4"
-      # gpu_driver_version          = "LATEST"
-      # gpu_sharing_strategy        = "TIME_SHARING"
-      # max_shared_clients_per_gpu = 2
-    },
-    {
-      name = "stable-cpu-pool"
-      # machine_type       = "n4-standard-8"
-      machine_type    = "e2-standard-8"
-      node_locations  = "us-central1-a,us-central1-c"
-      min_count       = 0
-      max_count       = 20
-      local_ssd_count = 0
-      disk_size_gb    = 200
-      # disk_type = "hyperdisk-balanced"
-      enable_gcfs  = true
-      enable_gvnic = true
-      # service_account    = module.k8s_sa.email
-      initial_node_count = 1
-    },
-    # {
-    #   name = "stable-cpu-pool-2"
-    #   # machine_type       = "n4-standard-8"
-    #   machine_type       = "e2-standard-8"
-    #   min_count          = 0
-    #   max_count          = 20
-    #   local_ssd_count    = 0
-    #   disk_size_gb       = 200
-    #   enable_gcfs        = true
-    #   enable_gvnic       = true
-    #   service_account    = module.k8s_sa.email
-    #   initial_node_count = 1
-    # },
-  ]
+  node_pools = local.node_pools_combined
 
   # https://cloud.google.com/artifact-registry/docs/access-control#gke
   # node_pools_oauth_scopes = {
