ed1255a935c5b7f8738af28bf63d06ea9171ea37
Pascal Brokmeier <pascal@everycure.org>
2024-07-30 16:54:02 +0200

    wipe script update to take parameter

--- pipelines/matrix/scripts/wipe_neo4j.py
+++ pipelines/matrix/scripts/wipe_neo4j.py
@@ -1,5 +1,6 @@
 """Wipes neo4j instance locally."""
 from neo4j import GraphDatabase
+import typer
 from dotenv import load_dotenv
 import os
 
@@ -25,19 +26,29 @@ def get_user_databases(session):
     return [record["name"] for record in result if record["name"] != "system"]
 
 
-def main():
+def main(
+    db_name: str = typer.Argument(
+        None,
+        help="Name of the database to wipe. If not provided, all user databases will be wiped.",
+    ),
+):
     try:
         with connect_to_neo4j() as driver:
             with driver.session(database="system") as system_session:
-                databases = get_user_databases(system_session)
-                print(f"Found {len(databases)} user database(s)")
-                print(databases)
+                if db_name:
+                    print(f"Dropping database: {db_name}")
+                    system_session.run(f"DROP DATABASE `{db_name}` IF EXISTS")
+                    print(f"Database {db_name} has been wiped.")
+                else:
+                    databases = get_user_databases(system_session)
+                    print(f"Found {len(databases)} user database(s)")
+                    print(databases)
 
-                for db in databases:
-                    print(f"dropping {db}")
-                    system_session.run(f"DROP DATABASE `{db}` IF EXISTS")
+                    for db in databases:
+                        print(f"Dropping {db}")
+                        system_session.run(f"DROP DATABASE `{db}` IF EXISTS")
 
-            print("\nAll user databases have been wiped.")
+                    print("\nAll user databases have been wiped.")
 
     except Exception as ex:
         print(f"An error occurred: {ex}")
@@ -46,4 +57,4 @@ def main():
 if __name__ == "__main__":
     avoid_wiping_prod()
     load_dotenv()
-    main()
+    typer.run(main)
