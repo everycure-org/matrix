656bfc8ddf745266bbc6b1071af48988c2e37042
Laurens Vijnck <laurens@everycure.org>
2024-07-30 14:28:18 +0200

    add endpoint

--- services/synonymizer/api/modules/synonymize/routers.py
+++ services/synonymizer/api/modules/synonymize/routers.py
@@ -1,8 +1,24 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+from node_synonymizer import NodeSynonymizer
 
 router = APIRouter(prefix="/synonymize", tags=["synonymize"])
 
+synonymizer = NodeSynonymizer()
+
+
+class Search(BaseModel):
+    """Model to searches."""
+
+    name: str
+
 
 @router.get("")
-async def get_canonical_curies():
-    return "foo"
+async def get_canonical_curies(search: Search):
+    """Endpoint to get canonical curies.
+
+    Args:
+        Searhc: search string
+    """
+    return synonymizer.get_canonical_curies(names=[search.name])
