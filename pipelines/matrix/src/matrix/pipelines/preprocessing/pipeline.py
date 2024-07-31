fadd8f60741e088b911b2cb5531e405b82ec42b0
Laurens Vijnck <laurens@everycure.org>
2024-07-31 13:28:59 +0200

    hook up pipeline

--- pipelines/matrix/src/matrix/pipelines/preprocessing/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/preprocessing/pipeline.py
@@ -11,11 +11,35 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=nodes.resolve_nodes,
                 inputs=[
-                    "preprocessing.raw.exp.nodes@pandas",
+                    "preprocessing.raw.exp.nodes",
                     "params:preprocessing.synonymizer_endpoint",
                 ],
                 outputs="preprocessing.int.exp.nodes@pandas",
                 name="resolve_exp_nodes",
+                tags=["exp"],
+            ),
+            node(
+                func=lambda x: x,
+                inputs=["preprocessing.raw.exp.edges"],
+                outputs="preprocessing.int.exp.edges@pandas",
+                name="create_int_edges",
+                tags=["exp"],
+            ),
+            node(
+                func=nodes.create_prm_nodes,
+                inputs=["preprocessing.int.exp.nodes@spark"],
+                outputs="preprocessing.prm.exp.nodes",
+                name="create_prm_nodes",
+                tags=["exp"],
+            ),
+            node(
+                func=nodes.create_prm_edges,
+                inputs=[
+                    "preprocessing.prm.exp.nodes",
+                    "preprocessing.int.exp.edges@spark",
+                ],
+                outputs="preprocessing.prm.exp.edges",
+                name="create_prm_edges",
                 tags=["exp"],
             ),
         ]
