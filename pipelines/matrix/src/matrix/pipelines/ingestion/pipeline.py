0cb405c16d1b033fe253ff2b5bfad98741d0dea2
Jason Reilly <jdr0887@gmail.com>
2024-08-05 19:45:02 -0400

    initial commit

---
+++ pipelines/matrix/src/matrix/pipelines/ingestion/pipeline.py
@@ -0,0 +1,19 @@
+"""Ingestion pipeline."""
+from kedro.pipeline import Pipeline, node, pipeline
+
+from . import nodes
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    """Create ingestion pipeline."""
+    return pipeline(
+        [
+            node(
+                func=nodes.normalize_kg_data,
+                inputs=["ingestion.raw.robokop_kg.nodes@pandas", "ingestion.raw.robokop_kg.edges@pandas"],
+                outputs=["ingestion.int.robokop_kg.nodes@pandas", "ingestion.int.robokop_kg.edges@pandas"],
+                name="normalize_kg_data",
+                tags=["RobokopKG", "NodeNormalizer"],
+            ),
+        ]
+    )
