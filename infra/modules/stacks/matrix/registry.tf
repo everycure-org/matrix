7df340dd7d80d3ce32b6238504b8153c95be791e
Laurens Vijnck <laurens@everycure.org>
2024-08-07 11:04:50 +0200

    rm old resources

--- infra/modules/stacks/matrix/registry.tf
+++
@@ -1,6 +0,0 @@
-resource "google_artifact_registry_repository" "mflow-repo" {
-  location      = var.default_region
-  repository_id = "mlflow"
-  description   = "MLFlow docker repository"
-  format        = "DOCKER"
-}
