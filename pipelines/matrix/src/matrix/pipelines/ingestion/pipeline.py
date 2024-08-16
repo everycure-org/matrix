af2d0f4da35474c26d3b2a44c613c58915cbbd9a
Laurens Vijnck <laurens@everycure.org>
2024-08-16 09:00:38 +0200

    integrate into ingestion pipeline

--- pipelines/matrix/src/matrix/pipelines/ingestion/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/ingestion/pipeline.py
@@ -4,6 +4,7 @@ from kedro.pipeline import Pipeline, node, pipeline
 
 def create_pipeline(**kwargs) -> Pipeline:
     """Create ingestion pipeline."""
+    # FUTURE: Use dynamic pipeline for this good first issue
     return pipeline(
         [
             # rtx-kg2
@@ -20,6 +21,21 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="ingestion.prm.rtx_kg2.edges",
                 name="write_rtx_kg2_edges",
                 tags=["rtx_kg2"],
+            ),
+            # ec-medical-team
+            node(
+                func=lambda x: x,
+                inputs=["ingestion.raw.ec_medical_team.nodes@spark"],
+                outputs="ingestion.prm.ec_medical_team.nodes",
+                name="write_ec_medical_team_nodes",
+                tags=["ec_medical_team"],
+            ),
+            node(
+                func=lambda x: x,
+                inputs=["ingestion.raw.ec_medical_team.edges@spark"],
+                outputs="ingestion.prm.ec_medical_team.edges",
+                name="write_ec_medical_team_edges",
+                tags=["ec_medical_team"],
             ),
         ]
     )
