37fba57903d7d222acc23343d8f915f802d50cb1
Jason Reilly <jdr0887@gmail.com>
2024-08-19 11:09:54 -0400

    splitting upload_to_bq & normalize_kg_data

--- pipelines/matrix/src/matrix/pipelines/ingestion/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/ingestion/pipeline.py
@@ -9,11 +9,32 @@ def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=nodes.normalize_kg_data,
+                func=nodes.upload_to_bq,
                 inputs=["ingestion.raw.robokop_kg.nodes@pandas", "ingestion.raw.robokop_kg.edges@pandas"],
-                outputs=["ingestion.int.robokop_kg.nodes@spark", "ingestion.int.robokop_kg.edges@spark"],
+                outputs=["ingestion.raw.robokop_kg.nodes@gbq", "ingestion.raw.robokop_kg.edges@gbq"],
+                name="normalize_kg_data",
+                tags=["RobokopKG"],
+            ),
+            node(
+                func=nodes.normalize_kg_data,
+                inputs=["ingestion.raw.robokop_kg.nodes@gbq", "ingestion.raw.robokop_kg.edges@gbq"],
+                outputs=["ingestion.int.robokop_kg.nodes@gbq", "ingestion.int.robokop_kg.edges@gbq"],
                 name="normalize_kg_data",
                 tags=["RobokopKG", "NodeNormalizer"],
+            ),
+            node(
+                func=nodes.upload_to_bq,
+                inputs=["ingestion.raw.rtx_kg2.nodes@pandas", "ingestion.raw.rtx_kg2.edges@pandas"],
+                outputs=["ingestion.raw.rtx_kg2.nodes@gbq", "ingestion.raw.rtx_kg2.edges@gbq"],
+                name="normalize_kg_data",
+                tags=["RTX_KG2"],
+            ),
+            node(
+                func=nodes.normalize_kg_data,
+                inputs=["ingestion.raw.rtx_kg2.nodes@gbq", "ingestion.raw.rtx_kg2.edges@gbq"],
+                outputs=["ingestion.int.rtx_kg2.nodes@gbq", "ingestion.int.rtx_kg2.edges@gbq"],
+                name="normalize_kg_data",
+                tags=["RTX_KG2", "NodeNormalizer"],
             ),
         ]
     )
