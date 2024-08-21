244ca28a1a49faf558f20d8c713e3dfc55917a82
Laurens Vijnck <laurens@everycure.org>
2024-08-21 21:29:18 +0200

    working algo

--- pipelines/matrix/src/matrix/test.py
+++ pipelines/matrix/src/matrix/test.py
@@ -1,17 +1,19 @@
 # %%
+import re
 from pathlib import Path
 from typing import Callable, Iterable, Any, Tuple
 
 from kedro.framework.project import pipelines
 from kedro.framework.startup import bootstrap_project
 from kedro.pipeline.node import Node
+from kedro.pipeline import Pipeline
 
 
 project_path = "/Users/laurens/Documents/projects/matrix/pipelines/matrix"
 metadata = bootstrap_project(project_path)
 package_name = metadata.package_name
 
-embeddings = pipelines["embeddings"]
+embeddings = pipelines["__default__"]
 
 class FusedNode(Node):
 
@@ -29,8 +31,13 @@ class FusedNode(Node):
         self._parents.update(parents)
 
     @property
+    def outputs(self) -> set[str]:
+        return set().union(*[node.outputs for node in self._nodes])
+
+    @property
     def tags(self) -> set[str]:
-        # TODO
+        return set().union(*[node.tags for node in self._nodes])
+        
 
     @property
     def name(self):
@@ -51,50 +58,92 @@ class FusedNode(Node):
         return self.name, hashable(self._nodes)
 
 
-dep_translation = {}
-fuse_node = None
-new_dependencies = {}
-for node, parents in embeddings.node_dependencies.items():
-    print("node", node.name)
-    print("parent", ",".join([parent.name for parent in parents]))
+def clean_name(name: str) -> str:
+    """Function to clean the node name.
+
+    Args:
+        name: name of the node
+    Returns:
+        Clean node name, according to Argo's requirements
+    """
+    return re.sub(r"[\W_]+", "-", name).strip("-")
+
+
+nodes = {}
+
+pipeline: Pipeline = pipelines["__default__"]
+# for group in pipeline.grouped_nodes:
+#     for node in group:
+#         nodes[node] = node.outputs
+
+def remove_params(elements):
+    return [el for el in elements if not el.startswith("params:")]
+
+fused = []
+for group in pipeline.grouped_nodes:
+    for target_node in group:
+        # Find if there is a source node
+        found = False
+
+        if "argowf.fuse" in target_node.tags:
+            for source_node in [fn for fn in fused if "argowf.fuse" in fn.tags]:
+                if set(remove_params(target_node.inputs)) & set(source_node.outputs):
+                    found = True
+                    source_node.add_node(target_node)
+        
+        if not found:
+            fused_node = FusedNode()
+            fused_node.add_node(target_node)
+            fused.append(fused_node)
+
+# pipeline.grouped_nodes
+[fuse.name for fuse in fused]
+
+# %%
+# dep_translation = {}
+# fuse_node = None
+# new_dependencies = {}
+# for node, parents in embeddings.node_dependencies.items():
+#     print("node", node.name)
+#     print("parent", ",".join([parent.name for parent in parents]))
     
-    if "argo-wf.fuse" in node.tags and all(["argo-wf.fuse" in parent.tags for parent in parents]):
-        if fuse_node is None:
-            fuse_node = FusedNode()
+#     if "argowf.fuse" in node.tags and all(["argowf.fuse" in parent.tags for parent in parents]):
+#         if fuse_node is None:
+#             fuse_node = FusedNode()
 
-        fuse_node.add_node(node)
-        fuse_node.add_parents([parent if dep_translation.get(parent) is None else dep_translation[parent] for parent in parents])
+#         fuse_node.add_node(node)
+#         fuse_node.add_parents([parent if dep_translation.get(parent) is None else dep_translation[parent] for parent in parents if parent not in fuse_node._nodes])
 
-    else:
-        if fuse_node:
+#     else:
+#         if fuse_node:
 
-            # add translated nodes
-            for node in fuse_node._nodes:
-                dep_translation[node] = fuse_node
+#             print("stopped fusing", node.name,  all(["argowf.fuse" in parent.tags for parent in parents]))
 
-            new_dependencies[fuse_node] = fuse_node._parents
-            fuse_node = None
+#             # add translated nodes
+#             for fused_node in fuse_node._nodes:
+#                 dep_translation[fused_node] = fuse_node
 
-        new_dependencies[node] = [parent if dep_translation.get(parent) is None else dep_translation[parent] for parent in parents]
+#             new_dependencies[fuse_node] = fuse_node._parents
+#             fuse_node = None
 
-if fuse_node:
-    new_dependencies[fuse_node] = fuse_node._parents
+#         new_dependencies[node] = [parent if dep_translation.get(parent) is None else dep_translation[parent] for parent in parents]
 
+# if fuse_node:
+#     new_dependencies[fuse_node] = fuse_node._parents
 
-d = [
-    {
-        "name": node.name
-        # "name": clean_name(node.name),
-        # "deps": [clean_name(val.name) for val in sorted(parent_nodes)],
-        # **{
-        #     tag.split("-")[0][len("argo.") :]: tag.split("-")[1]
-        #     for tag in node.tags
-        #     if tag.startswith("argo.")
-        # },
-    }
-    for node, parents in new_dependencies.items()
-]
+# d = [
+#     {
+#         "name":  clean_name(node.name),
+#         # "name": clean_name(node.name),
+#         "deps": [ clean_name(val.name) for val in sorted(parents)],
+#         **{
+#             tag.split("-")[0][len("argowf.") :]: tag.split("-")[1]
+#             for tag in node.tags
+#             if tag.startswith("argowf.") and "-" in tag
+#         },
+#     }
+#     for node, parents in new_dependencies.items()
+# ]
 
-d
-
+# sorted(d, key=lambda d: d["name"])
 # %%
