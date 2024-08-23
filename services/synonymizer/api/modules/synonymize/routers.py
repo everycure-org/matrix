4573dfca826d1a8e9420bd82931bff02fb10251b
Laurens <90421718+lvijnck@users.noreply.github.com>
2024-08-23 12:39:22 +0200

    Integrate experimental source (first one is Long Covid medical team edges) (#129)
    * hibernate update
    
    * add service
    
    * rm locks
    
    * setup cloning
    
    * add endpoint
    
    * link synonimzer
    
    * add working code
    
    * working solution
    
    * working solution
    
    * setup preprocessing pipeline
    
    * move to preprocessing
    
    * hook up pipeline
    
    * hook up pipeline
    
    * add files
    
    * use new input file
    
    * add
    
    * revert
    
    * cleanup
    
    * add updates chunyu
    
    * move to services
    
    * update compose
    
    * install uvicorn
    
    * add endpoint
    
    * fix dataset wiring
    
    * add
    
    * rm pycache
    
    * update wireing
    
    * add node normalization
    
    * finalize wiring
    
    * add primary edge creation
    
    * fix name clash
    
    * move to bigquery
    
    * add medical dataset
    
    * add dataset
    
    * use correct dataset
    
    * rm obsolete code
    
    * try write to bq
    
    * cleanup methodS
    
    * reorganize code
    
    * add save functionality
    
    * add requirements
    
    * extract service file
    
    * add column writing
    
    * add column writing functionality
    
    * lazy load
    
    * add docs
    
    * consolidate learnings
    
    * add updates
    
    * disable node
    
    * add node validation
    
    * create prm
    
    * create prm
    
    * use correct bucket
    
    * update
    
    * remove obsolete section from docs
    
    * hook into ingestion pipeline
    
    * integrate into ingestion pipeline
    
    * add e2e flow
    
    * update preporcessing
    
    * split longcovid subtypes
    
    * split longcovid subtypes
    
    * curate proper nodes dataset
    
    * unify nodes
    
    * fix typo
    
    * fix other type
    
    * fix typo
    
    * add disclaimer
    
    * ensure node creation
    
    * process PR commnets
    
    * move synonymizer
    
    * fix integration test
    
    * update ignore
    
    * add ignore
    
    * Update pipelines/matrix/conf/base/spark.yml
    
    * missing web network removed
    
    * avoids pulling synonomiser in CI
    
    * fix gitignore
    
    ---------
    
    Co-authored-by: Pascal Brokmeier <pascal@everycure.org>

---
+++ services/synonymizer/api/modules/synonymize/routers.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import List
+
+from .node_synonymizer import NodeSynonymizer
+
+router = APIRouter(prefix="", tags=["synonymize"])
+
+synonymizer = NodeSynonymizer()
+
+
+class Search(BaseModel):
+    """Model to searches."""
+
+    names: List[str]
+
+
+@router.get("/synonymize")
+async def get_canonical_curies(search: Search):
+    """Endpoint to get canonical curies.
+
+    Args:
+        Searhc: search string
+    """
+    return synonymizer.get_canonical_curies(names=search.names)
+
+
+@router.get("/normalize")
+async def get_normalizer_result(search: Search):
+    """Endpoint to get canonical curies.
+
+    Args:
+        Searhc: search string
+    """
+    return synonymizer.get_normalizer_results(entities=search.names)
