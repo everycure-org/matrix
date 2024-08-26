fa43f8a1e925f12510d66172a257c3e249f245f0
Pascal Brokmeier <pascal@everycure.org>
2024-08-26 15:33:55 +0200

    rm commented out nodes

--- pipelines/matrix/src/matrix/pipelines/integration/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/integration/pipeline.py
@@ -27,27 +27,5 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="integration.prm.unified_edges",
                 name="create_prm_unified_edges",
             ),
-            # TODO: Needs a fix
-            # # Construct ground_truth
-            # node(
-            #     func=nodes.create_int_pairs,
-            #     inputs=[
-            #         "integration.raw.ground_truth.positives",
-            #         "integration.raw.ground_truth.negatives",
-            #     ],
-            #     outputs="integration.int.known_pairs@pandas",
-            #     name="create_int_known_pairs",
-            #     tags=["rtx_kg2", "neo4j", "first_copy"],
-            # ),
-            # node(
-            #     func=nodes.create_treats,
-            #     inputs=[
-            #         "integration.model_input.nodes",
-            #         "integration.int.known_pairs@spark",
-            #     ],
-            #     outputs="integration.model_input.ground_truth",
-            #     name="create_neo4j_known_pairs",
-            #     tags=["rtx_kg2", "neo4j"],
-            # ),
         ]
     )
