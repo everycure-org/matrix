27ee72b55ea2e5921998b04f1e27a50774bd34f1
Laurens <90421718+lvijnck@users.noreply.github.com>
2024-08-26 17:56:13 +0200

    Improve fusing (#281)
    * add example tmp. neo4j instance
    
    * add files
    
    * update
    
    * add example tmp. neo4j instance
    
    * add updates
    
    * add step
    
    * update
    
    * updates
    
    * updates
    
    * updates
    
    * reduce iterations
    
    * export nodes only
    
    * push work
    
    * add tags
    
    * push work
    
    * add
    
    * working algo
    
    * working algo
    
    * add
    
    * add
    
    * add fusing + update pipeline
    
    * move to argo
    
    * add template
    
    * reduce data
    
    * rm template
    
    * add fixed template
    
    * rm test file
    
    * code cleanup
    
    * extract out
    
    * add
    
    * add working code
    
    * stop node embeddings
    
    * attempt
    
    * cleanup
    
    * fix with integration
    
    * move to different location
    
    * enrich ground truth with topological embeddings
    
    * enrich ground truth with topological embeddings
    
    * add docstring
    
    * add path filters to main
    
    * reset changes from laurens on pipeline setup
    
    * Add node for visualizing topological embeddings through PCA (#257)
    
    * add node for pca
    
    * add hue for clusters
    
    * split nodes in 2
    
    * update two nodes split
    
    * fix test error
    
    * add updates
    
    * add topological
    
    * rm
    
    * not changing these params
    
    * 100 iters
    
    ---------
    
    Co-authored-by: Laurens Vijnck <laurens@everycure.org>
    Co-authored-by: Pascal Brokmeier <pascal@everycure.org>
    
    * rm faulty suffix
    
    * separate path in gcs
    
    * change dataset to be called "runs"
    
    * bup
    
    * fixes to curie and medical ingestion
    
    * pca fix for input dataset and hard coded image
    
    * lol missing a thing at the end
    
    * add artifact repo
    
    * sa
    
    * add utility script
    
    * select specific columns and repartition before executing write to n4j
    
    * move some scripts around
    
    * update build script
    
    * reactivate nodes
    
    * potential fix for #263
    
    * cleanup integration catalog
    
    * rm chart lock
    
    * rm commented out nodes
    
    * mini help to make argo workflows UI better
    
    * Apply suggestions from code review
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * Fuse subparts of modelling pipeline (#276)
    
    * add proposal for model pipeline fusing
    
    * add proposal for fusing
    
    * more fusing
    
    * rm retries
    
    * add ttl strategy
    
    ---------
    
    Co-authored-by: Pascal Brokmeier <pascal@everycure.org>
    Co-authored-by: Piotr Kaniewski <115791652+piotrkan@users.noreply.github.com>

--- pipelines/matrix/src/matrix/pipelines/embeddings/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/embeddings/pipeline.py
@@ -169,6 +169,10 @@ def create_pipeline(**kwargs) -> Pipeline:
                 },
                 outputs="embeddings.reporting.topological_pca",
                 name="apply_topological_pca",
+                tags=[
+                    "argowf.fuse",
+                    "argowf.fuse-group.topological_pca",
+                ],
             ),
             node(
                 func=nodes.visualise_pca,
@@ -178,6 +182,10 @@ def create_pipeline(**kwargs) -> Pipeline:
                 },
                 outputs="embeddings.reporting.topological_pca_plot",
                 name="create_pca_plot_topological_embeddings",
+                tags=[
+                    "argowf.fuse",
+                    "argowf.fuse-group.topological_pca",
+                ],
             ),
         ],
     )
