e3b7dc99d953d679bc38e457d6da30385ec942a2
piotrkan <pkaniewski998@gmail.com>
2024-08-29 21:02:36 +0100

    wip

--- pipelines/matrix/src/matrix/pipelines/embeddings/nodes.py
+++ pipelines/matrix/src/matrix/pipelines/embeddings/nodes.py
@@ -397,7 +397,8 @@ def write_topological_embeddings(
 
     # Retrieve the model
     model_name = estimator.get("modelName")
-    if model_name == "graphSage":
+    model = estimator.get("model")
+    if model == "graphSage":
         model = gds.model.get(model_name)
         # Write model output back to graph
         model.predict_write(graph, writeProperty=write_property)
