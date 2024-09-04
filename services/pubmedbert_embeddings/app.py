8f67408af22ebe27bbc3a76b61ddbaf02664e123
Pascal Brokmeier <pascal@everycure.org>
2024-09-04 22:53:30 +0200

    updated system to behave as expected in scale up-down behavior

--- services/pubmedbert_embeddings/app.py
+++ services/pubmedbert_embeddings/app.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, Field
-from transformers import AutoTokenizer, AutoModel
-import torch
+from transformers import pipeline
 import os
 from joblib import Memory
-
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 from typing import List
+from models import EmbeddingRequest, EmbeddingResponse, Usage, Embedding
+
+import multiprocessing
+
+CPU_COUNT = multiprocessing.cpu_count()
 
 app = FastAPI()
 
-# Load the model and tokenizer
+# Load the model and tokenizer using a pipeline
 model_name = os.getenv(
     "MODEL_NAME", "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
 )
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModel.from_pretrained(model_name)
+feature_extractor = pipeline(
+    "feature-extraction", model=model_name, tokenizer=model_name
+)
 
 # Setup joblib caching
 cache_dir = f"./.cache/{model_name}"
@@ -22,65 +27,39 @@ if not os.path.exists(cache_dir):
     os.makedirs(cache_dir)
 memory = Memory(cache_dir, verbose=0, mmap_mode="r")
 
-
-class EmbeddingRequest(BaseModel):
-    input: str | List[str]
-    model: str = model_name
+# Create a thread pool executor
+executor = ThreadPoolExecutor(max_workers=4)  # Adjust the number of workers as needed
 
 
-class Embedding(BaseModel):
-    object: str = Field(
-        "embedding",
-        description="The type of object returned, in this case it is an embedding.",
-    )
-    embedding: List[float] = Field(..., description="The embedding vector.")
-    index: int = Field(..., description="The index of the embedding in the input list.")
+# @memory.cache
+async def get_embedding(texts: List[str]) -> List[float]:
+    """Async call to get embedding using the pipeline API of HF."""
 
+    def run_pipeline():
+        features = feature_extractor(
+            texts, padding=True, truncation=True, max_length=512
+        )
+        embedding = features[0][0]  # Get the CLS token embedding
+        token_count = len(feature_extractor.tokenizer.encode(" ".join(texts)))
+        return embedding, token_count
 
-class Usage(BaseModel):
-    prompt_tokens: int = Field(..., description="The number of tokens in the prompt.")
-    total_tokens: int = Field(..., description="The total number of tokens used.")
-
-
-class EmbeddingResponse(BaseModel):
-    object: str = Field(
-        "list",
-        description="The type of object returned, in this case it is a list of embeddings.",
-    )
-    data: List[Embedding] = Field(..., description="The list of embedding vectors.")
-    model: str = Field(..., description="The name of the embedding model used.")
-    usage: Usage = Field(..., description="The usage statistics for the request.")
-
-
-@memory.cache
-async def get_embedding(text: str) -> List[float]:
-    tokens = tokenizer(
-        text, return_tensors="pt", truncation=True, max_length=512, padding=True
-    )
-    token_count = tokens.input_ids.shape[1]
-
-    with torch.no_grad():
-        output = model(**tokens, output_hidden_states=True).pooler_output
-        embedding = output.cpu().detach().numpy().tolist()
-
-    # we get a list of lists, but we just want the first one
-    return embedding[0], token_count
+    # Run the pipeline in a separate thread
+    return await asyncio.get_event_loop().run_in_executor(executor, run_pipeline)
 
 
 @app.post("/v1/embeddings")
 async def create_embedding(request: EmbeddingRequest):
+    """Main API endpoint for embedding generation."""
     try:
         # Ensure input is a list
         inputs = [request.input] if isinstance(request.input, str) else request.input
 
-        embeddings = []
-        total_tokens = 0
-
-        for i, text in enumerate(inputs):
-            # Tokenize the input
-            embedding, token_count = await get_embedding(text)
-            embeddings.append(embedding)
-            total_tokens += token_count
+        # Process all inputs concurrently
+        if len(inputs) > 0:
+            embeddings, token_count = await get_embedding(inputs)
+        else:
+            embeddings = []
+            token_count = 0
 
         # Create a response in OpenAI-like format
         response = EmbeddingResponse(
@@ -90,7 +69,7 @@ async def create_embedding(request: EmbeddingRequest):
                 for i, emb in enumerate(embeddings)
             ],
             model=model_name,
-            usage=Usage(prompt_tokens=total_tokens, total_tokens=total_tokens),
+            usage=Usage(prompt_tokens=token_count, total_tokens=token_count),
         )
 
         return response
@@ -100,18 +79,8 @@ async def create_embedding(request: EmbeddingRequest):
 
 @app.get("/")
 async def root():
-    return {"message": "success"}
-
-
-@app.get("/health/liveness")
-async def liveness_check():
-    return {"status": "alive"}
-
-
-@app.get("/health/readiness")
-async def readiness_check():
     # Perform a simple operation to check if the model is loaded
-    if tokenizer is not None:
+    if feature_extractor is not None:
         return {"status": "ready"}
     else:
         raise HTTPException(status_code=500, detail="Service not ready")
@@ -120,4 +89,5 @@ async def readiness_check():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app, host="0.0.0.0", port=8000, limit_concurrency=CPU_COUNT*4, backlog=CPU_COUNT*16)
+    uvicorn.run(app, host="0.0.0.0", port=8000, backlog=CPU_COUNT * 64)
