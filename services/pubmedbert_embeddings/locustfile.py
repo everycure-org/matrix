5d67b1a33e9091c2540bfdd83b6f3c6d09ab8b1f
Pascal Brokmeier <pascal@everycure.org>
2024-09-04 11:03:47 +0200

    send random number of requests in locust request

--- services/pubmedbert_embeddings/locustfile.py
+++ services/pubmedbert_embeddings/locustfile.py
@@ -1,4 +1,18 @@
 from locust import HttpUser, task, between
+import random
+
+MODEL = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
+INPUT = "The patient presents with fever and cough"
+
+
+def get_payload():
+    num = random.randrange(100)
+    payload = {"model": MODEL, "input": [INPUT for i in range(num)]}
+    # sometimes we give just the text itself
+    if len(payload.get("input")) == 1:
+        payload["input"] = INPUT
+
+    return payload
 
 
 class EmbeddingsUser(HttpUser):
@@ -6,8 +20,5 @@ class EmbeddingsUser(HttpUser):
 
     @task
     def get_embedding(self):
-        payload = {
-            "input": "The patient presents with fever and cough",
-            "model": "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext",
-        }
+        payload = get_payload()
         self.client.post("/v1/embeddings", json=payload)
