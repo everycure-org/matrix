bb038c88bbd5e999b0fc1d2798ebc75f7351a4e9
Laurens Vijnck <laurens@everycure.org>
2024-09-11 14:00:00 +0100

    rm breakpoint

--- pipelines/matrix/src/matrix/pipelines/modelling/nodes.py
+++ pipelines/matrix/src/matrix/pipelines/modelling/nodes.py
@@ -343,7 +343,6 @@ def create_model(*estimators) -> ModelWrapper:
     Returns:
         ModelWrapper encapsulating estimators
     """
-    breakpoint()
     return ModelWrapper(estimators=estimators, agg_func=np.mean)
 
 
