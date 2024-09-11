bdcda308b8729b7d6cae13f9e9d6ef809bf71827
Laurens <90421718+lvijnck@users.noreply.github.com>
2024-09-11 12:04:27 +0100

    MLFlow to GCS (#293)
    * add example
    
    * add work
    
    * push changes
    
    * rm breakpoint
    
    * call save
    
    * rm breakpoint
    
    * reenable save
    
    * rm debugging stuff
    
    * commit changes
    
    * rm mlflow file
    
    * rm lock file
    
    * rm test
    
    * allow proxying
    
    * add the release version to path
    
    * add changes
    
    * rm subpath in mlflow
    
    * Update onboarding.md
    
    * push
    
    * revert
    
    * revert
    
    * disable miniop
    
    * rm minio user
    
    * correct
    
    * reenable
    
    * set artifact location
    
    * revert commenting
    
    * Add 1 git-crypt collaborator (#343)
    
    New collaborators:
    
    	225C3B75 ahueb <alan@hueb.org>
    
    * Update index.md (#336)
    
    * Update index.md
    
    Updated onboarding content with remaining information from Notion which hasn't already been pulled across
    
    * Update docs/src/onboarding/index.md
    
    Co-authored-by: Pascal Bro <pascal@everycure.org>
    
    * Update index.md
    
    ---------
    
    Co-authored-by: Pascal Bro <pascal@everycure.org>
    
    * New script to retry docker compose_down in CI and debug when it's having issues (#357)
    
    * add new script to debug docker issues
    
    * cleanup structure a bit
    
    * Add Robokop data to ingestion pipeline (#188)
    
    * add
    
    * add todos
    
    * add todo pointers
    
    * Robokop Ingestion Pipeline
    
    added fields for Robokop ingestion.
    
    * update gitignore
    
    * update ignore
    
    * ignore idea files
    
    * add pointers for fabrication
    
    * Edits from Laurens comments
    
    modified files after Laurens comments
    
    * Cleaned up
    
    removed KC "TODO"s. Fixed Typos
    
    * flushing out additional columns per real robokop data
    
    * aligning fabricator column data with schema
    
    * renaming node name
    
    * removing duplicate spark_csv
    
    * updating
    
    * reverting to String
    
    * removing fabricator details
    
    * using LazySparkDataset, removing schema info
    
    * run pre-commit
    
    * Update pipelines/matrix/conf/base/ingestion/catalog.yml
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>
    
    * Update pipelines/matrix/conf/base/ingestion/catalog.yml
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>
    
    * Update pipelines/matrix/conf/base/ingestion/catalog.yml
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>
    
    * Update pipelines/matrix/conf/base/ingestion/catalog.yml
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>
    
    * fixing typo
    
    * fix layers
    
    * take subset of columns
    
    * setting header to true
    
    * overriding catalog due to change in raw path
    
    * add
    
    * fix dataset name as - is not supported by bq
    
    * Update spark.yml
    
    * add new node function for robokop nodes
    
    * update
    
    * set unit seperator
    
    * add descriptiopn
    
    ---------
    
    Co-authored-by: Kathleen Carter <163005214+eKathleenCarter@users.noreply.github.com>
    Co-authored-by: Jason Reilly <jdr0887@gmail.com>
    Co-authored-by: Jason Reilly <jdr0887@users.noreply.github.com>
    Co-authored-by: Pascal Brokmeier <pascal@everycure.org>
    
    * Update pipelines/matrix/conf/base/fabricator/parameters.yml
    
    ---------
    
    Co-authored-by: elliottsharp <elliott.sharp@hotmail.com>
    Co-authored-by: Pascal Bro <pascal@everycure.org>
    Co-authored-by: Kathleen Carter <163005214+eKathleenCarter@users.noreply.github.com>
    Co-authored-by: Jason Reilly <jdr0887@gmail.com>
    Co-authored-by: Jason Reilly <jdr0887@users.noreply.github.com>

--- pipelines/matrix/src/matrix/pipelines/ingestion/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/ingestion/pipeline.py
@@ -2,6 +2,7 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
 import pyspark.sql.functions as F
+from .nodes import ingest_robokop_nodes
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -13,14 +14,14 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=lambda x: x.withColumn("kg_source", F.lit("rtx_kg2")),
                 inputs=["ingestion.raw.rtx_kg2.nodes@spark"],
-                outputs="ingestion.prm.rtx_kg2.nodes",
+                outputs="ingestion.int.rtx_kg2.nodes",
                 name="write_rtx_kg2_nodes",
                 tags=["rtx_kg2"],
             ),
             node(
                 func=lambda x: x.withColumn("kg_source", F.lit("rtx_kg2")),
                 inputs=["ingestion.raw.rtx_kg2.edges@spark"],
-                outputs="ingestion.prm.rtx_kg2.edges",
+                outputs="ingestion.int.rtx_kg2.edges",
                 name="write_rtx_kg2_edges",
                 tags=["rtx_kg2"],
             ),
@@ -28,16 +29,34 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=lambda x: x.withColumn("kg_source", F.lit("ec_medical_team")),
                 inputs=["ingestion.raw.ec_medical_team.nodes@spark"],
-                outputs="ingestion.prm.ec_medical_team.nodes",
+                outputs="ingestion.int.ec_medical_team.nodes",
                 name="write_ec_medical_team_nodes",
                 tags=["ec_medical_team"],
             ),
             node(
                 func=lambda x: x.withColumn("kg_source", F.lit("ec_medical_team")),
                 inputs=["ingestion.raw.ec_medical_team.edges@spark"],
-                outputs="ingestion.prm.ec_medical_team.edges",
+                outputs="ingestion.int.ec_medical_team.edges",
                 name="write_ec_medical_team_edges",
                 tags=["ec_medical_team"],
+            ),
+            # robokop
+            node(
+                func=ingest_robokop_nodes,
+                inputs=["ingestion.raw.robokop.nodes@spark"],
+                outputs="ingestion.int.robokop.nodes",
+                name="write_robokop_nodes",
+                tags=["robokop"],
+            ),
+            node(
+                # FUTURE: Update selection
+                func=lambda x: x.select("subject", "predicate", "object").withColumn(
+                    "kg_source", F.lit("robokop")
+                ),
+                inputs=["ingestion.raw.robokop.edges@spark"],
+                outputs="ingestion.int.robokop.edges",
+                name="write_robokop_edges",
+                tags=["robokop"],
             ),
         ]
     )
