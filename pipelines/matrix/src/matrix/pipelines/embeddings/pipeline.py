60c315a69c6738eb2333331df499a96f16f20551
Alexei Stepanenko <alexei.stepa@gmail.com>
2024-09-13 11:24:06 +0100

    revert embedding pipeline changes

--- pipelines/matrix/src/matrix/pipelines/embeddings/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/embeddings/pipeline.py
@@ -59,9 +59,7 @@ def create_pipeline(**kwargs) -> Pipeline:
             ),
             # Load spark dataset into local neo instance
             node(
-                func=lambda x: x.select(
-                    "id", "name", "category", "description", "pca_embedding"
-                ),
+                func=lambda x: x.select("id", "name", "category", "pca_embedding"),
                 inputs=["embeddings.feat.graph.pca_node_embeddings"],
                 outputs="embeddings.tmp.input_nodes",
                 name="ingest_neo4j_input_nodes",
