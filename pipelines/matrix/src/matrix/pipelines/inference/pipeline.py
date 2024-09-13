3da5c5b9a5d8ecadcdf614b29255b3b9d86b8bd4
piotrkan <pkaniewski998@gmail.com>
2024-09-13 17:05:24 +0100

    bring back loop

--- pipelines/matrix/src/matrix/pipelines/inference/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/inference/pipeline.py
@@ -5,51 +5,58 @@ from . import nodes as nd
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    """Create fabricator pipeline."""
-    return pipeline(
-        [
-            node(
-                func=lambda x: x,
-                inputs="ingestion.raw.drug_list",
-                outputs="inference.sheet.drug_list",
-                name=f"ingest_drug_list",
-            ),
-            node(
-                func=lambda x: x,
-                inputs="ingestion.raw.disease_list",
-                outputs="inference.sheet.disease_list",
-                name=f"ingest_disease_list",
-            ),
-            node(
-                func=nd.resolve_input_sheet,
-                inputs={
-                    "sheet": "inference.sheet.normalized_inputs",
-                },
-                outputs="inference.request.type",
-                name="select_request_type",
-            ),
-            node(
-                func=nd.run_inference,
-                inputs={
-                    "model": "inference.model.xgb",  # TODO: link params.yaml so that it feeds into this line?
-                    "embeddings": "inference.embed.nodes",
-                    "infer_type": "inference.request.type",
-                    "drug_nodes": "ingestion.raw.drug_list",
-                    "disease_nodes": "ingestion.raw.disease_list",
-                    "train_df": "modelling.model_input.splits",
-                    "sheet": "inference.sheet.normalized_inputs",
-                },
-                outputs=f"model_outputs.node.predictions",
-                name=f"run_inference",
-            ),
-            node(
-                func=nd.visualise_treat_scores,
-                inputs={
-                    "scores": "model_outputs.node.predictions",
-                    "infer_type": "inference.nodes.type",
-                },
-                outputs="model_outputs.node.visualisations",
-                name=f"visualise_inference",
-            ),
-        ]
-    )
+    """Create requests pipeline."""
+    nodes = []
+    for model in settings.DYNAMIC_PIPELINES_MAPPING.get("modelling"):
+        if not model["run_inference"]:
+            continue
+        nodes.append(
+            pipeline(
+                [
+                    node(
+                        func=lambda x: x,
+                        inputs="ingestion.raw.drug_list",
+                        outputs="inference.sheet.drug_list",
+                        name=f"ingest_drug_list",
+                    ),
+                    node(
+                        func=lambda x: x,
+                        inputs="ingestion.raw.disease_list",
+                        outputs="inference.sheet.disease_list",
+                        name=f"ingest_disease_list",
+                    ),
+                    node(
+                        func=nd.resolve_input_sheet,
+                        inputs={
+                            "sheet": "inference.sheet.normalized_inputs",
+                        },
+                        outputs="inference.request.type",
+                        name="select_request_type",
+                    ),
+                    node(
+                        func=nd.run_inference,
+                        inputs={
+                            "model": f'modelling.{model["model_name"]}.models.model',
+                            "embeddings": "inference.embed.nodes",
+                            "infer_type": "inference.request.type",
+                            "drug_nodes": "ingestion.raw.drug_list",
+                            "disease_nodes": "ingestion.raw.disease_list",
+                            "train_df": "modelling.model_input.splits",
+                            "sheet": "inference.sheet.normalized_inputs",
+                        },
+                        outputs=f"model_outputs.{model['model_name']}.predictions",
+                        name=f"run_inference",
+                    ),
+                    node(
+                        func=nd.visualise_treat_scores,
+                        inputs={
+                            "scores": "model_outputs.node.predictions",
+                            "infer_type": "inference.request.type",
+                        },
+                        outputs=f"model_outputs.{model['model_name']}.visualisations",
+                        name=f"visualise_inference",
+                    ),
+                ]
+            )
+        )
+    return pipeline(nodes)
