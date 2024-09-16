ae4d0c2c51f58ca9be7355d13c28fa3076b9c819
Pascal Brokmeier <pascal@everycure.org>
2024-09-17 00:39:44 +0100

    optimized formatting for readability

--- pipelines/matrix/src/matrix/pipelines/integration/standardize.py
+++ pipelines/matrix/src/matrix/pipelines/integration/standardize.py
@@ -8,7 +8,7 @@ import pyspark.sql.types as T
 from pandera.pyspark import DataFrameModel
 from pandera.typing import Series
 from typing import List
-from matrix.schemas.data_api import KGEdgeSchema, KGNodeSchema
+from matrix.schemas.data_api import KGEdgeSchema, KGNodeSchema, cols_for_schema
 from pyspark.sql import DataFrame
 import pyspark.sql.functions as f
 
@@ -26,28 +26,15 @@ def transform_robo_edges(edges_df: DataFrame) -> DataFrame:
     # fmt: off
     return (
         edges_df
-        .withColumn("upstream_kg_source", f.array(f.lit("robokop")))
-        .withColumn("publications", f.split(f.col("publications"), "\x1f"))
+        .withColumn("upstream_kg_source",          f.array(f.lit("robokop")))
+        .withColumn("publications",                f.split(f.col("publications"), "\x1f"))
         .withColumn("aggregator_knowledge_source", f.split(f.col("aggregator_knowledge_source"), "\x1f"))
-        .withColumn("subject_aspect_qualifier", f.lit(None))  # FUTURE: not present in Robokop
+        .withColumn("subject_aspect_qualifier",    f.lit(None))  # FUTURE: not present in Robokop
         .withColumn("subject_direction_qualifier", f.lit(None))  # FUTURE: not present in Robokop
         # final selection of columns. 
-        .select(
-            "subject",
-            "predicate",
-            "object",
-            "knowledge_level",
-            "primary_knowledge_source",
-            "aggregator_knowledge_source",
-            "publications",
-            "object_aspect_qualifier",
-            "object_direction_qualifier",
-            "subject_aspect_qualifier",
-            "subject_direction_qualifier",
-            "upstream_kg_source",
-        )
+        .select(*cols_for_schema(KGEdgeSchema))
     )
-    # fmt: off
+    # fmt: on
 
 
 @pa.check_output(KGNodeSchema)
@@ -62,25 +49,73 @@ def transform_robo_nodes(nodes_df: DataFrame) -> DataFrame:
     """
     # fmt: off
     return (
-        nodes_df.withColumn("upstream_kg_source", f.array(f.lit("robokop")))
-        .withColumn("all_categories", f.split(f.col("category"), "\x1f"))
-        .withColumn( "equivalent_identifiers", f.split(f.col("equivalent_identifiers"), "\x1f"))
-        .drop("category")
-        .withColumn("category", f.col("all_categories").getItem(0))
-        .withColumn("label", f.col("category"))
-        .withColumn("publications", f.array(f.lit(None)))
+        nodes_df.withColumn("upstream_kg_source",        f.array(f.lit("robokop")))
+        .withColumn("all_categories",                    f.split(f.col("category"), "\x1f"))
+        .withColumn( "equivalent_identifiers",           f.split(f.col("equivalent_identifiers"), "\x1f"))
+        .withColumn("category",                          f.col("all_categories").getItem(0))
+        .withColumn("label",                             f.col("category"))
+        .withColumn("publications",                      f.array(f.lit(None)))
         .withColumn("international_resource_identifier", f.lit(None))
-        .select(
-            "id",
-            "name",
-            "category",
-            "description",
-            "equivalent_identifiers",
-            "all_categories",
-            "publications",
-            "label",
-            "international_resource_identifier",
-            "upstream_kg_source",
-        )
+        .select(*cols_for_schema(KGNodeSchema))
+    )
+    # fmt: on
+
+
+@pa.check_output(KGNodeSchema)
+def transform_rtxkg2_nodes(nodes_df: DataFrame) -> DataFrame:
+    """Transform RTX KG2 nodes to our target schema.
+
+    Args:
+        nodes_df: Nodes DataFrame.
+
+    Returns:
+        Transformed DataFrame.
+    """
+    SEP = "\u01c2"
+
+    # fmt: off
+    return (
+        nodes_df
+        .withColumn("upstream_kg_source",                f.array(f.lit("rtxkg2")))
+        .withColumn("labels",                            f.split(f.col("label"), SEP))
+        .withColumn("all_categories",                    f.split(f.col("all_categories"), SEP))
+        .withColumn("all_categories",                    f.array_distinct(f.concat("labels", "all_categories")))
+        .withColumn("equivalent_identifiers",            f.split(f.col("equivalent_curies"), SEP))
+        .withColumn("publications",                      f.split(f.col("publications"), SEP))
+        .withColumn("international_resource_identifier", f.col("iri"))
+        # .withColumn("name", f.split(f.col("name"), "\x1f").getItem(0))
+        .select(*cols_for_schema(KGNodeSchema))
+    )
+    # fmt: on
+
+
+@pa.check_output(KGEdgeSchema)
+def transform_rtxkg2_edges(edges_df: DataFrame) -> DataFrame:
+    """Transform RTX KG2 edges to our target schema.
+
+    Args:
+        edges_df: Edges DataFrame.
+
+    Returns:
+        Transformed DataFrame.
+    """
+    SEP = "\u01c2"
+
+    # fmt: off
+    return (
+        edges_df
+        .withColumn("upstream_kg_source",          f.array(f.lit("rtxkg2")))
+        .withColumn("subject",                     f.col("subject"))
+        .withColumn("object",                      f.col("object"))
+        .withColumn("predicate",                   f.col("predicate"))
+        .withColumn("knowledge_level",             f.lit(None))  # TODO / Not present in RTX KG2
+        .withColumn("primary_knowledge_source",    f.col("knowledge_source"))
+        .withColumn("aggregator_knowledge_source", f.array())
+        .withColumn("publications",                f.split(f.col("publications"), SEP))
+        .withColumn("subject_aspect_qualifier",    f.lit(None))
+        .withColumn("subject_direction_qualifier", f.lit(None))
+        .withColumn("object_aspect_qualifier",     f.lit(None))
+        .withColumn("object_direction_qualifier",  f.lit(None))
+        .select(*cols_for_schema(KGEdgeSchema))
     )
     # fmt: on
