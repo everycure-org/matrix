c5153d09f16aa58277ffa59650c7fa03b727bb62
Laurens Vijnck <laurens@everycure.org>
2024-09-17 15:55:46 +0200

    rn synonymizer updates

--- services/synonymizer/api/modules/synonymize/routers.py
+++ services/synonymizer/api/modules/synonymize/routers.py
@@ -1,19 +1,10 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from typing import List
-
-from api.configuration import settings
 from .node_synonymizer import NodeSynonymizer
 
 router = APIRouter(prefix="", tags=["synonymize"])
 
 synonymizer = NodeSynonymizer()
-
-from neo4j import GraphDatabase
-
-driver = GraphDatabase.driver(
-    settings.NEO4J_HOST, auth=(settings.NEO4J_USER, settings.NEO4J_PASSWORD)
-)
 
 
 class Search(BaseModel):
@@ -39,21 +30,4 @@ async def get_normalizer_result(search: Search):
     Args:
         Searhc: search string
     """
-    result = synonymizer.get_normalizer_results(entities=[search.name])
-
-    if result.get(search.name) and check_node_exists(
-        result[search.name]["id"]["identifier"], "Entity"
-    ):
-        return result
-
-    return {}
-
-
-def check_node_exists(id: str, label: str):
-    with driver.session(database=settings.NEO4J_DATABASE) as session:
-        query = f"""
-        MATCH (n:{label} {{id: $id}})
-        RETURN count(n) > 0 AS node_exists
-        """
-        result = session.run(query, id=id)
-        return result.single()["node_exists"]
+    return synonymizer.get_normalizer_results(entities=[search.name])
