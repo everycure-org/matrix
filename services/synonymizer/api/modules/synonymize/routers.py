2238d6a6ddcbeb1b14990c39008aea42ac249309
Laurens Vijnck <laurens@everycure.org>
2024-09-17 14:12:12 +0200

    revert synonymizer changes

--- services/synonymizer/api/modules/synonymize/routers.py
+++ services/synonymizer/api/modules/synonymize/routers.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from typing import List
 
-from api.configuration import settings
 from .node_synonymizer import NodeSynonymizer
 
 router = APIRouter(prefix="", tags=["synonymize"])
@@ -10,10 +8,6 @@ router = APIRouter(prefix="", tags=["synonymize"])
 synonymizer = NodeSynonymizer()
 
 from neo4j import GraphDatabase
-
-driver = GraphDatabase.driver(
-    settings.NEO4J_HOST, auth=(settings.NEO4J_USER, settings.NEO4J_PASSWORD)
-)
 
 
 class Search(BaseModel):
@@ -27,7 +21,7 @@ async def get_canonical_curies(search: Search):
     """Endpoint to get canonical curies.
 
     Args:
-        Searhc: search string
+        Search: search string
     """
     return synonymizer.get_canonical_curies(names=[search.name])
 
@@ -37,23 +31,6 @@ async def get_normalizer_result(search: Search):
     """Endpoint to get canonical curies.
 
     Args:
-        Searhc: search string
+        Search: search string
     """
-    result = synonymizer.get_normalizer_results(entities=[search.name])
-
-    if result.get(search.name) and check_node_exists(
-        result[search.name]["id"]["identifier"], "Entity"
-    ):
-        return result
-
-    return {}
-
-
-def check_node_exists(id: str, label: str):
-    with driver.session(database=settings.NEO4J_DATABASE) as session:
-        query = f"""
-        MATCH (n:{label} {{id: $id}})
-        RETURN count(n) > 0 AS node_exists
-        """
-        result = session.run(query, id=id)
-        return result.single()["node_exists"]
+    return synonymizer.get_normalizer_results(entities=[search.name])
