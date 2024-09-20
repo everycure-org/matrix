8990a898d132da62fd521b01e069cdd5a1e0861f
Alexei Stepanenko <alexei.stepa@gmail.com>
2024-09-20 11:02:31 +0100

    ensure fabricated ground truth and trials lie in matrix

--- pipelines/matrix/src/matrix/pipelines/fabricator/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/fabricator/pipeline.py
@@ -7,26 +7,31 @@ from kedro.pipeline import Pipeline, node, pipeline
 
 from data_fabricator.v0.nodes.fabrication import fabricate_datasets
 
-from pyspark.sql import DataFrame
+from pyspark.sql import DataFrame  # TODO: REMOVE?
 
 import pyspark.sql.functions as F
 
 
-def _create_pairs(nodes: DataFrame, num: int = 50) -> pd.DataFrame:
-    """Creating 2 sets of random pairs from the nodes."""
-    # NOTE: This is here because the dataset is generated without
-    # header as per the KG2 schema. The spark version of the
-    # dataset re-introduces the correct schema.
-    nodes = nodes.toPandas()
-
+def _create_pairs(
+    drug_list: pd.DataFrame, disease_list: pd.DataFrame, num: int = 100, seed: int = 42
+) -> pd.DataFrame:
+    """Creating 2 sets of random pairs from the nodes. Ensures no duplicate pairs."""
     # Sample random pairs
-    random_drugs = nodes["id"].sample(num * 2, replace=True, ignore_index=True)
-    random_diseases = nodes["id"].sample(num * 2, replace=True, ignore_index=True)
+    random_drugs = drug_list["curie"].sample(
+        num * 2, replace=True, ignore_index=True, random_state=seed
+    )
+    random_diseases = disease_list["curie"].sample(
+        num * 2, replace=True, ignore_index=True, random_state=seed
+    )
 
     df = pd.DataFrame(
         data=[[drug, disease] for drug, disease in zip(random_drugs, random_diseases)],
         columns=["source", "target"],
     )
+
+    # Remove duplicate pairs
+    df = df.drop_duplicates()
+
     # split df in half and return two df
     return df[:num], df[num:]
 
@@ -67,7 +72,10 @@ def create_pipeline(**kwargs) -> Pipeline:
             ),
             node(
                 func=_create_pairs,
-                inputs=["ingestion.raw.rtx_kg2.nodes@spark"],
+                inputs=[
+                    "ingestion.raw.drug_list@pandas",
+                    "ingestion.raw.disease_list@pandas",
+                ],
                 outputs=[
                     "modelling.raw.ground_truth.positives",
                     "modelling.raw.ground_truth.negatives",
