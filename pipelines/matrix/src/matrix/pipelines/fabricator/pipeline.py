0d69fe45c3c9de8478e4a46c4f11662edba4f1ff
Alexei Stepanenko <alexei.stepa@gmail.com>
2024-09-26 15:24:37 +0100

    update fabricator

--- pipelines/matrix/src/matrix/pipelines/fabricator/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/fabricator/pipeline.py
@@ -7,33 +7,47 @@ from kedro.pipeline import Pipeline, node, pipeline
 
 from data_fabricator.v0.nodes.fabrication import fabricate_datasets
 
-from pyspark.sql import DataFrame  # TODO: REMOVE?
-
-import pyspark.sql.functions as F
-
 
 def _create_pairs(
     drug_list: pd.DataFrame, disease_list: pd.DataFrame, num: int = 100, seed: int = 42
 ) -> pd.DataFrame:
-    """Creating 2 sets of random pairs from the nodes. Ensures no duplicate pairs."""
-    # Sample random pairs
-    random_drugs = drug_list["curie"].sample(
-        num * 2, replace=True, ignore_index=True, random_state=seed
-    )
-    random_diseases = disease_list["curie"].sample(
-        num * 2, replace=True, ignore_index=True, random_state=seed
-    )
+    """Create 2 sets of random drug-disease pairs. Ensures no duplicate pairs.
 
-    df = pd.DataFrame(
-        data=[[drug, disease] for drug, disease in zip(random_drugs, random_diseases)],
-        columns=["source", "target"],
-    )
+    Args:
+        drug_list: Dataframe containing the list of drugs.
+        disease_list: Dataframe containing the list of diseases.
+        num: Size of each set of random pairs. Defaults to 100.
+        seed: Random seed. Defaults to 42.
 
-    # Remove duplicate pairs
-    df = df.drop_duplicates()
+    Returns:
+        Two dataframes, each containing 'num' unique drug-disease pairs.
+    """
+    # NOTE: This function was partially generated using AI assistance.
+    is_enough_generated = False
+    while not is_enough_generated:
+        # Sample random pairs (we sample twice the required amount in case duplicates are removed)
+        random_drugs = drug_list["curie"].sample(
+            num * 4, replace=True, ignore_index=True, random_state=seed
+        )
+        random_diseases = disease_list["curie"].sample(
+            num * 4, replace=True, ignore_index=True, random_state=2 * seed
+        )
+
+        df = pd.DataFrame(
+            data=[
+                [drug, disease] for drug, disease in zip(random_drugs, random_diseases)
+            ],
+            columns=["source", "target"],
+        )
+
+        # Remove duplicate pairs
+        df = df.drop_duplicates()
+
+        # Check that we still have enough fabricated pairs
+        is_enough_generated = len(df) >= 2 * num
 
     # split df in half and return two df
-    return df[:num], df[num:]
+    return df[:num], df[num : 2 * num]
 
 
 def create_pipeline(**kwargs) -> Pipeline:
