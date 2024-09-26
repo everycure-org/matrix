9761852b8999fb724986c419552d6c25d8a9da68
Alan Huebschen <alan@hueb.org>
2024-09-26 12:40:27 -0500

    renamed and moved parameters, added per env parameter files, changed debug_query_limit to n_diseases_limit, updated docstrings and docstring formatting, added @has_schema decorator, removed debugging file and print outputs

--- pipelines/matrix/src/matrix/pipelines/bte/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/bte/pipeline.py
@@ -7,8 +7,11 @@ from .nodes import run_bte_queries
 def create_pipeline(**kwargs) -> Pipeline:
     """Creates a Kedro pipeline for the BTE process.
 
-    :param kwargs: Additional keyword arguments.
-    :return: A Kedro pipeline object.
+    Args:
+        kwargs: Additional keyword arguments.
+
+    Returns:
+        Pipeline(): A Kedro pipeline object.
     """
     return Pipeline(
         [
@@ -16,12 +19,12 @@ def create_pipeline(**kwargs) -> Pipeline:
                 func=run_bte_queries,
                 inputs={
                     "disease_list": "ingestion.raw.disease_list@pandas",
+                    "drug_list": "ingestion.raw.drug_list@pandas",
                     "async_query_url": "params:bte.async_query_url",
                     "max_concurrent_requests": "params:bte.max_concurrent_requests",
                     "default_timeout": "params:bte.default_timeout",
                     "job_check_sleep": "params:bte.job_check_sleep",
-                    "debug_query_limiter": "params:bte.debug_query_limiter",
-                    "debug_csv_path": "params:bte.debug_csv_path",
+                    "n_diseases_limit": "params:bte.n_diseases_limit",
                 },
                 outputs="bte.model_output.predictions",
                 name="bte-single_processing_node",
