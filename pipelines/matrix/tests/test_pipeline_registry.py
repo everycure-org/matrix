4c962240b0a01337eca94a2dbfab396049ba288e
matwasilewski <mat.p.wasilewski@gmail.com>
2024-10-04 13:45:07 +0000

    trim down pipeline number

--- pipelines/matrix/tests/test_pipeline_registry.py
+++ pipelines/matrix/tests/test_pipeline_registry.py
@@ -14,21 +14,9 @@ def test_register_pipelines_returns_dict(pipelines):
 
 def test_all_pipeline_names_present(pipelines):
     expected_pipelines = [
-        "make_embeddings",
-        "make_modelling",
-        "__default__",
-        "preprocessing",
-        "ingestion",
-        "modelling",
         "embeddings",
-        "fabricator",
-        "integration",
-        "evaluation",
-        "release",
-        "matrix_generation",
-        "test",
-        "all",
-        "experiment",
+        "modelling",
+        "__default__",
     ]
     for pipeline_name in expected_pipelines:
         assert pipeline_name in pipelines
@@ -45,8 +33,7 @@ def test_default_pipeline_composition(pipelines):
     make_modelling = pipelines["make_modelling"]
 
     assert (
-        len(default_pipeline.nodes)
-        == len(make_embeddings.nodes) + len(make_modelling.nodes)
+        len(default_pipeline.nodes) == len(make_embeddings.nodes) + len(make_modelling.nodes)
     ), f"Default pipeline (len: {len(default_pipeline.nodes)}) should be the sum of make_embeddings (len: {len(make_embeddings.nodes)}) and make_modelling (len: {len(make_modelling.nodes)})"
     assert default_pipeline.nodes == (make_embeddings + make_modelling).nodes
 
@@ -70,8 +57,7 @@ def test_make_modelling_composition(pipelines):
     evaluation = pipelines["evaluation"]
 
     assert (
-        len(make_modelling.nodes)
-        == len(modelling.nodes) + len(matrix_generation.nodes) + len(evaluation.nodes)
+        len(make_modelling.nodes) == len(modelling.nodes) + len(matrix_generation.nodes) + len(evaluation.nodes)
     ), f"Make modelling pipeline (len: {len(make_modelling.nodes)}) should be the sum of modelling (len: {len(modelling.nodes)}), matrix_generation (len: {len(matrix_generation.nodes)}) and evaluation (len: {len(evaluation.nodes)})"
     assert make_modelling.nodes == (modelling + matrix_generation + evaluation).nodes
 
@@ -92,10 +78,7 @@ def test_test_pipeline_composition(pipelines):
         + len(make_modelling.nodes)
         + len(release.nodes)
     ), f"Test pipeline (len: {len(test_pipeline.nodes)}) should be the sum of fabricator (len: {len(fabricator.nodes)}), ingestion (len: {len(ingestion.nodes)}), make_embeddings (len: {len(make_embeddings.nodes)}), make_modelling (len: {len(make_modelling.nodes)}) and release (len: {len(release.nodes)})"
-    assert (
-        test_pipeline.nodes
-        == (fabricator + ingestion + make_embeddings + make_modelling + release).nodes
-    )
+    assert test_pipeline.nodes == (fabricator + ingestion + make_embeddings + make_modelling + release).nodes
 
 
 def test_all_pipeline_composition(pipelines):
