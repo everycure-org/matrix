0f7e62195c3c7c8bfdeb085986cb7cbde60e4b2f
vjsykora <jsykora@Joe>
2024-10-04 12:01:07 +0100

    adding node and pipeline to produce a random sample of the KG from ingestion

---
+++ pipelines/matrix/src/matrix/pipelines/kg_sample_generation/nodes.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from pyspark.sql import DataFrame
+
+def get_random_selection_of_edges(
+    nodes: DataFrame,
+    edges: DataFrame
+) -> DataFrame:
+    """Function that accepts the whole Edges and Nodes
+       and makes a random selection of the Edges.
+       The idea is to have a dataset that resembles the
+       original KG data but which is much smaller so
+       it can be ran on a local environment
+
+    Args:
+        nodes: Dataframe with raw nodes
+        edges: Dataframe with raw edges
+    """
+    edges_sample_df = edges.sample(withReplacement=False, fraction=0.1, seed=123)
+    edges_subcols_df = edges_sample_df.select('start_id', 'end_id')
+    # Now we need to select the nodes that have been included in the selection of edges
+    cond = (nodes.id == edges_subcols_df.start_id) | (nodes.id == edges_subcols_df.end_id)
+    nodes_sample_df = nodes.join(edges_subcols_df, how='inner', on=cond).select(nodes.columns)
+    return edges_sample_df, nodes_sample_df
+
+
+
---
+++ pipelines/matrix/src/matrix/pipelines/kg_sample_generation/pipeline.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+from . import nodes
+
+def create_pipeline(**kwargs) -> Pipeline:
+    """Create KG sample pipeline."""
+    return pipeline(
+        [
+            node(
+                func=nodes.get_random_selection_of_edges,
+                inputs={
+                    "nodes": "ingestion.raw.rtx_kg2.nodes@spark",
+                    "edges": "ingestion.raw.rtx_kg2.edges@spark",
+                },
+                outputs={
+                    "nodes": "ingestion.sample.rtx_kg2.nodes@spark",
+                    "edges": "ingestion.sample.rtx_kg2.edges@spark",
+                },
+                name="fabricate_kg2_datasets",
+            ),
+        ]
+    )
+
+
