fa9e870daa3c86448ef34baed64de8ccbc6835a4
Alexei Stepanenko <alexei.stepa@gmail.com>
2024-10-09 21:13:52 +0100

    add runners and tags

--- pipelines/matrix/src/matrix/pipelines/moa_extraction/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/moa_extraction/pipeline.py
@@ -2,8 +2,28 @@
 MOA extraction pipeline.
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, node
+from kedro.pipeline.modular_pipeline import pipeline
+
+from . import nodes
+
+
+def _preprocessing_pipeline() -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=nodes.add_tags,
+                inputs=[
+                    "params:moa_extraction.neo4j_runner",
+                    "params:moa_extraction.drug_types",
+                    "params:moa_extraction.disease_types",
+                ],
+                outputs=None,
+                name="add_tags",
+            )
+        ]
+    )
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return _preprocessing_pipeline()
