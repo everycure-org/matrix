f4ba3ae82810c330a9804f2dbd3d494e46ecd5ac
vjsykora <jsykora@Joe>
2024-10-10 11:08:50 +0100

    fixing sampled_test to read sample from .sample. dir

--- pipelines/matrix/src/matrix/pipelines/kg_sample_generation/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/kg_sample_generation/pipeline.py
@@ -9,31 +9,31 @@ def create_pipeline(**kwargs) -> Pipeline:
     """Create KG sample pipeline."""
     return pipeline(
         [
-            # Takes a sample of RTX
+            # Takes a sample from RTX
             node(
                 func=nodes.get_random_selection_from_rtx,
                 inputs={
-                    "nodes": "ingestion.raw.rtx_kg2.nodes@spark",
-                    "edges": "ingestion.raw.rtx_kg2.edges@spark",
-                },
-                outputs={
                     "nodes": "ingestion.int.rtx_kg2.nodes",
                     "edges": "ingestion.int.rtx_kg2.edges",
+                },
+                outputs={
+                    "nodes": "ingestion.sample.rtx_kg2.nodes",
+                    "edges": "ingestion.sample.rtx_kg2.edges",
                 },
                 name="sampled_kg2_datasets",
             ),
             # ec-medical-team
             node(
-                func=lambda x: x.withColumn("kg_source", F.lit("ec_medical_team")),
-                inputs=["ingestion.raw.ec_medical_team.nodes@spark"],
-                outputs="ingestion.int.ec_medical_team.nodes",
+                func=lambda x: x,
+                inputs=["ingestion.int.ec_medical_team.nodes"],
+                outputs="ingestion.sample.ec_medical_team.nodes",
                 name="write_ec_medical_team_nodes",
                 tags=["ec_medical_team"],
             ),
             node(
-                func=lambda x: x.withColumn("kg_source", F.lit("ec_medical_team")),
-                inputs=["ingestion.raw.ec_medical_team.edges@spark"],
-                outputs="ingestion.int.ec_medical_team.edges",
+                func=lambda x: x,
+                inputs=["ingestion.int.ec_medical_team.edges"],
+                outputs="ingestion.sample.ec_medical_team.edges",
                 name="write_ec_medical_team_edges",
                 tags=["ec_medical_team"],
             ),
