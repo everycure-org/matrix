f71ac5d59a3b6afcce235ed2210032724b0f1ef8
Pascal Brokmeier <pascal@everycure.org>
2024-10-16 15:25:21 +0200

    cleanup

--- pipelines/matrix/src/matrix/pipelines/integration/robokop.py
+++ pipelines/matrix/src/matrix/pipelines/integration/robokop.py
@@ -15,6 +15,35 @@ from matrix.schemas.knowledge_graph import KGEdgeSchema, KGNodeSchema, cols_for_
 # https://github.com/everycure-org/matrix/issues/474
 
 
+@pa.check_output(KGNodeSchema)
+def transform_robo_nodes(nodes_df: DataFrame) -> DataFrame:
+    """Transform Robokop nodes to our target schema.
+
+    Args:
+        nodes_df: Nodes DataFrame.
+
+    Returns:
+        Transformed DataFrame.
+    """
+    # NOTE: This function was partially generated using AI assistance.
+    # fmt: off
+    return (
+        nodes_df
+        .withColumn("upstream_data_source",              f.array(f.lit("robokop")))
+        .withColumn("all_categories",                    f.split(f.col("category:LABEL"), "\x1f"))
+        .withColumn("equivalent_identifiers",            f.split(f.col("equivalent_identifiers:string[]"), "\x1f"))
+        .withColumn("category",                          f.element_at(f.col("all_categories"), f.size(f.col("all_categories"))))
+        .withColumn("labels",                            f.array(f.col("all_categories")))
+        .withColumn("publications",                      f.array(f.lit(None)))
+        .withColumn("international_resource_identifier", f.lit(None))
+        .withColumnRenamed("id:ID", "id")
+        .withColumnRenamed("name:string", "name")
+        .withColumnRenamed("description:string", "description")
+        .select(*cols_for_schema(KGNodeSchema))
+    )
+    # fmt: on
+
+
 @pa.check_output(KGEdgeSchema)
 def transform_robo_edges(edges_df: DataFrame) -> DataFrame:
     """Transform Robokop edges to our target schema.
@@ -43,33 +72,5 @@ def transform_robo_edges(edges_df: DataFrame) -> DataFrame:
         .withColumn("subject_direction_qualifier",              f.lit(None).cast(T.StringType()))
         # final selection of columns
         .select(*cols_for_schema(KGEdgeSchema))
-    )
-    # fmt: on
-
-
-@pa.check_output(KGNodeSchema)
-def transform_robo_nodes(nodes_df: DataFrame) -> DataFrame:
-    """Transform Robokop nodes to our target schema.
-
-    Args:
-        nodes_df: Nodes DataFrame.
-
-    Returns:
-        Transformed DataFrame.
-    """
-    # NOTE: This function was partially generated using AI assistance.
-    # fmt: off
-    return (
-        nodes_df.withColumn("upstream_data_source",       f.array(f.lit("robokop")))
-        .withColumn("all_categories",                    f.split(f.col("category:LABEL"), "\x1f"))
-        .withColumn("equivalent_identifiers",            f.split(f.col("equivalent_identifiers:string[]"), "\x1f"))
-        .withColumn("category",                          f.element_at(f.col("all_categories"), f.size(f.col("all_categories"))))
-        .withColumn("labels",                            f.array(f.col("all_categories")))
-        .withColumn("publications",                      f.array(f.lit(None)))
-        .withColumn("international_resource_identifier", f.lit(None))
-        .withColumnRenamed("id:ID", "id")
-        .withColumnRenamed("name:string", "name")
-        .withColumnRenamed("description:string", "description")
-        .select(*cols_for_schema(KGNodeSchema))
     )
     # fmt: on
