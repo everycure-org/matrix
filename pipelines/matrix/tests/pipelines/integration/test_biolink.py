f7386161b3d9d7bfbd571db161d04fd7d0e39efc
Laurens Vijnck <laurens@everycure.org>
2024-10-17 19:19:10 +0200

    implement biolink hierarchy reduction

--- pipelines/matrix/tests/pipelines/integration/test_biolink.py
+++ pipelines/matrix/tests/pipelines/integration/test_biolink.py
@@ -1,11 +1,15 @@
 import pandas as pd
+import pytest
 
 from matrix.pipelines.integration import biolink
 
+from pyspark.testing import assertDataFrameEqual
+from pyspark.sql.types import ArrayType, StringType, StructField, StructType
 
-def test_unnest():
-    # Given an input dictionary of hierarchical predicate definition
-    predicates = [
+
+@pytest.fixture
+def sample_predicates():
+    return [
         {
             "name": "related_to",
             "children": [
@@ -21,17 +25,96 @@ def test_unnest():
         }
     ]
 
+
+@pytest.fixture
+def sample_edges(spark):
+    return spark.createDataFrame(
+        [
+            (
+                "CHEBI:001",
+                "CHEBI:002",
+                "biolink:related_to",
+            ),
+            (
+                "CHEBI:001",
+                "CHEBI:002",
+                "biolink:composed_primarily_of",
+            ),
+            (
+                "CHEBI:001",
+                "CHEBI:002",
+                "biolink:related_to_at_concept_level",
+            ),
+            (
+                "CHEBI:002",
+                "CHEBI:003",
+                "biolink:related_to_at_concept_level",
+            ),
+        ],
+        schema=StructType(
+            [
+                StructField("subject", StringType(), False),
+                StructField("object", StringType(), False),
+                StructField("predicate", StringType(), False),
+            ]
+        ),
+    )
+
+
+def test_unnest(sample_predicates):
+    # Given an input dictionary of hierarchical predicate definition
+
     # When calling the unnest function
-    result = biolink.unnest(predicates, parents=[])
+    result = biolink.unnest(sample_predicates, parents=[])
     expected = pd.DataFrame(
         [
-            ["composed_primarily_of", ["related_to"], 2],
-            ["broad_match", ["related_to", "related_to_at_concept_level"], 3],
-            ["related_to_at_concept_level", ["related_to"], 2],
-            ["related_to", [], 1],
+            ["composed_primarily_of", ["related_to"]],
+            ["broad_match", ["related_to", "related_to_at_concept_level"]],
+            ["related_to_at_concept_level", ["related_to"]],
+            ["related_to", []],
         ],
-        columns=["name", "parents", "depth"],
+        columns=["predicate", "parents"],
     )
 
     # Then correct mapping returned
     assert result.equals(expected)
+
+
+def test_biolink_deduplicate(spark, sample_edges, sample_predicates):
+    # Given sample edges and predicates
+    predicates = spark.createDataFrame(biolink.unnest(sample_predicates))
+
+    # When applying the biolink deduplicate
+    result = biolink.biolink_deduplicate(sample_edges, predicates)
+    expected = spark.createDataFrame(
+        [
+            (
+                "CHEBI:001",
+                "CHEBI:002",
+                "biolink:composed_primarily_of",
+            ),
+            (
+                "CHEBI:001",
+                "CHEBI:002",
+                "biolink:related_to_at_concept_level",
+            ),
+            (
+                "CHEBI:002",
+                "CHEBI:003",
+                "biolink:related_to_at_concept_level",
+            ),
+        ],
+        schema=StructType(
+            [
+                StructField("subject", StringType(), False),
+                StructField("object", StringType(), False),
+                StructField("predicate", StringType(), False),
+            ]
+        ),
+    )
+
+    # Then dataframe filtered correctly
+    # NOTE: PySpark is a pain to compare?
+    assert sorted(result.select("subject", "object", "predicate").collect()) == sorted(
+        expected.select("subject", "object", "predicate").collect()
+    )
