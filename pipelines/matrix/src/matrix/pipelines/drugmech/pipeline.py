20c129de269286029e94385d2efaa61cb5e19af8
Laurens Vijnck <laurens@everycure.org>
2024-10-25 11:22:18 +0200

    push work

--- pipelines/matrix/src/matrix/pipelines/drugmech/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/drugmech/pipeline.py
@@ -5,7 +5,7 @@ from pyspark.sql import DataFrame, SparkSession
 
 
 def parse_gt(gt: DataFrame) -> DataFrame:
-    return gt.withColumnRenamed("source", "drug_id").withColumnRenamed("target", "disease_id").limit(1000)
+    return gt.withColumnRenamed("source", "drug_id").withColumnRenamed("target", "disease_id").limit(100)
 
 
 def apply_join(tps: DataFrame):
@@ -21,37 +21,21 @@ def apply_join(tps: DataFrame):
         .option(
             "query",
             """
-            MATCH p=(drug:is_drug)-[*2..2]->(disease:is_disease)
-            WITH p, drug, disease 
-            RETURN drug.id AS drug_id, disease.id AS disease_id, [node IN nodes(p) | node.id] as path""",
+            MATCH (source:is_drug), (source:is_drug)-[:is_treats]->(target:is_disease)
+            WITH id(source) as sourceId, collect(id(target)) as targets
+            CALL apoc.path.expandConfig(sourceId, {
+                terminatorNodes: targets,
+                relationshipFilter: '>',
+                labelFilter: '-Drug-SmallMolecule/is_disease',
+                minLevel: 2,
+                maxLevel: 3,
+                uniqueness: 'NODE_PATH'
+            })
+            YIELD path
+            WITH [n IN nodes(path) | n.id] as p
+            RETURN p
+            """,
         )
-        # Additional filters
-        # WHERE ALL(node IN nodes(p)[1..-1] WHERE NONE(label IN labels(node) WHERE label IN ['Drug', 'SmallMolecule']))
-        # WHERE (drug)-[:is_treats]->(disease)
-        # Approach 2: expandConfig
-        # .option("query", """
-        #     MATCH (source:is_drug), (source:is_drug)-[:is_treats]->(target:is_disease)
-        #     WITH source, collect(target) as targets
-        #     CALL apoc.path.expandConfig(source, {
-        #         terminatorNodes: targets,
-        #         relationshipFilter: '>',
-        #         labelFilter: '-Drug-SmallMolecule/is_disease',
-        #         minLevel: 2,
-        #         maxLevel: 2,
-        #         uniqueness: 'NODE_PATH'
-        #     })
-        #     YIELD path
-        #     WITH [n IN nodes(path) | n.id] as p
-        #     RETURN p
-        #     """
-        # )
-        # Approach 2: expand
-        # .option("query", """
-        #     MATCH (n:is_drug)
-        #     WITH n
-        #     CALL apoc.path.expand(n, ">", "-Drug-SmallMolecule/is_disease", 1, 2) YIELD path
-        #     RETURN path, length(path) AS hops
-        # """)
         .option("partitions", 4)
     ).load()
 
@@ -62,7 +46,7 @@ def apply_join(tps: DataFrame):
     # )
 
     # all_pairs.show()
-    print(all_pairs.show(truncate=False))
+    # print(all_pairs.show(truncate=False))
     print(all_pairs.count())
 
     return all_pairs
@@ -72,13 +56,7 @@ def create_pipeline(**kwargs) -> Pipeline:
     """Create integration pipeline."""
     return pipeline(
         [
-            # node(
-            #     func=parse_gt,
-            #     inputs=["tps"],
-            #     outputs="int.tps",
-            #     name="preprocess_tps",
-            #     tags=["index"]
-            # ),
+            node(func=parse_gt, inputs=["tps"], outputs="int.tps", name="preprocess_tps", tags=["index"]),
             node(
                 func=lambda x: x.select("drug_id").withColumnRenamed("drug_id", "id"),
                 inputs=["int.tps"],
@@ -111,55 +89,3 @@ def create_pipeline(**kwargs) -> Pipeline:
             ),
         ]
     )
-
-
-"""
-CALL gds.graph.project(
-    'trav',
-    '*', 
-    '*'
-)
-YIELD graphName, nodeCount, relationshipCount;
-"""
-
-"""
-MATCH (drug:is_drug), (drug)-[:is_treats]-(disease:is_disease)
-WITH drug, COLLECT(disease) AS diseases
-CALL gds.bfs.stream('trav', {
-  sourceNode: drug,
-  targetNodes: diseases,
-  maxDepth: 2
-})
-YIELD path
-RETURN path
-LIMIT 1
-"""
-
-
-"""
-MATCH (drug:is_drug {id:'CHEMBL.COMPOUND:CHEMBL3989646'}), (disease: {id:'MONDO:0003781'}) 
-WITH drug, COLLECT(disease) AS diseases
-CALL gds.bfs.stream('trav', {
-  sourceNode: drug,
-  targetNodes: diseases,
-  maxDepth: 2
-})
-YIELD path
-RETURN path
-LIMIT 1
-"""
-
-
-"""
-MATCH (source:is_drug), (source:is_drug)-[:is_treats]->(target:is_disease)
-WITH source, collect(target)
-CALL apoc.path.expandConfig(source, {
-  targetNodes: target,      
-  relationshipFilter: '>', 
-  minLevel: 1,               
-  maxLevel: 2,             
-  uniqueness: 'NODE_PATH'    
-})
-YIELD path
-RETURN path
-"""
