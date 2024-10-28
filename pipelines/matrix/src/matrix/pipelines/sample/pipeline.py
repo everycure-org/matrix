263c1e8786c55c340726630b6eab320fba2f42bb
vjsykora <jsykora@Joe>
2024-10-28 15:12:04 +0000

    sampling Ground Truths

--- pipelines/matrix/src/matrix/pipelines/sample/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/sample/pipeline.py
@@ -9,7 +9,8 @@ def create_pipeline(**kwargs) -> Pipeline:
     """Create KG sample pipeline."""
     return pipeline(
         [
-            # Copy datasets locally
+            # Copy datasets locally (to avoid re-copying from bucket every time)
+            #  1. RTX nodes and edges
             node(
                 func=lambda x: x,
                 inputs="ingestion.pre_sample.rtx_kg2.nodes@spark",
@@ -24,6 +25,22 @@ def create_pipeline(**kwargs) -> Pipeline:
                 name="copy-rtx-edges",
                 tags=["copy"]
             ),
+            #  2. Ground Truths - we do the sample straight-away
+            node(
+                func=nodes.get_random_selection_of_ground_truths,
+                inputs="modelling.pre_sample.ground_truth.positives",
+                outputs="modelling.raw.ground_truth.positives",
+                name="copy-ground_truths-pos",
+                tags=["copy"]
+            ),
+            node(
+                func=nodes.get_random_selection_of_ground_truths,
+                inputs="modelling.pre_sample.ground_truth.negatives",
+                outputs="modelling.raw.ground_truth.negatives",
+                name="copy-ground_truths-neg",
+                tags=["copy"]
+            ),
+            # Make sample and save files in sample directory
             node(
                 func=nodes.get_random_selection_from_rtx,
                 inputs={
@@ -36,8 +53,13 @@ def create_pipeline(**kwargs) -> Pipeline:
                 },
                 name="prefilter_rtx_kg2",
                 tags=["sample"]
-            ),
+            )
         ]
     )
+
+
+
+'''
+'''
 
 
