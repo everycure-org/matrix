bc264b9bc0e2bca4f619ea2ba9b9e9cf55241bf2
Pascal Brokmeier <pascal@everycure.org>
2024-10-29 18:47:31 +0100

    fix tests

--- pipelines/matrix/src/matrix/pipelines/data_release/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/data_release/pipeline.py
@@ -11,14 +11,14 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=ingest_nodes,
                 inputs=["integration.prm.filtered_nodes"],
-                outputs="release.prm.kg_nodes",
+                outputs="data_release.prm.kg_nodes",
                 name="ingest_kg_nodes",
                 tags=["neo4j"],
             ),
             node(
                 func=ingest_edges,
-                inputs=["release.prm.kg_nodes", "integration.prm.filtered_edges"],
-                outputs="release.prm.kg_edges",
+                inputs=["data_release.prm.kg_nodes", "integration.prm.filtered_edges"],
+                outputs="data_release.prm.kg_edges",
                 name="ingest_kg_edges",
                 tags=["neo4j"],
             ),
@@ -26,22 +26,22 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=lambda x: x,
                 inputs=["integration.prm.filtered_edges"],
-                outputs="release.prm.bigquery_edges",
+                outputs="data_release.prm.bigquery_edges",
                 name="write_edges_to_bigquery",
                 tags=["bigquery"],
             ),
             node(
                 func=lambda x: x,
                 inputs=["integration.prm.filtered_nodes"],
-                outputs="release.prm.bigquery_nodes",
+                outputs="data_release.prm.bigquery_nodes",
                 name="write_nodes_to_bigquery",
                 tags=["bigquery"],
             ),
             # NOTE: Enable if you want embeddings
             # node(
             #     func=lambda _, x: x,
-            #     inputs=["release.prm.kg_nodes", "embeddings.feat.nodes"],
-            #     outputs="release.prm.kg_embeddings",
+            #     inputs=["data_release.prm.kg_nodes", "embeddings.feat.nodes"],
+            #     outputs="data_release.prm.kg_embeddings",
             # )
             # need nodes that bring the nodes/edges to BigQuery
         ]
