b0546efe73d88b2195924fd22a61b48e4a0473d0
vjsykora <jsykora@Joe>
2024-10-30 19:39:40 +0000

    sampling based on unified nodes and edges

--- pipelines/matrix/src/matrix/pipelines/sample/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/sample/pipeline.py
@@ -1,20 +1,36 @@
 import pandas as pd
-import pyspark.sql.functions as F
-
 from kedro.pipeline import Pipeline, node, pipeline
 
 from . import nodes
 
 def create_pipeline(**kwargs) -> Pipeline:
-    """Create KG sample pipeline."""
     return pipeline(
         [
-            # Make sample and save files in sample directory
+            # Sample the Ground Truths:
+            # Read from bucket and store unified Ground Truths
+            # in the intermediate directory location so we can
+            # access them later to merge them with the
+            # rest of the sampled nodes and edges
+            node(
+                func=nodes.get_random_selection_of_ground_truths,
+                inputs={
+                    "gt_positives": "modelling.raw.ground_truth.positives",
+                    "gt_negatives": "modelling.raw.ground_truth.negatives",
+                },
+                outputs="modelling.int.known_pairs",
+                name="sample-ground_truths",
+                tags=["sample"]
+            ),
+            # Make sample from unified nodes/edges and
+            # save to the same dir for sampled_test.
+            # Add the Ground Truths as the modelling pipeline
+            # need the embeddings of the GTs
             node(
                 func=nodes.get_random_sample_from_unified_files,
                 inputs={
                     "nodes" : "integration.prm_ingest.unified_nodes",
                     "edges" : "integration.prm_ingest.unified_edges",
+                    "known_pairs": "modelling.int.known_pairs",
                 },
                 outputs={
                     "nodes" : "integration.prm.unified_nodes",
