4153ab5d43dc5386ea92557c7f594880f4d1d03e
vjsykora <jsykora@Joe>
2024-10-30 10:31:39 +0000

    now sampling from unified nodes-edges

--- pipelines/matrix/src/matrix/pipelines/sample/nodes.py
+++ pipelines/matrix/src/matrix/pipelines/sample/nodes.py
@@ -1,72 +1,33 @@
 import pandas as pd
-from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql import DataFrame
 from typing import Tuple
-import pyspark.sql.functions as F
 
-from matrix.pipelines.modelling.nodes import create_int_pairs
 
-def get_random_selection_from_rtx(
-    nodes : DataFrame,
-    edges : DataFrame,
+def get_random_sample_from_unified_files(
+    nodes: DataFrame,
+    edges: DataFrame
 ) -> Tuple[DataFrame, DataFrame]:
-    """Function that accepts the whole Edges and Nodes
-       and makes a random selection of the Edges.
-       The idea is to have a dataset that resembles the
-       original KG data but which is much smaller so
-       it can be ran on a local environment
+    '''
+    Function accepts unified nodes and edges,
+    and returns a random selection of both.
 
     Args:
-        nodes: Dataframe with raw nodes
-        edges: Dataframe with raw edges
-    """
-
-    # Make the sample
-    edges_sample_df = edges.sample(withReplacement=False, fraction=0.0000005, seed=123)
-    
-    # Now we need to select the nodes. First, make a list with all the IDs
+        nodes_sdf: Spark Dataframe with unified nodes
+        edges_sdf: Spark Dataframe with unified edges
+    '''
+    # Take the sample
+    edges_sample_df = edges.sample(withReplacement=False, fraction=0.00005, seed=123)
+    # Now we need to select the nodes from the selected edges.
+    # First, make a list with all the IDs
     edges_node_ids_df = (
         edges_sample_df.select('subject').withColumnRenamed("subject", "id")
         .unionByName(
             edges_sample_df.select('object').withColumnRenamed("object", "id")
         )
     ).distinct()
-
-    nodes_sample_df = nodes.join(edges_node_ids_df, how='inner', on="id").select(nodes.columns)
-
-    return {
-        'nodes': nodes_sample_df,
-        'edges': edges_sample_df
-    }
-
-
-def get_random_selection_from_robokop(
-    nodes : DataFrame,
-    edges : DataFrame,
-) -> Tuple[DataFrame, DataFrame]:
-    """Function that accepts the whole Edges and Nodes
-       and makes a random selection of the Edges.
-       The idea is to have a dataset that resembles the
-       original KG data but which is much smaller so
-       it can be ran on a local environment
-
-    Args:
-        nodes: Dataframe with raw nodes
-        edges: Dataframe with raw edges
-    """
-
-    # Make the sample
-    edges_sample_df = edges.sample(withReplacement=False, fraction=0.00005, seed=123)
-    
-    # Now we need to select the nodes. First, make a list with all the IDs
-    edges_node_ids_df = (
-        edges_sample_df.select('subject:START_ID').withColumnRenamed("subject:START_ID", "id:ID")
-        .unionByName(
-            edges_sample_df.select('object:END_ID').withColumnRenamed("object:END_ID", "id:ID")
-        )
-    ).distinct()
-
-    nodes_sample_df = nodes.join(edges_node_ids_df, how='inner', on="id:ID").select(nodes.columns)
-
+    nodes_sample_df = nodes.join(
+        edges_node_ids_df, how='inner', on="id"
+    ).select(nodes.columns)
     return {
         'nodes': nodes_sample_df,
         'edges': edges_sample_df
--- pipelines/matrix/src/matrix/pipelines/sample/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/sample/pipeline.py
@@ -9,66 +9,19 @@ def create_pipeline(**kwargs) -> Pipeline:
     """Create KG sample pipeline."""
     return pipeline(
         [
-            # Copy datasets locally (to avoid re-copying from bucket every time)
-            #  1. RTX nodes and edges
-            node(
-                func=lambda x: x,
-                inputs="ingestion.pre_sample.rtx_kg2.nodes@spark",
-                outputs="sample.int.rtx-nodes",
-                name="copy-rtx-nodes",
-                tags=["copy"]
-            ),
-            node(
-                func=lambda x: x,
-                inputs="ingestion.pre_sample.rtx_kg2.edges@spark",
-                outputs="sample.int.rtx-edges",
-                name="copy-rtx-edges",
-                tags=["copy"]
-            ),
-            #  2. Ground Truths - we do the sample straight-away
-            node(
-                func=nodes.get_random_selection_of_ground_truths,
-                inputs="modelling.pre_sample.ground_truth.positives",
-                outputs="modelling.raw.ground_truth.positives",
-                name="copy-ground_truths-pos",
-                tags=["copy"]
-            ),
-            node(
-                func=nodes.get_random_selection_of_ground_truths,
-                inputs="modelling.pre_sample.ground_truth.negatives",
-                outputs="modelling.raw.ground_truth.negatives",
-                name="copy-ground_truths-neg",
-                tags=["copy"]
-            ),
-            
             # Make sample and save files in sample directory
             node(
-                func=nodes.get_random_selection_from_rtx,
+                func=nodes.get_random_sample_from_unified_files,
                 inputs={
-                    "nodes" : "sample.int.rtx-nodes",
-                    "edges" : "sample.int.rtx-edges",
+                    "nodes" : "integration.prm_ingest.unified_nodes",
+                    "edges" : "integration.prm_ingest.unified_edges",
                 },
                 outputs={
-                    "nodes": "ingestion.int.rtx_kg2.nodes",
-                    "edges": "ingestion.int.rtx_kg2.edges",
+                    "nodes" : "integration.prm.unified_nodes",
+                    "edges" : "integration.prm.unified_edges",
                 },
-                name="prefilter_rtx_kg2",
+                name="sample_unified_files",
                 tags=["sample"]
-            ),
-            # ec-medical-team
-            node(
-                func=lambda x: x.withColumn("kg_source", F.lit("ec_medical_team")),
-                inputs=["ingestion.raw.ec_medical_team.nodes@spark"],
-                outputs="ingestion.int.ec_medical_team.nodes",
-                name="write_ec_medical_team_nodes",
-                tags=["ec_medical_team"],
-            ),
-            node(
-                func=lambda x: x.withColumn("kg_source", F.lit("ec_medical_team")),
-                inputs=["ingestion.raw.ec_medical_team.edges@spark"],
-                outputs="ingestion.int.ec_medical_team.edges",
-                name="write_ec_medical_team_edges",
-                tags=["ec_medical_team"],
             ),
         ]
     )
