6244474ef180c156d2f28c2cf49863a44e2e60e4
Laurens Vijnck <laurens@everycure.org>
2024-10-31 11:48:35 +0100

    set up params

--- pipelines/matrix/src/matrix/pipeline_registry.py
+++ pipelines/matrix/src/matrix/pipeline_registry.py
@@ -42,6 +42,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     pipelines["kg_release"] = (
           pipelines["ingestion"]
         + pipelines["integration"]
+        + pipelines["embeddings"]
         + pipelines["data_release"] 
     )  # + embeddings, #TODO currently excluded
     pipelines["modelling_run"] = (
@@ -51,7 +52,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     )
     pipelines["__default__"] = (
           pipelines["kg_release"]
-        + pipelines["embeddings"] # TODO move this back to the kg_release once embedding works well
+        #+ pipelines["embeddings"] # TODO move this back to the kg_release once embedding works well
         + pipelines["modelling_run"]
     )
 
--- pipelines/matrix/src/matrix/pipelines/ingestion/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/ingestion/pipeline.py
@@ -1,6 +1,5 @@
 """Ingestion pipeline."""
 
-import pyspark.sql.functions as F
 from kedro.pipeline import Pipeline, node, pipeline
 
 
@@ -15,52 +14,52 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs=["ingestion.raw.rtx_kg2.nodes@spark"],
                 outputs="ingestion.int.rtx_kg2.nodes",
                 name="write_rtx_kg2_nodes",
-                tags=["rtx_kg2"],
+                tags=["rtx_kg2", "argowf.template-neo4j"],
             ),
-            node(
-                func=lambda x: x,
-                inputs=["ingestion.raw.rtx_kg2.edges@spark"],
-                outputs="ingestion.int.rtx_kg2.edges",
-                name="write_rtx_kg2_edges",
-                tags=["rtx_kg2"],
-            ),
-            node(
-                func=lambda x: x,
-                inputs=["ingestion.raw.rtx_kg2.curie_to_pmids@spark"],
-                outputs="ingestion.int.rtx_kg2.curie_to_pmids",
-                name="write_rtx_kg2_curie_to_pmids",
-                tags=["rtx_kg2"],
-            ),
-            # ec-medical-team
-            node(
-                func=lambda x: x.withColumn("upstream_data_source", F.array(F.lit("ec_medical_team"))),
-                inputs=["ingestion.raw.ec_medical_team.nodes@spark"],
-                outputs="ingestion.int.ec_medical_team.nodes",
-                name="write_ec_medical_team_nodes",
-                tags=["ec_medical_team"],
-            ),
-            node(
-                func=lambda x: x.withColumn("upstream_data_source", F.array(F.lit("ec_medical_team"))),
-                inputs=["ingestion.raw.ec_medical_team.edges@spark"],
-                outputs="ingestion.int.ec_medical_team.edges",
-                name="write_ec_medical_team_edges",
-                tags=["ec_medical_team"],
-            ),
-            # robokop
-            node(
-                func=lambda x: x,
-                inputs=["ingestion.raw.robokop.nodes@spark"],
-                outputs="ingestion.int.robokop.nodes",
-                name="ingest_robokop_nodes",
-                tags=["robokop"],
-            ),
-            node(
-                # FUTURE: Update selection
-                func=lambda x: x,
-                inputs=["ingestion.raw.robokop.edges@spark"],
-                outputs="ingestion.int.robokop.edges",
-                name="ingest_robokop_edges",
-                tags=["robokop"],
-            ),
+            # node(
+            #     func=lambda x: x,
+            #     inputs=["ingestion.raw.rtx_kg2.edges@spark"],
+            #     outputs="ingestion.int.rtx_kg2.edges",
+            #     name="write_rtx_kg2_edges",
+            #     tags=["rtx_kg2"],
+            # ),
+            # node(
+            #     func=lambda x: x,
+            #     inputs=["ingestion.raw.rtx_kg2.curie_to_pmids@spark"],
+            #     outputs="ingestion.int.rtx_kg2.curie_to_pmids",
+            #     name="write_rtx_kg2_curie_to_pmids",
+            #     tags=["rtx_kg2"],
+            # ),
+            # # ec-medical-team
+            # node(
+            #     func=lambda x: x.withColumn("upstream_data_source", F.array(F.lit("ec_medical_team"))),
+            #     inputs=["ingestion.raw.ec_medical_team.nodes@spark"],
+            #     outputs="ingestion.int.ec_medical_team.nodes",
+            #     name="write_ec_medical_team_nodes",
+            #     tags=["ec_medical_team"],
+            # ),
+            # node(
+            #     func=lambda x: x.withColumn("upstream_data_source", F.array(F.lit("ec_medical_team"))),
+            #     inputs=["ingestion.raw.ec_medical_team.edges@spark"],
+            #     outputs="ingestion.int.ec_medical_team.edges",
+            #     name="write_ec_medical_team_edges",
+            #     tags=["ec_medical_team"],
+            # ),
+            # # robokop
+            # node(
+            #     func=lambda x: x,
+            #     inputs=["ingestion.raw.robokop.nodes@spark"],
+            #     outputs="ingestion.int.robokop.nodes",
+            #     name="ingest_robokop_nodes",
+            #     tags=["robokop"],
+            # ),
+            # node(
+            #     # FUTURE: Update selection
+            #     func=lambda x: x,
+            #     inputs=["ingestion.raw.robokop.edges@spark"],
+            #     outputs="ingestion.int.robokop.edges",
+            #     name="ingest_robokop_edges",
+            #     tags=["robokop"],
+            # ),
         ]
     )
