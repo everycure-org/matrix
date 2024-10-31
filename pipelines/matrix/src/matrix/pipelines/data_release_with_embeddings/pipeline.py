61c17d09c609d79e6c4216a297a90ac0cee4c42d
matwasilewski <mat.p.wasilewski@gmail.com>
2024-10-31 18:03:59 +0000

    fix embeddings pipeline

--- pipelines/matrix/src/matrix/pipelines/data_release_with_embeddings/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/data_release_with_embeddings/pipeline.py
@@ -1,6 +1,6 @@
 """Pipeline to release data."""
 
-from kedro.pipeline import Pipeline, node
+from kedro.pipeline import Pipeline, node, pipeline
 
 from matrix.pipelines.data_release.pipeline import create_pipeline as create_data_release_pipeline
 
@@ -10,11 +10,14 @@ def create_pipeline(**kwargs) -> Pipeline:
 
     This is an extension of the data release pipeline, adding the embeddings.
     """
-    data_release_pipeline = create_data_release_pipeline()
-    data_release_pipeline += node(
-        func=lambda _, x: x,
-        inputs=["data_release.prm.kg_nodes", "embeddings.feat.nodes"],
-        outputs="data_release.prm.kg_embeddings",
+    data_release_pipeline = create_data_release_pipeline() + pipeline(
+        [
+            node(
+                func=lambda _, x: x,
+                inputs=["data_release.prm.kg_nodes", "embeddings.feat.nodes"],
+                outputs="data_release.prm.kg_embeddings",
+            )
+        ]
     )
 
     return data_release_pipeline
