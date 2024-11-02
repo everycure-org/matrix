7954daa5228e08e69403fb92100075055d892875
Pascal Brokmeier <pascal@everycure.org>
2024-11-02 18:40:11 +0100

    refactor some locations

--- apps/matrix-cli/src/matrix_cli/utils.py
+++ apps/matrix-cli/src/matrix_cli/utils.py
@@ -1,7 +1,31 @@
 import subprocess
 
+from rich.console import Console
+from tenacity import retry, stop_after_attempt, wait_exponential
+from vertexai.generative_models import (
+    GenerationConfig,
+)
+
+from matrix_cli.cache import memory
+
+console = Console()
+
 
 def get_git_root() -> str:
     return subprocess.run(
         ["git", "rev-parse", "--show-toplevel"], check=True, capture_output=True, text=True
     ).stdout.strip()
+
+
+@memory.cache()
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=120))
+def invoke_model(prompt: str, model: str, generation_config: GenerationConfig = None) -> str:
+    import vertexai
+    from vertexai.generative_models import GenerativeModel
+
+    vertexai.init()
+    model_object = GenerativeModel(model)
+    console.print(f"[bold green] Calling Gemini with a prompt of length: {len(prompt)} characters")
+    response = model_object.generate_content(prompt, generation_config=generation_config).text
+    console.print(f"[bold green] Response received. Total length: {len(response)} characters")
+    return response
