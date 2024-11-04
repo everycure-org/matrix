d869cc84530609f7dd674e0bbca8c3390e1f2020
Laurens Vijnck <laurens@everycure.org>
2024-11-04 17:25:04 +0000

    final changes

--- pipelines/matrix/src/matrix/pipelines/drugmech/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/drugmech/pipeline.py
@@ -1,28 +1,17 @@
 """Integration pipeline."""
 
+import pandas as pd
 from kedro.pipeline import Pipeline, node, pipeline
 from pyspark.sql import DataFrame, SparkSession
 
 
-def parse_gt(gt: DataFrame) -> DataFrame:
-    return gt.withColumnRenamed("source", "drug_id").withColumnRenamed("target", "disease_id").limit(1000)
-
-
-def apply_join(tps: DataFrame):
+def apply_join(gt_pos: pd.DataFrame):
     spark_session = SparkSession.builder.getOrCreate()
 
     shards = {}
 
-    for idx, (drug, disease) in enumerate(
-        [
-            ("CHEMBL.COMPOUND:CHEMBL137", "MONDO:0002635"),
-            ("CHEMBL.COMPOUND:CHEMBL1201258", "MONDO:0005065"),
-            ("CHEMBL.COMPOUND:CHEMBL1201258", "MONDO:0005065"),
-            ("CHEMBL.COMPOUND:CHEMBL30", "MONDO:0007186"),
-            # ("CHEMBL.COMPOUND:CHEMBL3707202", "MONDO:0007186")
-        ]
-    ):
-        shards[f"shard={idx}"] = lambda: (
+    for index, row in gt_pos.toPandas().iloc[:10].iterrows():
+        shards[f"shard={index}"] = lambda: (
             spark_session.read.format("org.neo4j.spark.DataSource")
             .option("database", "moa")
             .option("url", "bolt://127.0.0.1:7687")
@@ -32,7 +21,7 @@ def apply_join(tps: DataFrame):
             .option(
                 "query",
                 f"""
-                MATCH p=(drug:Entity {{id: '{drug}'}})-[*2..3]->(disease:Entity {{id: '{disease}'}})
+                MATCH p=(drug:Entity {{id: '{row["source"]}'}})-[*2..3]->(disease:Entity {{id: '{row["target"]}'}})
                 WITH [node IN nodes(p) | node.id] as nodes, [node in nodes(p) | labels(node)[1]] as labels, [rel in relationships(p) | type(rel)] as rels
                 RETURN nodes, labels, rels
             """,
@@ -51,32 +40,31 @@ def create_pipeline(**kwargs) -> Pipeline:
     """Create integration pipeline."""
     return pipeline(
         [
-            node(func=parse_gt, inputs=["tps"], outputs="int.tps", name="preprocess_tps", tags=["index"]),
-            node(
-                func=lambda x: x.select("drug_id").withColumnRenamed("drug_id", "id"),
-                inputs=["int.tps"],
-                outputs="int.drug.index",
-                name="create_input_drug_index",
-                tags=["index"],
-            ),
-            node(
-                func=lambda x: x.select("disease_id").withColumnRenamed("disease_id", "id"),
-                inputs=["int.tps"],
-                outputs="int.disease.index",
-                name="create_input_disease_index",
-                tags=["index"],
-            ),
-            node(
-                func=lambda x: x.withColumnRenamed("source", "drug_id").withColumnRenamed("target", "disease_id"),
-                inputs=["tps"],
-                outputs="int.edges_index",
-                name="create_edges_disease_index",
-                tags=["index"],
-            ),
+            # node(
+            #     func=lambda x: x.select("drug_id").withColumnRenamed("drug_id", "id"),
+            #     inputs=["int.tps"],
+            #     outputs="int.drug.index",
+            #     name="create_input_drug_index",
+            #     tags=["index"],
+            # ),
+            # node(
+            #     func=lambda x: x.select("disease_id").withColumnRenamed("disease_id", "id"),
+            #     inputs=["int.tps"],
+            #     outputs="int.disease.index",
+            #     name="create_input_disease_index",
+            #     tags=["index"],
+            # ),
+            # node(
+            #     func=lambda x: x.withColumnRenamed("source", "drug_id").withColumnRenamed("target", "disease_id"),
+            #     inputs=["tps"],
+            #     outputs="int.edges_index",
+            #     name="create_edges_disease_index",
+            #     tags=["index"],
+            # ),
             node(
                 func=apply_join,
                 inputs=[
-                    "int.tps",
+                    "modelling.raw.positives",
                 ],
                 outputs="drugmech.paths@partitioned",
                 name="create_paths",
