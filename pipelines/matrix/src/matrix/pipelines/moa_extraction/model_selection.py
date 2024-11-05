fe2acdfca8fdf63f682715ce5bd363216fb86181
Alexei Stepanenko <alexei.stepa@gmail.com>
2024-11-05 16:26:09 +0000

    add MOA model_selection file

--- pipelines/matrix/src/matrix/pipelines/moa_extraction/model_selection.py
+++ pipelines/matrix/src/matrix/pipelines/moa_extraction/model_selection.py
@@ -1,23 +1,27 @@
-"""This module contains custom cross-validation classes for drug-related data."""
+"""This module contains custom cross-validation classes used in the MOA extraction pipeline."""
 
 import numpy as np
 from sklearn.model_selection import BaseCrossValidator
 
 
-class DrugStratifiedSplit(BaseCrossValidator):
+class GroupAwareSplit(BaseCrossValidator):
     """A cross-validator that provides train/test indices to split data in train/test sets.
 
-    This cross-validator ensures each drug is represented in both training and test sets.
+    This cross-validator ensures that the values of a chosen feature do not overlap between the test and train splits.
     """
 
-    def __init__(self, n_splits=1, test_size=0.1, random_state=None):
-        """Initialize the DrugStratifiedSplit cross-validator.
+    def __init__(
+        self, group_by_column: str = "source_id", n_splits: int = 1, test_size: float = 0.1, random_state: float = None
+    ) -> None:
+        """Initialize the GroupAwareSplit cross-validator.
 
         Args:
-            n_splits (int): Number of re-shuffling & splitting iterations.
-            test_size (float): Proportion of the dataset to include in the test split.
-            random_state (int): Controls the randomness of the training and testing indices produced.
+            group_by_column: The column name to use for grouping the data.
+            n_splits: Number of re-shuffling & splitting iterations.
+            test_size: Proportion of the dataset to include in the test split.
+            random_state: Controls the randomness of the training and testing indices produced.
         """
+        self.group_by_column = group_by_column
         self.n_splits = n_splits
         self.test_size = test_size
         self.random_state = random_state
@@ -36,17 +40,17 @@ class DrugStratifiedSplit(BaseCrossValidator):
         rng = np.random.RandomState(self.random_state)
 
         for _ in range(self.n_splits):
-            train_indices, test_indices = [], []
+            unique_groups = X[self.group_by_column].unique()
+            rng.shuffle(unique_groups)
 
-            for _, group in X.groupby("source"):
-                indices = group.index.tolist()
-                rng.shuffle(indices)
-                n = len(indices)
-                n_test = max(1, int(np.round(n * self.test_size)))
-                n_train = n - n_test
+            n_groups = len(unique_groups)
+            n_test_groups = max(1, int(np.round(n_groups * self.test_size)))
 
-                train_indices.extend(indices[:n_train])
-                test_indices.extend(indices[n_train:])
+            test_groups = unique_groups[:n_test_groups]
+            train_groups = unique_groups[n_test_groups:]
+
+            train_indices = X[X[self.group_by_column].isin(train_groups)].index.tolist()
+            test_indices = X[X[self.group_by_column].isin(test_groups)].index.tolist()
 
             yield train_indices, test_indices
 
