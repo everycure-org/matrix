1654292ae089fdbb7acd8411c04c0f2775f7a28c
Alexei Stepanenko <alexei.stepa@gmail.com>
2024-11-07 13:49:41 +0000

    add iam terraform module

--- infra/deployments/hub/dev/iam.tf
+++ infra/deployments/hub/dev/iam.tf
@@ -1,19 +1,3 @@
-variable "gitops_repo_url" {
-
-}
-variable "gitops_repo_creds" {
-  sensitive = true
-}
-variable "k8s_secrets" {
-  sensitive = true
-  type      = map(string)
-}
-
-variable "storage_bucket_name" {
-  default = "mtrx-us-central1-hub-dev-storage"
-
-}
-
 locals {
   matrix_all_group = "group:matrix-all@everycure.org"
 
