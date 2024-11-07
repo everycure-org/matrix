5b23e74d2503fd6dc8c672ca7a92805a3053547a
matwasilewski <mat.p.wasilewski@gmail.com>
2024-11-07 11:52:32 +0000

    stubs for ArgoPipeline

--- pipelines/matrix/tests/test_argo_pipeline.py
+++ pipelines/matrix/tests/test_argo_pipeline.py
@@ -8,3 +8,15 @@
 # Then, the only thing remaining is to render the Argo Template.
 
 # OUT: Argo template, which can be rendered to an Argo workflow.
+
+from matrix.argo_pipeline import ArgoPipeline
+
+
+def test_argo_pipeline_without_fusing(parallel_pipelines):
+    k8s_pipeline, _ = parallel_pipelines
+    argo_pipeline = ArgoPipeline(k8s_pipeline)
+    argo_kedro_command = argo_pipeline.kedro_command()
+    argo_tasks = argo_pipeline.tasks()
+
+    assert argo_kedro_command is not None
+    assert len(argo_tasks) == len(k8s_pipeline.nodes)
