3210f6df427f2920cbed8947ec266c55b7677d6f
matwasilewski <mat.p.wasilewski@gmail.com>
2024-11-12 11:56:06 +0000

    remove commented out tests

--- pipelines/matrix/tests/test_argo.py
+++ pipelines/matrix/tests/test_argo.py
@@ -199,21 +199,6 @@ def test_simple_fusing_with_argo_config():
     assert isinstance(pipeline_where_first_node_is_input_for_second.nodes[0], ArgoNode)
     assert isinstance(pipeline_where_first_node_is_input_for_second.nodes[1], ArgoNode)
 
-    # TODO(mateusz.wasilewski): Fix these tests.
-    # fused_pipeline = fuse(pipeline_where_first_node_is_input_for_second)
-
-    # assert len(fused_pipeline) == 1, "Only one node should be fused"
-    # assert fused_pipeline[0].name == "dummy", "Fused node should have name 'dummy'"
-    # assert fused_pipeline[0].outputs == set(
-    #     ["dataset_1", "dataset_2"]
-    # ), "Fused node should have outputs 'dataset_1' and 'dataset_2'"
-    # assert len(fused_pipeline[0]._parents) == 0, "Fused node should have no parents"
-    # assert fused_pipeline[0].argo_config.cpu_request == 2
-    # assert fused_pipeline[0].argo_config.cpu_limit == 2
-    # assert fused_pipeline[0].argo_config.memory_request == 32
-    # assert fused_pipeline[0].argo_config.memory_limit == 64
-    # assert fused_pipeline[0].argo_config.num_gpus
-
 
 @pytest.mark.parametrize(
     "input_name, expected",
