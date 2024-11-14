89cb8c9d99eea4ff32694062d03279518e62aca7
Alexei Stepanenko <alexei.stepa@gmail.com>
2024-11-14 22:42:39 +0000

    begin move to translator

--- pipelines/matrix/src/matrix/pipelines/drugmechdb_entity_resolution/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/drugmechdb_entity_resolution/pipeline.py
@@ -13,7 +13,12 @@ def create_pipeline(**kwargs) -> Pipeline:
                 func=nodes.normalize_drugmechdb_entities,
                 inputs={
                     "drug_mech_db": "moa_extraction.raw.drug_mech_db",
-                    "synonymizer_endpoint": "params:drugmechdb_entity_resolution.synonymizer_endpoint",
+                    "name_resolver": "params:preprocessing.translator.name_resolver",
+                    "endpoint": "params:preprocessing.translator.normalizer",
+                    "conflate": "params:integration.nodenorm.conflate",
+                    "drug_chemical_conflate": "params:integration.nodenorm.drug_chemical_conflate",
+                    "batch_size": "params:integration.nodenorm.batch_size",
+                    "parallelism": "params:integration.nodenorm.parallelism",
                 },
                 outputs="moa_extraction.raw.drugmechdb_entities",
                 name="normalize_drugmechdb_entities",
