6e7de10ea5a0fea7161fc68c683fa64119b274a1
matwasilewski <mat.p.wasilewski@gmail.com>
2024-11-26 11:29:05 +0000

    remove comments, add a minor refactor

--- pipelines/matrix/src/matrix/argo.py
+++ pipelines/matrix/src/matrix/argo.py
@@ -33,7 +33,7 @@ def generate_argo_config(
     template = template_env.get_template(ARGO_TEMPLATE_FILE)
     pipeline_tasks = get_dependencies(fuse(pipeline), default_execution_resources)
 
-    output = template.render(
+    rendered_template = template.render(
         package_name=package_name,
         pipeline_tasks=pipeline_tasks,
         pipeline_name=pipeline.name,
@@ -46,14 +46,10 @@ def generate_argo_config(
         release_folder_name=release_folder_name,
         default_execution_resources=default_execution_resources.model_dump(),
     )
+    yaml_data = yaml.safe_load(rendered_template)
+    yaml_without_anchors = yaml.dump(yaml_data, sort_keys=False, default_flow_style=False)
 
-    # Load the rendered YAML into a Python object
-    yaml_data = yaml.safe_load(output)
-
-    # Dump the final YAML without anchors
-    final_yaml = yaml.dump(yaml_data, sort_keys=False, default_flow_style=False)
-
-    return final_yaml
+    return yaml_without_anchors
 
 
 class FusedNode(Node):
