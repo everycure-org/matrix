167fef1797f1cad370f52200f4ff1d8471c006d3
Pascal Brokmeier <pascal@everycure.org>
2024-11-26 21:17:27 +0100

    remove duplicate import that showed up after cherry picking

--- pipelines/matrix/src/matrix/cli.py
+++ pipelines/matrix/src/matrix/cli.py
@@ -3,8 +3,9 @@
 import click
 from kedro.framework.cli.utils import KedroCliError
 
-from matrix.cli_commands.submit import submit
+from matrix.cli_commands.extract import extract
 from matrix.cli_commands.run import run
+from matrix.cli_commands.submit import submit
 
 from kedro.framework.cli.utils import CONTEXT_SETTINGS
 
@@ -17,9 +18,10 @@ def cli():
 
 # Add the submit command
 cli.add_command(submit)
-
 # Add the run command
 cli.add_command(run)
+# Add the extract command
+cli.add_command(extract)
 
 if __name__ == "__main__":
     try:
