ee68c556899ad8c2076a6bf3f8b56a4af0a25a4c
drhodesbrc <14894770+drhodesbrc@users.noreply.github.com>
2024-11-27 11:31:07 +0000

    create init script

---
+++ services/moa_visualisation/src/init.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import sqlite3
+import os
+from google.cloud import storage
+
+DATA_INPUT_PATH = "gs://silc-storage/moa_vis/"
+MOA_DB_PATH = "data/moa_extraction.db"
+
+
+def main():
+    os.environ["DATA_INPUT_PATH"] = DATA_INPUT_PATH
+    os.environ["MOA_DB_PATH"] = MOA_DB_PATH
+
+    client = storage.Client()  # define project here - where is this from
+    blobs = client.list_blobs(DATA_INPUT_PATH, suffix="all.parquet")
+    for blob in blobs:
+        # Read from GS
+        df = pd.read_parquet(blob.path)
+
+        # Write to SQLite DB
+        with sqlite3.connect(MOA_DB_PATH) as conn:
+            # Extract table name from blob path
+            table_name = os.path.splitext(os.path.basename(blob.name))[0]
+            df.to_sql(table_name, conn, if_exists="replace", index=False)
+
+
+if __name__ == "__main__":
+    main()
