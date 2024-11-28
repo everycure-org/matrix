37b660040ce6ed67ba44e5028afe5233599f58d9
Laurens Vijnck <laurens@everycure.org>
2024-11-28 10:40:03 +0100

    extract filtering out of neo4j and move to spark

--- pipelines/matrix/src/matrix/pipelines/data_release/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/data_release/pipeline.py
@@ -1,25 +1,26 @@
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+
+from matrix.pipelines.embeddings.nodes import ingest_edges, ingest_nodes
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     """Create release pipeline."""
     return pipeline(
         [
-            # TODO: Fix the labels etc
-            # node(
-            #     func=ingest_nodes,
-            #     inputs=["integration.prm.filtered_nodes"],
-            #     outputs="data_release.prm.kg_nodes",
-            #     name="ingest_kg_nodes",
-            #     tags=["neo4j"],
-            # ),
-            # node(
-            #     func=ingest_edges,
-            #     inputs=["data_release.prm.kg_nodes", "integration.prm.filtered_edges"],
-            #     outputs="data_release.prm.kg_edges",
-            #     name="ingest_kg_edges",
-            #     tags=["neo4j"],
-            # ),
+            node(
+                func=ingest_nodes,
+                inputs=["integration.prm.filtered_nodes"],
+                outputs="data_release.prm.kg_nodes",
+                name="ingest_kg_nodes",
+                tags=["neo4j"],
+            ),
+            node(
+                func=ingest_edges,
+                inputs=["data_release.prm.kg_nodes", "integration.prm.filtered_edges"],
+                outputs="data_release.prm.kg_edges",
+                name="ingest_kg_edges",
+                tags=["neo4j"],
+            ),
             # NOTE: Enable if you want embeddings
             # node(
             #     func=lambda _, x: x,
