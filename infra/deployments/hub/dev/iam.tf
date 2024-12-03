daa029fe42a7114096cbb60763dbadb9bca3b065
Siyan Luo <siyanluo@Siyans-MacBook-Pro.local>
2024-12-03 16:50:32 +0100

    fix format issue

--- infra/deployments/hub/dev/iam.tf
+++ infra/deployments/hub/dev/iam.tf
@@ -3,7 +3,7 @@ locals {
 
   matrix_viewers_group              = [local.matrix_all_group, "group:matrix-viewers@everycure.org"]
   tech_team_group                   = ["group:techteam@everycure.org", "group:ext.tech.dataminded@everycure.org"]
-  github_actions_rw_service_account = "sa-github-actions-rw@mtrx-hub-dev-3of.iam.gserviceaccount.com"
+  github_actions_rw_service_account = ["sa-github-actions-rw@mtrx-hub-dev-3of.iam.gserviceaccount.com"]
 }
 
 module "project_iam_bindings" {
