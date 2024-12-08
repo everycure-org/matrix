7f4a5bf5eb0eae9019570fa0ff1a374c18e673c9
Jason Reilly <jdr0887@gmail.com>
2024-12-08 06:31:42 -0500

    moving logic to nodes.py

--- pipelines/matrix/src/matrix/pipelines/data_release/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/data_release/pipeline.py
@@ -1,11 +1,21 @@
-from kedro.pipeline import Pipeline, node, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from pyspark.sql.connect.functions import array_join
+from pyspark.sql.functions import col
+
 from matrix.pipelines.embeddings.nodes import ingest_edges, ingest_nodes
+from matrix.pipelines.data_release.nodes import filtered_edges_to_kgx, filtered_nodes_to_kgx
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     """Create release pipeline."""
     return pipeline(
         [
+            node(
+                func=lambda x: x,
+                inputs=["embeddings.feat.nodes"],
+                outputs="data_release.feat.nodes_with_embeddings",
+                name="ingest_nodes_with_embeddings",
+            ),
             node(
                 func=ingest_nodes,
                 inputs=["integration.prm.filtered_nodes"],
@@ -21,14 +31,14 @@ def create_pipeline(**kwargs) -> Pipeline:
                 tags=["neo4j"],
             ),
             node(
-                func=lambda x: x,
+                func=filtered_edges_to_kgx,
                 inputs=["integration.prm.filtered_edges"],
                 outputs="data_release.prm.kgx_edges",
                 name="write_edges_to_kgx",
                 tags=["kgx"],
             ),
             node(
-                func=lambda x: x,
+                func=filtered_nodes_to_kgx,
                 inputs=["integration.prm.filtered_nodes"],
                 outputs="data_release.prm.kgx_nodes",
                 name="write_nodes_to_kgx",
