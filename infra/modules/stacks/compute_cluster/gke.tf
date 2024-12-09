8e1c24fc8be88e0ba1ce488d3187506bd1ef33e6
Pascal Bro <pascal@everycure.org>
2024-12-09 18:11:19 +0200

    Big memory /cost optimized nodes  (#767)
    * add new group types
    
    * big instances get ssds

--- infra/modules/stacks/compute_cluster/gke.tf
+++ infra/modules/stacks/compute_cluster/gke.tf
@@ -17,6 +17,21 @@ locals {
     spot               = false
     }
   ]
+  n2d_node_pools = [for size in [32, 48, 64] : {
+    name               = "n2d-highmem-${size}-nodes"
+    machine_type       = "n2d-highmem-${size}"
+    node_locations     = local.default_node_locations
+    min_count          = 0
+    max_count          = 5
+    local_ssd_count    = 0
+    disk_type          = "pd-ssd"
+    disk_size_gb       = 400
+    enable_gcfs        = true
+    enable_gvnic       = true
+    initial_node_count = 0
+    }
+  ]
+
 
   standard_node_pools = [for size in [4, 8, 16, 32, 48, 64] : {
     name               = "n2-standard-${size}-nodes"
@@ -25,6 +40,7 @@ locals {
     min_count          = 0
     max_count          = 20
     local_ssd_count    = 0
+    disk_type          = size > 32 ? "pd-ssd" : "pd-standard"
     disk_size_gb       = 200
     enable_gcfs        = true
     enable_gvnic       = true
@@ -50,7 +66,7 @@ locals {
     },
   ]
 
-  node_pools_combined = concat(local.standard_node_pools, local.mem_node_pools, local.gpu_node_pools)
+  node_pools_combined = concat(local.standard_node_pools, local.mem_node_pools, local.gpu_node_pools, local.n2d_node_pools)
 }
 
 # docs here https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest/submodules/private-cluster
