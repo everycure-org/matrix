50a21559e1e76d8410b55b18d3e048b45e84e8d4
Jason Reilly <jdr0887@gmail.com>
2024-12-09 14:19:47 -0500

    using common separator

--- pipelines/matrix/src/matrix/pipelines/data_release/nodes.py
+++ pipelines/matrix/src/matrix/pipelines/data_release/nodes.py
@@ -7,6 +7,7 @@ from matrix.schemas.knowledge_graph import cols_for_schema, KGNodeSchema, KGEdge
 
 logger = logging.getLogger(__name__)
 
+SEPARATOR = "\x1f"
 
 def filtered_edges_to_kgx(df: DataFrame) -> DataFrame:
     """Function to create KGX formatted edges.
@@ -16,10 +17,9 @@ def filtered_edges_to_kgx(df: DataFrame) -> DataFrame:
     """
     return (
         df
-        .withColumn("upstream_data_source", F.array_join(F.col("upstream_data_source"), ","))
-        .withColumn("aggregator_knowledge_source", F.array_join(F.col("aggregator_knowledge_source"), ","))
-        .withColumn("publications", F.array_join(F.col("publications"), ","))
-        # .select("subject", "predicate", "object", "knowledge_level", "primary_knowledge_source", "object_aspect_qualifier", "object_direction_qualifier", "upstream_data_source", "publications", "aggregator_knowledge_source", "subject_aspect_qualifier", "subject_direction_qualifier")
+        .withColumn("upstream_data_source", F.array_join(F.col("upstream_data_source"), SEPARATOR))
+        .withColumn("aggregator_knowledge_source", F.array_join(F.col("aggregator_knowledge_source"), SEPARATOR))
+        .withColumn("publications", F.array_join(F.col("publications"), SEPARATOR))
         .select(*cols_for_schema(KGEdgeSchema))
     )
 
@@ -32,11 +32,10 @@ def filtered_nodes_to_kgx(df: DataFrame) -> DataFrame:
     """
     return (
         df
-        .withColumn("equivalent_identifiers", F.array_join(F.col("equivalent_identifiers"), ","))
-        .withColumn("all_categories", F.array_join(F.col("all_categories"), ","))
-        .withColumn("publications", F.array_join(F.col("publications"), ","))
-        .withColumn("labels", F.array_join(F.col("labels"), ","))
-        .withColumn("upstream_data_source", F.array_join(F.col("upstream_data_source"), ","))
-        #.select("id", "name", "category", "description", "equivalent_identifiers", "all_categories", "publications", "labels", "international_resource_identifier", "upstream_data_source")
+        .withColumn("equivalent_identifiers", F.array_join(F.col("equivalent_identifiers"), SEPARATOR))
+        .withColumn("all_categories", F.array_join(F.col("all_categories"), SEPARATOR))
+        .withColumn("publications", F.array_join(F.col("publications"), SEPARATOR))
+        .withColumn("labels", F.array_join(F.col("labels"), SEPARATOR))
+        .withColumn("upstream_data_source", F.array_join(F.col("upstream_data_source"), SEPARATOR))
         .select(*cols_for_schema(KGNodeSchema))
     )
