d8c87a963e110aa42d012ebd572fc475ed8e25f6
emil-k <emil.krause.44@gmail.com>
2024-12-11 17:19:35 +0100

    adding kedro hook to export context

--- pipelines/matrix/src/matrix/settings.py
+++ pipelines/matrix/src/matrix/settings.py
@@ -21,6 +21,7 @@ HOOKS = (
     MlflowHook(),
     matrix_hooks.MLFlowHooks(),
     matrix_hooks.SparkHooks(),
+    matrix_hooks.InfoHooks(),
 )
 
 # Installed plugins for which to disable hook auto-registration.
