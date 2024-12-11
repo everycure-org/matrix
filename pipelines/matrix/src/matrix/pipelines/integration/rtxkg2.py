be7e0849892d8d437fdbe045f58cec5f0e9dfbd8
Piotr Kaniewski <115791652+piotrkan@users.noreply.github.com>
2024-12-11 10:48:13 +0000

    Update pipelines/matrix/src/matrix/pipelines/integration/rtxkg2.py

--- pipelines/matrix/src/matrix/pipelines/integration/rtxkg2.py
+++ pipelines/matrix/src/matrix/pipelines/integration/rtxkg2.py
@@ -125,7 +125,7 @@ def filter_semmed(
         .withColumn("num_publications", f.size(f.col("publications")))
         # fmt: off
         .filter(
-            # Retain only semmed edges more than 10 publications or ndg score below 0.6
+            # Retain only semmed edges more than 10 publications or ndg score above 0.6
             (f.col("num_publications") >= f.lit(publication_threshold)) & (f.col("ngd") > f.lit(ngd_threshold))
         )
         # fmt: on
