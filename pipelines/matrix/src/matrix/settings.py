9d0839e87a95528531fcd82c54465067a77ea2b3
Jason Reilly <jdr0887@users.noreply.github.com>
2024-12-12 10:04:41 -0500

    Update pipelines/matrix/src/matrix/settings.py
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>

--- pipelines/matrix/src/matrix/settings.py
+++ pipelines/matrix/src/matrix/settings.py
@@ -64,7 +64,7 @@ CONFIG_LOADER_ARGS = {
 DYNAMIC_PIPELINES_MAPPING = {
     "integration": [
         {"name": "rtx_kg2"},
-        {"name": "spoke"},
+        # {"name": "spoke"},
         {"name": "robokop"},
         {"name": "ec_medical_team"},
     ],
