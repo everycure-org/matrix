15b023a69d75fb1019291245e16725f4e38477b4
emil-k <emil.krause.44@gmail.com>
2024-12-13 07:35:48 +0100

    make the last node dependency explicit.

--- pipelines/matrix/src/matrix/pipelines/data_release/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/data_release/pipeline.py
@@ -1,5 +1,18 @@
-from kedro.pipeline import Pipeline, node, pipeline
+from kedro.pipeline import Pipeline, pipeline
+from matrix.kedro4argo_node import ArgoNode
 from matrix.pipelines.embeddings.nodes import ingest_edges, ingest_nodes
+
+# Last node is made explicit because there's a kedro hook after_node_run
+# being triggered after the completion of the last node of this pipeline.
+last_node = (
+    ArgoNode(
+        func=ingest_edges,
+        inputs=["data_release.prm.kg_nodes", "integration.prm.filtered_edges"],
+        outputs="data_release.prm.kg_edges",
+        name="ingest_kg_edges",
+        tags=["neo4j"],
+    ),
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -7,41 +20,35 @@ def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             # release to bigquery
-            node(
+            ArgoNode(
                 func=lambda x: x,
                 inputs=["integration.prm.filtered_edges"],
                 outputs="data_release.prm.bigquery_edges",
                 name="release_edges_to_bigquery",
             ),
-            node(
+            ArgoNode(
                 func=lambda x: x,
                 inputs=["integration.prm.filtered_nodes"],
                 outputs="data_release.prm.bigquery_nodes",
                 name="release_nodes_to_bigquery",
             ),
             # release to neo4j
-            node(
+            ArgoNode(
                 func=lambda x: x,
                 inputs=["embeddings.feat.nodes"],
                 outputs="data_release.feat.nodes_with_embeddings",
                 name="ingest_nodes_with_embeddings",
             ),
-            node(
+            ArgoNode(
                 func=ingest_nodes,
                 inputs=["integration.prm.filtered_nodes"],
                 outputs="data_release.prm.kg_nodes",
                 name="ingest_kg_nodes",
                 tags=["neo4j"],
             ),
-            node(
-                func=ingest_edges,
-                inputs=["data_release.prm.kg_nodes", "integration.prm.filtered_edges"],
-                outputs="data_release.prm.kg_edges",
-                name="ingest_kg_edges",
-                tags=["neo4j"],
-            ),
+            last_node,
             # NOTE: Enable if you want embeddings
-            # node(
+            # ArgoNode(
             #     func=lambda _, x: x,
             #     inputs=["data_release.prm.kg_nodes", "embeddings.feat.nodes"],
             #     outputs="data_release.prm.kg_embeddings",
