613722d5d5adeff25c2f9d147853a7527fa13e0c
matwasilewski <mat.p.wasilewski@gmail.com>
2024-12-13 09:46:42 +0000

    add unpack params to inject

--- pipelines/matrix/src/matrix/inject.py
+++ pipelines/matrix/src/matrix/inject.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, List, Tuple
 
 OBJECT_KW = "object"
 INSTANTIATE_KW = "instantiate"
+UNPACK_KW = "unpack"
 
 
 def _load_obj(obj_path: str, default_obj_path: str = None) -> Any:
@@ -143,6 +144,59 @@ def inject_object(exclude_kwargs: List[str] = None):
             result_df = func(*args, **kwargs)
 
             return result_df
+
+        return wrapper
+
+    return decorate
+
+
+def _unpack_params(*args, **kwargs):
+    """Unpacks top level dictionaries in args and kwargs by 1 level.
+
+    Most beneficial if used as part of a kedro node. In cases where we need to pass
+    lots of parameters to function, we can use this unpack decorator where we use
+    unpack arg or kwarg which will contain most of the parameters.
+
+    To enable unpacking, the keyword "unpack" can be provided via a dictionary in
+    args and/or kwargs.
+    """
+    unpacked_kwargs_from_args = {}
+    args = list(args)
+    remove_list = []
+    for i, arg in enumerate(args):
+        if isinstance(arg, dict):
+            if UNPACK_KW in arg.keys():
+                remove_list.append(i)
+                new_kwargs = args[i]
+                unpacked_kwargs_from_args = {
+                    **unpacked_kwargs_from_args,
+                    **new_kwargs.pop(UNPACK_KW),
+                    **new_kwargs,
+                }
+    args = [i for j, i in enumerate(args) if j not in remove_list]
+
+    if UNPACK_KW in kwargs:
+        unpack_kwargs_from_kwargs = kwargs.pop(UNPACK_KW, {})
+    else:
+        unpack_kwargs_from_kwargs = {}
+
+    return args, {**unpacked_kwargs_from_args, **kwargs, **unpack_kwargs_from_kwargs}
+
+
+def unpack_params():  # pylint: disable=missing-return-type-doc
+    """Unpack params decorator.
+
+    Returns:
+        Wrapper function.
+    """
+
+    def decorate(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            args, kwargs = _unpack_params(*args, **kwargs)
+            result = func(*args, **kwargs)
+
+            return result
 
         return wrapper
 
