fc7d78c8dd997e97d20b21395422d28f5cddb243
Oliver Willekens <4733178+oliverw1@users.noreply.github.com>
2024-12-13 11:06:40 +0100

    fix: source latest release version from OmegaConfig
    The specified alternative was not existant, leading to `kedro run` not
    finding the datasets.

--- pipelines/matrix/src/matrix/hooks.py
+++ pipelines/matrix/src/matrix/hooks.py
@@ -142,7 +142,7 @@ class SparkHooks:
                     k: v for k, v in parameters.items() if not k.startswith("spark.hadoop.google.cloud.auth.service")
                 }
             else:
-                logger.info(f"Executing for enviornment: {cls._kedro_context.env}")
+                logger.info(f"Executing for environment: {cls._kedro_context.env}")
                 logger.info(f'With ARGO_POD_UID set to: {os.environ.get("ARGO_NODE_ID", "")}')
                 logger.info("Thus determined not to be in k8s cluster and executing with service-account.json file")
 
