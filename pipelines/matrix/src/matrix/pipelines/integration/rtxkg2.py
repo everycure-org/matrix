3dff0275d3e1da17a381962f8086d13763383d29
matwasilewski <mat.p.wasilewski@gmail.com>
2024-12-16 12:44:48 +0000

    use F instead of f for imports

--- pipelines/matrix/src/matrix/pipelines/integration/rtxkg2.py
+++ pipelines/matrix/src/matrix/pipelines/integration/rtxkg2.py
@@ -2,10 +2,9 @@ import logging
 from typing import Dict
 
 import pandera
-import pyspark.sql.functions as f
+import pyspark.sql.functions as F
 import pyspark.sql.types as T
 from pyspark.sql import DataFrame
-from refit.v1.core.inline_primary_key import primary_key
 
 from .transformer import GraphTransformer
 
@@ -31,12 +30,12 @@ class RTXTransformer(GraphTransformer):
         # fmt: off
         return (
             nodes_df
-            .withColumn("upstream_data_source",              f.array(f.lit("rtxkg2")))
-            .withColumn("labels",                            f.split(f.col(":LABEL"), RTX_SEPARATOR))
-            .withColumn("all_categories",                    f.split(f.col("all_categories:string[]"), RTX_SEPARATOR))
-            .withColumn("equivalent_identifiers",            f.split(f.col("equivalent_curies:string[]"), RTX_SEPARATOR))
-            .withColumn("publications",                      f.split(f.col("publications:string[]"), RTX_SEPARATOR).cast(T.ArrayType(T.StringType())))
-            .withColumn("international_resource_identifier", f.col("iri"))
+            .withColumn("upstream_data_source",              F.array(F.lit("rtxkg2")))
+            .withColumn("labels",                            F.split(F.col(":LABEL"), RTX_SEPARATOR))
+            .withColumn("all_categories",                    F.split(F.col("all_categories:string[]"), RTX_SEPARATOR))
+            .withColumn("equivalent_identifiers",            F.split(F.col("equivalent_curies:string[]"), RTX_SEPARATOR))
+            .withColumn("publications",                      F.split(F.col("publications:string[]"), RTX_SEPARATOR).cast(T.ArrayType(T.StringType())))
+            .withColumn("international_resource_identifier", F.col("iri"))
             .withColumnRenamed("id:ID", "id")
             .select(*cols_for_schema(KGNodeSchema))
         )
@@ -58,21 +57,32 @@ class RTXTransformer(GraphTransformer):
         # fmt: off
         return (
             edges_df
-            .withColumn("upstream_data_source",          f.array(f.lit("rtxkg2")))
-            .withColumn("knowledge_level",               f.lit(None).cast(T.StringType()))
-            .withColumn("aggregator_knowledge_source",   f.split(f.col("knowledge_source:string[]"), RTX_SEPARATOR)) # RTX KG2 2.10 does not exist
-            .withColumn("primary_knowledge_source",      f.col("aggregator_knowledge_source").getItem(0)) # RTX KG2 2.10 `primary_knowledge_source``
-            .withColumn("publications",                  f.split(f.col("publications:string[]"), RTX_SEPARATOR))
-            .withColumn("subject_aspect_qualifier",      f.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
-            .withColumn("subject_direction_qualifier",   f.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
-            .withColumn("object_aspect_qualifier",       f.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
-            .withColumn("object_direction_qualifier",    f.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
+            .withColumn("upstream_data_source",          F.array(F.lit("rtxkg2")))
+            .withColumn("knowledge_level",               F.lit(None).cast(T.StringType()))
+            .withColumn("aggregator_knowledge_source",   F.split(F.col("knowledge_source:string[]"), RTX_SEPARATOR)) # RTX KG2 2.10 does not exist
+            .withColumn("primary_knowledge_source",      F.col("aggregator_knowledge_source").getItem(0)) # RTX KG2 2.10 `primary_knowledge_source``
+            .withColumn("publications",                  F.split(F.col("publications:string[]"), RTX_SEPARATOR))
+            .withColumn("subject_aspect_qualifier",      F.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
+            .withColumn("subject_direction_qualifier",   F.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
+            .withColumn("object_aspect_qualifier",       F.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
+            .withColumn("object_direction_qualifier",    F.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
             .select(*cols_for_schema(KGEdgeSchema))
         ).transform(filter_semmed, curie_to_pmids, **semmed_filters)
         # fmt: on
 
 
-@primary_key(df="curie_to_pmids", primary_key=["curie"])
+# Define schema for curie_to_pmids DataFrame
+CurieToPMIDsSchema = pandera.pyspark.DataFrameSchema(
+    {
+        "curie": pandera.Column(T.StringType(), nullable=False),  # primary key
+        "pmids": pandera.Column(T.ArrayType(T.IntegerType()), nullable=True),  # array type column
+        "num_pmids": pandera.Column(T.IntegerType(), nullable=True),
+    },
+    unique=["curie"],  # enforce uniqueness constraint
+)
+
+
+@pandera.pyspark.check_output(CurieToPMIDsSchema)
 def filter_semmed(
     edges_df: DataFrame,
     curie_to_pmids: DataFrame,
@@ -94,45 +104,45 @@ def filter_semmed(
         Filtered dataframe
     """
     curie_to_pmids = (
-        curie_to_pmids.withColumn("pmids", f.from_json("pmids", T.ArrayType(T.IntegerType())))
-        .withColumn("pmids", f.sort_array(f.col("pmids")))
-        .withColumn("limited_pmids", f.slice(f.col("pmids"), 1, limit_pmids))
+        curie_to_pmids.withColumn("pmids", F.from_json("pmids", T.ArrayType(T.IntegerType())))
+        .withColumn("pmids", F.sort_array(F.col("pmids")))
+        .withColumn("limited_pmids", F.slice(F.col("pmids"), 1, limit_pmids))
         .drop("pmids")
         .withColumnRenamed("limited_pmids", "pmids")
-        .withColumn("num_pmids", f.array_size(f.col("pmids")))
+        .withColumn("num_pmids", F.array_size(F.col("pmids")))
         .withColumnRenamed("curie", "id")
         .persist()
     )
 
-    table = f.broadcast(curie_to_pmids)
+    table = F.broadcast(curie_to_pmids)
 
     semmed_edges = (
         edges_df.alias("edges")
-        .filter(f.col("primary_knowledge_source") == f.lit("infores:semmeddb"))
+        .filter(F.col("primary_knowledge_source") == F.lit("infores:semmeddb"))
         # Enrich subject pubmed identifiers
         .join(
             table.alias("subj"),
-            on=[f.col("edges.subject") == f.col("subj.id")],
+            on=[F.col("edges.subject") == F.col("subj.id")],
             how="left",
         )
         # Enrich object pubmed identifiers
         .join(
             table.alias("obj"),
-            on=[f.col("edges.object") == f.col("obj.id")],
+            on=[F.col("edges.object") == F.col("obj.id")],
             how="left",
         )
         .transform(compute_ngd)
-        .withColumn("num_publications", f.size(f.col("publications")))
+        .withColumn("num_publications", F.size(F.col("publications")))
         # fmt: off
         .filter(
             # Retain only semmed edges more than 10 publications or ndg score above 0.6
-            (f.col("num_publications") >= f.lit(publication_threshold)) & (f.col("ngd") > f.lit(ngd_threshold))
+            (F.col("num_publications") >= F.lit(publication_threshold)) & (F.col("ngd") > F.lit(ngd_threshold))
         )
         # fmt: on
         .select("edges.*")
     )
 
-    edges_filtered = edges_df.filter(f.col("primary_knowledge_source") != f.lit("infores:semmeddb")).unionByName(
+    edges_filtered = edges_df.filter(F.col("primary_knowledge_source") != F.lit("infores:semmeddb")).unionByName(
         semmed_edges
     )
     return edges_filtered
@@ -151,13 +161,13 @@ def compute_ngd(df: DataFrame, num_pairs: int = 3.7e7 * 20) -> DataFrame:
     return (
         # Take first max_pmids elements from each array
         df.withColumn(
-            "num_common_pmids", f.array_size(f.array_intersect(f.col("subj.pmids"), f.col("obj.pmids")))
+            "num_common_pmids", F.array_size(F.array_intersect(F.col("subj.pmids"), F.col("obj.pmids")))
         ).withColumn(
             "ngd",
             (
-                f.greatest(f.log2(f.col("subj.num_pmids")), f.log2(f.col("obj.num_pmids")))
-                - f.log2(f.col("num_common_pmids"))
+                F.greatest(F.log2(F.col("subj.num_pmids")), F.log2(F.col("obj.num_pmids")))
+                - F.log2(F.col("num_common_pmids"))
             )
-            / (f.log2(f.lit(num_pairs)) - f.least(f.log2(f.col("subj.num_pmids")), f.log2(f.col("obj.num_pmids")))),
+            / (F.log2(F.lit(num_pairs)) - F.least(F.log2(F.col("subj.num_pmids")), F.log2(F.col("obj.num_pmids")))),
         )
     )
