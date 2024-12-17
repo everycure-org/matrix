073f679d35a7d6f44a8f1cf86860f667415071b7
Daniel Rhodes <14894770+drhodesbrc@users.noreply.github.com>
2024-12-17 09:33:50 +0000

    Add MoA visualizer service (#712)
    * add deployment
    
    * update name
    :
    
    * setup route
    
    * change protocol
    
    * use cluster ip
    
    * add moa_vis to repo
    
    * create init script
    
    * remove suffix arg
    
    * fix env variables
    
    * fix env variables
    
    * setup init contaier
    
    * add deployment comments
    
    * push ci
    
    * push ci
    
    * switch to visualizer
    
    * add step with correct permissions
    
    * correct paths
    
    * fix paths
    
    * try fix
    
    * fix typo
    
    * fix typo
    
    * remove unused paths
    
    * clean comment
    
    * fix makefile
    
    * hook up img
    
    * fix imports
    
    * update init script
    
    * update init script
    
    * redeploy
    
    * redeploy
    
    * use env vars
    
    * fix env
    
    * fix templating
    
    * update table names
    
    * update sql query
    
    * update image tag
    
    * tag name
    
    * change the correct image tag
    
    * use correct ing
    
    * update to new data
    
    * change path to data
    
    * fix path
    
    * reintroduce assets
    
    * correct path
    
    * update filename
    
    * add to streamlit
    
    * switch to pydantic
    
    * fix display cols
    
    * remove caching and feedback col
    
    * fix conflicts
    
    * try symlink
    
    * fix interpolation
    
    * add files
    
    * fix issues
    
    * fix ci
    
    * fix template
    
    * setup correct entrypoint
    
    * rm old rs
    
    * add base settings
    
    * fix init script
    
    * fix types
    
    * add image pull policy
    
    * add gs
    
    * add gs
    
    * img path as string
    
    * bump version
    
    * pydantic testing
    
    * attempt fixes
    
    * push changes
    
    * push changes
    
    * fix settings
    
    * add image version
    
    * sync vars
    
    * push
    
    * update ci
    
    ---------
    
    Co-authored-by: Laurens Vijnck <laurens@everycure.org>
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>

---
+++ services/moa_visualizer/src/config.py
@@ -0,0 +1,165 @@
+from pydantic_settings import BaseSettings
+from pydantic import Field, HttpUrl, BaseModel
+import streamlit as st
+# from enum import Enum
+
+
+# class BiolinkClass(str, Enum):
+#     """Enumeration of supported biolink classes."""
+#     SMALL_MOLECULE = "small_molecule"
+#     DISEASE = "disease"
+#     PROTEIN = "protein"
+
+
+# class ColourMap(BaseSettings):
+#     """Pastel colours for biolink classes."""
+#     colour_map: Dict[BiolinkClass, str] = {
+#         BiolinkClass.SMALL_MOLECULE: '#77DD77',
+#         BiolinkClass.DISEASE: '#B3EBF2',
+#         BiolinkClass.PROTEIN: '#FF964F',
+#     }
+
+
+class OntologyUrls(BaseModel):
+    """URLs for different ontologies."""
+
+    MONDO: HttpUrl = Field("http://purl.obolibrary.org/obo/", description="MONDO ontology base URL")
+    UniProtKB: HttpUrl = Field("https://www.uniprot.org/uniprotkb/", description="UniProtKB database URL")
+    CHEMBL: HttpUrl = Field("https://www.ebi.ac.uk/chembl/", description="ChEMBL database URL")
+
+    class Config:
+        frozen = True
+
+
+class NodeColumns(BaseModel):
+    """Configuration for individual pathway node columns."""
+
+    predicates: str
+    id: str
+    name: str
+    type: str
+    model_config = {
+        "extra": "allow",
+    }
+
+
+class DisplayColumns(BaseModel):
+    """Configuration for display columns."""
+
+    # Note this was generated using AI assistance
+    all_columns: list[str] = Field(default_factory=list, description="All column display names in order")
+    all_keys: list[str] = Field(default_factory=list, description="All field names including node columns")
+    n_nodes: int = Field(default=0, description="Number of nodes")
+    drug_name: str = Field("Drug", description="Display name for drug column")
+    pair_id: str = Field("Pair ID", description="Pair UID")
+    disease_name: str = Field("Disease", description="Display name for disease column")
+    MOA_score: str = Field("MoA Score", description="Display name for MOA score")
+    feedback: str = Field("Feedback", description="Display name for feedback column")
+    drug_disease_pair_id: str = Field("Drug-Disease Pair ID", description="Display name for drug-disease pair ID")
+    drug_id: str = Field("Drug Curie", description="Display name for drug ID")
+    disease_id: str = Field("Disease Curie", description="Display name for disease ID")
+    node_columns: list[NodeColumns] = Field(default_factory=list, description="Node columns")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        updated_values = {
+            "all_columns": self.get_all_columns(),
+            "all_keys": self.get_all_keys(),
+            "n_nodes": len(self.node_fields),
+        }
+        object.__setattr__(self, "__dict__", {**self.__dict__, **updated_values})
+
+    @property
+    def basic_fields(self) -> list[str]:
+        """List of fields that we want to display,
+        Ignores nodes + edges as these are variable."""
+        return [
+            "drug_name",
+            "disease_name",
+            "MOA_score",
+            "pair_id",
+            "feedback",
+            "drug_disease_pair_id",
+            "drug_id",
+            "disease_id",
+        ]
+
+    @property
+    def node_fields(self) -> list[NodeColumns]:
+        """Get the node columns."""
+        node_fields = []
+        for i in range(1, 4):
+            node_fields.append(
+                NodeColumns(
+                    predicates=f"Edge {i}",
+                    id=f"Node {i} ID",
+                    name=f"Node {i} Name",
+                    type=f"Node {i} Type",
+                )
+            )
+        return node_fields
+
+    def get_all_columns(self) -> list[str]:
+        """Get all column display names in order."""
+        basic_display_fields = [getattr(self, field) for field in self.basic_fields]
+
+        node_fields = [field for node in self.node_fields for field in [node.predicates, node.id, node.name, node.type]]
+
+        return basic_display_fields + node_fields
+
+    def get_all_keys(self) -> list[str]:
+        """Get all field names including node column fields."""
+        node_fields = []
+        for i in range(1, 4):
+            for field in ["predicates", "id", "name", "type"]:
+                if field == "predicates":
+                    node_fields.append(f"predicates_{i}")
+                else:
+                    node_fields.append(f"intermediate_{field}_{i}")
+        return self.basic_fields + node_fields
+
+
+class Settings(BaseSettings):
+    """Main configuration settings."""
+
+    DATA_INPUT_PATH: str
+    MOA_DB_PATH: str
+    GCP_PROJECT: str
+    IMAGE_VERSION: str
+
+    # TODO: Move elsewhere
+    moa_info_img: str = Field("assets/moa_info.svg")
+    moa_info_text: str = Field(
+        default="""
+Mechanism of action (MOA) prediction models aim to predict the biological 
+pathways by which a given drug acts on a given disease. The MOA extraction 
+module is an example of a path-based approach to MOA prediction. It extracts 
+paths in the knowledge graph that are likely to be relevant to the MOA of a 
+given drug-disease pair. The MOA extraction module is loosely inspired by the 
+[KGML-xDTD](https://github.com/chunyuma/KGML-xDTD) MOA module which extracts 
+paths in the KG using an adversarial actor-critic reinforcement learning 
+algorithm. In contrast, we use a simpler approach of extracting paths using a 
+supervised binary classifier.
+
+The main component of the MOA extraction system is a binary classifier that 
+predicts whether a given path in the knowledge graph is likely to represent a 
+mechanism of action for a given drug-disease pair. The binary classifier is 
+trained on a dataset of manually annotated MoA pathways. The prediction 
+process is illustrated by the following diagram:
+""",
+        description="Explainer for MOA information",
+    )
+
+    class Config:
+        case_sensitive = True
+        validate_assignment = True
+
+
+def set_wide_space_default():
+    """Set the default layout for the Streamlit app."""
+    st.set_page_config(layout="wide")
+
+
+settings: Settings = Settings()
+ont_urls = OntologyUrls()
+display_cols = DisplayColumns()
