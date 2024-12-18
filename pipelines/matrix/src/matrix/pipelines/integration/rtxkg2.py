06e3f369ac302c74e2e9f3c93368aa475b4c24bb
matwasilewski <mat.p.wasilewski@gmail.com>
2024-12-18 14:25:47 +0000

    restore older import conventions

--- pipelines/matrix/src/matrix/pipelines/integration/rtxkg2.py
+++ pipelines/matrix/src/matrix/pipelines/integration/rtxkg2.py
@@ -1,11 +1,11 @@
 import logging
 from typing import Dict
 
-import pandera
+import pandera as pa
 from pandera.pyspark import DataFrameModel, Field
 
-import pyspark
-import pyspark.sql.functions as F
+from pyspark.sql import DataFrame
+import pyspark.sql.functions as f
 import pyspark.sql.types as T
 
 from .transformer import GraphTransformer
@@ -19,8 +19,8 @@ RTX_SEPARATOR = "\u01c2"
 
 
 class RTXTransformer(GraphTransformer):
-    @pandera.check_output(KGNodeSchema)
-    def transform_nodes(self, nodes_df: pyspark.sql.DataFrame, **kwargs) -> pyspark.sql.DataFrame:
+    @pa.check_output(KGNodeSchema)
+    def transform_nodes(self, nodes_df: DataFrame, **kwargs) -> DataFrame:
         """Transform RTX KG2 nodes to our target schema.
 
         Args:
@@ -32,25 +32,25 @@ class RTXTransformer(GraphTransformer):
         # fmt: off
         return (
             nodes_df
-            .withColumn("upstream_data_source",              F.array(F.lit("rtxkg2")))
-            .withColumn("labels",                            F.split(F.col(":LABEL"), RTX_SEPARATOR))
-            .withColumn("all_categories",                    F.split(F.col("all_categories:string[]"), RTX_SEPARATOR))
-            .withColumn("equivalent_identifiers",            F.split(F.col("equivalent_curies:string[]"), RTX_SEPARATOR))
-            .withColumn("publications",                      F.split(F.col("publications:string[]"), RTX_SEPARATOR).cast(T.ArrayType(T.StringType())))
-            .withColumn("international_resource_identifier", F.col("iri"))
+            .withColumn("upstream_data_source",              f.array(f.lit("rtxkg2")))
+            .withColumn("labels",                            f.split(f.col(":LABEL"), RTX_SEPARATOR))
+            .withColumn("all_categories",                    f.split(f.col("all_categories:string[]"), RTX_SEPARATOR))
+            .withColumn("equivalent_identifiers",            f.split(f.col("equivalent_curies:string[]"), RTX_SEPARATOR))
+            .withColumn("publications",                      f.split(f.col("publications:string[]"), RTX_SEPARATOR).cast(T.ArrayType(T.StringType())))
+            .withColumn("international_resource_identifier", f.col("iri"))
             .withColumnRenamed("id:ID", "id")
             .select(*cols_for_schema(KGNodeSchema))
         )
         # fmt: on
 
-    @pandera.check_output(KGEdgeSchema)
+    @pa.check_output(KGEdgeSchema)
     def transform_edges(
         self,
-        edges_df: pyspark.sql.DataFrame,
-        curie_to_pmids: pyspark.sql.DataFrame,
+        edges_df: DataFrame,
+        curie_to_pmids: DataFrame,
         semmed_filters: Dict[str, str],
         **kwargs,
-    ) -> pyspark.sql.DataFrame:
+    ) -> DataFrame:
         """Transform RTX KG2 edges to our target schema.
 
         Args:
@@ -63,15 +63,15 @@ class RTXTransformer(GraphTransformer):
         # fmt: off
         return (
             edges_df
-            .withColumn("upstream_data_source",          F.array(F.lit("rtxkg2")))
-            .withColumn("knowledge_level",               F.lit(None).cast(T.StringType()))
-            .withColumn("aggregator_knowledge_source",   F.split(F.col("knowledge_source:string[]"), RTX_SEPARATOR)) # RTX KG2 2.10 does not exist
-            .withColumn("primary_knowledge_source",      F.col("aggregator_knowledge_source").getItem(0)) # RTX KG2 2.10 `primary_knowledge_source``
-            .withColumn("publications",                  F.split(F.col("publications:string[]"), RTX_SEPARATOR))
-            .withColumn("subject_aspect_qualifier",      F.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
-            .withColumn("subject_direction_qualifier",   F.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
-            .withColumn("object_aspect_qualifier",       F.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
-            .withColumn("object_direction_qualifier",    F.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
+            .withColumn("upstream_data_source",          f.array(f.lit("rtxkg2")))
+            .withColumn("knowledge_level",               f.lit(None).cast(T.StringType()))
+            .withColumn("aggregator_knowledge_source",   f.split(f.col("knowledge_source:string[]"), RTX_SEPARATOR)) # RTX KG2 2.10 does not exist
+            .withColumn("primary_knowledge_source",      f.col("aggregator_knowledge_source").getItem(0)) # RTX KG2 2.10 `primary_knowledge_source``
+            .withColumn("publications",                  f.split(f.col("publications:string[]"), RTX_SEPARATOR))
+            .withColumn("subject_aspect_qualifier",      f.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
+            .withColumn("subject_direction_qualifier",   f.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
+            .withColumn("object_aspect_qualifier",       f.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
+            .withColumn("object_direction_qualifier",    f.lit(None).cast(T.StringType())) #not present in RTX KG2 at this time
             .select(*cols_for_schema(KGEdgeSchema))
         ).transform(filter_semmed, curie_to_pmids, **semmed_filters)
         # fmt: on
@@ -87,14 +87,14 @@ class CurieToPMIDsSchema(DataFrameModel):
         unique = ["curie"]
 
 
-@pandera.check_input(CurieToPMIDsSchema, obj_getter="curie_to_pmids")
+@pa.check_input(CurieToPMIDsSchema, obj_getter="curie_to_pmids")
 def filter_semmed(
-    edges_df: pyspark.sql.DataFrame,
-    curie_to_pmids: pyspark.sql.DataFrame,
+    edges_df: DataFrame,
+    curie_to_pmids: DataFrame,
     publication_threshold: int,
     ngd_threshold: float,
     limit_pmids: int,
-) -> pyspark.sql.DataFrame:
+) -> DataFrame:
     """Function to filter semmed edges.
 
     Function that performs additional cleaning on RTX edges obtained by SemMedDB. This
@@ -109,51 +109,51 @@ def filter_semmed(
         Filtered dataframe
     """
     curie_to_pmids = (
-        curie_to_pmids.withColumn("pmids", F.from_json("pmids", T.ArrayType(T.IntegerType())))
-        .withColumn("pmids", F.sort_array(F.col("pmids")))
-        .withColumn("limited_pmids", F.slice(F.col("pmids"), 1, limit_pmids))
+        curie_to_pmids.withColumn("pmids", f.from_json("pmids", T.ArrayType(T.IntegerType())))
+        .withColumn("pmids", f.sort_array(f.col("pmids")))
+        .withColumn("limited_pmids", f.slice(f.col("pmids"), 1, limit_pmids))
         .drop("pmids")
         .withColumnRenamed("limited_pmids", "pmids")
-        .withColumn("num_pmids", F.array_size(F.col("pmids")))
+        .withColumn("num_pmids", f.array_size(f.col("pmids")))
         .withColumnRenamed("curie", "id")
         .persist()
     )
 
-    table = F.broadcast(curie_to_pmids)
+    table = f.broadcast(curie_to_pmids)
 
     semmed_edges = (
         edges_df.alias("edges")
-        .filter(F.col("primary_knowledge_source") == F.lit("infores:semmeddb"))
+        .filter(f.col("primary_knowledge_source") == f.lit("infores:semmeddb"))
         # Enrich subject pubmed identifiers
         .join(
             table.alias("subj"),
-            on=[F.col("edges.subject") == F.col("subj.id")],
+            on=[f.col("edges.subject") == f.col("subj.id")],
             how="left",
         )
         # Enrich object pubmed identifiers
         .join(
             table.alias("obj"),
-            on=[F.col("edges.object") == F.col("obj.id")],
+            on=[f.col("edges.object") == f.col("obj.id")],
             how="left",
         )
         .transform(compute_ngd)
-        .withColumn("num_publications", F.size(F.col("publications")))
+        .withColumn("num_publications", f.size(f.col("publications")))
         # fmt: off
         .filter(
             # Retain only semmed edges more than 10 publications or ndg score below/equal 0.6
-            (F.col("num_publications") >= F.lit(publication_threshold)) & (F.col("ngd") <= F.lit(ngd_threshold))
+            (f.col("num_publications") >= f.lit(publication_threshold)) & (f.col("ngd") <= f.lit(ngd_threshold))
         )
         # fmt: on
         .select("edges.*")
     )
 
-    edges_filtered = edges_df.filter(F.col("primary_knowledge_source") != F.lit("infores:semmeddb")).unionByName(
+    edges_filtered = edges_df.filter(f.col("primary_knowledge_source") != f.lit("infores:semmeddb")).unionByName(
         semmed_edges
     )
     return edges_filtered
 
 
-def compute_ngd(df: pyspark.sql.DataFrame, num_pairs: int = 3.7e7 * 20) -> pyspark.sql.DataFrame:
+def compute_ngd(df: DataFrame, num_pairs: int = 3.7e7 * 20) -> DataFrame:
     """
     PySpark transformation to compute Normalized Google Distance (NGD).
 
@@ -166,13 +166,13 @@ def compute_ngd(df: pyspark.sql.DataFrame, num_pairs: int = 3.7e7 * 20) -> pyspa
     return (
         # Take first max_pmids elements from each array
         df.withColumn(
-            "num_common_pmids", F.array_size(F.array_intersect(F.col("subj.pmids"), F.col("obj.pmids")))
+            "num_common_pmids", f.array_size(f.array_intersect(f.col("subj.pmids"), f.col("obj.pmids")))
         ).withColumn(
             "ngd",
             (
-                F.greatest(F.log2(F.col("subj.num_pmids")), F.log2(F.col("obj.num_pmids")))
-                - F.log2(F.col("num_common_pmids"))
+                f.greatest(f.log2(f.col("subj.num_pmids")), f.log2(f.col("obj.num_pmids")))
+                - f.log2(f.col("num_common_pmids"))
             )
-            / (F.log2(F.lit(num_pairs)) - F.least(F.log2(F.col("subj.num_pmids")), F.log2(F.col("obj.num_pmids")))),
+            / (f.log2(f.lit(num_pairs)) - f.least(f.log2(f.col("subj.num_pmids")), f.log2(f.col("obj.num_pmids")))),
         )
     )
