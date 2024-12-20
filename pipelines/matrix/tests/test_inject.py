3fcb5a3f6f6c8e2aacd8d000a26831c84e3421b1
matwasilewski <mat.p.wasilewski@gmail.com>
2024-12-20 11:36:12 +0000

    add unit test coverage for unpack params

--- pipelines/matrix/tests/test_inject.py
+++ pipelines/matrix/tests/test_inject.py
@@ -8,6 +8,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import MinMaxScaler
 
+import pyspark.sql.functions as F
 from pyspark.sql.types import (
     FloatType,
     IntegerType,
@@ -16,7 +17,14 @@ from pyspark.sql.types import (
     StructType,
 )
 
-from matrix.inject import _inject_object, _parse_for_objects, inject_object, make_list_regexable
+from matrix.inject import (
+    _inject_object,
+    _parse_for_objects,
+    _unpack_params,
+    inject_object,
+    make_list_regexable,
+    unpack_params,
+)
 
 
 def dummy_func(x):
@@ -615,3 +623,115 @@ def test_make_list_regexable_with_wrong_input_type_pandas(pandas_df):
         match="'int' object is not iterable",
     ):
         accept_regexable_list(pandas_df, 7, True)
+
+
+@unpack_params()
+def my_func(*args, **kwargs):
+    def test_func(x):
+        return x["c1"]
+
+    return test_func(*args, **kwargs)
+
+
+def test_unpack_params_with_kwargs():
+    result_arg, result_kwarg = _unpack_params(unpack={"c1": 1})
+
+    assert result_arg == []
+    assert result_kwarg == {"c1": 1}
+
+
+def test_unpack_params_with_args():
+    @unpack_params()
+    def dummy_func(x, y, z, params):
+        params["test"] == "test"
+        return x + y - z
+
+    x = 1
+    param = {"params": {"test": "test"}, "unpack": {"y": 1, "z": 2}}
+    result = dummy_func(x, param)
+    assert result == 0
+
+
+@unpack_params()
+def dummy_func2(x, y, z, params1, params2):
+    params1["test"] == 1
+    params2["test"] == 2
+    return x + y - z
+
+
+def test_unpack_params_with_multiple_args():
+    x = 1
+    param1 = {"params1": {"test": 1}, "unpack": {"y": 2}}
+    param2 = {"params2": {"test": 2}, "unpack": {"z": 3}}
+    result = dummy_func2(x, param1, param2)
+    assert result == 0
+
+
+def test_unpack_params_with_multiple_args_and_kwargs():
+    param1 = {"params1": {"test": 1}, "unpack": {"y": 2}}
+    param2 = {"params2": {"test": 2}, "unpack": {"z": 3}}
+    result = dummy_func2(param1, param2, unpack={"x": 1})
+    assert result == 0
+
+
+def test_unpack_params_disable_via_args():
+    result_arg, result_kwarg = _unpack_params({"c1": 1})
+
+    assert result_arg == [
+        {"c1": 1},
+    ]
+    assert result_kwarg == {}
+
+
+def test_unpack_params_disable_via_args2():
+    result_arg, result_kwarg = _unpack_params(**{"c1": 1})
+
+    assert result_arg == []
+    assert result_kwarg == {"c1": 1}
+
+
+def test_unpack_params_disable_via_kwargs():
+    result_arg, result_kwarg = _unpack_params(x={"c1": 1})
+
+    assert result_arg == []
+    assert result_kwarg == {"x": {"c1": 1}}
+
+
+def test_unpack_params_with_pd_df(dummy_pd_df):
+    result_arg, result_kwarg = _unpack_params(unpack={"df": dummy_pd_df, "x": 1})
+
+    def dummy_func(df, x):
+        df["new"] = df["c1"] + x
+        return df
+
+    result = dummy_func(*result_arg, **result_kwarg)
+    assert result["new"].tolist() == [2]
+
+
+@pytest.fixture
+def dummy_spark_df(spark):
+    """Dummy spark dataframe."""
+    dummy_pd_df = pd.DataFrame([{"c1": 1}])
+    dummy_spark_df = spark.createDataFrame(dummy_pd_df)
+    return dummy_spark_df
+
+
+def test_unpack_params_with_spark_df(dummy_spark_df):
+    result_arg, result_kwarg = _unpack_params(unpack={"df": dummy_spark_df, "x": 1})
+
+    def dummy_func(df, x):
+        df = df.withColumn("new", F.lit(x) + 1)
+        return df
+
+    result = dummy_func(*result_arg, **result_kwarg)
+    assert [x.asDict() for x in result.select("new").collect()] == [{"new": 2}]
+
+
+def test_unpack_params_true_decorator():
+    result = my_func(unpack={"x": {"c1": 1}})
+    assert result == 1
+
+
+def test_unpack_params_false_decorator():
+    with pytest.raises(KeyError, match="c1"):
+        my_func({"x": {"c1": 1}})
