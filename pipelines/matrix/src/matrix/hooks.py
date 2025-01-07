ac3b70b17638f46f1046592bb8c559b8466464ac
emil-k <emil.krause.44@gmail.com>
2025-01-07 15:19:59 +0000

    change before ---> after node run

--- pipelines/matrix/src/matrix/hooks.py
+++ pipelines/matrix/src/matrix/hooks.py
@@ -325,7 +325,7 @@ class ReleaseInfoHooks:
             f.write(json.dumps(release_info).encode("utf-8"))
 
     @hook_impl
-    def before_node_run(self, node: Node) -> None:
+    def after_node_run(self, node: Node) -> None:
         """Runs after the last node of the data_release pipeline"""
         # We chose to add this using the `after_node_run` hook, rather than
         # `after_pipeline_run`, because one does not know a priori which
