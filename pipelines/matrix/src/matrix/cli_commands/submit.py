1d97e1cd9e86dcc9f382b7a680eca4a4db2b7377
emil-k <emil.krause.44@gmail.com>
2025-01-08 15:43:23 +0000

    bugfix stuck docker push

--- pipelines/matrix/src/matrix/cli_commands/submit.py
+++ pipelines/matrix/src/matrix/cli_commands/submit.py
@@ -142,7 +142,7 @@ def _submit(
         if dry_run:
             return
 
-        build_push_docker(run_name, verbose=verbose)
+        build_push_docker(run_name, verbose=False)
 
         ensure_namespace(namespace, verbose=verbose)
 
