479115ad371dccf7c0fc04a0ab46a3a77ed7586f
emil-k <emil.krause.44@gmail.com>
2025-01-08 15:58:01 +0000

    bugfix stuck docker push 4

--- pipelines/matrix/src/matrix/cli_commands/submit.py
+++ pipelines/matrix/src/matrix/cli_commands/submit.py
@@ -416,7 +416,7 @@ def submit_workflow(run_name: str, namespace: str, verbose: bool):
         "-o json"
     ])
     console.print(f"Running submit command: [blue]{cmd}[/blue]")
-    result = run_subprocess(cmd)
+    result = run_subprocess(cmd, stream_output=False)
     job_name = json.loads(result.stdout).get("metadata", {}).get("name")
 
     if not job_name:
