c0a917c144a64edb90651c36fa4e384aec824e38
Mateusz <39764611+matwasilewski@users.noreply.github.com>
2025-01-08 18:29:38 +0000

    use pyspark.sql consistently (#923)
    * use pyspark.sql consistently
    
    * add ps
    
    * use ps where viable

--- pipelines/matrix/src/matrix/pipelines/integration/transformer.py
+++ pipelines/matrix/src/matrix/pipelines/integration/transformer.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 
-from pyspark.sql import DataFrame
+import pyspark.sql as ps
 
 
 class GraphTransformer(ABC):
     @abstractmethod
-    def transform_nodes(self, nodes_df: DataFrame, **kwargs) -> DataFrame:
+    def transform_nodes(self, nodes_df: ps.DataFrame, **kwargs) -> ps.DataFrame:
         """
         Function to transform nodes into the common format.
 
@@ -17,7 +17,7 @@ class GraphTransformer(ABC):
         ...
 
     @abstractmethod
-    def transform_edges(self, edges_df: DataFrame, **kwargs) -> DataFrame:
+    def transform_edges(self, edges_df: ps.DataFrame, **kwargs) -> ps.DataFrame:
         """
         Function to transform edges into the common format.
 
