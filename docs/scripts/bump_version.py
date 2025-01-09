dc807c32d3bf5ca86858118786f537d4c2a49d0f
Siyan Luo <luosiyanemily@gmail.com>
2025-01-09 10:10:04 +0000

    test bumping

--- docs/scripts/bump_version.py
+++ docs/scripts/bump_version.py
@@ -2,14 +2,14 @@ import os
 import semver
 
 
-def bump_version():
+def bump_version(type: None):
     latest_tag = os.getenv("latest_tag", "v0.0.0").lstrip("v")
-    bump_type = os.getenv("bump_type", "minor")
+    # bump_type = os.getenv("bump_type", "minor")
     version = semver.Version.parse(latest_tag)
 
-    if bump_type == "minor":
+    if type == "minor":
         new_version = version.bump_minor()
-    elif bump_type == "patch":
+    elif type == "patch":
         new_version = version.bump_patch()
 
     with open(os.getenv("GITHUB_ENV"), "a") as env_file:
