04813e38a284c7f9e637b8e2dbdd0db03be005ce
Laurens Vijnck <laurens@everycure.org>
2025-01-15 14:22:17 +0100

    update tests

--- pipelines/matrix/tests/utils/test_pa_utils.py
+++ pipelines/matrix/tests/utils/test_pa_utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pandera as pa
 import pyspark.sql as ps
 import pytest
-from matrix.utils.pa_utils import ArrayType, Column, DataFrameSchema, Type, check_output
+from matrix.utils.pa_utils import Column, DataFrameSchema, check_output
 from pyspark.sql import types as T
 from pyspark.testing import assertDataFrameEqual
 
@@ -35,7 +35,7 @@ def spark_df(spark) -> ps.DataFrame:
 def test_function_without_typehint_throws_error(pandas_df):
     with pytest.raises(TypeError):
         # Given function without output typehint
-        @check_output(DataFrameSchema(columns={"id": Column(Type(int))}))
+        @check_output(DataFrameSchema(columns={"id": Column(int)}))
         def test_fn(pandas_df):
             return pandas_df
 
@@ -49,14 +49,14 @@ def test_function_without_typehint_throws_error(pandas_df):
     "schema",
     [
         # Schema validating int column
-        DataFrameSchema(columns={"int": Column(Type(int))}),
+        DataFrameSchema(columns={"int": Column(int)}),
         # Schema validating array column
-        DataFrameSchema(columns={"bool": Column(Type(bool))}),
+        DataFrameSchema(columns={"bool": Column(bool)}),
         # Schema validating array column
-        DataFrameSchema(columns={"list": Column(ArrayType(float))}),
+        DataFrameSchema(columns={"list": Column(list[float])}),
         # Schema validating multiple columns and uniques
         DataFrameSchema(
-            columns={"id": Column(Type(int)), "bool": Column(Type(bool)), "list": Column(ArrayType(float))},
+            columns={"id": Column(int), "bool": Column(bool)},
             unique=["id"],
         ),
     ],
@@ -76,7 +76,7 @@ def test_pandas_dataframe_valid(pandas_df, schema):
 
 @pytest.mark.parametrize(
     "schema",
-    [DataFrameSchema(columns={"int": Column(Type(int))})],
+    [DataFrameSchema(columns={"int": Column(int)})],
 )
 def test_pandas_dataframe_dict_valid(pandas_df, schema):
     # Given a function that validates the output schema
@@ -95,20 +95,20 @@ def test_pandas_dataframe_dict_valid(pandas_df, schema):
     "schema",
     [
         # Int column is not of type float
-        DataFrameSchema(columns={"int": Column(Type(float))}),
+        DataFrameSchema(columns={"int": Column(float)}),
         # Float array not int array
-        DataFrameSchema(columns={"list": Column(ArrayType(int))}),
+        DataFrameSchema(columns={"list": Column(int)}),
         # Validata primary key
         DataFrameSchema(columns={}, unique=["int"]),
     ],
 )
 def test_pandas_dataframe_failures(pandas_df, schema):
-    with pytest.raises(pa.errors.SchemaError):
-        # Given a function that validates the output schema
-        @check_output(schema)
-        def to_test(df: pd.DataFrame) -> pd.DataFrame:
-            return df
+    # Given a function that validates the output schema
+    @check_output(schema)
+    def to_test(df: pd.DataFrame) -> pd.DataFrame:
+        return df
 
+    with pytest.raises(pa.errors.SchemaError):
         # When invoking the test function
         to_test(pandas_df)
 
@@ -119,22 +119,22 @@ def test_pandas_dataframe_failures(pandas_df, schema):
     "schema",
     [
         # Schema validating int column
-        DataFrameSchema(columns={"int": Column(Type(int))}),
+        DataFrameSchema(columns={"int": Column(T.IntegerType())}),
         # Schema validating long column
-        DataFrameSchema(columns={"long": Column(Type(int, is64=True))}),
+        DataFrameSchema(columns={"long": Column(T.LongType)}),
         # Schema validating bool column
-        DataFrameSchema(columns={"bool": Column(Type(bool))}),
+        DataFrameSchema(columns={"bool": Column(T.BooleanType())}),
         # Schema validating float array column
-        DataFrameSchema(columns={"float_list": Column(ArrayType(float))}),
+        DataFrameSchema(columns={"float_list": Column(T.ArrayType(T.FloatType()))}),
         # Schema validating int array column
-        DataFrameSchema(columns={"double_list": Column(ArrayType(float, is64=True))}),
+        DataFrameSchema(columns={"double_list": Column(T.ArrayType(T.DoubleType()))}),
         # Schema validating multiple columns and uniques
         DataFrameSchema(
             columns={
-                "bool": Column(Type(bool)),
-                "long": Column(Type(int, is64=True)),
-                "float_list": Column(ArrayType(float)),
-                "double_list": Column(ArrayType(float, is64=True)),
+                "bool": Column(T.BooleanType()),
+                "long": Column(T.LongType()),
+                "float_list": Column(T.ArrayType(T.FloatType())),
+                "double_list": Column(T.ArrayType(T.DoubleType())),
             },
             unique=["id"],
         ),
@@ -157,24 +157,24 @@ def test_pyspark_dataframe_valid(spark_df, schema):
     "schema",
     [
         # Int is not long
-        DataFrameSchema(columns={"int": Column(Type(int, is64=True))}),
+        DataFrameSchema(columns={"int": Column(T.LongType())}),
         # Long is not int
-        DataFrameSchema(columns={"long": Column(Type(int))}),
+        DataFrameSchema(columns={"long": Column(T.IntegerType())}),
         # Float list is not long
-        DataFrameSchema(columns={"float_list": Column(ArrayType(float, is64=True))}),
+        DataFrameSchema(columns={"float_list": Column(T.ArrayType(T.LongType()))}),
         # Long list is not float
-        DataFrameSchema(columns={"double_list": Column(ArrayType(float))}),
+        DataFrameSchema(columns={"double_list": Column(T.ArrayType(T.FloatType()))}),
         # Schema validating multiple columns and uniques
         DataFrameSchema(columns={}, unique=["int"]),
     ],
 )
 def test_pyspark_dataframe_errors(spark_df, schema):
-    with pytest.raises(pa.errors.SchemaError):
-        # Given a function that validates the output schema
-        @check_output(schema)
-        def to_test(df: ps.DataFrame) -> ps.DataFrame:
-            return df
+    # Given a function that validates the output schema
+    @check_output(schema)
+    def to_test(df: ps.DataFrame) -> ps.DataFrame:
+        return df
 
+    with pytest.raises(pa.errors.SchemaError):
         # When invoking the test function
         to_test(spark_df)
 
