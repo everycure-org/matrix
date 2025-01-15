c46d1121342283975a09a88afc72e4a2abef3665
Jacques Vergine <jacques.vergine35@gmail.com>
2025-01-15 16:14:46 +0000

    Simplify pipeline definition

--- pipelines/matrix/src/matrix/pipelines/sampling/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/sampling/pipeline.py
@@ -10,29 +10,25 @@ logger = logging.getLogger(__name__)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    pipelines = [
-        pipeline(
-            [
-                ArgoNode(
-                    func=nodes.sample_nodes,
-                    inputs=[
-                        "params:sampling.configuration.sampler",
-                        "integration.prm.original_filtered_nodes",
-                        "integration.prm.original_filtered_edges",
-                        "modelling.raw.ground_truth.original_positives@spark",
-                        "modelling.raw.ground_truth.original_negatives@spark",
-                        "embeddings.feat.original_nodes",
-                    ],
-                    outputs=[
-                        "integration.prm.filtered_nodes",
-                        "modelling.raw.ground_truth.positives",
-                        "modelling.raw.ground_truth.negatives",
-                        "embeddings.feat.nodes",
-                    ],
-                    name="sample",
-                )
-            ]
-        )
-    ]
-
-    return sum(pipelines)
+    return pipeline(
+        [
+            ArgoNode(
+                func=nodes.sample_nodes,
+                inputs=[
+                    "params:sampling.configuration.sampler",
+                    "integration.prm.original_filtered_nodes",
+                    "integration.prm.original_filtered_edges",
+                    "modelling.raw.ground_truth.original_positives@spark",
+                    "modelling.raw.ground_truth.original_negatives@spark",
+                    "embeddings.feat.original_nodes",
+                ],
+                outputs=[
+                    "integration.prm.filtered_nodes",
+                    "modelling.raw.ground_truth.positives",
+                    "modelling.raw.ground_truth.negatives",
+                    "embeddings.feat.nodes",
+                ],
+                name="sample",
+            )
+        ]
+    )
