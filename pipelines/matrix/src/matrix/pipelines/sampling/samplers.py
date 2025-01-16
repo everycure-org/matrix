0111263fad370f0da84904cb4fea1b0c78e97d9e
Jacques Vergine <jacques.vergine35@gmail.com>
2025-01-16 17:46:25 +0000

    Remove acronyms from code

--- pipelines/matrix/src/matrix/pipelines/sampling/samplers.py
+++ pipelines/matrix/src/matrix/pipelines/sampling/samplers.py
@@ -12,26 +12,26 @@ class Sampler(ABC):
     @abstractmethod
     def sample(
         self,
-        kg_nodes: ps.DataFrame,
-        kg_edges: ps.DataFrame,
-        gt_p: ps.DataFrame,
-        gt_n: ps.DataFrame,
+        knowledge_graph_nodes: ps.DataFrame,
+        knowledge_graph_edges: ps.DataFrame,
+        ground_truth_positive_pair: ps.DataFrame,
+        ground_truth_negative_pair: ps.DataFrame,
         embeddings_nodes: ps.DataFrame,
     ) -> Tuple[ps.DataFrame, ps.DataFrame, ps.DataFrame, ps.DataFrame]:
         """
         Sample the KG, ground truth, and embeddings.
 
         Args:
-            kg_nodes: KG nodes
-            kg_edges: KG edges
-            gt_p: Ground truth positives
-            gt_n: Ground truth negatives
+            knowledge_graph_nodes: Knowledge graph nodes
+            knowledge_graph_edges: Knowledge graph edges
+            ground_truth_positive_pair: Ground truth positive pairs
+            ground_truth_negative_pair: Ground truth negative pairs
             embeddings_nodes: Embeddings nodes
 
         Returns:
-            sampled_kg_nodes: Sampled KG nodes
-            sampled_gt_p: Sampled ground truth positives
-            sampled_gt_n: Sampled ground truth negatives
+            sampled_knowledge_graph_nodes: Sampled knowledge graph nodes
+            sampled_ground_truth_positive_pair: Sampled ground truth positive pairs
+            sampled_ground_truth_negative_pair: Sampled ground truth negative pairs
             sampled_embeddings_nodes: Sampled embeddings nodes
         """
 
@@ -39,69 +39,89 @@ class Sampler(ABC):
 class GroundTruthSampler(Sampler):
     """
     A sampler that will sample from ground truth positives and negatives pairs, and then from the rest of the graph.
+
+    The aim is to keep the proportion of ground truth nodes in the output.
     """
 
     def __init__(
         self,
         ground_truth_positive_sample_ratio: float,
         ground_truth_negative_sample_ratio: float,
-        kg_nodes_sample_ratio: float,
+        knowledge_graph_nodes_sample_ratio: float,
         seed: int,
     ):
         self.ground_truth_positive_sample_ratio = ground_truth_positive_sample_ratio
         self.ground_truth_negative_sample_ratio = ground_truth_negative_sample_ratio
-        self.kg_nodes_sample_ratio = kg_nodes_sample_ratio
+        self.knowledge_graph_nodes_sample_ratio = knowledge_graph_nodes_sample_ratio
         self.seed = seed
 
     def sample(
         self,
-        kg_nodes: ps.DataFrame,
-        kg_edges: ps.DataFrame,
-        gt_p: ps.DataFrame,
-        gt_n: ps.DataFrame,
+        knowledge_graph_nodes: ps.DataFrame,
+        knowledge_graph_edges: ps.DataFrame,
+        ground_truth_positive_pair: ps.DataFrame,
+        ground_truth_negative_pair: ps.DataFrame,
         embeddings_nodes: ps.DataFrame,
     ) -> Tuple[ps.DataFrame, ps.DataFrame, ps.DataFrame, ps.DataFrame]:
-        logger.info(f"Received {kg_nodes.count()} KG nodes")
-        logger.info(f"Received {kg_edges.count()} KG edges")
-        logger.info(f"Received {gt_p.count()} ground truth positives")
-        logger.info(f"Received {gt_n.count()} ground truth negatives")
+        logger.info(f"Received {knowledge_graph_nodes.count()} knowledge graph nodes")
+        logger.info(f"Received {knowledge_graph_edges.count()} knowledge graph edges")
+        logger.info(f"Received {ground_truth_positive_pair.count()} ground truth positives")
+        logger.info(f"Received {ground_truth_negative_pair.count()} ground truth negatives")
         logger.info(f"Received {embeddings_nodes.count()} embeddings")
 
-        # ... select GT edges that are in the KG
-        gt_tp_in_kg = (
-            gt_p.join(kg_nodes.alias("source_nodes"), gt_p.source == f.col("source_nodes.id"))
-            .join(kg_nodes.alias("target_nodes"), gt_p.target == f.col("target_nodes.id"))
-            .select(gt_p["*"])
+        # ... select ground truth pairs that are in the knowledge graph
+        ground_truth_positive_pair_in_knowledge_graph = (
+            ground_truth_positive_pair.join(
+                knowledge_graph_nodes.alias("source_nodes"),
+                ground_truth_positive_pair.source == f.col("source_nodes.id"),
+            )
+            .join(
+                knowledge_graph_nodes.alias("target_nodes"),
+                ground_truth_positive_pair.target == f.col("target_nodes.id"),
+            )
+            .select(ground_truth_positive_pair["*"])
         )
-        gt_tn_in_kg = (
-            gt_n.join(kg_nodes.alias("source_nodes"), gt_n.source == f.col("source_nodes.id"))
-            .join(kg_nodes.alias("target_nodes"), gt_n.target == f.col("target_nodes.id"))
-            .select(gt_n["*"])
+        ground_truth_negative_pair_in_knowledge_graph = (
+            ground_truth_negative_pair.join(
+                knowledge_graph_nodes.alias("source_nodes"),
+                ground_truth_negative_pair.source == f.col("source_nodes.id"),
+            )
+            .join(
+                knowledge_graph_nodes.alias("target_nodes"),
+                ground_truth_negative_pair.target == f.col("target_nodes.id"),
+            )
+            .select(ground_truth_negative_pair["*"])
         )
 
-        # ... sample from GT edges
-        sampled_gt_p = gt_tp_in_kg.sample(self.ground_truth_positive_sample_ratio, self.seed)
-        sampled_gt_n = gt_tn_in_kg.sample(self.ground_truth_negative_sample_ratio, self.seed)
+        # ... sample from ground truth pairs
+        sampled_ground_truth_positive_pair = ground_truth_positive_pair_in_knowledge_graph.sample(
+            self.ground_truth_positive_sample_ratio, self.seed
+        )
+        sampled_ground_truth_negative_pair = ground_truth_negative_pair_in_knowledge_graph.sample(
+            self.ground_truth_negative_sample_ratio, self.seed
+        )
 
-        # ... select nodes attached to sampled GT edges
-        all_sampled_gt = sampled_gt_p.union(sampled_gt_n)
-        gt_node_ids = (
-            all_sampled_gt.withColumn("ids", f.array("source", "target"))
+        # ... select nodes attached to sampled ground truth pairs
+        all_sampled_ground_truth = sampled_ground_truth_positive_pair.union(sampled_ground_truth_negative_pair)
+        sampled_ground_truth_node_ids = (
+            all_sampled_ground_truth.withColumn("ids", f.array("source", "target"))
             .select(f.explode("ids").alias("id"))
             .distinct()
         )
 
         # ... sample from all KG nodes
-        sampled_kg_node_ids = kg_nodes.select("id").sample(self.kg_nodes_sample_ratio, self.seed)
+        sampled_knowledge_graph_node_ids = knowledge_graph_nodes.select("id").sample(
+            self.knowledge_graph_nodes_sample_ratio, self.seed
+        )
 
         # ... aggregate nodes and drop duplicated node ids
-        sampled_node_ids = sampled_kg_node_ids.union(gt_node_ids).distinct()
+        sampled_node_ids = sampled_knowledge_graph_node_ids.union(sampled_ground_truth_node_ids).distinct()
 
         # ... join on KG nodes
         sampled_nodes = (
-            kg_nodes.alias("kg_nodes")
-            .join(sampled_node_ids, sampled_node_ids.id == kg_nodes.id, "inner")
-            .select("kg_nodes.*")
+            knowledge_graph_nodes.alias("knowledge_graph_nodes")
+            .join(sampled_node_ids, sampled_node_ids.id == knowledge_graph_nodes.id, "inner")
+            .select("knowledge_graph_nodes.*")
         )
 
         # ... join on embeddings nodes
@@ -111,11 +131,16 @@ class GroundTruthSampler(Sampler):
             .select("embeddings_nodes.*")
         )
 
-        logger.info(f"Sampled {sampled_gt_p.count()} ground truth positives")
-        logger.info(f"Sampled {sampled_gt_n.count()} ground truth negatives")
-        logger.info(f"Sampled {gt_node_ids.count()} sampled unique nodes from GT")
-        logger.info(f"Sampled {sampled_kg_node_ids.count()} KG nodes")
+        logger.info(f"Sampled {sampled_ground_truth_positive_pair.count()} ground truth positives")
+        logger.info(f"Sampled {sampled_ground_truth_negative_pair.count()} ground truth negatives")
+        logger.info(f"Sampled {sampled_ground_truth_node_ids.count()} sampled unique nodes from ground truth")
+        logger.info(f"Sampled {sampled_knowledge_graph_node_ids.count()} KG nodes")
         logger.info(f"Sampled {sampled_nodes.count()} sampled nodes in total")
         logger.info(f"Sampled {sampled_embeddings_nodes.count()} embeddings nodes")
 
-        return sampled_nodes, sampled_gt_p, sampled_gt_n, sampled_embeddings_nodes
+        return (
+            sampled_nodes,
+            sampled_ground_truth_positive_pair,
+            sampled_ground_truth_negative_pair,
+            sampled_embeddings_nodes,
+        )
