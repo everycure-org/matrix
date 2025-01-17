2ade32adc308e484a4a88019492f157a11efd174
emil-k <emil.krause.44@gmail.com>
2025-01-17 12:15:11 +0100

    parametrize the tests

--- pipelines/matrix/tests/utils/test_git_utilities.py
+++ pipelines/matrix/tests/utils/test_git_utilities.py
@@ -4,17 +4,24 @@ import pytest
 from matrix.git_utils import has_legal_branch_name
 
 
+@pytest.mark.parametrize(
+    "branch_name",
+    [
+        "release/v1.0.0",
+        "release/v2.3.4-alpha",
+        "release/v10.15.20-beta123",
+    ],
+)
 @patch("matrix.git_utils.get_current_git_branch")
-def test_legal_branch_name_valid(mock_get_branch):
-    valid_branches = ["release/v1.0.0", "release/v2.3.4-alpha", "release/v10.15.20-beta123"]
-    for branch in valid_branches:
-        mock_get_branch.return_value = branch
-        assert has_legal_branch_name()
+def test_legal_branch_name_valid(mock_get_branch, branch_name):
+    """Test valid branch names."""
+    mock_get_branch.return_value = branch_name
+    assert has_legal_branch_name()
 
 
-@patch("matrix.git_utils.get_current_git_branch")
-def test_legal_branch_name_invalid(mock_get_branch):
-    invalid_branches = [
+@pytest.mark.parametrize(
+    "branch_name",
+    [
         "/release/v1",
         "release/v1",
         "release/1.0.0",
@@ -23,7 +30,10 @@ def test_legal_branch_name_invalid(mock_get_branch):
         "v1.0.0",
         "feature/awesome-feature",
         "release/v1.0.0--invalid",
-    ]
-    for branch in invalid_branches:
-        mock_get_branch.return_value = branch
-        assert not has_legal_branch_name()
+    ],
+)
+@patch("matrix.git_utils.get_current_git_branch")
+def test_legal_branch_name_invalid(mock_get_branch, branch_name):
+    """Test invalid branch names."""
+    mock_get_branch.return_value = branch_name
+    assert not has_legal_branch_name()
