dfd89638df2f7d1051fa5ea32df40a8c01909c0c
Jacques Vergine <jacques.vergine35@gmail.com>
2025-01-17 12:19:06 +0000

    Only allow one model per modelling pipeline (#924)
    * Change settings.py to have only one model
    
    * Adapt pipelines to get only one model
    
    * Check if branch starts with 'release' when triggering a data release. (#921)
    
    * incrementing spoke version (#914)
    
    * Remove run_inference parameter, and legacy parameters
    
    * Remove redundant square brackets around single tag
    
    * Add model names in comment for user friendliness
    
    * Simplify model_name tag
    
    * Remove modelling parameters from unused parameters tests, add a warning instead
    
    * Rename model to modelling in settings.py
    
    * Modelling cleanup - unify splits (#907)
    
    * add code
    
    * add code
    
    * add code
    
    * add code
    
    * remove settings loading in nodes
    
    * fix failing test
    
    * add schema
    
    * move enrich + update catalog
    
    * refactor modelling pipeline
    
    * refactpr matrix generation and evaluation
    
    * fixes
    
    * fix splits unit test
    
    * fix generate matrix pairs unit test
    
    * fix pair generator unit test
    
    * fix make splits unittest
    
    * fix splits schema
    
    * fix unit test
    
    * fix unit test
    
    * remove aggregation
    
    * rename n_splits
    
    * change name of make_splits to make_folds
    
    * modigy partial_folds to allow data argument rename
    
    * generate_pairs arg rename
    
    * fix test
    
    * change n_splits name in settings + add into docker compose CI settings
    
    ---------
    
    Co-authored-by: Laurens Vijnck <laurens@everycure.org>
    
    * Adapt pipelines to get only one model
    
    * Rename model to modelling in settings.py
    
    * Fix rebase duplicates
    
    * Fix typo model_namesuggestions -> model_name suggestions
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>
    
    * Fix model to model_name following conflicts
    
    * Remove duplicated line created during conflicts
    
    * Fixing code issues created during the conflicts
    
    * Change model to model_name
    
    * Run pre-commit hooks
    
    * Run make precommit
    
    * Set xg_ensemble as default model
    
    * Remove model name from catalog entries and tags
    
    * Fix modelling catalog .. typo
    
    * Fix evaluation_names type to List[str]
    
    * Only run inference on models trained on ground truth datasets
    
    Co-authored-by: Alexei Stepanenko <alexei.stepa@gmail.com>
    
    * Fix matrix_pipeline in inference
    
    * Remove run_inference parameter
    
    * Remove useless f-strings
    
    * fix pipeline
    
    * push fix
    
    ---------
    
    Co-authored-by: Emil <emil.krause.44@gmail.com>
    Co-authored-by: Jason Reilly <jdr0887@users.noreply.github.com>
    Co-authored-by: Alexei Stepanenko <alexei.stepa@gmail.com>
    Co-authored-by: Laurens Vijnck <laurens@everycure.org>
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>

--- pipelines/matrix/src/matrix/pipelines/matrix_generation/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/matrix_generation/pipeline.py
@@ -9,7 +9,7 @@ from . import nodes
 def create_pipeline(**kwargs) -> Pipeline:
     """Create matrix generation pipeline."""
 
-    models = settings.DYNAMIC_PIPELINES_MAPPING.get("modelling")
+    model_name = settings.DYNAMIC_PIPELINES_MAPPING.get("modelling")["model_name"]
 
     # Load cross-validation information
     cross_validation_settings = settings.DYNAMIC_PIPELINES_MAPPING.get("cross_validation")
@@ -58,45 +58,42 @@ def create_pipeline(**kwargs) -> Pipeline:
             )
         )
 
-        # For each model, create pipeline to generate pairs
-        for model in models.keys():
-            pipelines.append(
-                pipeline(
-                    [
-                        ArgoNode(
-                            func=nodes.make_predictions_and_sort,
-                            inputs=[
-                                "matrix_generation.feat.nodes@kg",
-                                f"matrix_generation.prm.fold_{fold}.matrix_pairs",
-                                f"modelling.{model}.fold_{fold}.model_input.transformers",
-                                f"modelling.{model}.fold_{fold}.models.model",
-                                f"params:modelling.{model}.model_options.model_tuning_args.features",
-                                "params:matrix_generation.treat_score_col_name",
-                                "params:matrix_generation.not_treat_score_col_name",
-                                "params:matrix_generation.unknown_score_col_name",
-                                "params:matrix_generation.matrix_generation_options.batch_by",
-                            ],
-                            outputs=f"matrix_generation.{model}.fold_{fold}.model_output.sorted_matrix_predictions@pandas",
-                            name=f"make_{model}_predictions_and_sort_fold_{fold}",
-                            argo_config=ARGO_GPU_NODE_MEDIUM,
-                        ),
-                        ArgoNode(
-                            func=nodes.generate_report,
-                            inputs=[
-                                f"matrix_generation.{model}.fold_{fold}.model_output.sorted_matrix_predictions@pandas",
-                                "params:matrix_generation.matrix_generation_options.n_reporting",
-                                "integration.int.drug_list.nodes.norm@pandas",
-                                "integration.int.disease_list.nodes.norm@pandas",
-                                "params:matrix_generation.treat_score_col_name",
-                                "params:matrix_generation.matrix",
-                                "params:matrix_generation.run",
-                            ],
-                            outputs=f"matrix_generation.{model}.fold_{fold}.reporting.matrix_report",
-                            name=f"generate_{model}_report_fold_{fold}",
-                        ),
-                    ],
-                    tags=model,
-                )
+        pipelines.append(
+            pipeline(
+                [
+                    ArgoNode(
+                        func=nodes.make_predictions_and_sort,
+                        inputs=[
+                            "matrix_generation.feat.nodes@kg",
+                            f"matrix_generation.prm.fold_{fold}.matrix_pairs",
+                            f"modelling.fold_{fold}.model_input.transformers",
+                            f"modelling.fold_{fold}.models.model",
+                            f"params:modelling.{model_name}.model_options.model_tuning_args.features",
+                            "params:matrix_generation.treat_score_col_name",
+                            "params:matrix_generation.not_treat_score_col_name",
+                            "params:matrix_generation.unknown_score_col_name",
+                            "params:matrix_generation.matrix_generation_options.batch_by",
+                        ],
+                        outputs=f"matrix_generation.fold_{fold}.model_output.sorted_matrix_predictions@pandas",
+                        name=f"make_predictions_and_sort_fold_{fold}",
+                        argo_config=ARGO_GPU_NODE_MEDIUM,
+                    ),
+                    ArgoNode(
+                        func=nodes.generate_report,
+                        inputs=[
+                            f"matrix_generation.fold_{fold}.model_output.sorted_matrix_predictions@pandas",
+                            "params:matrix_generation.matrix_generation_options.n_reporting",
+                            "integration.int.drug_list.nodes.norm@pandas",
+                            "integration.int.disease_list.nodes.norm@pandas",
+                            "params:matrix_generation.treat_score_col_name",
+                            "params:matrix_generation.matrix",
+                            "params:matrix_generation.run",
+                        ],
+                        outputs=f"matrix_generation.fold_{fold}.reporting.matrix_report",
+                        name=f"generate_report_fold_{fold}",
+                    ),
+                ],
             )
+        )
 
     return sum(pipelines)
