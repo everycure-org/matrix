370150b71ecfcae8e78e3e8c10a73c90e4aa7299
emil-k <emil.krause.44@gmail.com>
2025-01-20 14:34:26 +0100

    add unit test

--- pipelines/matrix/tests/utils/test_git_utilities.py
+++ pipelines/matrix/tests/utils/test_git_utilities.py
@@ -1,7 +1,7 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
-from matrix.git_utils import has_legal_branch_name
+from matrix.git_utils import git_tag_exists, has_legal_branch_name
 
 
 @pytest.mark.parametrize(
@@ -37,3 +37,19 @@ def test_legal_branch_name_invalid(mock_get_branch, branch_name):
     """Test invalid branch names."""
     mock_get_branch.return_value = branch_name
     assert not has_legal_branch_name()
+
+
+@pytest.mark.parametrize(
+    "tag, mock_stdout, expected_result",
+    [
+        ("v1.0.0", "v1.0.0\n", True),
+        ("v2.0.0", "", False),
+    ],
+)
+@patch("subprocess.run")
+def test_git_tag_exists(mock_subprocess_run, tag, mock_stdout, expected_result):
+    mock_result = MagicMock()
+    mock_result.stdout = mock_stdout
+    mock_subprocess_run.return_value = mock_result
+    result = git_tag_exists(tag)
+    assert result == expected_result
