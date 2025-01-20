7a3286c6059ac58fbfc6ccb0ada80fccd08f27fd
emil-k <emil.krause.44@gmail.com>
2025-01-20 17:07:29 +0100

    take out other network parameters as a variable

--- infra/deployments/hub/dev/variables.tf
+++ infra/deployments/hub/dev/variables.tf
@@ -16,3 +16,20 @@ variable "storage_bucket_name" {
 variable "project_id" {
   default = "mtrx-hub-dev-3of"
 }
+
+
+variable "network_name" {
+  default = "matrix-hub-dev-nw"
+}
+
+variable "k8s_subnetwork" {
+  default = "sn-hub-dev-us"
+}
+
+variable "k8s_pod_ip_range" {
+  default = "sn-hub-dev-us-pod-range"
+}
+
+variable "k8s_svc_ip_range" {
+  default = "sn-hub-dev-us-svc-range"
+}
