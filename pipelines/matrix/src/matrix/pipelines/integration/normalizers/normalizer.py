e1600ae6cb51567bb222d2103cf5da31766a2fbf
Piotr Kaniewski <115791652+piotrkan@users.noreply.github.com>
2025-01-21 17:06:24 +0000

    Add fixes to integration layer (#982)
    * improve pandera setup
    
    * add unique
    
    * ensure fix
    
    * proper type hinting
    
    * fixes
    
    * add tests
    
    * update
    
    * ensure dict output types work
    
    * update
    
    * Update pipelines/matrix/tests/utils/test_pa_utils.py
    
    * update tests
    
    * cleanup integration layer
    
    * do embeddings
    
    * solve modelling
    
    * clean
    
    * finish port to new decorator
    
    * update
    
    * fix
    
    * finish
    
    * clean biolink
    
    * simplify to use rtx only without normalization
    
    * add
    
    * extract schema
    
    * update
    
    * update
    
    * Update pipelines/matrix/conf/base/integration/catalog.yml
    
    * fix
    
    * update tests
    
    * update
    
    * fix
    
    * update
    
    * Update pipelines/matrix/src/matrix/pipelines/integration/normalizers/normalizer.py
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Laurens Vijnck <laurens@everycure.org>
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>

--- pipelines/matrix/src/matrix/pipelines/integration/normalizers/normalizer.py
+++ pipelines/matrix/src/matrix/pipelines/integration/normalizers/normalizer.py
@@ -7,6 +7,18 @@ class Normalizer(ABC):
     """Base class to represent normalizer strategies."""
 
     @abstractmethod
-    async def apply(df: pd.DataFrame, **kwargs) -> pd.DataFrame:
+    async def apply(self, df: pd.DataFrame, **kwargs) -> pd.DataFrame:
         """Function to apply normalization."""
         ...
+
+
+class NopNormalizer(ABC):
+    """Normalizer that does not perform normalization."""
+
+    async def apply(self, df: pd.DataFrame, **kwargs) -> pd.DataFrame:
+        """Function to apply normalization."""
+
+        df["normalized_id"] = df["id"]
+        df["normalized_id"] = df["normalized_id"].astype(pd.StringDtype())
+
+        return df
