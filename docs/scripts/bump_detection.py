d7d7e85e8f507b1a96dd3eb79e78bf1f5b2ba9e5
Siyan Luo <luosiyanemily@gmail.com>
2025-01-22 11:52:31 +0100

    refactor bump_detection.py

--- docs/scripts/bump_detection.py
+++ docs/scripts/bump_detection.py
@@ -5,34 +5,25 @@ import os
 import semver
 
 
-def bump_type():
+def get_generate_notes_flag():
     latest_official_release = os.getenv("latest_official_release", "v0.0.0").lstrip("v")
     tag_version = semver.Version.parse(latest_official_release)
 
     release = os.getenv("release", "v0.0.0").lstrip("v")
     release_version = semver.Version.parse(release)
 
-    if tag_version.major == release_version.major and tag_version.minor < release_version.minor:
-        bump_type = "minor"
-    elif (
+    if (
         tag_version.major == release_version.major
         and tag_version.minor == release_version.minor
         and tag_version.patch < release_version.patch
     ):
-        bump_type = "patch"
-    elif tag_version.major < release_version.major:
-        bump_type = "major"
-    else:
-        bump_type = "intermediate"
-
-    if bump_type != "patch":
-        generate_notes = True
-    else:
+        # This is a patch bump
         generate_notes = False
+    else:
+        generate_notes = True
 
-    # print(f"{bump_type=}")
     print(f"{generate_notes=}")
 
 
 if __name__ == "__main__":
-    bump_type()
+    get_generate_notes_flag()
