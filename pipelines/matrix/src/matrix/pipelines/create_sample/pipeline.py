346632c6d85cf94e4b2305576fb636873dbef71e
Jacques Vergine <jacques.vergine35@gmail.com>
2025-01-23 10:37:56 +0000

    Add local sample environment and sampling pipeline (#953)
    * Make dummy sample pipeline with original nodes and edges in catalog
    
    * Very logic for nodes and edges sampling
    
    * Disable tracking for sample pipeline
    
    * Add dummy parameters for sampling
    
    * Rename sample pipeline to sampling to avoid confusion with environment
    
    * Add ground truth dataset to catalog
    
    * Sampling working on its own
    
    * Refactor sampler to be passed as a yaml parameter
    
    * Simplify pipeline definition
    
    * Update sampling pipeling name in mlflow
    
    * Rename sampler attribute to ratio
    
    * Remove acronyms from code
    
    * Remove remaining acronym
    
    * Change sampler return to NamedTuple and comment out logs for now
    
    * Revert some main changes for tests to pass
    
    * Add sampler test
    
    * Prevent running the sampling pipeline outside of the sample environment
    
    * ADR - Data Fabricator refinement (#937)
    
    * Refine data fabricator v1
    
    * Fix minor typo
    
    * Add more thoughts short term and long term
    
    * Amend that users can replicate the cloud issue already
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>
    
    * WIP
    
    * Update ADR
    
    * Update docs/src/infrastructure/architecture_decision_records/refine_data_fabricator.md
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>
    
    * Add numbers to issues
    
    ---------
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>
    
    * Update ADR on sampling
    
    * Update ADR following discussion with Laurens
    
    * Update kedro pipeline name to test_sample
    
    * WIP - Test passing, need update of globals
    
    * create_sample pipeline working locally
    
    * WIP - test_sample working until modelling as not enough GT pairs
    
    * Add ground truth specific sampling
    
    * Amend ADR
    
    * Add xg_ensemble parameters to reduce run time
    
    * Update tests with ground truth parameter
    
    * Fix catalog and globals
    
    * Add documentation about sample environment
    
    * Update docs/src/onboarding/environments_overview.md
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>
    
    * Add note about release version
    
    * Update PR  template
    
    * Add pandera checks for sampling outputs
    
    ---------
    
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>

---
+++ pipelines/matrix/src/matrix/pipelines/create_sample/pipeline.py
@@ -0,0 +1,30 @@
+import logging
+
+from kedro.pipeline import Pipeline, pipeline
+
+from matrix.kedro4argo_node import ArgoNode
+
+from . import nodes
+
+logger = logging.getLogger(__name__)
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            ArgoNode(
+                func=nodes.sample_nodes,
+                inputs=[
+                    "params:create_sample.configuration.sampler",
+                    "integration.prm.original.filtered_nodes",
+                    "integration.prm.original.filtered_edges",
+                    "integration.int.ground_truth.edges.norm@spark",
+                ],
+                outputs=[
+                    "integration.prm.filtered_nodes",
+                    "integration.prm.filtered_edges",
+                ],
+                name="sample",
+            )
+        ]
+    )
