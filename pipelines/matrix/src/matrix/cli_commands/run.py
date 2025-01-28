5fb28fc3550c966327de65c6cefcb130a2b98626
emil-k <emil.krause.44@gmail.com>
2025-01-28 11:18:17 +0100

    remove intentional bug

--- pipelines/matrix/src/matrix/cli_commands/run.py
+++ pipelines/matrix/src/matrix/cli_commands/run.py
@@ -74,8 +74,6 @@ def run(tags: list[str], without_tags: list[str], env:str, runner: str, is_async
     pipeline_name = pipeline
     pipeline_obj = pipelines[pipeline_name]
 
-    #intentional bug
-    print(data)
     config = RunConfig(
         pipeline_obj=pipeline_obj,
         pipeline_name=pipeline_name,
