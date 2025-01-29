6df0792a919f941b7161d15220a358ad473a5d6d
Jacques Vergine <jacques.vergine35@gmail.com>
2025-01-29 17:56:07 +0000

    Move HTTP request from init to load

--- pipelines/matrix/src/matrix/datasets/github.py
+++ pipelines/matrix/src/matrix/datasets/github.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import requests
+from kedro.io.core import PROTOCOL_DELIMITER
 from kedro_datasets.pandas import CSVDataset
 
 
@@ -14,36 +15,54 @@ class GitHubReleaseCSVDataset(CSVDataset):
         fs_args: dict[str, Any] | None = None,
         metadata: dict[str, Any] | None = None,
     ) -> None:
+        self.repository_url = repository_url
+        self.release_name = release_name
+        self.release_asset_name = release_asset_name
+        self.load_args = load_args
+        self.fs_args = fs_args
+        self.metadata = metadata
+
+        pwal = repository_url.replace("github.com", "api.github.com/repos") + "/releases"
+
+        super().__init__(
+            # ... we are not getting the actual file path in the init to avoid HTTP requests during tests
+            filepath=pwal,
+            fs_args=fs_args,
+            load_args=load_args,
+            metadata=metadata,
+        )
+
+    def load(self):
         # ... get release API URL from repository URL
-        release_url = repository_url.replace("github.com", "api.github.com/repos") + "/releases"
+        release_url = self.repository_url.replace("github.com", "api.github.com/repos") + "/releases"
 
         # ... get releases' JSON
-        requests_headers = {"Accept": "application/json"}
-        if fs_args and "Authorization" in fs_args.keys():
-            requests_headers["Authorization"] = fs_args["Authorization"]
-        response = requests.get(release_url, headers=requests_headers)
+        request_headers = {"Accept": "application/json"}
+        if self.fs_args and "Authorization" in self.fs_args.keys():
+            request_headers["Authorization"] = self.fs_args["Authorization"]
+        response = requests.get(release_url, headers=request_headers)
         if response.status_code == 200:
             release_json = response.json()
         else:
             raise ValueError(f"Failed to get release JSON: {response.status_code} - {response.text}")
 
         # ... find the release
-        release = next((r for r in release_json if r["name"] == release_name), None)
+        release = next((r for r in release_json if r["name"] == self.release_name), None)
         if not release:
-            raise ValueError(f"Could not find release {release_name} in releases {[r['name'] for r in release_json]}")
-
-        # ... find the asset_id in release's assets
-        asset_id = next((a["id"] for a in release["assets"] if a["name"] == release_asset_name), None)
-        if not asset_id:
             raise ValueError(
-                f"Could not find file {release_asset_name} in release {release_name} assets {[a['name'] for a in release['assets']]}"
+                f"Could not find release {self.release_name} in releases {[r['name'] for r in release_json]}"
             )
 
-        asset_url = f"{release_url}/assets/{asset_id}"
+        # ... find the asset_id in release's assets
+        asset_id = next((a["id"] for a in release["assets"] if a["name"] == self.release_asset_name), None)
+        if not asset_id:
+            raise ValueError(
+                f"Could not find file {self.release_asset_name} in release {self.release_name} assets {[a['name'] for a in release['assets']]}"
+            )
 
-        super().__init__(
-            filepath=asset_url,
-            fs_args=fs_args,
-            load_args=load_args,
-            metadata=metadata,
-        )
+        # ... CSVDataset load function adds the protocol and protocol delimiter to filepath, so we need to remove them
+        # ... link to source code: https://github.com/kedro-org/kedro-plugins/blob/main/kedro-datasets/kedro_datasets/pandas/csv_dataset.py
+        asset_url = f"{release_url}/assets/{asset_id}".removeprefix(f"{self._protocol}{PROTOCOL_DELIMITER}")
+        self._filepath = asset_url
+
+        return super().load()
