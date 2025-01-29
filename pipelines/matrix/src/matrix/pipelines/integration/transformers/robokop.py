2abca1f23d438b981863383d924cb88cb0e2f3e6
Laurens Vijnck <laurens@everycure.org>
2025-01-29 14:03:44 +0100

    attempt

--- pipelines/matrix/src/matrix/pipelines/integration/transformers/robokop.py
+++ pipelines/matrix/src/matrix/pipelines/integration/transformers/robokop.py
@@ -3,22 +3,24 @@ import pyspark.sql as ps
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
 
-from matrix.pipelines.integration.filters import determine_most_specific_category
-
-from .transformer import GraphTransformer
-
 # FUTURE: We should likely not need to rename these columns as we do below
 # However, KGX is currently not as performant as we need it to be thus
 # we do it manually with spark. This ought to be improved, e.g. by
 # adding parquet support to KGX.
 # or implementing a custom KGX version that leverages spark for higher performance
 # https://github.com/everycure-org/matrix/issues/474
+from matrix.pipelines.integration import schema
+from matrix.pipelines.integration.filters import determine_most_specific_category
+from matrix.utils.pa_utils import check_output
+
+from .transformer import GraphTransformer
 
 ROBOKOP_SEPARATOR = "\x1f"
 
 
 class RobokopTransformer(GraphTransformer):
-    def transform_nodes(self, nodes_df: ps.DataFrame, biolink_categories_df: pd.DataFrame, **kwargs) -> ps.DataFrame:
+    #  @check_output(schema.BIOLINK_KG_NODE_SCHEMA)
+    def transform_nodes(self, nodes_df: ps.DataFrame, **kwargs) -> ps.DataFrame:
         """Transform Robokop nodes to our target schema.
 
         Args:
@@ -36,15 +38,16 @@ class RobokopTransformer(GraphTransformer):
             .withColumn("equivalent_identifiers",            F.split(F.col("equivalent_identifiers:string[]"), ROBOKOP_SEPARATOR))
             .withColumn("labels",                            F.array(F.col("all_categories")))
             .withColumn("publications",                      F.lit(None).cast(T.ArrayType(T.StringType())))
-            .withColumn("international_resource_identifier", F.lit(None))
+            .withColumn("international_resource_identifier", F.lit(None).cast(T.StringType()))
             .withColumnRenamed("id:ID", "id")
             .withColumnRenamed("name:string", "name")
             .withColumnRenamed("description:string", "description")
             # getting most specific category
-            .transform(determine_most_specific_category, biolink_categories_df)
+            .transform(determine_most_specific_category)
         )
         # fmt: on
 
+    # @check_output(schema.BIOLINK_KG_EDGE_SCHEMA)
     def transform_edges(self, edges_df: ps.DataFrame, **kwargs) -> ps.DataFrame:
         """Transform Robokop edges to our target schema.
 
@@ -59,7 +62,7 @@ class RobokopTransformer(GraphTransformer):
             edges_df
             .withColumnRenamed("subject:START_ID",                  "subject")
             .withColumnRenamed("predicate:TYPE",                    "predicate")
-            .withColumnRenamed("object:END_ID",                     "object")
+            .withColumnRenamed("object:END_ID",                      "object")
             .withColumnRenamed("knowledge_level:string",            "knowledge_level")
             .withColumnRenamed("primary_knowledge_source:string",   "primary_knowledge_source")
             .withColumnRenamed("object_aspect_qualifier:string",    "object_aspect_qualifier")
