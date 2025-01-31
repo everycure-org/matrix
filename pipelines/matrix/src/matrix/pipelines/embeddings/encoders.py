df926aeb81c6cc4c2dc94e7aa5ab72592f7e90a8
Siyan Luo <luosiyanemily@gmail.com>
2025-01-31 14:32:08 +0100

    use RandomizedEncoder for dummy embeddings

--- pipelines/matrix/src/matrix/pipelines/embeddings/encoders.py
+++ pipelines/matrix/src/matrix/pipelines/embeddings/encoders.py
@@ -104,9 +104,7 @@ class RandomizedEncoder(AttributeEncoder):
         """
         df = df.copy()
         # Generate random embeddings
-        # df["text_to_embed"] = df[input_features].apply(lambda row: "".join(row)[0:max_input_len], axis=1)
         df["embedding"] = [np.random.rand(self._embedding_dim).astype(np.float32) for _ in range(len(df))]
-        # df = df.drop(columns=["text_to_embed"])
         return df
 
 
