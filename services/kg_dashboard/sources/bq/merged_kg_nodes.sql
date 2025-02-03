3de2353fa94d36ff0422876100795e7ed6c6e3df
Kevin Schaper <kevinschaper@gmail.com>
2025-02-03 15:38:27 -0800

    update node & edge dashboard sql to flatten nested upstream_data_source columns at query time

--- services/kg_dashboard/sources/bq/merged_kg_nodes.sql
+++ services/kg_dashboard/sources/bq/merged_kg_nodes.sql
@@ -1,7 +1,8 @@
 SELECT
     category, 
     SPLIT(id, ':')[OFFSET(0)] as prefix,
-    upstream_data_source.list AS upstream_data_source,
+    STRING_AGG(DISTINCT upstream_data_source.list[SAFE_OFFSET(0)].element, ',' ORDER BY upstream_data_source.list[SAFE_OFFSET(0)].element) AS upstream_data_source,
+    '${release_version}' AS release_version,
     count(*) as count
 FROM  `mtrx-hub-dev-3of.release_${release_version}.nodes`
 group by all
