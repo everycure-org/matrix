df1ffd6f64ff9fb3e6d32040890ea6749cb92def
Pascal Bro <pascal@everycure.org>
2025-02-03 15:38:03 +0100

    Update variables.tf

--- infra/modules/stacks/data_release_zone/variables.tf
+++ infra/modules/stacks/data_release_zone/variables.tf
@@ -1,5 +1,3 @@
-// NOTE: This configuration was partially generated using AI assistance.
-
 variable "project_id" {
   description = "The ID of the Google Cloud project where resources will be created"
   type        = string
