1e1fae9cc9ea2d8dc19d3aade508020e76623d4e
Kathleen Carter <163005214+eKathleenCarter@users.noreply.github.com>
2025-02-04 11:47:34 -0500

    Update metric_utilities.py
    Adding if statement to only log to mlflow if its running. otherwise, print to logger

--- pipelines/matrix/src/matrix/utils/metric_utilities.py
+++ pipelines/matrix/src/matrix/utils/metric_utilities.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from typing import Union
 
 import mlflow
@@ -7,7 +8,18 @@ logger = logging.getLogger(__name__)
 
 
 def log_metric(context, name, value: Union[int, float]):
+    disable_mlflow = (
+        os.getenv("KEDRO_HOOKS_DISABLE_MLFLOW", "false").lower() == "true"
+        or os.getenv("KEDRO_HOOKS_DISABLE_MLFLOW_KEDRO", "false").lower() == "true"
+        or os.getenv("KEDRO_HOOKS_DISABLE_RELEASE", "false").lower() == "true"
+    )
+
     logger.info(f"{name}: {value}")
     # TODO: can we figure out context so we don't have to pass it.
     # What kedro node am I running? Can I set this as my "context" variable
     mlflow.log_metric(f"{context}/{name}", value)
+
+    if not disable_mlflow:
+        mlflow.log_metric(f"{context}/{name}", value)
+    else:
+        logger.info("MLFlow logging is disabled via environment variables.")
