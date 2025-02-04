4c0c8aac3fe82609443721dadfa173f234da4232
Pascal Brokmeier <pascal@everycure.org>
2025-02-04 14:10:09 +0100

    bump

--- services/kg_dashboard/sources/bq/merged_kg_nodes.sql
+++ services/kg_dashboard/sources/bq/merged_kg_nodes.sql
@@ -2,7 +2,7 @@ SELECT
     category, 
     SPLIT(id, ':')[OFFSET(0)] as prefix,
     STRING_AGG(DISTINCT upstream_data_source.list[SAFE_OFFSET(0)].element, ',' ORDER BY upstream_data_source.list[SAFE_OFFSET(0)].element) AS upstream_data_source,
-    '${release_version}' AS release_version,
+    '${bq_release_version}' AS release_version,
     count(*) as count
-FROM  `mtrx-hub-dev-3of.release_${release_version}.nodes`
+FROM  `mtrx-hub-dev-3of.release_${bq_release_version}.nodes`
 group by all
