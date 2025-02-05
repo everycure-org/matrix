cfac8e1ef158a58760340108bc75cea4d5f3b6ee
Siyan Luo <89979939+Siyan-Luo@users.noreply.github.com>
2025-02-05 09:32:26 +0100

    Update pipelines/matrix/src/matrix/git_utils.py
    Fix return type hint
    
    Co-authored-by: Oliver W. <4733178+oliverw1@users.noreply.github.com>

--- pipelines/matrix/src/matrix/git_utils.py
+++ pipelines/matrix/src/matrix/git_utils.py
@@ -64,7 +64,7 @@ def get_latest_minor_release(releases_list: List[str]) -> str:
     return original_to_mapped[f"v{latest_minor_release}"]
 
 
-def correct_non_semver_compliant_release_names(releases_list: List[str]) -> tuple[List[str], dict[str, str]]:
+def correct_non_semver_compliant_release_names(releases_list: List[str]) -> dict[str, str]:
 """Map versions that aren't semver compliant to compliant ones."""
     mapper = {"v0.1": "v0.1.0", "v0.2": "v0.2.0"}
     original_to_mapped = {mapper.get(release, release): release for release in releases_list}
