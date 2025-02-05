43c622d53971de8ff3642a7c076bc2c6079c114a
Jacques Vergine <jacques.vergine35@gmail.com>
2025-02-05 17:26:10 +0000

    Remove quality_control flag from settings.py

--- pipelines/matrix/src/matrix/settings.py
+++ pipelines/matrix/src/matrix/settings.py
@@ -46,7 +46,7 @@ DYNAMIC_PIPELINES_MAPPING = generate_dynamic_pipeline_mapping(
             "n_cross_val_folds": 3,
         },
         "integration": [
-            {"name": "rtx_kg2", "integrate_in_kg": True, "quality_control": True},
+            {"name": "rtx_kg2", "integrate_in_kg": True},
             # {"name": "spoke"},
             # {"name": "robokop"},
             {"name": "ec_medical_team", "integrate_in_kg": True},
