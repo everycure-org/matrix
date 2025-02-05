230cbd9d56e0abfe2d5cdaa488c2bf018eee037a
Siyan Luo <89979939+Siyan-Luo@users.noreply.github.com>
2025-02-05 11:02:25 +0100

    Debug/Notes and articles generation (#1059)
    * revert change
    
    * revert change
    
    * print variable & test patch bump
    
    * correct logic in bump_detection
    
    * test minor bump
    
    * cancel comments out
    
    * reverse changes on argo
    
    * get the official releases not tags
    
    * get the official releases not tags
    
    * only generate notes for major and minor bump, not for patch or any intermediate
    
    * generate notes base off latest minor release
    
    * forbade intermediate minor bump
    
    * forbade intermediate minor bump
    
    * forbade intermediate minor bump
    
    * forbade intermediate minor bump
    
    * add semver to requirements
    
    * delete importing not needed
    
    * iterate get_latest_minor_release function with semver
    
    * add unit tests
    
    * add unit tests
    
    * improve functions and unit tests
    
    * add test for bump_detection
    
    * remove comments
    
    * fix: allow test_utils to run
    
    * improvement: name=ing and dependency inversion
    
    * test bump_detection
    
    * uncomment out
    
    * add documentation
    
    * add documentation
    
    * Update apps/matrix-cli/src/matrix_cli/components/utils.py
    
    Fix return type hint
    
    Co-authored-by: Oliver W. <4733178+oliverw1@users.noreply.github.com>
    
    * Update apps/matrix-cli/src/matrix_cli/components/utils.py
    
    Improve docstring
    
    Co-authored-by: Oliver W. <4733178+oliverw1@users.noreply.github.com>
    
    * Update docs/src/infrastructure/runbooks/04_notes_generation.md
    
    add docs
    
    Co-authored-by: Oliver W. <4733178+oliverw1@users.noreply.github.com>
    
    * Update pipelines/matrix/src/matrix/git_utils.py
    
    Improve docstring
    
    Co-authored-by: Oliver W. <4733178+oliverw1@users.noreply.github.com>
    
    * Update pipelines/matrix/src/matrix/git_utils.py
    
    Fix return type hint
    
    Co-authored-by: Oliver W. <4733178+oliverw1@users.noreply.github.com>
    
    * fix indention
    
    * rename header
    
    * correct syntax
    
    ---------
    
    Co-authored-by: Oliver Willekens <4733178+oliverw1@users.noreply.github.com>

--- pipelines/matrix/src/matrix/git_utils.py
+++ pipelines/matrix/src/matrix/git_utils.py
@@ -1,5 +1,8 @@
 import re
 import subprocess
+from typing import TYPE_CHECKING, List
+
+import semver
 
 BRANCH_NAME_REGEX = r"^release/v\d+\.\d+\.\d+(-[a-zA-Z0-9]+)?$"
 
@@ -39,3 +42,30 @@ def has_unpushed_commits() -> bool:
 def git_tag_exists(tag: str) -> bool:
     result = subprocess.check_output(f"git ls-remote --tags origin {tag}", shell=True, text=True)
     return tag in result
+
+
+def get_releases() -> List[str]:
+    return (
+        (subprocess.check_output(["gh", "release", "list", "--json", "tagName", "--jq", ".[].tagName"], text=True))
+        .strip("\n")
+        .split("\n")
+    )
+
+
+def get_latest_minor_release(releases_list: List[str]) -> str:
+    original_to_mapped = correct_non_semver_compliant_release_names(releases_list)
+    parsed_versions = [semver.Version.parse(v.lstrip("v")) for v in original_to_mapped]
+    latest_major_minor = max(parsed_versions)
+    # Find the earliest release in the latest major-minor series.
+    latest_minor_release = min(
+        [v for v in parsed_versions if v.major == latest_major_minor.major and v.minor == latest_major_minor.minor]
+    )
+
+    return original_to_mapped[f"v{latest_minor_release}"]
+
+
+def correct_non_semver_compliant_release_names(releases_list: List[str]) -> dict[str, str]:
+    """Map versions that aren't semver compliant to compliant ones."""
+    mapper = {"v0.1": "v0.1.0", "v0.2": "v0.2.0"}
+    original_to_mapped = {mapper.get(release, release): release for release in releases_list}
+    return original_to_mapped
