3ec070830b25ff25fdc6064fbe381aaf6e160cd5
Jacques Vergine <jacques.vergine35@gmail.com>
2025-02-07 09:54:55 +0000

    Update following Laurens' comments

--- pipelines/matrix/src/matrix/datasets/gcp.py
+++ pipelines/matrix/src/matrix/datasets/gcp.py
@@ -160,11 +160,10 @@ class SparkDatasetWithBQExternalTable(LazySparkDataset):
         table = bigquery.Table(f"{self._dataset_id}.{self._table}")
         table.labels = self._labels
         table.external_data_configuration = external_config
-        if not self._table_exists(table):
-            table = self._client.create_table(table)
-        else:
-            # just to update the "last modified" timestamp of the table
-            table = self._client.update_table(table, fields=["labels"])
+        try:
+            self._client.create_table(table, exists_ok=False)
+        except exceptions.Conflict:
+            self._client.update_table(table, fields=["labels"])
 
     def _create_dataset(self) -> str:
         try:
@@ -189,13 +188,6 @@ class SparkDatasetWithBQExternalTable(LazySparkDataset):
             Sanitized name
         """
         return re.sub(r"[^a-zA-Z0-9_]", "_", str(name))
-
-    def _table_exists(self, table: bigquery.Table) -> bool:
-        try:
-            self._client.get_table(table.reference)
-            return True
-        except exceptions.NotFound:
-            return False
 
 
 class GoogleSheetsDataset(AbstractVersionedDataset[pd.DataFrame, pd.DataFrame]):
