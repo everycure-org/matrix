5cd4b25b6e2cdb1cb6306f359da59b5680af89e6
Siyan Luo <luosiyanemily@gmail.com>
2025-02-07 14:00:38 +0100

    debug tests for ci pass

--- pipelines/matrix/tests/pipelines/test_encoders.py
+++ pipelines/matrix/tests/pipelines/test_encoders.py
@@ -1,11 +1,8 @@
-from unittest.mock import Mock
-
 from matrix.pipelines.embeddings.encoders import LangChainEncoder
 
 
 def test_dummy():
-    mock = Mock()
-    encoder = LangChainEncoder(dimensions=10, encoder=mock, batch_size=2)
+    encoder = LangChainEncoder(dimensions=10, batch_size=2)
     result = list(encoder.apply(list(range(7))))
     print(result)
     assert False
