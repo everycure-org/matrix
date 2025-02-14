5461eefa4236f8cbb98dad8a3fec227d419c3046
Laurens Vijnck <laurens@everycure.org>
2025-02-14 15:02:31 +0100

    apply selection in transformer

--- pipelines/matrix/src/matrix/pipelines/integration/transformers/transformer.py
+++ pipelines/matrix/src/matrix/pipelines/integration/transformers/transformer.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import Dict
+from typing import Dict, List
 
 import pyspark.sql as ps
 
-from matrix.pipelines.integration import schema
+from matrix.pipelines.integration.schema import BIOLINK_KG_EDGE_SCHEMA, BIOLINK_KG_NODE_SCHEMA
 
 
 class Transformer(ABC):
@@ -13,13 +13,28 @@ class Transformer(ABC):
     def transform(self, **kwargs) -> Dict[str, ps.DataFrame]: ...
 
 
+def select_if(df: ps.DataFrame, cols: List[str], cond: bool):
+    if cond:
+        return df.select(*cols)
+
+    return df
+
+
 class GraphTransformer(Transformer):
     """Transformer for graph input sources."""
 
+    def __init__(self, select_cols: str = True) -> None:
+        self._select_cols = select_cols
+        super().__init__()
+
     def transform(self, nodes_df: ps.DataFrame, edges_df: ps.DataFrame, **kwargs) -> Dict[str, ps.DataFrame]:
         return {
-            "nodes": self.transform_nodes(nodes_df, **kwargs).select(*schema.BIOLINK_KG_NODE_SCHEMA.columns.keys()),
-            "edges": self.transform_edges(edges_df, **kwargs).select(*schema.BIOLINK_KG_EDGE_SCHEMA.columns.keys()),
+            "nodes": self.transform_nodes(nodes_df, **kwargs).transform(
+                select_if, cols=BIOLINK_KG_NODE_SCHEMA.columns.keys(), cond=self._select_cols
+            ),
+            "edges": self.transform_edges(edges_df, **kwargs).transform(
+                select_if, cols=BIOLINK_KG_EDGE_SCHEMA.columns.keys(), cond=self._select_cols
+            ),
         }
 
     @abstractmethod
