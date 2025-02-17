554f930e4f13e6cb5bbdeb5f05a091b66b9ae1d4
Kathleen Carter <163005214+eKathleenCarter@users.noreply.github.com>
2025-02-17 10:53:48 -0500

    Update metric_utilities.py

--- pipelines/matrix/src/matrix/utils/metric_utilities.py
+++ pipelines/matrix/src/matrix/utils/metric_utilities.py
@@ -6,20 +6,20 @@ import mlflow
 
 logger = logging.getLogger(__name__)
 
+DISABlE_MLFLOW = (
+    os.getenv("KEDRO_HOOKS_DISABLE_MLFLOW", "false").lower() == "true"
+    or os.getenv("KEDRO_HOOKS_DISABLE_MLFLOW_KEDRO", "false").lower() == "true"
+    or os.getenv("KEDRO_HOOKS_DISABLE_RELEASE", "false").lower() == "true"
+)
+
 
 def log_metric(context, name, value: Union[int, float]):
-    disable_mlflow = (
-        os.getenv("KEDRO_HOOKS_DISABLE_MLFLOW", "false").lower() == "true"
-        or os.getenv("KEDRO_HOOKS_DISABLE_MLFLOW_KEDRO", "false").lower() == "true"
-        or os.getenv("KEDRO_HOOKS_DISABLE_RELEASE", "false").lower() == "true"
-    )
-
     logger.info(f"{name}: {value}")
     # TODO: can we figure out context so we don't have to pass it.
     # What kedro node am I running? Can I set this as my "context" variable
     mlflow.log_metric(f"{context}/{name}", value)
 
-    if not disable_mlflow:
+    if not DISABlE_MLFLOW:
         mlflow.log_metric(f"{context}/{name}", value)
     else:
-        logger.info("MLFlow logging is disabled via environment variables.")
+        logger.debug("MLFlow logging is disabled via environment variables.")
