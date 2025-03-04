195e3eb119ee91e5df3d83ec9ed93a75ac699126
Amy Ford <amy@everycure.org>
2025-03-04 17:23:02 +0000

    Uupdate all other inputs/outputs and catalog

--- pipelines/matrix/src/matrix/pipelines/integration/nodes.py
+++ pipelines/matrix/src/matrix/pipelines/integration/nodes.py
@@ -100,7 +100,6 @@ def union_and_deduplicate_nodes(retrieve_most_specific_category: bool, *nodes, c
     # next we need to apply a number of transformations to the nodes to ensure grouping by id did not select wrong information
     # this is especially important if we integrate multiple KGs
 
-    # Is this integration or filtering?
     if retrieve_most_specific_category:
         unioned_datasets = unioned_datasets.transform(determine_most_specific_category)
 
