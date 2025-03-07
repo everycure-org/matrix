c1e28025067c4ac7023599a8dab33f693031b807
Siyan Luo <luosiyanemily@gmail.com>
2025-03-07 14:29:01 +0100

    remove debugging warning

--- pipelines/matrix/src/matrix/pipelines/batch/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/batch/pipeline.py
@@ -204,8 +204,6 @@ def cache_miss_resolver_wrapper(
         logger.info(f"embedding batch with key: {batch[0]}")
         transformed = await transformer.apply(batch)
         logger.info(f"received embedding for batch with key: {batch[0]}")
-        if not all(isinstance(val, str) for val in transformed):
-            logger.warning(f"Transformed data contains non-string values: {transformed}")
         return pa.table([batch, transformed, [api] * len(batch)], schema=cache_schema).to_pandas()
 
     def prep(
