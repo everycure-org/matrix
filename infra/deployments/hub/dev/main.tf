791fd07a5d4a456bc7d7e01bc93985b2fedb1e00
Emil <emil.krause.44@gmail.com>
2025-03-10 09:58:55 +0100

    Use DNS module's variables as outputs, not data, to create a dependency. (#1254)
    * use DNS's module output in other modules, so it creates a dependency, as opposed to the data resource.
    
    * Remove commented data resource.

--- infra/deployments/hub/dev/main.tf
+++ infra/deployments/hub/dev/main.tf
@@ -9,7 +9,6 @@ module "compute_cluster" {
   environment       = "dev"
   gitops_repo_url   = var.gitops_repo_url
   gitops_repo_creds = var.gitops_repo_creds
-  dns_zone          = module.dns.dns_zone
   k8s_secrets       = var.k8s_secrets
   bucket_name       = "mtrx-us-central1-hub-dev-storage"
 }
@@ -24,6 +23,7 @@ module "data_release_zone" {
   source                = "../../../modules/stacks/data_release_zone"
   project_id            = var.project_id
   region                = var.default_region
-  dns_managed_zone_name = module.dns.dns_zone
+  dns_managed_zone_name = module.dns.dns_zone_name
+  dns_name              = module.dns.dns_name
   environment           = "dev"
 }
