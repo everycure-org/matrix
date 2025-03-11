7a08bbfbbb49b5bdfd3230623e40ac7313722812
piotrkan <piotr@everycure.org>
2025-03-11 12:40:12 +0000

    robokop exp

--- pipelines/matrix/src/matrix/settings.py
+++ pipelines/matrix/src/matrix/settings.py
@@ -46,9 +46,9 @@ DYNAMIC_PIPELINES_MAPPING = generate_dynamic_pipeline_mapping(
             "n_cross_val_folds": 3,
         },
         "integration": [
-            {"name": "rtx_kg2", "integrate_in_kg": True},
+            # {"name": "rtx_kg2", "integrate_in_kg": True},
             # {"name": "spoke", "integrate_in_kg": True},
-            # {"name": "robokop", "integrate_in_kg": True},
+            {"name": "robokop", "integrate_in_kg": True},
             {"name": "ec_medical_team", "integrate_in_kg": True},
             {"name": "drug_list", "integrate_in_kg": False, "has_edges": False},
             {"name": "disease_list", "integrate_in_kg": False, "has_edges": False},
