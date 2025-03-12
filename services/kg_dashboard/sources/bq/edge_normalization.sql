ccdaa7dc03d09973225f9d71dc94194f72b3afad
Kevin Schaper <kevinschaper@gmail.com>
2025-03-11 18:47:17 -0700

    Fix subject repeatedly being selected on the object side of unions

--- services/kg_dashboard/sources/bq/edge_normalization.sql
+++ services/kg_dashboard/sources/bq/edge_normalization.sql
@@ -8,18 +8,18 @@ SELECT
   count(*) AS count
 FROM (
   SELECT 
-    subject AS id, 
-    original_subject AS original_id, 
+    subject AS id,
+    original_subject AS original_id,
     category,
-    subject_normalization_success AS normalization_success,        
+    subject_normalization_success AS normalization_success,
     CASE WHEN original_subject = subject THEN true ELSE false END AS no_normalization_change 
   FROM 
     `mtrx-hub-dev-3of.release_${bq_release_version}.rtx_kg2_edges_normalized`
     JOIN `mtrx-hub-dev-3of.release_${bq_release_version}.rtx_kg2_nodes_normalized` ON subject = id
-  UNION DISTINCT
+  UNION ALL
   SELECT 
-    subject AS id, 
-    original_object AS original_id, 
+    object AS id,
+    original_object AS original_id,
     category, 
     object_normalization_success AS normalization_success,
     CASE WHEN original_object = object THEN true ELSE false END AS no_normalization_change 
@@ -41,8 +41,8 @@ SELECT
   count(*) AS count
 FROM (
   SELECT 
-    subject AS id, 
-    original_subject AS original_id, 
+    subject AS id,
+    original_subject AS original_id,
     category,
     subject_normalization_success AS normalization_success,
     CASE WHEN original_subject = subject THEN true ELSE false END AS no_normalization_change 
@@ -51,7 +51,7 @@ FROM (
     JOIN `mtrx-hub-dev-3of.release_${bq_release_version}.robokop_nodes_normalized` ON subject = id
   UNION DISTINCT
   SELECT 
-    subject AS id, 
+    object AS id,
     original_object AS original_id, 
     category,
     object_normalization_success AS normalization_success,
@@ -74,8 +74,8 @@ SELECT
   count(*) AS count
 FROM (
   SELECT 
-    subject AS id, 
-    original_subject AS original_id, 
+    subject AS id,
+    original_subject AS original_id,
     '' as category,
     subject_normalization_success AS normalization_success,
     CASE WHEN original_subject = subject THEN true ELSE false END AS no_normalization_change 
@@ -83,8 +83,8 @@ FROM (
     `mtrx-hub-dev-3of.release_${bq_release_version}.ground_truth_edges_normalized`
   UNION DISTINCT
   SELECT 
-    subject AS id, 
-    original_object AS original_id, 
+    object AS id,
+    original_object AS original_id,
     '' as category,
     object_normalization_success AS normalization_success,
     CASE WHEN original_object = object THEN true ELSE false END AS no_normalization_change 
@@ -105,8 +105,8 @@ SELECT
   count(*) AS count
 FROM (
   SELECT 
-    subject AS id, 
-    original_subject AS original_id, 
+    subject AS id,
+    original_subject AS original_id,
     category,
     subject_normalization_success AS normalization_success,
     CASE WHEN original_subject = subject THEN true ELSE false END AS no_normalization_change 
@@ -115,8 +115,8 @@ FROM (
     JOIN `mtrx-hub-dev-3of.release_${bq_release_version}.rtx_kg2_nodes_normalized` ON subject = id
   UNION DISTINCT
   SELECT 
-    subject AS id, 
-    original_object AS original_id, 
+    object AS id, 
+    original_object AS original_id,
     category,
     object_normalization_success AS normalization_success,
     CASE WHEN original_object = object THEN true ELSE false END AS no_normalization_change 
@@ -125,8 +125,8 @@ FROM (
     JOIN `mtrx-hub-dev-3of.release_${bq_release_version}.rtx_kg2_nodes_normalized` ON object = id
   UNION DISTINCT
   SELECT 
-    subject AS id, 
-    original_subject AS original_id, 
+    subject AS id,
+    original_subject AS original_id,
     category,
     subject_normalization_success AS normalization_success,
     CASE WHEN original_subject = subject THEN true ELSE false END AS no_normalization_change
@@ -135,8 +135,8 @@ FROM (
     JOIN `mtrx-hub-dev-3of.release_${bq_release_version}.robokop_nodes_normalized` ON subject = id
   UNION DISTINCT
   SELECT 
-    subject AS id, 
-    original_object AS original_id, 
+    object AS id,
+    original_object AS original_id,
     category,
     object_normalization_success AS normalization_success,
     CASE WHEN original_object = object THEN true ELSE false END AS no_normalization_change
@@ -145,8 +145,8 @@ FROM (
     JOIN `mtrx-hub-dev-3of.release_${bq_release_version}.robokop_nodes_normalized` ON object = id
   UNION DISTINCT
   SELECT 
-    subject AS id, 
-    original_subject AS original_id, 
+    subject AS id,
+    original_subject AS original_id,
     '' AS category,
     subject_normalization_success AS normalization_success,
     CASE WHEN original_subject = subject THEN true ELSE false END AS no_normalization_change
@@ -154,8 +154,8 @@ FROM (
     `mtrx-hub-dev-3of.release_${bq_release_version}.ground_truth_edges_normalized`        
   UNION DISTINCT
   SELECT 
-    subject AS id, 
-    original_object AS original_id, 
+    object AS id,
+    original_object AS original_id,
     '' AS category,
     object_normalization_success AS normalization_success,
     CASE WHEN original_object = object THEN true ELSE false END AS no_normalization_change
