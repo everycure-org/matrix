57e3e5fd05dd54f8e56555382a992bc36299fd08
emil-k <emil.krause.44@gmail.com>
2025-03-12 13:19:27 +0100

    bugfix - argo worfklows namespace non existent

--- infra/modules/stacks/compute_cluster/workflows-prep.tf
+++ infra/modules/stacks/compute_cluster/workflows-prep.tf
@@ -12,3 +12,9 @@ resource "kubernetes_config_map" "parameters" {
     GCP_BUCKET     = var.bucket_name
   }
 }
+
+resource "kubernetes_namespace" "argo_workflows_ns" {
+  metadata {
+    name = "argo-workflows"
+  }
+}
