d75449e209fb6a56a9c6bbca4095a7b7ea22e62c
Kathleen Carter <163005214+eKathleenCarter@users.noreply.github.com>
2025-03-14 13:21:31 -0400

    Modifying the drug list queries
    split the drug list queries into two files
    refactored per Jacques's suggestions

---
+++ services/kg_dashboard/sources/bq/drug_list_success_prefixes.sql
@@ -0,0 +1,15 @@
+with success_stats as (
+  select
+    id,
+    split(id, ':')[offset(0)] as prefix -- Extract prefix from 'id'
+  from `mtrx-hub-dev-3of.release_${bq_release_version}.drug_list_nodes_normalized`
+  where normalization_success = true
+)
+
+select
+  prefix,
+  count(*) as count,
+  round(100 * count(*) / sum(count(*)) over(), 2) as percentage
+from success_stats
+group by prefix
+order by count desc;
