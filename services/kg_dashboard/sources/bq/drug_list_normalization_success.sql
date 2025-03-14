88c1303b270a35f82a96f4101f505b54eb09bc54
Kathleen Carter <163005214+eKathleenCarter@users.noreply.github.com>
2025-03-14 13:21:31 -0400

    Modifying the drug list queries
    split the drug list queries into two files
    refactored per Jacques's suggestions

--- services/kg_dashboard/sources/bq/drug_list_normalization_success.sql
+++ services/kg_dashboard/sources/bq/drug_list_normalization_success.sql
@@ -1,11 +1,9 @@
 with normalization_stats as (
   select
-    normalization_success,
     case
-      when original_id = id and normalization_success then 'success'
-      when original_id = id and not normalization_success then 'miss'
-      when original_id != id and normalization_success then 'success'
-      when original_id != id and not normalization_success then 'miss_review'
+      when normalization_success then 'success'
+      when original_id = id then 'miss'
+      when original_id != id then 'miss_review'
     end as normalization_status
   from `mtrx-hub-dev-3of.release_${bq_release_version}.drug_list_nodes_normalized`
 )
@@ -17,19 +15,3 @@ select
 from normalization_stats
 where normalization_status in ('success', 'miss')
 group by normalization_status;
-
-with success_stats as (
-  select
-    id,
-    split(id, ':')[offset(0)] as prefix -- Extract prefix from 'id'
-  from `mtrx-hub-dev-3of.release_${bq_release_version}.drug_list_nodes_normalized`
-  where normalization_success = true
-)
-
-select
-  prefix,
-  count(*) as count,
-  round(100 * count(*) / sum(count(*)) over(), 2) as percentage
-from success_stats
-group by prefix
-order by count desc;
