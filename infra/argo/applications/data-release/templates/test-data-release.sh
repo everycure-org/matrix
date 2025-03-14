87b13aff549cf67779b182c9bbd224a3ff27c630
emil-k <emil.krause.44@gmail.com>
2025-03-14 12:04:03 +0100

    comment out sh script in data release

--- infra/argo/applications/data-release/templates/test-data-release.sh
+++ infra/argo/applications/data-release/templates/test-data-release.sh
@@ -7,6 +7,6 @@
 
 # It's okay if these fail, it just means they don't exist in the remote.
 # Bash, without the -e option will just continue with the next command if the previous one failed.
-git push  --delete origin v0.0.1-release-test
-git push  --delete origin release/v0.0.1-release-test
-argo submit ./infra/argo/applications/data-release/templates/TestDataReleaseWorkflow.yaml --labels git_sha=$(git rev-parse HEAD)
+#git push  --delete origin v0.0.1-release-test
+#git push  --delete origin release/v0.0.1-release-test
+#argo submit ./infra/argo/applications/data-release/templates/TestDataReleaseWorkflow.yaml --labels git_sha=$(git rev-parse HEAD)
