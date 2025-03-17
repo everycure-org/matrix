7e70fcb85c57e4274703269c33db4c2938fde8df
emil-k <emil.krause.44@gmail.com>
2025-03-17 12:35:42 +0100

    test data release app debug

--- infra/argo/applications/data-release/templates/test-data-release.sh
+++ infra/argo/applications/data-release/templates/test-data-release.sh
@@ -9,4 +9,4 @@
 # Bash, without the -e option will just continue with the next command if the previous one failed.
 #git push  --delete origin v0.0.1-release-test
 #git push  --delete origin release/v0.0.1-release-test
-#argo submit ./infra/argo/applications/data-release/templates/TestDataReleaseWorkflow.yaml --labels git_sha=$(git rev-parse HEAD)
+#argo submit --from workflowtemplate/test-data-release-template -n argo-workflows --labels git_sha=$(git rev-parse HEAD)
