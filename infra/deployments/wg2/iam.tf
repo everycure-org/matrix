8531b652089b7207ee55cd9bf27f954d20894d89
Pascal Brokmeier <pascal@everycure.org>
2025-03-18 11:08:38 +0100

    apply better iam for CI

---
+++ infra/deployments/wg2/iam.tf
@@ -0,0 +1,15 @@
+# Giving hub-dev SA access to this project to allow it to deploy via CI/CD
+
+module "project_iam_bindings" {
+  source   = "terraform-google-modules/iam/google//modules/projects_iam"
+  projects = [var.project_id]
+  version  = "~> 8.0"
+
+  mode = "additive"
+
+  bindings = {
+    "roles/viewer" = ["serviceAccount:sa-github-actions-ro@mtrx-hub-dev-3of.iam.gserviceaccount.com"]
+    "roles/owner"  = ["serviceAccount:sa-github-actions-rw@mtrx-hub-dev-3of.iam.gserviceaccount.com"]
+  }
+
+}
