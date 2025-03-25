395c4e99d51f3aa9bcafdfc1f6be15d21be0ef53
Siyan Luo <luosiyanemily@gmail.com>
2025-03-25 17:26:06 +0100

    bump version until no branch is taken

--- docs/scripts/bump_version.py
+++ docs/scripts/bump_version.py
@@ -1,19 +1,34 @@
 import os
+import subprocess
 import sys
 
 import semver
 
 
-def bump_version(type: str) -> None:
+def branch_exists(branch_name: str) -> bool:
+    result = subprocess.run(
+        ["git", "ls-remote", "exit-code", "orgin", branch_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    return result.returncode == 0
+
+
+def bump_version(bump_type: str) -> None:
     latest_tag = os.getenv("latest_tag", "v0.0.0").lstrip("v")
     version = semver.Version.parse(latest_tag)
+    while True:
+        if bump_type == "minor":
+            new_version = version.bump_minor()
+        elif bump_type == "patch":
+            new_version = version.bump_patch()
+        else:
+            raise NotImplementedError()
 
-    if type == "minor":
-        new_version = version.bump_minor()
-    elif type == "patch":
-        new_version = version.bump_patch()
-    else:
-        raise NotImplementedError()
+        new_branch = f"release/v{new_version}"
+        if not branch_exists(new_branch):
+            break
+
+        # Increment version again if branch exists
+        version = semver.Version.parse(str(new_version))
 
     print(f"release_version=v{new_version}")
 
