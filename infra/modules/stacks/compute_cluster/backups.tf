9c0392840930d5fd55200445afcf76721d29035a
Oliver Willekens <4733178+oliverw1@users.noreply.github.com>
2025-03-26 16:11:45 +0100

    add restore plan

--- infra/modules/stacks/compute_cluster/backups.tf
+++ infra/modules/stacks/compute_cluster/backups.tf
@@ -1,7 +1,7 @@
 resource "google_gke_backup_backup_plan" "rpo_daily_window" {
   name     = "rpo-daily-window"
   cluster  = module.gke.cluster_id
-  location = "us-central1"
+  location = var.default_region
   retention_policy {
     backup_delete_lock_days = 7
     backup_retain_days      = 14
@@ -16,5 +16,23 @@ resource "google_gke_backup_backup_plan" "rpo_daily_window" {
     include_volume_data = true
     include_secrets     = false
     all_namespaces      = true
+  }
+}
+
+resource "google_gke_backup_restore_plan" "all_ns" {
+  name        = "restore-neo-mlflow-ns"
+  location    = var.default_region
+  backup_plan = google_gke_backup_backup_plan.rpo_daily_window.id
+  cluster     = module.gke.cluster_id
+  restore_config {
+    selected_namespaces {
+      namespaces = ["neo4j", "mlflow"]
+    }
+    namespaced_resource_restore_mode = "MERGE_REPLACE_ON_CONFLICT"
+    volume_data_restore_policy       = "RESTORE_VOLUME_DATA_FROM_BACKUP"
+    cluster_resource_restore_scope {
+      no_group_kinds = true
+    }
+    cluster_resource_conflict_policy = "USE_EXISTING_VERSION"
   }
 }
