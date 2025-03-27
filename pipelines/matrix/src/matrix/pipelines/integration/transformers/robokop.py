d85a7d7bbf28ebda85490b713ab6f399389e5a81
Kathleen Carter <163005214+eKathleenCarter@users.noreply.github.com>
2025-03-27 13:06:57 -0400

    Modifying transformers
    modifying transformers to work with new data format.

--- pipelines/matrix/src/matrix/pipelines/integration/transformers/robokop.py
+++ pipelines/matrix/src/matrix/pipelines/integration/transformers/robokop.py
@@ -34,7 +34,7 @@ class RobokopTransformer(GraphTransformer):
             .withColumn("upstream_data_source",              F.array(F.lit("robokop")))
             .withColumn("all_categories",                    F.split(F.col("category"), ROBOKOP_SEPARATOR))
             .withColumn("equivalent_identifiers",            F.split(F.col("equivalent_identifiers"), ROBOKOP_SEPARATOR))
-            #.withColumn("labels",                            F.col("all_categories"))
+            .withColumn("labels",                            F.col("all_categories"))
             .withColumn("publications",                      F.lit(None).cast(T.ArrayType(T.StringType())))
             .withColumn("international_resource_identifier", F.lit(None).cast(T.StringType()))
             #.withColumnRenamed("id:ID", "id")
