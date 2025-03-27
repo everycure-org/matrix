53c211784fc6baf4d000f8985369048f07e8aba7
Kathleen Carter <163005214+eKathleenCarter@users.noreply.github.com>
2025-03-27 14:45:06 -0400

    modifying transformers
    adjusting the delimiter so that the pipe is read as a character not an "or"

--- pipelines/matrix/src/matrix/pipelines/integration/transformers/robokop.py
+++ pipelines/matrix/src/matrix/pipelines/integration/transformers/robokop.py
@@ -14,7 +14,7 @@ from matrix.pipelines.integration.filters import determine_most_specific_categor
 
 from .transformer import GraphTransformer
 
-ROBOKOP_SEPARATOR = "|"  # "\x1f"
+ROBOKOP_SEPARATOR = r"\|"  # "\x1f"
 
 
 class RobokopTransformer(GraphTransformer):
