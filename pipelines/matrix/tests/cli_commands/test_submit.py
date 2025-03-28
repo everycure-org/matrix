63987b0ea504e9b9ddea5e9a9979b70a8e2feb0b
emil-k <emil.krause.44@gmail.com>
2025-03-28 14:54:41 +0100

    pre commit format 2 more files

--- pipelines/matrix/tests/cli_commands/test_submit.py
+++ pipelines/matrix/tests/cli_commands/test_submit.py
@@ -9,6 +9,7 @@ import yaml
 from click.testing import CliRunner
 from kedro.pipeline import Pipeline
 from kedro.pipeline.node import Node
+
 from matrix.cli_commands.submit import (
     _submit,
     apply_argo_template,
