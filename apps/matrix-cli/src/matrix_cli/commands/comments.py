5503d8f6c0b1770342380fe79b706ca851827810
Pascal Brokmeier <pascal@everycure.org>
2025-03-31 13:40:40 +0200

    working classifcation of commands script

--- apps/matrix-cli/src/matrix_cli/commands/comments.py
+++ apps/matrix-cli/src/matrix_cli/commands/comments.py
@@ -1,15 +1,20 @@
 #!/bin/python
+import json
 import logging
 import os
 from enum import Enum
-from typing import Optional
+from typing import List, Optional, Set
 
+import pandas as pd
 import typer
 from dotenv import load_dotenv
-from github import Github, Issue, IssueComment, Repository
-from google import genai
-from google.genai import types
-from joblib import Memory, Parallel, delayed
+from github import Github
+from github.Issue import Issue
+from github.IssueComment import IssueComment
+from github.Repository import Repository
+from google import genai  # type: ignore
+from google.genai import types  # type: ignore
+from joblib import Memory, Parallel, delayed  # type: ignore
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 from rich import print as rprint
@@ -20,13 +25,12 @@ from tqdm.rich import tqdm
 from urllib3.exceptions import NameResolutionError
 
 from matrix_cli.components.settings import settings as cli_settings
-from matrix_cli.components.utils import invoke_model
 
 # Need to ensure this happens before we initialize the genai client
 load_dotenv()
 
 app = typer.Typer(help="Github comment-related utility commands", no_args_is_help=True)
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 memory = Memory(location=".cache", verbose=0)
 console = Console()
 
@@ -36,9 +40,10 @@ gemini = genai.Client(
 
 
 class Classification(str, Enum):
-    NEUTRAL = "neutral"
-    CONCERNING = "concerning"
     RISK = "risk"
+    CONCERNING = "concerning"
+    UNCLASSIFIED = "unclassified"
+    NEUTRAL = "neutral"
 
 
 class CommentEvaluation(BaseModel):
@@ -48,7 +53,7 @@ class CommentEvaluation(BaseModel):
     classification: Optional[Classification] = None
     extract: Optional[str] = None
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash((self.link))
 
 
@@ -63,6 +68,27 @@ class CommentEvaluationSettings(BaseSettings):
       are unprofessional
     - Identified Risk: Comments that clearly break common code of conducts or are unfair,
       mean or otherwise risky to be put online.
+
+    A few examples of comments:
+    - Neutral: "wops yer rite"
+    - Neutral: "Docstring args don't match"
+    - Neutral: "Let's not discuss this here"
+    - Neutral: "This is sick" (not a risk, the programmer is congratulating their colleague)
+    - Neutral: "Am I missing something?"
+    - Neutral: "I'm thinking this is too specific tbh."
+    - Neutral: "Needs to be removed" (not a risk, but a direction from a maintainer)
+    - Neutral: "This should not exist" (not a risk but a direct statement, albeit negative)
+    - Concerning: "I'm not sure if you're aware, but your code is full of bugs"
+    - risk: "You are a bad programmer"
+    - risk: "What an idiot"
+
+
+    Overall the bar should be that a concerning comment is one that can be perceived as hostile and
+    a risk is a straight out mean or aggressive statement.
+
+    Anker yourself around common code of conducts of major open source projects. Also note
+    programmers are trying to get stuff done, not giving a perfect speech. Thus, some
+    banter is fine.
     """
     model: str = cli_settings.base_model
 
@@ -78,27 +104,39 @@ def detect_risk(
     github_token: str = typer.Option(help="The Github token to use", envvar="GITHUB_TOKEN"),
     repo_id: str = typer.Option(help="The repo id to use", default="everycure-org/matrix"),
     prompt: str = typer.Option(help="The prompt to use", default=comment_settings.prompt),
-    limit: int = typer.Option(help="The limit of comments to evaluate", default=None),
-):
+    limit: Optional[int] = typer.Option(help="The limit of comments to evaluate", default=None),
+    excel_output: str = typer.Option(
+        help="The path to the excel file to save the results", default=None
+    ),
+) -> None:
     try:
         comments = collect_github_data(github_token, repo_id)
     except (ConnectionError, NameResolutionError):
         logging.error("A networking error occured, aborting")
         exit(1)
 
-    evaluations = set(convert_comments(comments))
+    evaluations: Set[CommentEvaluation] = set(convert_comments(comments))
 
     rprint(f"Evaluating {len(evaluations)} comments")
     # evaluate each comment
-    for comment in tqdm(evaluations[:limit]):
-        # replace object with the one containing evaluation
-        evaluations.add(evaluate_comment(comment, prompt))
-        # evaluations.add(dummy_evaluate_comment(comment, prompt))
+    comments_to_process = list(evaluations)[:limit]
+    processed_comments = Parallel(n_jobs=-2)(
+        delayed(evaluate_comment)(comment, prompt) for comment in tqdm(comments_to_process)
+    )
+    if excel_output:
+        print_results_to_excel(processed_comments, excel_output)
+    else:
+        print_results(processed_comments)
 
-    print_results(evaluations)
+
+def print_results_to_excel(evaluations: List[CommentEvaluation], path: str) -> None:
+    df = pd.DataFrame(evaluations)
+    # ensure the folder exists
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    df.to_excel(path, index=False)
 
 
-def print_results(evaluations: set[CommentEvaluation]):
+def print_results(evaluations: List[CommentEvaluation]) -> None:
     table = Table(title="Results")
     table.add_column("Classification")
     table.add_column("Author")
@@ -109,11 +147,15 @@ def print_results(evaluations: set[CommentEvaluation]):
     priority = {
         Classification.RISK: 0,
         Classification.CONCERNING: 1,
-        Classification.NEUTRAL: 2,
+        Classification.UNCLASSIFIED: 2,
+        Classification.NEUTRAL: 3,
     }
 
     # Convert set to list and sort by classification priority
-    sorted_evaluations = sorted(evaluations, key=lambda x: priority[x.classification])
+    sorted_evaluations = sorted(
+        [ev for ev in evaluations if ev.classification is not None],
+        key=lambda x: priority[x.classification],  # type: ignore
+    )
 
     for ev in sorted_evaluations:
         if ev.classification != Classification.NEUTRAL:
@@ -122,17 +164,19 @@ def print_results(evaluations: set[CommentEvaluation]):
     console.print(table)
 
 
-def convert_comments(comments: list[IssueComment]) -> list[CommentEvaluation]:
-    return [CommentEvaluation(content=c.body, link=c.url, author=c.user.login) for c in comments]
+def convert_comments(comments: List[IssueComment]) -> List[CommentEvaluation]:
+    return [
+        CommentEvaluation(content=c.body, link=c.html_url, author=c.user.login) for c in comments
+    ]
 
 
-def collect_github_data(github_token, repo_id) -> list[IssueComment]:
+def collect_github_data(github_token: str, repo_id: str) -> List[IssueComment]:
     # loop over all issues
     # get all comments of each issues
     gh = Github(github_token)
     repo = gh.get_repo(repo_id)
     issues = fetch_issues_for_repo(repo_id, gh)
-    comments = []
+    comments: List[str] = []
 
     # grab the first posts  of issues
     for issue in issues:
@@ -142,43 +186,33 @@ def collect_github_data(github_token, repo_id) -> list[IssueComment]:
     responses = Parallel(n_jobs=-2)(
         delayed(fetch_comments_for_issue)(issue.number, repo) for issue in tqdm(issues)
     )
-    comments = [comment for response in responses for comment in response]
-    return comments
+    comments_list: List[IssueComment] = [comment for response in responses for comment in response]
+    return comments_list
 
 
-@memory.cache(ignore=["github"])
-def fetch_issues_for_repo(repo_id: str, github: Github) -> list[Issue]:
+@memory.cache(ignore=["github"])  # type: ignore
+def fetch_issues_for_repo(repo_id: str, github: Github) -> List[Issue]:
     # reinitiate repo object here to enable caching
     repo = github.get_repo(repo_id)
     return list(repo.get_issues(state="all"))
 
 
 @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=15))
-@memory.cache(ignore=["repo"])
-def fetch_comments_for_issue(issue_id: int, repo: Repository) -> list[IssueComment]:
-    comments = []
+@memory.cache(ignore=["repo"])  # type: ignore
+def fetch_comments_for_issue(issue_id: int, repo: Repository) -> List[IssueComment]:
+    comments: List[IssueComment] = []
     issue = repo.get_issue(issue_id)
     if issue.pull_request:
         pr = issue.as_pull_request()
-        comments.extend(list(pr.get_review_comments()))
+        comments.extend(list(pr.get_review_comments()))  # type: ignore
         comments.extend(list(pr.get_issue_comments()))
     else:
         comments.extend(list(issue.get_comments()))
     return comments
 
 
-def dummy_evaluate_comment(
-    comment: CommentEvaluation, prompt: str, model: str = cli_settings.base_model
-):
-    import random
-
-    comment.classification = random.choice([x.value for x in Classification])
-    comment.extract = "NOT AN EXTRACT"
-    return comment
-
-
 @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=15))
-@memory.cache
+@memory.cache(ignore=["model"])  # type: ignore
 def evaluate_comment(
     comment: CommentEvaluation, prompt: str, model: str = cli_settings.base_model
 ) -> CommentEvaluation:
@@ -188,8 +222,6 @@ def evaluate_comment(
         top_p=0.95,
         top_k=40,
         max_output_tokens=1000,
-        # by default, gemini will not block unsafe content
-        # safety_settings=[types.SafetySetting()],
         response_mime_type="application/json",
         response_schema=genai.types.Schema(
             type=genai.types.Type.OBJECT,
@@ -197,7 +229,7 @@ def evaluate_comment(
             properties={
                 "classification": genai.types.Schema(
                     type=genai.types.Type.STRING,
-                    enum=[x.value for x in Classification],
+                    enum=[x.value for x in Classification if x != Classification.UNCLASSIFIED],
                 ),
                 "extract": genai.types.Schema(
                     type=genai.types.Type.STRING,
@@ -209,10 +241,21 @@ def evaluate_comment(
         ],
     )
 
-    response = invoke_model(prompt=input_, model=model, generation_config=generate_content_config)
     response = gemini.models.generate_content(
         model=model,
         contents=input_,
         config=generate_content_config,
     ).text
-    return response
+
+    # try parsing the response as json
+    try:
+        response = json.loads(response)
+        comment.classification = Classification(
+            response.get("classification", Classification.UNCLASSIFIED)
+        )
+        comment.extract = response.get("extract", "")
+    except json.JSONDecodeError:
+        logging.error(f"Failed to parse response as json: {response}")
+        comment.classification = Classification.UNCLASSIFIED
+
+    return comment
