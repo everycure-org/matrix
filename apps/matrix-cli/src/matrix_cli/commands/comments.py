9be9cfc0ee9fde4b49bc6e31ea7deb4fb86b97ac
Pascal Brokmeier <pascal@everycure.org>
2025-03-31 12:17:59 +0200

    add mypy, prep for fixing

--- apps/matrix-cli/src/matrix_cli/commands/comments.py
+++ apps/matrix-cli/src/matrix_cli/commands/comments.py
@@ -1,9 +1,11 @@
 #!/bin/python
-import base64
 import logging
 import os
+from enum import Enum
+from typing import Optional
 
 import typer
+from dotenv import load_dotenv
 from github import Github, Issue, IssueComment, Repository
 from google import genai
 from google.genai import types
@@ -11,51 +13,137 @@ from joblib import Memory, Parallel, delayed
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 from rich import print as rprint
+from rich.console import Console
+from rich.table import Table
+from tenacity import retry, stop_after_attempt, wait_exponential
 from tqdm.rich import tqdm
+from urllib3.exceptions import NameResolutionError
 
 from matrix_cli.components.settings import settings as cli_settings
 from matrix_cli.components.utils import invoke_model
 
+# Need to ensure this happens before we initialize the genai client
+load_dotenv()
+
 app = typer.Typer(help="Github comment-related utility commands", no_args_is_help=True)
 logging.basicConfig(level=logging.INFO)
 memory = Memory(location=".cache", verbose=0)
+console = Console()
+
+gemini = genai.Client(
+    api_key=os.environ.get("GEMINI_API_KEY"),
+)
+
+
+class Classification(str, Enum):
+    NEUTRAL = "neutral"
+    CONCERNING = "concerning"
+    RISK = "risk"
+
+
+class CommentEvaluation(BaseModel):
+    content: str
+    link: str
+    author: str
+    classification: Optional[Classification] = None
+    extract: Optional[str] = None
+
+    def __hash__(self):
+        return hash((self.link))
 
 
 class CommentEvaluationSettings(BaseSettings):
     prompt: str = """
-    Your objective is the detection of harmful comments on our github as we prepare for an open sourcing of the repository. 
+    Your objective is the detection of harmful comments on our github as we prepare for
+    an open sourcing of the repository.
     Please identify each comment as "neutral", "concerning" or "identified risk".
 
     - Neutral: Comments that are just focused on the topic or are otherwise not noteworthy
-    - Concerning: Comments that may be misinterpreted, could be perceived as hostile or are unprofessional
-    - Identified Risk: Comments that clearly break common code of conducts or are unfair, mean or otherwise risky to be put online.
+    - Concerning: Comments that may be misinterpreted, could be perceived as hostile or
+      are unprofessional
+    - Identified Risk: Comments that clearly break common code of conducts or are unfair,
+      mean or otherwise risky to be put online.
     """
     model: str = cli_settings.base_model
 
 
+comment_settings = CommentEvaluationSettings()
+
+
 @app.command()
 def detect_risk(
-    ids: str = typer.Option(help="The issues or PRs to evaluate. Comma separated list", default=None),
+    ids: str = typer.Option(
+        help="The issues or PRs to evaluate. Comma separated list", default=None
+    ),
     github_token: str = typer.Option(help="The Github token to use", envvar="GITHUB_TOKEN"),
     repo_id: str = typer.Option(help="The repo id to use", default="everycure-org/matrix"),
+    prompt: str = typer.Option(help="The prompt to use", default=comment_settings.prompt),
+    limit: int = typer.Option(help="The limit of comments to evaluate", default=None),
 ):
+    try:
+        comments = collect_github_data(github_token, repo_id)
+    except (ConnectionError, NameResolutionError):
+        logging.error("A networking error occured, aborting")
+        exit(1)
+
+    evaluations = set(convert_comments(comments))
+
+    rprint(f"Evaluating {len(evaluations)} comments")
+    # evaluate each comment
+    for comment in tqdm(evaluations[:limit]):
+        # replace object with the one containing evaluation
+        evaluations.add(evaluate_comment(comment, prompt))
+        # evaluations.add(dummy_evaluate_comment(comment, prompt))
+
+    print_results(evaluations)
+
+
+def print_results(evaluations: set[CommentEvaluation]):
+    table = Table(title="Results")
+    table.add_column("Classification")
+    table.add_column("Author")
+    table.add_column("Content")
+    table.add_column("Link")
+
+    # Create a priority mapping for sorting
+    priority = {
+        Classification.RISK: 0,
+        Classification.CONCERNING: 1,
+        Classification.NEUTRAL: 2,
+    }
+
+    # Convert set to list and sort by classification priority
+    sorted_evaluations = sorted(evaluations, key=lambda x: priority[x.classification])
+
+    for ev in sorted_evaluations:
+        if ev.classification != Classification.NEUTRAL:
+            table.add_row(ev.classification, ev.author, ev.extract, ev.link)
+
+    console.print(table)
+
+
+def convert_comments(comments: list[IssueComment]) -> list[CommentEvaluation]:
+    return [CommentEvaluation(content=c.body, link=c.url, author=c.user.login) for c in comments]
+
+
+def collect_github_data(github_token, repo_id) -> list[IssueComment]:
     # loop over all issues
     # get all comments of each issues
     gh = Github(github_token)
     repo = gh.get_repo(repo_id)
     issues = fetch_issues_for_repo(repo_id, gh)
     comments = []
+
+    # grab the first posts  of issues
     for issue in issues:
         comments.append(issue.body)
 
     rprint(f"Fetching all comments a total of {len(issues)} issues")
-    responses = Parallel(n_jobs=-2)(delayed(fetch_comments_for_issue)(issue.number, repo) for issue in tqdm(issues))
+    responses = Parallel(n_jobs=-2)(
+        delayed(fetch_comments_for_issue)(issue.number, repo) for issue in tqdm(issues)
+    )
     comments = [comment for response in responses for comment in response]
-
-    rprint(f"Evaluating {len(comments)} comments")
-    # evaluate each comment
-    for comment in tqdm(comments):
-        evaluate_comment(comment.body, prompt)
+    return comments
 
 
 @memory.cache(ignore=["github"])
@@ -65,6 +153,7 @@ def fetch_issues_for_repo(repo_id: str, github: Github) -> list[Issue]:
     return list(repo.get_issues(state="all"))
 
 
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=15))
 @memory.cache(ignore=["repo"])
 def fetch_comments_for_issue(issue_id: int, repo: Repository) -> list[IssueComment]:
     comments = []
@@ -78,15 +167,22 @@ def fetch_comments_for_issue(issue_id: int, repo: Repository) -> list[IssueComme
     return comments
 
 
-class CommentEvaluation(BaseModel):
-    content: str
-    link: str
-    author: str
-    classification: str
-    extract: str
+def dummy_evaluate_comment(
+    comment: CommentEvaluation, prompt: str, model: str = cli_settings.base_model
+):
+    import random
+
+    comment.classification = random.choice([x.value for x in Classification])
+    comment.extract = "NOT AN EXTRACT"
+    return comment
 
 
-def evaluate_comment(input: str, prompt: str, model: str = cli_settings.base_model) -> CommentEvaluation:
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=15))
+@memory.cache
+def evaluate_comment(
+    comment: CommentEvaluation, prompt: str, model: str = cli_settings.base_model
+) -> CommentEvaluation:
+    input_ = comment.content
     generate_content_config = types.GenerateContentConfig(
         temperature=0.15,
         top_p=0.95,
@@ -101,7 +197,7 @@ def evaluate_comment(input: str, prompt: str, model: str = cli_settings.base_mod
             properties={
                 "classification": genai.types.Schema(
                     type=genai.types.Type.STRING,
-                    enum=["neutral", "concerning", "identified_risk"],
+                    enum=[x.value for x in Classification],
                 ),
                 "extract": genai.types.Schema(
                     type=genai.types.Type.STRING,
@@ -113,9 +209,10 @@ def evaluate_comment(input: str, prompt: str, model: str = cli_settings.base_mod
         ],
     )
 
-    response = invoke_model(prompt=input, model=model, generation_config=generate_content_config)
+    response = invoke_model(prompt=input_, model=model, generation_config=generate_content_config)
+    response = gemini.models.generate_content(
+        model=model,
+        contents=input_,
+        config=generate_content_config,
+    ).text
     return response
-
-
-if __name__ == "__main__":
-    generate()
