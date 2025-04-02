53dc7d7859d5ec953e0825cd2e49867028a7caa3
Oliver Willekens <4733178+oliverw1@users.noreply.github.com>
2025-04-02 11:12:54 +0200

    Revert "fix: drop column from data that is manually lifted to Hive partition layout"
    This reverts commit 4171ecdcd1401c969848619113f8f0b3100faee7.

--- pipelines/matrix/src/matrix/pipelines/batch/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/batch/pipeline.py
@@ -197,7 +197,7 @@ def cache_miss_resolver_wrapper(
         logger.info(f"Processing batch with key: {batch[0]}")
         transformed = await transformer.apply(batch)
         logger.info(f"received response for batch with key: {batch[0]}")
-        return pa.table([batch, transformed], schema=cache_schema).to_pandas()
+        return pa.table([batch, transformed, [api] * len(batch)], schema=cache_schema).to_pandas()
 
     def prep(
         batches: Iterable[Sequence[T]], api: str
