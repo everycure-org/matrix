9590888184aed82e59b6c320e6bbc071f2840c04
piotrkan <piotr@everycure.org>
2025-04-17 12:30:23 +0200

    add embiology transformer

---
+++ pipelines/matrix/src/matrix/pipelines/integration/transformers/embiology.py
@@ -0,0 +1,63 @@
+import logging
+from typing import Dict
+
+import pyspark.sql as ps
+import pyspark.sql.functions as f
+import pyspark.sql.types as T
+
+from .transformer import GraphTransformer
+
+logger = logging.getLogger(__name__)
+
+
+class EmbiologyTransformer(GraphTransformer):
+    def transform_nodes(self, nodes_df: ps.DataFrame, **kwargs) -> ps.DataFrame:
+        """Transform Embiology nodes to our target schema.
+
+        Args:
+            nodes_df: Nodes DataFrame.
+
+        Returns:
+            Transformed DataFrame.
+        """
+        # fmt: off
+        return (
+            nodes_df
+            .withColumn("upstream_data_source",              f.array(f.lit("embiology")))
+            .withColumn("labels",                            f.lit(None).cast(T.ArrayType(T.StringType())))
+            .withColumn("all_categories",                    f.col("all_categories"))
+            .withColumn("equivalent_identifiers",            f.lit(None).cast(T.ArrayType(T.StringType()))) # TODO: add equivalent identifiers
+            .withColumn("publications",                      f.lit(None).cast(T.ArrayType(T.StringType())))
+            .withColumn("international_resource_identifier", f.lit(None).cast(T.StringType())) # TODO: add international resource identifier
+        )
+        # fmt: on
+
+    def transform_edges(
+        self,
+        edges_df: ps.DataFrame,
+        **kwargs,
+    ) -> ps.DataFrame:
+        """Transform Embiology edges to our target schema.
+
+        Args:
+            edges_df: Edges DataFrame.
+            pubmed_mapping: pubmed mapping
+        Returns:
+            Transformed DataFrame.
+        """
+
+        # fmt: off
+        return (
+            edges_df
+            .withColumn("upstream_data_source",          f.array(f.lit("embiology")))
+            .withColumn("aggregator_knowledge_source",   f.lit(None).cast(T.StringType())) #Embiology 2.10 has a column for aggregator knowledge source
+            .withColumn("publications",                  f.lit(None).cast(T.ArrayType(T.StringType())))
+            .withColumn("knowledge_level",               f.lit(None).cast(T.StringType()))
+            .withColumn("agent_type",                    f.lit(None).cast(T.StringType()))
+            .withColumn("primary_knowledge_source",      f.lit(None).cast(T.StringType())) #not present in Embiology at this time
+            .withColumn("subject_aspect_qualifier",      f.lit(None).cast(T.StringType())) #not present in Embiology at this time
+            .withColumn("subject_direction_qualifier",   f.lit(None).cast(T.StringType())) #not present in Embiology at this time
+            .withColumn("object_aspect_qualifier",       f.lit(None).cast(T.StringType())) #not present in Embiology at this time
+            .withColumn("object_direction_qualifier",    f.lit(None).cast(T.StringType())) #not present in Embiology at this time
+        )
+        # fmt: on
