bd1a46eaeb9c03bdf41b06d4421b041813d1c292
james0032 <jchung@renci.org>
2025-04-21 13:24:55 -0400

    use random vector instead of OpenAI generated ones for featureProperty

--- pipelines/matrix/src/matrix/pipelines/embeddings/nodes.py
+++ pipelines/matrix/src/matrix/pipelines/embeddings/nodes.py
@@ -120,7 +120,8 @@ def reduce_dimension(df: ps.DataFrame, transformer, input: str, output: str, ski
                    embeddings, depending on the 'skip' parameter.
     """
     if skip:
-        return df.withColumn(output, ps.functions.col(input).cast("array<float>"))
+        return df.withColumn("output", F.expr("transform(input, x -> rand())").cast(ArrayType(FloatType())))
+        # return df.withColumn(output, ps.functions.col(input).cast("array<float>"))
 
     # Convert into correct type
     df = df.withColumn("features", array_to_vector(ps.functions.col(input).cast("array<float>")))
