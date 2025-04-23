0d3a20cdbd191ebae1f69d4a72f90a47ed586bc0
emil-k <emil.krause.44@gmail.com>
2025-04-23 13:56:01 +0200

    adapt unit tests

---
+++ pipelines/matrix/tests/test_env_vars_validation.py
@@ -0,0 +1,56 @@
+import pytest
+
+from matrix.cli_commands.run import _validate_env_vars
+
+
+@pytest.mark.parametrize(
+    "env,env_vars,should_raise_error",
+    [
+        (
+            "prod",
+            {
+                "RUNTIME_GCP_PROJECT_ID": "mtrx-hub-prod-sms",
+                "RUNTIME_GCP_BUCKET": "mtrx-us-central1-hub-prod-storage",
+                "MLFLOW_URL": "https://mlflow.platform.prod.everycure.org/",
+            },
+            False,
+        ),
+        (
+            "dev",
+            {
+                "RUNTIME_GCP_PROJECT_ID": "mtrx-hub-dev-sms",
+                "RUNTIME_GCP_BUCKET": "mtrx-us-central1-hub-dev-storage",
+                "MLFLOW_URL": "https://mlflow.platform.dev.everycure.org/",
+            },
+            False,
+        ),
+        (
+            "prod",
+            {
+                "RUNTIME_GCP_PROJECT_ID": "mtrx-hub-dev-sms",
+                "RUNTIME_GCP_BUCKET": "mtrx-us-central1-hub-dev-storage",
+                "MLFLOW_URL": "https://mlflow.platform.dev.everycure.org/",
+            },
+            True,
+        ),
+        (
+            "dev",
+            {
+                "RUNTIME_GCP_PROJECT_ID": "mtrx-hub-prod-sms",
+                "RUNTIME_GCP_BUCKET": "mtrx-us-central1-hub-prod-storage",
+                "MLFLOW_URL": "https://mlflow.platform.prod.everycure.org/",
+            },
+            True,
+        ),
+    ],
+    ids=["prod_success", "dev_success", "prod_with_dev_vars_error", "dev_with_prod_vars_error"],
+)
+def test_validate_env_vars(monkeypatch, env, env_vars, should_raise_error):
+    for key, value in env_vars.items():
+        monkeypatch.setenv(key, value)
+
+    if should_raise_error:
+        with pytest.raises(RuntimeError):
+            _validate_env_vars(env)
+    else:
+        _validate_env_vars(env)
