cc5408d9046c6444ce57679bc721e16f9dbd1b0c
piotrkan <piotr@everycure.org>
2025-04-26 22:49:18 +0200

    update test

--- pipelines/matrix/tests/cli_commands/test_submit.py
+++ pipelines/matrix/tests/cli_commands/test_submit.py
@@ -141,7 +141,7 @@ def test_apply_argo_template(mock_run_subprocess: None) -> None:
 
 def test_submit_workflow(mock_run_subprocess: None) -> None:
     mock_run_subprocess.return_value.stdout = '{"metadata": {"name": "test-job"}}'
-    submit_workflow("test_run", "test_namespace", gcp_env="dev", verbose=True)
+    submit_workflow("test_run", "test_namespace", verbose=True)
     assert mock_run_subprocess.call_count == 1
 
 
@@ -241,7 +241,6 @@ def test_workflow_submission(
         pipeline_obj=pipeline_obj,
         verbose=True,
         dry_run=False,
-        gcp_env="dev",
         template_directory=temporary_directory,
         mlflow_experiment_id=1,
         allow_interactions=False,
