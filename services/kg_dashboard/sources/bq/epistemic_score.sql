646f1978a249669c16384b601ca57ade38a9d0d7
Kathleen Carter <163005214+eKathleenCarter@users.noreply.github.com>
2025-04-28 14:07:12 -0400

    Rescaling the scales
    After reading through comments, discussions on slack and talking to Nico, I have implemented the following changes to this section of the summary.
    
    - Updated knowledge_level and agent_type scoring to align with Biolink Model guidance.
    - Rescaled scores from [0, 1] to [-1, 1] to better distinguish high and low confidence.
    - Treated 'not_provided' and NULL as neutral (score = 0 after rescaling).
    - Expanded and adjusted CASE mappings to reflect more accurate confidence levels.
    - Updated combined average_score calculation to use new rescaled values.

--- services/kg_dashboard/sources/bq/epistemic_score.sql
+++ services/kg_dashboard/sources/bq/epistemic_score.sql
@@ -2,28 +2,27 @@ WITH scored_edges AS (
   SELECT
     knowledge_level,
     agent_type,
-    CASE
+    (2 * CASE
       WHEN knowledge_level = 'knowledge_assertion' THEN 1.0
-      WHEN knowledge_level = 'logical_entailment' THEN 0.99
-      WHEN knowledge_level = 'prediction' THEN 0.5
-      WHEN knowledge_level = 'statistical_association' THEN 0.5
-      WHEN knowledge_level = 'observation' THEN 0.5
-      WHEN knowledge_level = 'not_provided' THEN 0.25
-    END AS kl_score,
-    CASE
+      WHEN knowledge_level = 'logical_entailment' THEN 0.85
+      WHEN knowledge_level = 'statistical_association' THEN 0.65
+      WHEN knowledge_level = 'observation' THEN 0.4
+      WHEN knowledge_level = 'prediction' THEN 0.2
+      WHEN knowledge_level = 'not_provided' THEN 0.5
+      WHEN knowledge_level IS NULL OR knowledge_level = '' THEN 0.5
+    END) - 1 AS kl_score,
+    (2 * CASE
       WHEN agent_type = 'manual_agent' THEN 1.0
-      WHEN agent_type = 'manual_validation_of_automated_agent' THEN 1.0
-      WHEN agent_type = 'automated_agent' THEN 0.75
-      WHEN agent_type = 'data_analysis_pipeline' THEN 0.5
+      WHEN agent_type = 'manual_validation_of_automated_agent' THEN 0.95
+      WHEN agent_type = 'data_analysis_pipeline' THEN 0.7
+      WHEN agent_type = 'automated_agent' THEN 0.6
       WHEN agent_type = 'computational_model' THEN 0.5
-      WHEN agent_type = 'text_mining_agent' THEN 0.3
-      WHEN agent_type = 'image_processing_agent' THEN 0.3
-      WHEN agent_type = 'not_provided' THEN 0.25
-    END AS at_score
+      WHEN agent_type = 'text_mining_agent' THEN 0.35
+      WHEN agent_type = 'image_processing_agent' THEN 0.35
+      WHEN agent_type = 'not_provided' THEN 0.5
+      WHEN agent_type IS NULL OR agent_type = '' THEN 0.5
+    END) - 1 AS at_score
   FROM `mtrx-hub-dev-3of.release_${bq_release_version}.edges`
-  WHERE
-    knowledge_level IS NOT NULL AND knowledge_level != ''
-    AND agent_type IS NOT NULL AND agent_type != ''
 )
 
 SELECT
