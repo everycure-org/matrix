d9ae2379112fc0e12ea787162115daa2280831f9
Emil <emil.krause.44@gmail.com>
2025-04-29 15:52:23 +0200

    Update test_private_dataset_exclusion.py

--- pipelines/matrix/tests/utils/test_private_dataset_exclusion.py
+++ pipelines/matrix/tests/utils/test_private_dataset_exclusion.py
@@ -40,4 +40,3 @@ def test_integration_sources_filtering(integration_mapping, monkeypatch, include
     result_nested = disable_private_datasets(generate_dynamic_pipeline_mapping(integration_mapping))
     assert result["integration"] == expected_sources
     assert result_nested["integration"] == expected_sources
-
