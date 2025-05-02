15d20d0b579bbcae5651fe00c32050eda830356d
Amy Ford <amy@everycure.org>
2025-05-02 16:23:23 +0100

    Tidy up MlFlow hook

--- pipelines/matrix/src/matrix/hooks.py
+++ pipelines/matrix/src/matrix/hooks.py
@@ -104,29 +104,9 @@ class MLFlowHooks:
         other hooks to consume.
         """
         MLFlowHooks.set_context(context)
-        # MLFlowHooks.get_pipeline_inputs()
         cfg = OmegaConf.create(context.config_loader["mlflow"])
-        globs = OmegaConf.create(context.config_loader["globals"])
-        # Set tracking uri
-        # NOTE: This piece of code ensures that every MLFlow experiment
-        # is created by our Kedro pipeline with the right artifact root.
         mlflow.set_tracking_uri(cfg.server.mlflow_tracking_uri)
-
-        # Temporary - while we still support kedro submit alongside kedro experiment
-        # Once kedro submit is deprecated we can probably remove this entire hook
-        # TODO: test this
-        if globs.mlflow_experiment_id and globs.mlflow_experiment_id != "None":
-            experiment_id = globs.mlflow_experiment_id
-            mlflow.start_run(run_id=cfg.tracking.run.id)
-        else:
-            experiment_id = self._create_experiment(cfg.tracking.experiment.name, globs.mlflow_artifact_root)
-
-            if cfg.tracking.run.name:
-                run_id = self._create_run(cfg.tracking.run.name, experiment_id)
-
-                # Update catalog
-                OmegaConf.update(cfg, "tracking.run.id", run_id)
-                context.config_loader["mlflow"] = cfg
+        mlflow.start_run(run_id=cfg.tracking.run.id, nested=True)
 
     @staticmethod
     def _create_run(run_name: str, experiment_id: str) -> str:
