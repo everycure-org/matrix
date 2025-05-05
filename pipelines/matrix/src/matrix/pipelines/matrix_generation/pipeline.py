fc25ad9773ff378214ba5b7cc881ff58d7e35cc4
Siyan Luo <89979939+Siyan-Luo@users.noreply.github.com>
2025-05-05 14:37:22 +0200

    Model prediction on spark dataframe instead of pandas dataframe (#1311)
    * use pandas udf and spark dataframe for making predictions
    
    * use mappartitions
    
    * cleanup
    
    * more cleanup
    
    * more cleanup
    
    * more cleanup
    
    * more cleanup
    
    * look into memory issues
    
    * fix schema
    
    * add workaround for empty partitions
    
    * config argo resources
    
    * config argo resources
    
    * debug memory
    
    # Conflicts:
    #	pipelines/matrix/src/matrix/pipelines/matrix_generation/pipeline.py
    
    * drop cache to at least see the size of the dataframe
    
    * use more cpu
    
    * sidestep transformers
    
    * fix: transpose X for model feed
    
    * drop big feature col to save some memory, repartition as well
    
    * cleanup unused code
    
    * move ranking to reporting node
    
    * don't cache
    
    * drop repartition, rename func to cover what it's doing
    
    * clean up
    
    * reduce memory limit
    
    * load graph as spark
    
    * cleanup print statements
    
    * cleanup print statements
    
    * fix test
    
    * fix catalog transcoding
    
    * fix catalog, return sorted predictions
    
    * use simple syntax
    
    * test: assert sorted data
    
    * remove kg dataset from matrix_generation
    
    * set to remove duplicate
    
    * fix test
    
    * factor out commonalities to help find related items
    
    * correct function annotations
    
    * increase logging for specific bug
    
    * ensure filepath is a string
    
    * add intermediate node for sorting
    
    * sort the pandas dataframe within make predictions
    
    * remove fuze-node tag
    
    * don't mask the error
    
    * refactor: simplify the boilerplate
    
    * bump memory to avoid OOMKilled
    
    * Revert "remove fuze-node tag"
    
    This reverts commit ea38743cfa40168d0020193343d9f6b9eab8254e.
    
    * remove redundant f-string formatting
    
    * make test suite go over the current issue
    
    * uncomment out fuze tag in evaluation pipeline
    
    * drop duplicated id and add check_output decorator
    
    * revert added type in catalog
    
    * revert type in catalog
    
    * fix: test pipeline may generate ids which appear in both diseases and drugs
    
    * bump change version which invalidates cache
    
    * fix duplicated id in fabrication data
    
    * push changes
    
    * Refactor create_pairs (#1428)
    
    * add modified GT fabricator
    
    * add parameter
    
    * fix GT fabricator
    
    * join to matrix in GT pairgenerator eval. uncomment drugmech fabricator
    
    * reinit submodule
    
    * reinit submodule
    
    * remove duplicate entry
    
    * use correct ud
    
    * Update pipelines/matrix/src/matrix/pipeline_registry.py
    
    * r,
    
    * improved fabrication
    
    ---------
    
    Co-authored-by: Oliver Willekens <4733178+oliverw1@users.noreply.github.com>
    Co-authored-by: Pascal Brokmeier <pascal@everycure.org>
    Co-authored-by: amyford <30394098+amyford@users.noreply.github.com>
    Co-authored-by: Laurens Vijnck <laurens@everycure.org>
    Co-authored-by: Alexei Stepanenko <alexei.stepa@gmail.com>
    Co-authored-by: Laurens <90421718+lvijnck@users.noreply.github.com>

--- pipelines/matrix/src/matrix/pipelines/matrix_generation/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/matrix_generation/pipeline.py
@@ -1,6 +1,6 @@
 from kedro.pipeline import Pipeline, pipeline
 from matrix import settings
-from matrix.kedro4argo_node import ARGO_GPU_NODE_MEDIUM, ArgoNode
+from matrix.kedro4argo_node import ArgoNode, ArgoResourceConfig
 from matrix.pipelines.modelling.utils import partial_fold
 
 from . import nodes
@@ -48,10 +48,10 @@ def create_pipeline(**kwargs) -> Pipeline:
                             "known_pairs": "modelling.model_input.splits",
                             "drugs": "integration.int.drug_list.nodes.norm@pandas",
                             "diseases": "integration.int.disease_list.nodes.norm@pandas",
-                            "graph": "matrix_generation.feat.nodes@kg",
+                            "graph": "matrix_generation.feat.nodes@spark",
                             "clinical_trials": "integration.int.ec_clinical_trails.edges.norm@pandas",
                         },
-                        outputs=f"matrix_generation.prm.fold_{fold}.matrix_pairs",
+                        outputs=f"matrix_generation.prm.fold_{fold}.matrix_pairs@pandas",
                         name=f"generate_matrix_pairs_fold_{fold}",
                     )
                 ]
@@ -64,19 +64,20 @@ def create_pipeline(**kwargs) -> Pipeline:
                     ArgoNode(
                         func=nodes.make_predictions_and_sort,
                         inputs=[
-                            "matrix_generation.feat.nodes@kg",
-                            f"matrix_generation.prm.fold_{fold}.matrix_pairs",
+                            "matrix_generation.feat.nodes@spark",
+                            f"matrix_generation.prm.fold_{fold}.matrix_pairs@spark",
                             f"modelling.fold_{fold}.model_input.transformers",
                             f"modelling.fold_{fold}.models.model",
                             f"params:modelling.{model_name}.model_options.model_tuning_args.features",
                             "params:matrix_generation.treat_score_col_name",
                             "params:matrix_generation.not_treat_score_col_name",
                             "params:matrix_generation.unknown_score_col_name",
-                            "params:matrix_generation.matrix_generation_options.batch_by",
                         ],
                         outputs=f"matrix_generation.fold_{fold}.model_output.sorted_matrix_predictions@pandas",
                         name=f"make_predictions_and_sort_fold_{fold}",
-                        argo_config=ARGO_GPU_NODE_MEDIUM,
+                        argo_config=ArgoResourceConfig(
+                            cpu_limit=14, cpu_request=14, memory_limit=310, memory_request=155
+                        ),
                     ),
                     ArgoNode(
                         func=nodes.generate_reports,
