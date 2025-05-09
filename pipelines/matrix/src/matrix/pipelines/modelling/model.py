f4cdeb2a0f8e36085011d92d4996f25104625966
ahueb <alan@hueb.org>
2025-05-09 18:32:06 +0000

    adding initial per-fold GT degree weighting classes in pipeline

--- pipelines/matrix/src/matrix/pipelines/modelling/model.py
+++ pipelines/matrix/src/matrix/pipelines/modelling/model.py
@@ -2,6 +2,7 @@ from typing import Callable, List
 
 import numpy as np
 from sklearn.base import BaseEstimator
+from xgboost import XGBClassifier
 
 
 class ModelWrapper:
@@ -54,3 +55,25 @@ class ModelWrapper:
         """
         all_preds = np.array([estimator.predict_proba(X) for estimator in self._estimators])
         return np.apply_along_axis(self._agg_func, 0, all_preds)
+
+
+class XGBClassifierWeighted(XGBClassifier):
+    """XGBoost classifier that accepts sample weights during training."""
+
+    def fit(self, X, y, sample_weight=None, **kwargs):
+        """Fit the model with optional sample weights.
+
+        Args:
+            X: Training data
+            y: Target values
+            sample_weight: Sample weights
+            **kwargs: Additional arguments passed to XGBClassifier.fit()
+        """
+        if sample_weight is not None:
+            kwargs["sample_weight"] = sample_weight
+        return super().fit(X, y, **kwargs)
+
+    # def fit(self, X, y):
+    #     X_reduced = X[:, :-1]
+    #     weights = 1/X[:, -1]
+    #     return self.model.fit(X_reduced, y, sample_weight = weights)
