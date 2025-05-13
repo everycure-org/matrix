4b58fadd41b87964a4bfa21740395a1652ca4d3a
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-05-13 11:48:34 +0100

    Replaced unused var with used var

--- infra/deployments/wg2/main.tf
+++ infra/deployments/wg2/main.tf
@@ -1,10 +1,10 @@
 module "cloudbuild" {
-  source                       = "../../modules/components/cloudbuild"
-  github_repo_path_to_folder   = path.module
-  project_id                   = var.project_id
-  github_app_installation_id   = var.github_app_installation_id
-  github_repo_owner            = var.github_repo_owner
-  github_repo_name             = var.github_repo_name
-  github_repo_token            = var.gitops_repo_creds
-  github_repo_branch_to_run_on = var.github_branch_to_run_on
+  source                     = "../../modules/components/cloudbuild"
+  github_repo_path_to_folder = path.module
+  project_id                 = var.project_id
+  github_app_installation_id = var.github_app_installation_id
+  github_repo_owner          = var.github_repo_owner
+  github_repo_name           = var.github_repo_name
+  github_repo_token          = var.gitops_repo_creds
+  github_repo_deploy_branch  = var.github_branch_to_run_on
 }
