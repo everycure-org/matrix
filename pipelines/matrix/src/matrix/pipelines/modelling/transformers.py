87a059631c7c3011c5e393817a263d191e6116cb
ahueb <alan@hueb.org>
2025-05-18 17:57:04 +0000

    change plot path

--- pipelines/matrix/src/matrix/pipelines/modelling/transformers.py
+++ pipelines/matrix/src/matrix/pipelines/modelling/transformers.py
@@ -429,6 +429,6 @@ class WeightingTransformer(BaseEstimator, TransformerMixin):
         # out_dir = Path(f"{save_path}/datasets/reports/figures/weights")
         # out_dir = Path("gs://mtrx-us-central1-hub-dev-storage/kedro/data/tests/v0.4.5/datasets/reports/figures/weights")
         # out_dir.mkdir(parents=True, exist_ok=True)
-        catalog.save("weight_plot", plt.gcf(), node_name=os.getenv("KEDRO_NODE_NAME").replace(" ", "_"))
+        catalog.save("weight_diagnostic_plot", plt.gcf(), node_name=os.getenv("KEDRO_NODE_NAME").replace(" ", "_"))
         # plt.savefig(out_dir / f"{safe_name}.png")
         # plt.close()
