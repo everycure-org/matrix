7d2ebef6fdf6b0acebfeecc0551468ed547edfcf
Jacques Vergine <jacques.vergine35@gmail.com>
2025-05-26 16:29:18 +0200

    Fix sentinel tests and add sentinel for patch & minor releases

---
+++ pipelines/matrix/src/matrix/pipelines/sentinel/pipeline.py
@@ -0,0 +1,46 @@
+from kedro.pipeline import Pipeline, pipeline
+from matrix import settings
+from matrix.kedro4argo_node import ArgoNode
+from matrix.pipelines.data_release import last_node_name
+
+# Last node is made explicit because there's a kedro hook after_node_run
+# being triggered after the completion of the last node of this pipeline.
+# This node is monitored by the data release workflow for successful completion.
+# It's a sentinel indicating all data-delivering nodes are really done executing.
+# It _must_ be the very last node in this pipeline.
+
+kg_release_patch_outputs = ["data_release.prm.kgx_edges", "data_release.prm.kgx_nodes"] + [
+    f"integration.int.{source['name']}.normalization_summary"
+    for source in settings.DYNAMIC_PIPELINES_MAPPING().get("integration")
+]
+kg_release_outputs = kg_release_patch_outputs + ["data_release.prm.kg_edges", "data_release.prm.kg_nodes"]
+
+
+def sentinel_function(*args):
+    return True
+
+
+def create_sentinel_pipeline_kg_release_patch(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            ArgoNode(
+                func=sentinel_function,
+                inputs=kg_release_patch_outputs,
+                outputs="data_release.dummy",
+                name=last_node_name,
+            )
+        ]
+    )
+
+
+def create_sentinel_pipeline_kg_release(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            ArgoNode(
+                func=sentinel_function,
+                inputs=kg_release_outputs,
+                outputs="data_release.dummy",
+                name=last_node_name,
+            )
+        ]
+    )
