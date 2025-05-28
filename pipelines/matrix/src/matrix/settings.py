0ec9770fa394f7f034c97ea9242d3b23b98926eb
ahueb <alan@hueb.org>
2025-05-28 11:31:06 -0500

    removed off label from seetings.py

--- pipelines/matrix/src/matrix/settings.py
+++ pipelines/matrix/src/matrix/settings.py
@@ -57,7 +57,7 @@ DYNAMIC_PIPELINES_MAPPING = lambda: disable_private_datasets(
                 {"name": "ground_truth", "integrate_in_kg": False, "has_nodes": False},
                 # {"name": "drugmech", "integrate_in_kg": False, "has_nodes": False},
                 {"name": "ec_clinical_trails", "integrate_in_kg": False},
-                {"name": "off_label", "integrate_in_kg": False, "has_nodes": False},
+                # {"name": "off_label", "integrate_in_kg": False, "has_nodes": False},
             ],
             "modelling": {
                 "model_name": "xg_ensemble_weighted",  # model_name suggestions: xg_baseline, xg_ensemble, rf, xg_synth
@@ -71,8 +71,8 @@ DYNAMIC_PIPELINES_MAPPING = lambda: disable_private_datasets(
                 {"evaluation_name": "simple_classification_trials"},
                 {"evaluation_name": "disease_specific_trials"},
                 {"evaluation_name": "full_matrix_trials"},
-                {"evaluation_name": "disease_specific_off_label"},
-                {"evaluation_name": "full_matrix_off_label"},
+                # {"evaluation_name": "disease_specific_off_label"},
+                # {"evaluation_name": "full_matrix_off_label"},
             ],
             "stability": [
                 {"stability_name": "stability_overlap"},
