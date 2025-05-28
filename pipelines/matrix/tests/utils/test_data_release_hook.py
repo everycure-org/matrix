07552e228a9bf57d8b51556332a557521410cc61
amyford <30394098+amyford@users.noreply.github.com>
2025-05-28 08:18:52 +0100

    Remove unused imports via ruff (#1531)

--- pipelines/matrix/tests/utils/test_data_release_hook.py
+++ pipelines/matrix/tests/utils/test_data_release_hook.py
@@ -2,7 +2,6 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 from kedro.framework.context import KedroContext
-from kedro.framework.hooks import _create_hook_manager
 from matrix.hooks import ReleaseInfoHooks
 
 
