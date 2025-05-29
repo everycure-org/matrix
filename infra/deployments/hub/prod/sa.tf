599e788cf4cc132ec2c1d6ef08def93a0b4735d0
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-05-29 18:03:59 +0100

    Refactored code

--- infra/deployments/hub/prod/sa.tf
+++ infra/deployments/hub/prod/sa.tf
@@ -1,20 +1,3 @@
-locals {
-  group_sa_map = {
-    internal_data_science = {
-      group      = "group:data-science@everycure.org"
-      account_id = "sa-internal-data-science"
-    }
-    external_subcon_standard = {
-      group      = "group:ext.subcontractors.standard@everycure.org"
-      account_id = "sa-subcon-standard"
-    }
-    external_subcon_embiology = {
-      group      = "group:ext.subcontractors.embiology@everycure.org"
-      account_id = "sa-subcon-embiology"
-    }
-  }
-}
-
 resource "google_service_account" "sa" {
   for_each     = local.group_sa_map
   account_id   = each.value.account_id
