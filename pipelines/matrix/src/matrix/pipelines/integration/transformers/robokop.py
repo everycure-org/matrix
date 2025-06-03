43d5286ec5cb117a96e7be4de18d14e5db66a13a
Jason Reilly <jdr0887@gmail.com>
2025-06-03 13:18:22 -0400

    adding init with version, match/case on version

--- pipelines/matrix/src/matrix/pipelines/integration/transformers/robokop.py
+++ pipelines/matrix/src/matrix/pipelines/integration/transformers/robokop.py
@@ -18,6 +18,10 @@ ROBOKOP_SEPARATOR = r"\|"
 
 
 class RobokopTransformer(GraphTransformer):
+    def __init__(self, version: str, select_cols: str = True):
+        super().__init__(select_cols)
+        self._version = version
+
     def transform_nodes(self, nodes_df: ps.DataFrame, **kwargs) -> ps.DataFrame:
         """Transform Robokop nodes to our target schema.
 
@@ -27,21 +31,42 @@ class RobokopTransformer(GraphTransformer):
         Returns:
             Transformed DataFrame.
         """
-        # fmt: off
-        return (
-            nodes_df
-            .withColumnRenamed("name:string", "name")
-            .withColumnRenamed("description:string", "description")
-            .withColumn("upstream_data_source",              F.array(F.lit("robokop")))
-            .withColumn("all_categories",                    F.split(F.col("category"), ROBOKOP_SEPARATOR))
-            .withColumn("equivalent_identifiers",            F.split(F.col("equivalent_identifiers"), ROBOKOP_SEPARATOR))
-            .withColumn("labels",                            F.col("all_categories"))
-            .withColumn("publications",                      F.lit(None).cast(T.ArrayType(T.StringType())))
-            .withColumn("international_resource_identifier", F.lit(None).cast(T.StringType()))
-            # getting most specific category
-            .transform(determine_most_specific_category)
-        )
-        # fmt: on
+        match self._version:
+            case "c5ec1f282158182f":
+                # fmt: off
+                df = (nodes_df
+                    .withColumn("upstream_data_source",              F.array(F.lit("robokop")))
+                    .withColumn("all_categories",                    F.split(F.col("category:LABEL"), ROBOKOP_SEPARATOR))
+                    .withColumn("equivalent_identifiers",            F.split(F.col("equivalent_identifiers:string[]"), ROBOKOP_SEPARATOR))
+                    .withColumn("labels",                            F.col("all_categories"))
+                    .withColumn("publications",                      F.lit(None).cast(T.ArrayType(T.StringType())))
+                    .withColumn("international_resource_identifier", F.lit(None).cast(T.StringType()))
+                    .withColumnRenamed("id:ID", "id")
+                    .withColumnRenamed("name:string", "name")
+                    .withColumnRenamed("description:string", "description")
+                    # getting most specific category
+                    .transform(determine_most_specific_category)
+                )
+                # fmt: on
+            case "30fd1bfc18cd5ccb.1":
+                # fmt: off
+                df = (nodes_df
+                      .withColumn("upstream_data_source",              F.array(F.lit("robokop")))
+                      .withColumn("all_categories",                    F.split(F.col("category"), ROBOKOP_SEPARATOR))
+                      .withColumn("equivalent_identifiers",            F.split(F.col("equivalent_identifiers"), ROBOKOP_SEPARATOR))
+                      .withColumn("labels",                            F.col("all_categories"))
+                      .withColumn("publications",                      F.lit(None).cast(T.ArrayType(T.StringType())))
+                      .withColumn("international_resource_identifier", F.lit(None).cast(T.StringType()))
+                      # getting most specific category
+                      .transform(determine_most_specific_category)
+                      )
+                # fmt: on
+            case "30fd1bfc18cd5ccb":
+                df = latest_nodes_dataframe(nodes_df)
+            case _:
+                df = latest_nodes_dataframe(nodes_df)
+
+        return df
 
     def transform_edges(self, edges_df: ps.DataFrame, **kwargs) -> ps.DataFrame:
         """Transform Robokop edges to our target schema.
@@ -52,17 +77,75 @@ class RobokopTransformer(GraphTransformer):
         Returns:
             Transformed DataFrame.
         """
-        # fmt: off
-        return (
-            edges_df
-            .withColumn("aggregator_knowledge_source",              F.split(F.col("aggregator_knowledge_source"), ROBOKOP_SEPARATOR))
-            .withColumn("publications",                             F.split(F.col("publications:string[]"), ROBOKOP_SEPARATOR))
-            .withColumn("upstream_data_source",                     F.array(F.lit("robokop")))
-            .withColumn("subject_aspect_qualifier",                 F.lit(None).cast(T.StringType()))
-            .withColumn("subject_direction_qualifier",              F.lit(None).cast(T.StringType()))
-            .withColumn("num_references",                           F.lit(None).cast(T.IntegerType())) # Required to match EmBiology schema
-            .withColumn("num_sentences",                            F.lit(None).cast(T.IntegerType())) # Required to match EmBiology schema
-            .withColumnRenamed("object_aspect_qualifier:string",    "object_aspect_qualifier")
-            .withColumnRenamed("object_direction_qualifier:string", "object_direction_qualifier")
-        )
-        # fmt: on
+        match self._version:
+            case "c5ec1f282158182f":
+                # fmt: off
+                df = (edges_df
+                      .withColumnRenamed("subject:START_ID",                  "subject")
+                      .withColumnRenamed("predicate:TYPE",                    "predicate")
+                      .withColumnRenamed("object:END_ID",                      "object")
+                      .withColumnRenamed("knowledge_level:string",            "knowledge_level")
+                      .withColumnRenamed("primary_knowledge_source:string",   "primary_knowledge_source")
+                      .withColumnRenamed("object_aspect_qualifier:string",    "object_aspect_qualifier")
+                      .withColumnRenamed("object_direction_qualifier:string", "object_direction_qualifier")
+                      .withColumn("upstream_data_source",                     F.array(F.lit("robokop")))
+                      .withColumn("publications",                             F.split(F.col("publications:string[]"), ROBOKOP_SEPARATOR))
+                      .withColumn("aggregator_knowledge_source",              F.split(F.col("aggregator_knowledge_source:string[]"), ROBOKOP_SEPARATOR))
+                      .withColumn("subject_aspect_qualifier",                 F.lit(None).cast(T.StringType()))
+                      .withColumn("subject_direction_qualifier",              F.lit(None).cast(T.StringType()))
+                      )
+                # fmt: off
+            case "30fd1bfc18cd5ccb.1":
+                # fmt: off
+                df = (edges_df
+                       .withColumn("aggregator_knowledge_source",              F.split(F.col("aggregator_knowledge_source"), ROBOKOP_SEPARATOR))
+                       .withColumn("publications",                             F.split(F.col("publications:string[]"), ROBOKOP_SEPARATOR))
+                       .withColumn("upstream_data_source",                     F.array(F.lit("robokop")))
+                       .withColumn("subject_aspect_qualifier",                 F.lit(None).cast(T.StringType()))
+                       .withColumn("subject_direction_qualifier",              F.lit(None).cast(T.StringType()))
+                       .withColumn("num_references",                           F.lit(None).cast(T.IntegerType())) # Required to match EmBiology schema
+                       .withColumn("num_sentences",                            F.lit(None).cast(T.IntegerType())) # Required to match EmBiology schema
+                       .withColumnRenamed("object_aspect_qualifier:string",    "object_aspect_qualifier")
+                       .withColumnRenamed("object_direction_qualifier:string", "object_direction_qualifier")
+                )
+                # fmt: on
+            case "30fd1bfc18cd5ccb":
+                df = latest_edges_dataframe(edges_df)
+            case _:
+                df = latest_edges_dataframe(edges_df)
+        return df
+
+
+def latest_nodes_dataframe(nodes_df: ps.DataFrame):
+    # fmt: off
+    df = (nodes_df
+          .withColumnRenamed("name:string", "name")
+          .withColumnRenamed("description:string", "description")
+          .withColumn("upstream_data_source",              F.array(F.lit("robokop")))
+          .withColumn("all_categories",                    F.split(F.col("category"), ROBOKOP_SEPARATOR))
+          .withColumn("equivalent_identifiers",            F.split(F.col("equivalent_identifiers"), ROBOKOP_SEPARATOR))
+          .withColumn("labels",                            F.col("all_categories"))
+          .withColumn("publications",                      F.lit(None).cast(T.ArrayType(T.StringType())))
+          .withColumn("international_resource_identifier", F.lit(None).cast(T.StringType()))
+          # getting most specific category
+          .transform(determine_most_specific_category)
+    )
+    # fmt: on
+    return df
+
+
+def latest_edges_dataframe(edges_df: ps.DataFrame):
+    # fmt: off
+    df = (edges_df
+          .withColumn("aggregator_knowledge_source",              F.split(F.col("aggregator_knowledge_source"), ROBOKOP_SEPARATOR))
+          .withColumn("publications",                             F.split(F.col("publications:string[]"), ROBOKOP_SEPARATOR))
+          .withColumn("upstream_data_source",                     F.array(F.lit("robokop")))
+          .withColumn("subject_aspect_qualifier",                 F.lit(None).cast(T.StringType()))
+          .withColumn("subject_direction_qualifier",              F.lit(None).cast(T.StringType()))
+          .withColumn("num_references",                           F.lit(None).cast(T.IntegerType())) # Required to match EmBiology schema
+          .withColumn("num_sentences",                            F.lit(None).cast(T.IntegerType())) # Required to match EmBiology schema
+          .withColumnRenamed("object_aspect_qualifier:string",    "object_aspect_qualifier")
+          .withColumnRenamed("object_direction_qualifier:string", "object_direction_qualifier")
+    )
+    # fmt: off
+    return df
