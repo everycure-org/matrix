855f3184ed1ed99e65dc520309aeace0300d168a
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-06-04 17:46:43 +0100

    Removed extra redundant role

--- infra/modules/components/cloudbuild/iam.tf
+++ infra/modules/components/cloudbuild/iam.tf
@@ -10,12 +10,6 @@ resource "google_project_iam_member" "cloudbuild_builder_owner_role" {
   member  = google_service_account.cloudbuild_sa.member
 }
 
-resource "google_project_iam_member" "cloudbuild_builder_log_role" {
-  project = var.project_id
-  role    = "roles/logging.logWriter"
-  member  = google_service_account.cloudbuild_sa.member
-}
-
 resource "google_secret_manager_secret_iam_policy" "policy" {
   secret_id   = google_secret_manager_secret.github_token.secret_id
   project     = var.project_id
