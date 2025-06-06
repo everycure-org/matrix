81f8b7754366651341cf061e87e5f6995d4f7d11
Jason Reilly <jdr0887@gmail.com>
2025-06-06 10:44:01 -0400

    removing match statement when there is only one case

--- pipelines/matrix/src/matrix/pipelines/integration/transformers/medical.py
+++ pipelines/matrix/src/matrix/pipelines/integration/transformers/medical.py
@@ -18,25 +18,23 @@ class MedicalTransformer(GraphTransformer):
         self._drop_duplicates = drop_duplicates
 
     def transform_nodes(self, nodes_df: ps.DataFrame, **kwargs) -> ps.DataFrame:
-        match self._version:
-            case _:
-                # fmt: off
-                df = (
-                    nodes_df
-                    .withColumn("id",                                f.col("normalized_curie"))
-                    .withColumn("name",                              f.col("label"))
-                    .withColumn("upstream_data_source",              f.array(f.lit("ec_medical")))
-                    .withColumn("category",                          f.lit("category")) # FUTURE: Let's get rid of the category
-                    .withColumn("labels",                            f.array(f.col("types")))
-                    .withColumn("all_categories",                    f.array(f.col("types")))
-                    .withColumn("equivalent_identifiers",            f.array(f.col("id")))
-                    .withColumn("publications",                      f.lit(None).cast(T.ArrayType(T.StringType())))
-                    .withColumn("international_resource_identifier", f.col("id"))
-                    # .transform(determine_most_specific_category, biolink_categories_df) need this?
-                    # Filter nodes we could not correctly resolve
-                    .filter(f.col("id").isNotNull())
-                )
-                # fmt: on
+        # fmt: off
+        df = (
+            nodes_df
+            .withColumn("id",                                f.col("normalized_curie"))
+            .withColumn("name",                              f.col("label"))
+            .withColumn("upstream_data_source",              f.array(f.lit("ec_medical")))
+            .withColumn("category",                          f.lit("category")) # FUTURE: Let's get rid of the category
+            .withColumn("labels",                            f.array(f.col("types")))
+            .withColumn("all_categories",                    f.array(f.col("types")))
+            .withColumn("equivalent_identifiers",            f.array(f.col("id")))
+            .withColumn("publications",                      f.lit(None).cast(T.ArrayType(T.StringType())))
+            .withColumn("international_resource_identifier", f.col("id"))
+            # .transform(determine_most_specific_category, biolink_categories_df) need this?
+            # Filter nodes we could not correctly resolve
+            .filter(f.col("id").isNotNull())
+        )
+        # fmt: on
 
         if self._drop_duplicates:
             df = df.dropDuplicates(["id"])  # Drop any duplicate nodes
@@ -44,30 +42,28 @@ class MedicalTransformer(GraphTransformer):
         return df
 
     def transform_edges(self, edges_df: ps.DataFrame, **kwargs) -> ps.DataFrame:
-        match self._version:
-            case _:
-                # fmt: off
-                df = (
-                    edges_df
-                    .withColumn("subject",                       f.col("SourceId"))
-                    .withColumn("object",                        f.col("TargetId"))
-                    .withColumn("predicate",                     f.concat(f.lit("biolink:"), f.lit(":"), f.col("Label")))
-                    .withColumn("upstream_data_source",          f.array(f.lit("ec_medical")))
-                    .withColumn("knowledge_level",               f.lit(None).cast(T.StringType()))
-                    .withColumn("agent_type",                    f.lit(None).cast(T.StringType()))
-                    .withColumn("aggregator_knowledge_source",   f.array(f.lit('medical team')))
-                    .withColumn("primary_knowledge_source",      f.lit('medical team').cast(T.StringType()))
-                    .withColumn("publications",                  f.array(f.lit('medical team')))
-                    .withColumn("subject_aspect_qualifier",      f.lit(None).cast(T.StringType())) #not present
-                    .withColumn("subject_direction_qualifier",   f.lit(None).cast(T.StringType())) #not present
-                    .withColumn("object_aspect_qualifier",       f.lit(None).cast(T.StringType())) #not present
-                    .withColumn("object_direction_qualifier",    f.lit(None).cast(T.StringType())) #not present
-                    .withColumn("num_references",                f.lit(None).cast(T.IntegerType()))
-                    .withColumn("num_sentences",                 f.lit(None).cast(T.IntegerType()))
-
-                    # Filter edges we could not correctly resolve
-                    .filter(f.col("subject").isNotNull() & f.col("object").isNotNull())
-                )
+        # fmt: off
+        df = (
+            edges_df
+            .withColumn("subject",                       f.col("SourceId"))
+            .withColumn("object",                        f.col("TargetId"))
+            .withColumn("predicate",                     f.concat(f.lit("biolink:"), f.lit(":"), f.col("Label")))
+            .withColumn("upstream_data_source",          f.array(f.lit("ec_medical")))
+            .withColumn("knowledge_level",               f.lit(None).cast(T.StringType()))
+            .withColumn("agent_type",                    f.lit(None).cast(T.StringType()))
+            .withColumn("aggregator_knowledge_source",   f.array(f.lit('medical team')))
+            .withColumn("primary_knowledge_source",      f.lit('medical team').cast(T.StringType()))
+            .withColumn("publications",                  f.array(f.lit('medical team')))
+            .withColumn("subject_aspect_qualifier",      f.lit(None).cast(T.StringType())) #not present
+            .withColumn("subject_direction_qualifier",   f.lit(None).cast(T.StringType())) #not present
+            .withColumn("object_aspect_qualifier",       f.lit(None).cast(T.StringType())) #not present
+            .withColumn("object_direction_qualifier",    f.lit(None).cast(T.StringType())) #not present
+            .withColumn("num_references",                f.lit(None).cast(T.IntegerType()))
+            .withColumn("num_sentences",                 f.lit(None).cast(T.IntegerType()))
+            # Filter edges we could not correctly resolve
+            .filter(f.col("subject").isNotNull() & f.col("object").isNotNull())
+        )
+        # fmt: on
 
         if self._drop_duplicates:
             df = df.dropDuplicates(["subject", "object", "predicate"])
