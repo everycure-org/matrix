319afd533784828e9cfd2f5375dc845a5c46ae35
Jason Reilly <jdr0887@gmail.com>
2025-06-11 15:33:04 -0400

    not able to run successfully while these columns aren't specified

--- pipelines/matrix/src/matrix/pipelines/integration/transformers/clinical_trails.py
+++ pipelines/matrix/src/matrix/pipelines/integration/transformers/clinical_trails.py
@@ -17,11 +17,13 @@ class ClinicalTrialsTransformer(GraphTransformer):
 
     def transform_nodes(self, nodes_df: DataFrame, **kwargs) -> DataFrame:
         # fmt: off
-        df = (
-            nodes_df
+        df = (nodes_df
             .withColumn("id",                                f.col("curie"))
+            .withColumn("name",                              f.lit(None).cast(T.StringType()))
+            .withColumn("description",                       f.lit(None).cast(T.StringType()))
             .withColumn("upstream_data_source",              f.array(f.lit("ec_clinical_trails")))
             .withColumn("labels",                            f.array(f.lit("entity label"))) # TODO: Fix entity labels for medical?
+            .withColumn("category",                          f.lit("category")) # TODO fix
             .withColumn("all_categories",                    f.array(f.lit("biolink:"))) # TODO fix
             .withColumn("equivalent_identifiers",            f.array(f.col("id")))
             .withColumn("publications",                      f.lit(None).cast(T.ArrayType(T.StringType())))
@@ -38,14 +40,26 @@ class ClinicalTrialsTransformer(GraphTransformer):
         # fmt: off
         df = (
             edges_df
-            .withColumn("subject",                  f.col("drug_curie"))
-            .withColumn("object",                   f.col("disease_curie"))
-            .withColumn("predicate",                f.lit("clinical_trails"))
+            .withColumn("subject",                      f.col("drug_curie"))
+            .withColumn("object",                       f.col("disease_curie"))
+            .withColumn("predicate",                    f.lit("clinical_trails"))
             .filter((f.col("subject").isNotNull()) & (f.col("object").isNotNull()))
-            .withColumn("significantly_better",     f.col('significantly_better').cast('int'))
-            .withColumn("significantly_worse",      f.col('significantly_worse').cast('int'))
-            .withColumn("non_significantly_worse",  f.col('non_significantly_worse').cast('int'))
-            .withColumn("non_significantly_better", f.col('non_significantly_better').cast('int'))
+            .withColumn("upstream_data_source",         f.array(f.lit("clinical_trails")))
+            .withColumn("knowledge_level",              f.lit(None).cast(T.StringType()))
+            .withColumn("agent_type",                   f.lit(None).cast(T.StringType()))
+            .withColumn("primary_knowledge_source",     f.lit(None).cast(T.StringType()))
+            .withColumn("subject_aspect_qualifier",     f.lit(None).cast(T.StringType()))
+            .withColumn("subject_direction_qualifier",  f.lit(None).cast(T.StringType()))
+            .withColumn("object_aspect_qualifier",      f.lit(None).cast(T.StringType()))
+            .withColumn("object_direction_qualifier",   f.lit(None).cast(T.StringType()))
+            .withColumn("aggregator_knowledge_source",  f.lit(None).cast(T.ArrayType(T.StringType())))
+            .withColumn("publications",                 f.lit(None).cast(T.ArrayType(T.StringType())))
+            .withColumn("num_references",               f.lit(None).cast(T.ArrayType(T.IntegerType())))
+            .withColumn("num_sentences",                f.lit(None).cast(T.ArrayType(T.IntegerType())))
+            .withColumn("significantly_better",         f.col('significantly_better').cast('int'))
+            .withColumn("significantly_worse",          f.col('significantly_worse').cast('int'))
+            .withColumn("non_significantly_worse",      f.col('non_significantly_worse').cast('int'))
+            .withColumn("non_significantly_better",     f.col('non_significantly_better').cast('int'))
         )
         # fmt: on
 
