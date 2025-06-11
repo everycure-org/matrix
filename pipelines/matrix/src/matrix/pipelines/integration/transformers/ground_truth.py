5cecbd53ea0ecab390e39dd890441bcb28f319db
Jason Reilly <jdr0887@gmail.com>
2025-06-11 15:31:33 -0400

    movnig constructor to parent

--- pipelines/matrix/src/matrix/pipelines/integration/transformers/ground_truth.py
+++ pipelines/matrix/src/matrix/pipelines/integration/transformers/ground_truth.py
@@ -12,10 +12,6 @@ logger = logging.getLogger(__name__)
 class KGMLGroundTruthTransformer(Transformer):
     """Transformer for ground truth data"""
 
-    def __init__(self, version: str):
-        super().__init__()
-        self._version = version
-
     def transform(self, edges_df: DataFrame, **kwargs) -> dict[str, DataFrame]:
         edges = self._extract_edges(edges_df)
         id_list = edges.select("subject").union(edges.select("object")).distinct().withColumnRenamed("subject", "id")
