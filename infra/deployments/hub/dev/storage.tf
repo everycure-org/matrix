680afbca8023dab4cbda85e709c86347e5b75f45
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-06-20 12:45:19 +0100

    Added missing permission for production SA

--- infra/deployments/hub/dev/storage.tf
+++ infra/deployments/hub/dev/storage.tf
@@ -101,3 +101,10 @@ resource "google_storage_bucket_iam_member" "compute_engine_default" {
   role   = "roles/storage.bucketViewer"
   member = local.orchard_prod_compute_service_account
 }
+
+# Add IAM binding for the custom Cloud Build service account from prod
+resource "google_storage_bucket_iam_member" "custom_cloud_build_sa_access" {
+  bucket = var.storage_bucket_name
+  role   = "roles/storage.admin"
+  member = "serviceAccount:custom-cloud-build-sa@mtrx-hub-prod-sms.iam.gserviceaccount.com"
+}
