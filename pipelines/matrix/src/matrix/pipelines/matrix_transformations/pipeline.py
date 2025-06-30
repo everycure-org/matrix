b71d87cf264960cdfbd6bd96054910f517cb0d87
Amy Ford <amy@everycure.org>
2025-06-30 16:21:37 +0100

    Union with known_pairs to generate dataset with training data included

--- pipelines/matrix/src/matrix/pipelines/matrix_transformations/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/matrix_transformations/pipeline.py
@@ -36,9 +36,10 @@ def create_pipeline(**kwargs) -> Pipeline:
             [
                 ArgoNode(
                     func=nodes.return_predictions,
-                    inputs=[
-                        f"matrix_transformations.fold_{n_cross_val_folds}.model_output.sorted_matrix_predictions@spark",
-                    ],
+                    inputs={
+                        "sorted_matrix_df": f"matrix_transformations.fold_{n_cross_val_folds}.model_output.sorted_matrix_predictions@spark",
+                        "known_pairs": "modelling.model_input.splits",
+                    },
                     outputs=f"matrix_transformations.full_matrix_output@spark",
                     name="store_transformed_predictions",
                 ),
