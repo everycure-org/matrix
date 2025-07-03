72d0f0c82ef51af5f59924371e5c4e8c0fb7e344
Kevin Schaper <kevinschaper@gmail.com>
2025-07-03 11:03:54 -0700

    add disease list nodes query

---
+++ services/kg_dashboard/sources/bq/disease_list_nodes.sql
@@ -0,0 +1,8 @@
+SELECT 
+    disease_list_nodes.*, 
+    COALESCE(COUNT(*), 0) as edge_count 
+FROM `mtrx-hub-dev-3of.release_${bq_release_version}.disease_list_nodes_normalized` as disease_list_nodes
+LEFT OUTER JOIN `mtrx-hub-dev-3of.release_${bq_release_version}.edges_unified` as edges
+    ON disease_list_nodes.id = edges.subject 
+    OR disease_list_nodes.id = edges.object
+GROUP BY ALL
