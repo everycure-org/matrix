9749b31eb632f16ceb19f8f8ba7439f61b539b63
Kevin Schaper <kevinschaper@gmail.com>
2025-07-03 11:03:37 -0700

    add drug list nodes query

---
+++ services/kg_dashboard/sources/bq/drug_list_nodes.sql
@@ -0,0 +1,8 @@
+SELECT 
+    drug_list_nodes.*, 
+    COALESCE(COUNT(*), 0) as edge_count 
+FROM `mtrx-hub-dev-3of.release_${bq_release_version}.drug_list_nodes_normalized` as drug_list_nodes
+LEFT OUTER JOIN `mtrx-hub-dev-3of.release_${bq_release_version}.edges_unified` as edges
+    ON drug_list_nodes.id = edges.subject 
+    OR drug_list_nodes.id = edges.object
+GROUP BY ALL
