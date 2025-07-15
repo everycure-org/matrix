af74daa37eb54f78397006d4aa144419d022a9a5
ahueb <alan@hueb.org>
2025-07-15 14:03:31 -0500

    merging updates from main

--- pipelines/matrix/src/matrix/pipelines/integration/transformers/off_label.py
+++ pipelines/matrix/src/matrix/pipelines/integration/transformers/off_label.py
@@ -1,21 +1,27 @@
 import logging
 
+import pyspark.sql as ps
+import pyspark.sql.functions as f
+from pyspark.sql import DataFrame
+
+from .transformer import Transformer
+
 logger = logging.getLogger(__name__)
 
 
-# class OffLabelTransformer(Transformer):
-#     """Transformer for off label data"""
+class OffLabelTransformer(Transformer):
+    """Transformer for off label data"""
 
-#     def transform(self, edges_df: DataFrame, **kwargs) -> dict[str, DataFrame]:
-#         edges = self._extract_edges(edges_df)
-#         id_list = edges.select("subject").union(edges.select("object")).distinct().withColumnRenamed("subject", "id")
-#         return {"nodes": id_list, "edges": edges}
+    def transform(self, edges_df: DataFrame, **kwargs) -> dict[str, DataFrame]:
+        edges = self._extract_edges(edges_df)
+        id_list = edges.select("subject").union(edges.select("object")).distinct().withColumnRenamed("subject", "id")
+        return {"nodes": id_list, "edges": edges}
 
-#     @staticmethod
-#     def _extract_edges(edges_df: ps.DataFrame) -> ps.DataFrame:
-#         return (
-#             edges_df.withColumnRenamed("curie_drug", "subject")
-#             .withColumnRenamed("curie_disease", "object")
-#             .withColumn("y", f.lit(1))  # all pairs are positive
-#             .withColumn("predicate", f.lit("off_label"))
-#         )
+    @staticmethod
+    def _extract_edges(edges_df: ps.DataFrame) -> ps.DataFrame:
+        return (
+            edges_df.withColumnRenamed("curie_drug", "subject")
+            .withColumnRenamed("curie_disease", "object")
+            .withColumn("y", f.lit(1))  # all pairs are positive
+            .withColumn("predicate", f.lit("off_label"))
+        )
