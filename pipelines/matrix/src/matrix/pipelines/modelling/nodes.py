3ef218983c7dd9cc00cb4bc60ac61f5f9a254005
Amy Ford <amy@everycure.org>
2025-07-16 08:30:24 +0100

    fix ruff error

--- pipelines/matrix/src/matrix/pipelines/modelling/nodes.py
+++ pipelines/matrix/src/matrix/pipelines/modelling/nodes.py
@@ -13,7 +13,7 @@ from sklearn.impute._base import _BaseImputer
 from sklearn.model_selection import BaseCrossValidator
 
 from matrix.datasets.graph import KnowledgeGraph
-from matrix.datasets.pair_generator import DiseaseSplitDrugDiseasePairGenerator, SingleLabelPairGenerator
+from matrix.datasets.pair_generator import SingleLabelPairGenerator
 from matrix.inject import OBJECT_KW, inject_object, make_list_regexable, unpack_params
 from matrix.utils.pandera_utils import Column, DataFrameSchema, check_output
 
