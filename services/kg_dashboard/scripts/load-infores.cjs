39c398365afad40be2f477aa765e548e4d7cce9b
Kevin Schaper <kevinschaper@gmail.com>
2025-07-16 19:57:22 -0700

    download infores & valid_edge_type files with curl first, then load into duckdb (to avoid worrying about httpfs extension)

--- services/kg_dashboard/scripts/load-infores.cjs
+++ services/kg_dashboard/scripts/load-infores.cjs
@@ -1,15 +1,18 @@
 const duckdb = require('duckdb');
+const path = require('path');
+
+const dataFile = path.join(__dirname, 'temp_infores_catalog.jsonl');
 
 const db = new duckdb.Database('sources/infores/infores.duckdb')
-
 const conn = db.connect(); 
 
-conn.all("create or replace table catalog as select * from 'https://github.com/biolink/information-resource-registry/releases/latest/download/infores_catalog.jsonl'", function(err, res) {
+conn.all(`create or replace table catalog as select * from '${dataFile}'`, function(err, res) {
   if (err) {
     console.error(err);
     return;
   }
+  console.log('Table created successfully');
+  
+  conn.close();
+  db.close();
 });
-
-conn.close();
-db.close();
--- services/kg_dashboard/scripts/load-valid-edge-types.cjs
+++ services/kg_dashboard/scripts/load-valid-edge-types.cjs
@@ -1,14 +1,18 @@
 const duckdb = require('duckdb');
+const path = require('path');
+
+const dataFile = path.join(__dirname, 'temp_valid_edge_types.tsv');
 
 const db = new duckdb.Database('sources/valid_edge_types/valid_edge_types.duckdb')
 
 const conn = db.connect(); 
 
-conn.all("create or replace table valid_edge_types as select * from 'https://github.com/everycure-org/matrix-schema/releases/latest/download/valid_biolink_edge_types.tsv'", function(err, res) {
+conn.all(`create or replace table valid_edge_types as select * from '${dataFile}'`, function(err, res) {
   if (err) {
     console.error(err);
     return;
   }
+  console.log('Table created successfully');
 });
 
 conn.close();
