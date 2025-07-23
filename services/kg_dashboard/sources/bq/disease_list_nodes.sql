412d3e48d9c78c08aa170f8e20a20e1175d2e803
Kevin Schaper <kevinschaper@gmail.com>
2025-07-23 13:14:16 -0700

    select only the columns we need for bq.drug_list_nodes & bq.disease_list_nodes

--- services/kg_dashboard/sources/bq/disease_list_nodes.sql
+++ services/kg_dashboard/sources/bq/disease_list_nodes.sql
@@ -1,5 +1,6 @@
 SELECT 
-    disease_list_nodes.*, 
+    disease_list_nodes.id,
+    disease_list_nodes.name,
     COALESCE(COUNT(*), 0) as edge_count 
 FROM `mtrx-hub-dev-3of.release_${bq_release_version}.disease_list_nodes_normalized` as disease_list_nodes
 LEFT OUTER JOIN `mtrx-hub-dev-3of.release_${bq_release_version}.edges_unified` as edges
--- services/kg_dashboard/sources/bq/drug_list_nodes.sql
+++ services/kg_dashboard/sources/bq/drug_list_nodes.sql
@@ -1,5 +1,6 @@
 SELECT 
-    drug_list_nodes.*, 
+    drug_list_nodes.id,
+    drug_list_nodes.name,
     COALESCE(COUNT(*), 0) as edge_count 
 FROM `mtrx-hub-dev-3of.release_${bq_release_version}.drug_list_nodes_normalized` as drug_list_nodes
 LEFT OUTER JOIN `mtrx-hub-dev-3of.release_${bq_release_version}.edges_unified` as edges
