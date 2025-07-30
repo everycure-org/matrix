42e2a9a40a6fe8515faecc32f31eed093e4c2f9c
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-07-30 14:14:45 +0100

    feat: Update cleanup policies and variable validations for artifact registry module

--- infra/modules/artifact_registry/variables.tf
+++ infra/modules/artifact_registry/variables.tf
@@ -9,14 +9,22 @@ variable "location" {
 }
 
 variable "repository_id" {
-  description = "The ID of the repository."
-  type        = string
+  description = "The ID of the repository. Should be separated by dashes (e.g., 'my-repo')."
+  validation {
+    condition     = can(regex("^[a-z][a-z0-9-]{4,63}$", var.repository_id))
+    error_message = "repository_id must start with a lowercase letter, followed by lowercase letters, digits, or dashes, and be between 5 and 63 characters long."
+  }
+  type = string
 }
 
 variable "format" {
   description = "The format of the repository (e.g., DOCKER, MAVEN, NPM)."
   type        = string
   default     = "DOCKER"
+  validation {
+    condition     = contains(["DOCKER", "MAVEN", "NPM"], var.format)
+    error_message = "format must be one of: DOCKER, MAVEN, NPM."
+  }
 }
 
 variable "description" {
@@ -27,8 +35,12 @@ variable "description" {
 
 variable "delete_older_than_days" {
   description = "The number of days after which to delete images. Set to 0 to disable. Currently set to 3 days."
-  type        = number
-  default     = 3
+  type        = string
+  validation {
+    condition     = can(regex("^[0-9]+[dD]$", var.delete_older_than_days))
+    error_message = "delete_older_than_days must be a number followed by 'd' (e.g., '3d' for 3 days)."
+  }
+  default = "3d" # Default value is set to 3 days
 }
 
 variable "keep_count" {
@@ -37,9 +49,9 @@ variable "keep_count" {
   default     = 0
 }
 
-variable "tag_state" {
-  description = "The tag state to apply to the cleanup policies. Defaults to ANY."
-  type        = string
-  default     = "ANY"
+variable "sample_run_tag_prefixes" {
+  description = "The tag prefixes for sample-run images."
+  type        = list(string)
+  default     = []
 
 }
--- infra/modules/stacks/compute_cluster/gcr.tf
+++ infra/modules/stacks/compute_cluster/gcr.tf
@@ -1,9 +1,11 @@
 module "image_repo" {
   source = "../../artifact_registry"
 
-  project_id    = var.project_id
-  location      = var.default_region
-  repository_id = "matrix-images"
-  description   = "images for matrix project"
-  format        = "DOCKER"
+  project_id              = var.project_id
+  location                = var.default_region
+  repository_id           = "matrix-images"
+  description             = "images for matrix project"
+  format                  = "DOCKER"
+  delete_older_than_days  = "14d"
+  sample_run_tag_prefixes = ["sample-run-"]
 }
