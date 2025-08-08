2976e0eae3ddf43ebbc3854d62fa5ccc85de7d0d
piotrkan <piotr@everycure.org>
2025-08-08 16:38:24 +0100

    make orchard private

--- pipelines/matrix/src/matrix/pipelines/fabricator/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/fabricator/pipeline.py
@@ -209,8 +209,8 @@ def create_pipeline(**kwargs) -> Pipeline:
                     "rtx_nodes": "ingestion.raw.rtx_kg2.nodes@pandas",
                 },
                 outputs={
-                    "nodes": "ingestion.raw.orchard.feedback_data.nodes@pandas",
-                    "edges": "ingestion.raw.orchard.feedback_data.edges@pandas",
+                    "nodes": "ingestion.raw.orchard.nodes@pandas",
+                    "edges": "ingestion.raw.orchard.edges@pandas",
                 },
                 name="fabricate_orchard_datasets",
             ),
--- pipelines/matrix/src/matrix/pipelines/ingestion/pipeline.py
+++ pipelines/matrix/src/matrix/pipelines/ingestion/pipeline.py
@@ -46,13 +46,6 @@ def create_pipeline(**kwargs) -> Pipeline:
                 name="write_disease_list",
                 tags=["disease-list"],
             ),
-            node(
-                func=lambda x: x,
-                inputs=["ingestion.raw.orchard.feedback_data@spark"],
-                outputs="ingestion.int.orchard.feedback_data.nodes@pandas",
-                name="write_orchard_feedback_data",
-                tags=["orchard"],
-            ),
         ]
     )
 
