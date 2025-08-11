ff0d5a77b8bffda6481c21dc8af3b36ff8599f6a
Jason Reilly <jdr0887@gmail.com>
2025-08-11 10:18:31 -0400

    removing pandas

--- pipelines/matrix/src/matrix/pipelines/fabricator/nodes.py
+++ pipelines/matrix/src/matrix/pipelines/fabricator/nodes.py
@@ -1,19 +1,14 @@
 import logging
 
-import pandas as pd
 import polars as pl
 from matrix_validator.validator_polars import ValidatorPolarsDataFrameImpl
 
 logger = logging.getLogger(__name__)
 
 
-def validate_datasets(nodes: pd.DataFrame, edges: pd.DataFrame) -> str:
+def validate_datasets(nodes: pl.DataFrame, edges: pl.DataFrame) -> str:
     """Function to run the Matrix Validator."""
-
-    nodes = nodes.astype(str)
-    edges = edges.astype(str)
-
-    validator = ValidatorPolarsDataFrameImpl(nodes=pl.from_pandas(nodes), edges=pl.from_pandas(edges))
+    validator = ValidatorPolarsDataFrameImpl(nodes=nodes, edges=edges)
     validator.validate()
     violations = validator.violations
     if violations:
