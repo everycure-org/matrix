ae8cc703ef7e6f56e62a378f8efe9021de6f2f39
piotrkan <piotr@everycure.org>
2025-08-18 08:10:32 +0100

    add drugbank to ingestion

--- pipelines/matrix/src/matrix/pipelines/integration/nodes.py
+++ pipelines/matrix/src/matrix/pipelines/integration/nodes.py
@@ -87,10 +87,6 @@ def union_edges(core_id_mapping: ps.DataFrame, *edges, cols: list[str]) -> ps.Da
     return unioned_dataset
 
 
-@check_output(
-    schema=get_matrix_edge_schema(validate_enumeration_values=False),
-    pass_columns=True,
-)
 def unify_ground_truth(*edges) -> ps.DataFrame:
     """Function to unify edges datasets."""
     # fmt: off
