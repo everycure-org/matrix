2c9263cbd9f757e54713e2ada3bc91de0de7f0b4
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-08-21 10:10:27 +0100

    fix: update cleanup policy configuration and adjust GKE disk size to 1500GB

--- infra/modules/artifact_registry/main.tf
+++ infra/modules/artifact_registry/main.tf
@@ -1,11 +1,12 @@
 resource "google_artifact_registry_repository" "this" {
-  project       = var.project_id
-  location      = var.location
-  repository_id = var.repository_id
-  format        = var.format
-  description   = var.description
+  project                = var.project_id
+  location               = var.location
+  repository_id          = var.repository_id
+  format                 = var.format
+  description            = var.description
+  cleanup_policy_dry_run = true
   # Dynamic block to create the DELETE policy only if delete_older_than_days is greater than 0.
-  dynamic "cleanup_policy_dry_run" {
+  dynamic "cleanup_policies" {
     for_each = var.delete_older_than_days != null ? [1] : []
     content {
       id     = "delete-old-artifacts"
@@ -18,7 +19,7 @@ resource "google_artifact_registry_repository" "this" {
   }
 
   # Policy to delete sample-run images after 1 day (configurable)
-  dynamic "cleanup_policy_dry_run" {
+  dynamic "cleanup_policies" {
     for_each = length(var.sample_run_tag_prefixes) > 0 ? [1] : []
     content {
       id     = "delete-sample-run-images"
