1abd9bf9ee3abc2de51fa1656783d679c3179975
Kathleen Carter <163005214+eKathleenCarter@users.noreply.github.com>
2025-08-27 05:24:36 -0400

    Fix the BQ SQL queries (#1808)

--- services/kg_dashboard/sources/bq/disease_list_success.sql
+++ services/kg_dashboard/sources/bq/disease_list_success.sql
@@ -7,20 +7,6 @@ FROM `${project_id}.release_${bq_release_version}.disease_list_normalization_sum
 GROUP BY
   normalization_success,
   name
-
-UNION ALL
-
-SELECT
-  normalization_success,
-  'category' AS dimension,
-  category AS name,
-  COUNT(*) AS count
-FROM `${project_id}.release_${bq_release_version}.disease_list_normalization_summary`
-GROUP BY
-  normalization_success,
-  category
-
 ORDER BY
   normalization_success DESC,
-  dimension,
   count DESC;
