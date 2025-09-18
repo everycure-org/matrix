f1f2ce78a8b56b8df4cf1c44fd70cf542ed255bf
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-09-18 18:09:45 +0200

    fix: update PostgreSQL connection URL in litellm configuration and clean up unused role resource

--- infra/modules/components/postgresql/main.tf
+++ infra/modules/components/postgresql/main.tf
@@ -12,16 +12,3 @@ provider "postgresql" {
 # You would need to port-forward the Kubernetes service to your localhost on port 5434 for this to work
 # Example command:
 # kubectl port-forward -n postgresql svc/postgresql-cloudnative-pg-cluster-pooler-rw 5432:5432
-
-resource "postgresql_role" "litellm" {
-  name             = "litellm"
-  password         = data.google_secret_manager_secret_version.litellm_postgres_password.secret_data
-  login            = true
-  superuser        = false
-  inherit          = true
-  replication      = false
-  connection_limit = -1
-  create_database  = true
-  create_role      = true
-
-}
