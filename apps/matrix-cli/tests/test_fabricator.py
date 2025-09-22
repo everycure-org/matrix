133bef4e2fcf37c64fc1ec4dc43dec5fb35a9e33
Jason Reilly <jdr0887@gmail.com>
2025-09-22 07:10:13 -0400

    initial commit

---
+++ apps/matrix-cli/tests/test_fabricator.py
@@ -0,0 +1,31 @@
+"""Matrix CLI Fabricator TestCase."""
+
+import unittest
+
+
+class FabricatorTestCase(unittest.TestCase):
+    """Suite of tests for Fabricator CLI functions."""
+
+    def build_column_summary(self):
+        """Build a summary of a DataFrame column."""
+        assert True
+
+    def test_filtered_columns(self):
+        """Determine columns after prefix exclusions."""
+        assert True
+
+    def test_create_nodes_map(self):
+        """Create a nodes map."""
+        # df = pl.DataFrame
+        # rows = int
+        assert True
+
+    def test_create_edges_map(self):
+        """Create an edges map."""
+        # df = pl.DataFrame
+        # rows = int
+        assert True
+
+
+if __name__ == "__main__":
+    unittest.main()
