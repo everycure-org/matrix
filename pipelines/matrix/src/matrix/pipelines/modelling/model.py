88aa223ab38f86c0bed27ff24feef8f8f5e6ec45
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-10-09 11:26:47 +0100

    Remove gpu_only parameter from predict_proba method in ModelWrapper and update usage in ModelWithTransformers

--- pipelines/matrix/src/matrix/pipelines/modelling/model.py
+++ pipelines/matrix/src/matrix/pipelines/modelling/model.py
@@ -4,7 +4,7 @@ from typing import Callable, List
 import numpy as np
 from sklearn.base import BaseEstimator
 
-from .utils import to_cpu, to_estimator_device
+from .utils import to_cpu
 
 logger = logging.getLogger(__name__)
 
@@ -48,27 +48,18 @@ class ModelWrapper:
         """
         return self.predict_proba(X).argmax(axis=1)
 
-    def predict_proba(self, X, gpu_only=False):
+    def predict_proba(self, X):
         """Method for probability scores of the ModelWrapper.
 
         Args:
             X: input features
-            gpu_only: If True, will attempt to run prediction on GPU if any estimator supports it.
 
         Returns:
             Aggregated probabilities scores of the individual models.
         """
-        if gpu_only:
-            # Convert input to CUDA once for all estimators
-            logger.info("ModelWrapper: Converting input data to estimator device")
-            X_cuda = to_estimator_device(X, self._estimators[0])
-
-            logger.info(f"ModelWrapper: Input data converted to estimator device: {X_cuda.device}")
-            all_preds = [np.asarray(estimator.predict_proba(X_cuda)) for estimator in self._estimators]
-            logger.info(f"ModelWrapper: All predictions collected: {len(all_preds)}")
-        else:
-            all_preds = [to_cpu(estimator.predict_proba(X)) for estimator in self._estimators]
-            logger.info(f"ModelWrapper: All predictions collected and converted to CPU: {len(all_preds)}")
+        logger.info("ModelWrapper: Collecting predictions from all estimators")
+        all_preds = [to_cpu(estimator.predict_proba(X)) for estimator in self._estimators]
+        logger.info(f"ModelWrapper: All predictions collected and converted to CPU: {len(all_preds)}")
         stacked_preds = np.stack(all_preds)
         logger.info(f"ModelWrapper: Stacked predictions shape: {stacked_preds.shape}")
         return np.apply_along_axis(self._agg_func, 0, stacked_preds)
