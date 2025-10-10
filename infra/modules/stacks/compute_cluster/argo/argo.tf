2524fb3c0612f7f3f230a4ee6b4f6ef1e20909b3
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-10-10 15:44:52 +0100

    fix: update targetRevision to use variable for repo revision in argo.tf

--- infra/modules/stacks/compute_cluster/argo/argo.tf
+++ infra/modules/stacks/compute_cluster/argo/argo.tf
@@ -252,11 +252,11 @@ spec:
   source:
     path: ${var.repo_path}/app-of-apps
     repoURL: ${var.repo_url}
-    targetRevision: nelson/aip-586-fix-mlflow-image-pull-issue-due-to-bitnami-changes
+    targetRevision: ${var.repo_revision}
     helm:
       parameters:
       - name: spec.source.targetRevision
-        value:  nelson/aip-586-fix-mlflow-image-pull-issue-due-to-bitnami-changes
+        value:  ${var.repo_revision}
       - name: spec.source.environment
         value:  ${var.environment}
       - name: spec.source.project_id
