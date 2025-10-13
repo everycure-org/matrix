0c6af2838d68d1cb0c586590d62fef8fc4dd6b0d
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-10-13 12:54:17 +0100

    feat: add Google Storage bucket resource for metrics and update Argo module configuration

--- infra/modules/stacks/compute_cluster/services.tf
+++ infra/modules/stacks/compute_cluster/services.tf
@@ -1,3 +1,13 @@
+resource "google_storage_bucket" "metrics_bucket" {
+  name                        = "everycure-${var.bucket_name}-metrics"
+  location                    = var.default_region
+  project                     = var.project_id
+  uniform_bucket_level_access = true
+  autoclass {
+    enabled = true
+  }
+}
+
 # NOTE: Don't call this ArgoCD, this will clash with the Helm naming
 # and give errors.
 module "argo" {
@@ -11,4 +21,5 @@ module "argo" {
   project_id          = var.project_id
   bucket_name         = var.bucket_name
   aip_oauth_client_id = var.aip_oauth_client_id
+  metrics_bucket_name = google_storage_bucket.metrics_bucket.name
 }
