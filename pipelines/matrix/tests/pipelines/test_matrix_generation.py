91be16e7df356dd8a365f3dbf77d95b45b7da6b0
piotrkan <piotr@everycure.org>
2025-10-13 10:46:11 +0100

    update test

--- pipelines/matrix/tests/pipelines/test_matrix_generation.py
+++ pipelines/matrix/tests/pipelines/test_matrix_generation.py
@@ -120,10 +120,10 @@ def sample_graph(sample_node_embeddings):
 def sample_matrix_data():
     return pd.DataFrame(
         [
-            {"ec_id_source": "ec_drug_1", "source": "drug_1", "target": "disease_1"},
-            {"ec_id_source": "ec_drug_2", "source": "drug_2", "target": "disease_1"},
-            {"ec_id_source": "ec_drug_1", "source": "drug_1", "target": "disease_2"},
-            {"ec_id_source": "ec_drug_2", "source": "drug_2", "target": "disease_2"},
+            {"ec_drug_id": "ec_drug_1", "source": "drug_1", "target": "disease_1"},
+            {"ec_drug_id": "ec_drug_2", "source": "drug_2", "target": "disease_1"},
+            {"ec_drug_id": "ec_drug_1", "source": "drug_1", "target": "disease_2"},
+            {"ec_drug_id": "ec_drug_2", "source": "drug_2", "target": "disease_2"},
         ]
     )
 
