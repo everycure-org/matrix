ef310db13c872926aaaef4b0a9ee9d4de13df547
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-10-16 15:02:22 +0100

    Add H3 spot node pools and update taints for large memory allocation

--- infra/modules/stacks/compute_cluster/gke.tf
+++ infra/modules/stacks/compute_cluster/gke.tf
@@ -132,7 +132,10 @@ locals {
       disk_type          = "pd-balanced"
       initial_node_count = 0
       location_policy    = "ANY"
-    },
+    }
+  ]
+
+  h3_spot_node_pools = [
     {
       name               = "h3-standard-88-nodes-spot" # 88 CPUs, 512GB RAM - for largest ML workloads
       machine_type       = "h3-standard-88"
@@ -236,6 +239,25 @@ locals {
     "github-runner-standard-nodes" = [
       {
         key    = "github-runner"
+        value  = "true"
+        effect = "NO_SCHEDULE"
+      }
+    ],
+    "h3-standard-88-nodes" = [
+      {
+        key    = "node-memory-size"
+        value  = "large"
+        effect = "NO_SCHEDULE"
+      },
+    ],
+    "h3-standard-88-nodes-spot" = [
+      {
+        key    = "node-memory-size"
+        value  = "large"
+        effect = "NO_SCHEDULE"
+      },
+      {
+        key    = "spot"
         value  = "true"
         effect = "NO_SCHEDULE"
       }
