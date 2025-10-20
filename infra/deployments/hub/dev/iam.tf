414098be1fcc22fc380567bb269439dfe717af5c
Pascal Brokmeier <pascal@everycure.org>
2025-10-20 19:12:25 +0200

    add permission for evidence project to read BQ

--- infra/deployments/hub/dev/iam.tf
+++ infra/deployments/hub/dev/iam.tf
@@ -1,15 +1,15 @@
 locals {
-  matrix_all_group                     = "group:matrix-all@everycure.org"
-  prod_sas                             = ["serviceAccount:sa-k8s-node@mtrx-hub-prod-sms.iam.gserviceaccount.com"]
-  matrix_viewers_group                 = [local.matrix_all_group, "group:matrix-viewers@everycure.org"]
-  tech_team_group                      = ["group:techteam@everycure.org"]
-  orchard_prod_compute_service_account = "serviceAccount:342224594736-compute@developer.gserviceaccount.com"
-  orchard_dev_compute_service_account  = "serviceAccount:299386668624-compute@developer.gserviceaccount.com"
+  matrix_all_group     = "group:matrix-all@everycure.org"
+  prod_sas             = ["serviceAccount:sa-k8s-node@mtrx-hub-prod-sms.iam.gserviceaccount.com"]
+  matrix_viewers_group = [local.matrix_all_group, "group:matrix-viewers@everycure.org"]
+  tech_team_group      = ["group:techteam@everycure.org"]
   cross_account_sas = [
-    "serviceAccount:vertex-ai-workbench-sa@mtrx-wg1-data-dev-nb5.iam.gserviceaccount.com",
-    "serviceAccount:vertex-ai-workbench-sa@mtrx-wg2-modeling-dev-9yj.iam.gserviceaccount.com",
-    local.orchard_dev_compute_service_account, # orchard dev
-    local.orchard_prod_compute_service_account # orchard prod
+    "serviceAccount:342224594736-compute@developer.gserviceaccount.com",                       # orchard prod
+    "serviceAccount:299386668624-compute@developer.gserviceaccount.com",                       # orchard dev
+    "serviceAccount:vertex-ai-workbench-sa@mtrx-wg1-data-dev-nb5.iam.gserviceaccount.com",     # wg1 data dev
+    "serviceAccount:vertex-ai-workbench-sa@mtrx-wg2-modeling-dev-9yj.iam.gserviceaccount.com", # wg2 modeling dev
+    "serviceAccount:1084953836048-compute@developer.gserviceaccount.com",                      # orchard dev compute
+    "serviceAccount:860701827038-compute@developer.gserviceaccount.com",                       # orchard prod compute
   ]
   github_actions_rw     = ["serviceAccount:sa-github-actions-rw@mtrx-hub-dev-3of.iam.gserviceaccount.com"]
   custom_cloud_build_sa = ["serviceAccount:custom-cloud-build-sa@mtrx-hub-prod-sms.iam.gserviceaccount.com"]
