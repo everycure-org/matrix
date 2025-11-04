9e1d65e1c2557188006aaf93a8f5bf12454f215d
Nelson Alfonso <nelson_alfonso@icloud.com>
2025-11-04 10:48:40 +0000

    Refactor code structure for improved readability and maintainability

--- infra/modules/stacks/compute_cluster/argo/argo.tf
+++ infra/modules/stacks/compute_cluster/argo/argo.tf
@@ -252,11 +252,11 @@ spec:
   source:
     path: ${var.repo_path}/app-of-apps
     repoURL: ${var.repo_url}
-    targetRevision: ${var.repo_revision}
+    targetRevision: nelson/aip-593-no-pr-created-for-v0112-release-completion
     helm:
       parameters:
       - name: spec.source.targetRevision
-        value:  ${var.repo_revision}
+        value:  nelson/aip-593-no-pr-created-for-v0112-release-completion
       - name: spec.source.environment
         value:  ${var.environment}
       - name: spec.source.project_id
