0b96e83c990968e172ae354123cf89ed0573daa5
Alexei Stepanenko <alexei.stepa@gmail.com>
2025-12-02 11:57:19 +0000

    create pipeline skeleton

---
+++ pipelines/matrix/src/matrix/pipelines/known_entity_removal/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'known_entity_removal'
+generated using Kedro 0.19.8
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
