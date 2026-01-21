d02f4464975a90e78b252877b9f19855eca08943
Jason Reilly <jdr0887@gmail.com>
2026-01-21 18:24:15 -0500

    reformatting

--- libs/matrix-validator/src/matrix_validator/generate_biolink_imports.py
+++ libs/matrix-validator/src/matrix_validator/generate_biolink_imports.py
@@ -4,7 +4,7 @@ from linkml.utils.schema_builder import SchemaBuilder
 from linkml_runtime.utils.formatutils import camelcase, underscore
 from linkml_runtime.dumpers import yaml_dumper
 import importlib.resources
-from linkml_runtime.linkml_model import (EnumDefinition, PermissibleValue)
+from linkml_runtime.linkml_model import EnumDefinition, PermissibleValue
 
 
 PREDICATE_ROOT = "related to"
@@ -12,15 +12,13 @@ NODE_CATEGORY_ROOT = "named thing"
 BIOLINK_IMPORT_SCHEMA_FILE = "biolink_imports.yaml"
 
 
-class FlatteningExtractor():
-
+class FlatteningExtractor:
     def __init__(self, sv: SchemaView):
         self.sv = sv
 
     def generate_enum(self, name: str, title_to_value_mapping: Dict[str, str]) -> EnumDefinition:
         permissible_values = [
-            PermissibleValue(title=title, text=value)
-            for title, value in title_to_value_mapping.items()
+            PermissibleValue(title=title, text=value) for title, value in title_to_value_mapping.items()
         ]
         enum = EnumDefinition(name=name, permissible_values=permissible_values)
         return enum
@@ -41,12 +39,12 @@ class FlatteningExtractor():
             values[title] = value
         return self.generate_enum(name, values)
 
+
 def get_biolink_model_schemaview() -> SchemaView:
     # Locate the YAML file for the biolink model within the biolink_model package
-    biolink_model_file = importlib.resources.files("biolink_model.schema").joinpath(
-        "biolink_model.yaml"
-    )
+    biolink_model_file = importlib.resources.files("biolink_model.schema").joinpath("biolink_model.yaml")
     return SchemaView(str(biolink_model_file))
+
 
 def generate_biolink_enum_schema():
     sv = get_biolink_model_schemaview()
@@ -59,20 +57,10 @@ def generate_biolink_enum_schema():
     sb.schema.license = "BSD-3"
 
     # Add all predicate values as an enum for the predicate slot in the matrix schema
-    sb.add_enum(
-        fe.generate_enum_from_slot_descendants(
-            name="PredicateEnum",
-            root_slot=PREDICATE_ROOT
-        )
-    )
+    sb.add_enum(fe.generate_enum_from_slot_descendants(name="PredicateEnum", root_slot=PREDICATE_ROOT))
 
     # Add all node category values as an enum for the node category slot in the matrix schema
-    sb.add_enum(
-        fe.generate_enum_from_class_descendants(
-            name="NodeCategoryEnum",
-            root_class=NODE_CATEGORY_ROOT
-        )
-    )
+    sb.add_enum(fe.generate_enum_from_class_descendants(name="NodeCategoryEnum", root_class=NODE_CATEGORY_ROOT))
 
     # Additionally, bring over KL & AT enums from the biolink model, stripping subsets that aren't defined here
     knowledge_level_enum = sv.get_enum("KnowledgeLevelEnum")
--- libs/matrix-validator/src/matrix_validator/generate_valid_edge_type_table.py
+++ libs/matrix-validator/src/matrix_validator/generate_valid_edge_type_table.py
@@ -4,6 +4,7 @@ from generate_biolink_imports import get_biolink_model_schemaview
 
 VALID_EDGE_TYPE_TABLE = "valid_biolink_edge_types.tsv"
 
+
 def generate_valid_biolink_edge_types():
     """
     Generate a table of valid edge types for the Biolink model.
@@ -14,58 +15,70 @@ def generate_valid_biolink_edge_types():
 
     predicate_valid_triples = []
 
-    predicates = sv.slot_descendants('related to')
+    predicates = sv.slot_descendants("related to")
     for predicate in predicates:
         slot = sv.get_slot(predicate)
 
-        all_subject_categories = [sv.get_uri(class_name) for class_name in sv.class_descendants(slot.domain)] if slot.domain else None
-        all_object_categories = [sv.get_uri(class_name) for class_name in sv.class_descendants(slot.range)] if slot.range else None
+        all_subject_categories = (
+            [sv.get_uri(class_name) for class_name in sv.class_descendants(slot.domain)] if slot.domain else None
+        )
+        all_object_categories = (
+            [sv.get_uri(class_name) for class_name in sv.class_descendants(slot.range)] if slot.range else None
+        )
         if all_subject_categories and all_object_categories:
             for subject_category in all_subject_categories:
                 for object_category in all_object_categories:
-                    predicate_valid_triples.append({
-                        "subject_category": subject_category,
-                        "predicate": sv.get_uri(predicate),
-                        "object_category": object_category
-                    })
+                    predicate_valid_triples.append(
+                        {
+                            "subject_category": subject_category,
+                            "predicate": sv.get_uri(predicate),
+                            "object_category": object_category,
+                        }
+                    )
 
     class_definition_valid_triples = []
 
-    for class_name in sv.class_descendants('association'):
+    for class_name in sv.class_descendants("association"):
         cls = sv.get_class(class_name)
         slot_usage = cls.slot_usage
         if slot_usage is None:
             continue
-        if 'subject' not in slot_usage or slot_usage['subject'] is None or slot_usage['subject'].range is None:
+        if "subject" not in slot_usage or slot_usage["subject"] is None or slot_usage["subject"].range is None:
             continue
-        if 'predicate' not in slot_usage or slot_usage['predicate'].subproperty_of is None:
+        if "predicate" not in slot_usage or slot_usage["predicate"].subproperty_of is None:
             continue
-        if 'object' not in slot_usage or slot_usage['object'] is None or slot_usage['object'].range is None:
+        if "object" not in slot_usage or slot_usage["object"] is None or slot_usage["object"].range is None:
             continue
 
-
-        subject_range = slot_usage['subject'].range
+        subject_range = slot_usage["subject"].range
         all_subject_categories = [sv.get_uri(class_name) for class_name in sv.class_descendants(subject_range)]
-        all_predicates = [sv.get_uri(predicate_name) for predicate_name in sv.slot_descendants(slot_usage['predicate'].subproperty_of)]
-        object_range = slot_usage['object'].range
+        all_predicates = [
+            sv.get_uri(predicate_name) for predicate_name in sv.slot_descendants(slot_usage["predicate"].subproperty_of)
+        ]
+        object_range = slot_usage["object"].range
         all_object_categories = [sv.get_uri(class_name) for class_name in sv.class_descendants(object_range)]
         for subject_category in all_subject_categories:
             for predicate in all_predicates:
                 for object_category in all_object_categories:
-                    class_definition_valid_triples.append({
-                        "subject_category": subject_category,
-                        "predicate": predicate,
-                        "object_category": object_category
-                    })
-
+                    class_definition_valid_triples.append(
+                        {
+                            "subject_category": subject_category,
+                            "predicate": predicate,
+                            "object_category": object_category,
+                        }
+                    )
 
     predicate_valid_triples_df = pd.DataFrame(predicate_valid_triples)
     association_valid_triples_df = pd.DataFrame(class_definition_valid_triples)
-    all_valid_triples_df = pd.merge(predicate_valid_triples_df, association_valid_triples_df,
-                                    how='outer', on=['subject_category','predicate', 'object_category'])
+    all_valid_triples_df = pd.merge(
+        predicate_valid_triples_df,
+        association_valid_triples_df,
+        how="outer",
+        on=["subject_category", "predicate", "object_category"],
+    )
 
     output_path = f"src/matrix_validator/resources/{VALID_EDGE_TYPE_TABLE}"
-    all_valid_triples_df.to_csv(output_path, sep='\t', index=False)
+    all_valid_triples_df.to_csv(output_path, sep="\t", index=False)
 
 
 if __name__ == "__main__":
