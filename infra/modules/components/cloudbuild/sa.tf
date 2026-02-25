e273defb279da2b03a28653f191e085852e52981
Nelson Alfonso <nelson_alfonso@icloud.com>
2026-02-25 16:07:39 +0000

    Add permissions for matrix-subcontractors to impersonate and create tokens for Cloud Build service account

--- infra/modules/components/cloudbuild/sa.tf
+++ infra/modules/components/cloudbuild/sa.tf
@@ -25,3 +25,17 @@ resource "google_service_account_iam_member" "data-science_cloudbuild_token_crea
   role               = "roles/iam.serviceAccountTokenCreator"
   member             = "group:data-science@everycure.org"
 }
+
+# Allow matrix-subcontractors group to impersonate the Cloud Build service account
+resource "google_service_account_iam_member" "matrix_subcontractors_cloudbuild_impersonation" {
+  service_account_id = google_service_account.cloudbuild_sa.id
+  role               = "roles/iam.serviceAccountUser"
+  member             = "group:matrix-subcontractors@everycure.org"
+}
+
+# Allow matrix-subcontractors group to create tokens for the Cloud Build service account
+resource "google_service_account_iam_member" "matrix_subcontractors_cloudbuild_token_creator" {
+  service_account_id = google_service_account.cloudbuild_sa.id
+  role               = "roles/iam.serviceAccountTokenCreator"
+  member             = "group:matrix-subcontractors@everycure.org"
+}
